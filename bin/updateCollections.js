403dae3747e71f8363965ce2bcec46e24b5317e2
Onur Alp Soner <oas@count.ly>
2012-10-03 19:03:09 +0300

    Fix countly.upgrade.sh
    Modified DB update to be the first item in the upgrade process since it
    may take a long time to complete depending on number of documents in
    app_users collection.

--- bin/updateCollections.js
+++ bin/updateCollections.js
@@ -11,17 +11,17 @@ seperateUsers = function (x) {
 		allAppIds[allAppIds.length] = userAppId;
 	}
 	
-	if (x.last_seen) {
+	if (x.last_seen || x.last_seen === 0) {
 		x["ls"] = parseInt(x.last_seen);
 		delete x.last_seen;
 	}
 	
-	if (x.session_count) {
+	if (x.session_count || x.session_count === 0) {
 		x["sc"] = parseInt(x.session_count);
 		delete x.session_count;
 	}
 	
-	if (x.session_duration) {
+	if (x.session_duration || x.session_duration === 0) {
 		x["sd"] = parseInt(x.session_duration);
 		delete x.session_duration;
 	}
