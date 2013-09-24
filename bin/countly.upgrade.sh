e5031b73a465db38d5333c150e9da8d7476275b4
Onur Alp Soner <oas@count.ly>
2013-09-25 01:09:43 +0300

    Update countly.upgrade.sh

--- bin/countly.upgrade.sh
+++ bin/countly.upgrade.sh
@@ -17,7 +17,7 @@ echo "
 
 "
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"ss
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #mongo countly $DIR/updateCollections.js
 
 #stop countly
