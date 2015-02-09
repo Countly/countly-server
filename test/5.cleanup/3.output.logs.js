f419f0813b269dd1d930d3e1b375e8be0a0ab229
Arturs Sosins <ar2rsawseen@gmail.com>
2015-02-09 20:54:45 +0200

    Fixed outputting logs

--- test/5.cleanup/3.output.logs.js
+++ test/5.cleanup/3.output.logs.js
@@ -3,22 +3,24 @@ var fs = require('fs');
 describe('Outputing logs', function(){
 	describe('countly-dashboard.log', function(){
 		it('should output', function(done){
-			fs.readFile('../../log/countly-dashboard.log', 'utf8', function (err,data) {
+			fs.readFile('log/countly-dashboard.log', 'utf8', function (err,data) {
 				if (err) {
-					return console.log(err);
+					console.log(err);
 				}
 				console.log(data);
+				done();
 			});
 		});
 	});
 	describe('countly-api.log', function(){
 		it('should output', function(done){
-			fs.readFile('../../log/countly-api.log', 'utf8', function (err,data) {
+			fs.readFile('log/countly-api.log', 'utf8', function (err,data) {
 				if (err) {
-					return console.log(err);
+					console.log(err);
 				}
 				console.log(data);
+				done();
 			});
-		}
+		});
 	});
 });
