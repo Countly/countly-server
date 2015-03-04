cc38c8eb6d19c8d3943fbe2e7038d2a3177c3866
Arturs Sosins <ar2rsawseen@gmail.com>
2015-03-04 14:45:22 +0200

    Fixing stats with no apps

--- api/parts/data/stats.js
+++ api/parts/data/stats.js
@@ -27,73 +27,93 @@ var stats = {},
 
     function getTotalUsers(callback) {
         common.db.collection("apps").find({}, {_id:1}).toArray(function (err, allApps) {
-            async.map(allApps, getUserCountForApp, function (err, results) {
-                if (err) {
-                    callback(0);
-                }
-
-                var userCount = 0;
-
-                for (var i = 0; i < results.length; i++) {
-                    userCount += results[i];
-                }
-
-                callback(userCount, allApps.length);
-            });
+			if(err || !allApps)
+				callback(0, 0);
+			else
+				async.map(allApps, getUserCountForApp, function (err, results) {
+					if (err)
+						callback(0, 0);
+	
+					var userCount = 0;
+	
+					for (var i = 0; i < results.length; i++) {
+						userCount += results[i];
+					}
+	
+					callback(userCount, allApps.length);
+				});
         });
     }
 
     function getTotalEvents(callback) {
         common.db.collection("events").find({}, {'list':1}).toArray(function (err, events) {
-            var eventCount = 0;
-
-            for (var i = 0; i < events.length; i++) {
-                if (events[i] && events[i]["list"]) {
-                    eventCount += events[i]["list"].length;
-                }
-            }
-
-            callback(eventCount);
+			if (err || !events)
+                callback(0);
+			else{
+				var eventCount = 0;
+	
+				for (var i = 0; i < events.length; i++) {
+					if (events[i] && events[i]["list"]) {
+						eventCount += events[i]["list"].length;
+					}
+				}
+	
+				callback(eventCount);
+			}
         });
     }
 
     function getTotalMsgUsers(callback) {
         common.db.collection("users").find({_id: {"$regex": ".*:0$"}}, {"d.m":1}).toArray(function (err, msgUsers) {
-            var msgUserCount = 0;
-
-            for (var i = 0; i < msgUsers.length; i++) {
-                if (msgUsers[i] && msgUsers[i]["d"] && msgUsers[i]["d"]["m"]) {
-                    msgUserCount += msgUsers[i]["d"]["m"];
-                }
-            }
-
-            callback(msgUserCount);
+			if (err || !msgUsers)
+                callback(0);
+			else{
+				var msgUserCount = 0;
+	
+				for (var i = 0; i < msgUsers.length; i++) {
+					if (msgUsers[i] && msgUsers[i]["d"] && msgUsers[i]["d"]["m"]) {
+						msgUserCount += msgUsers[i]["d"]["m"];
+					}
+				}
+	
+				callback(msgUserCount);
+			}
         });
     }
 
     function getTotalMsgCreated(callback) {
         common.db.collection("messages").count(function (err, msgCreated) {
-            callback(msgCreated);
+			if (err || !msgCreated)
+                callback(0);
+			else
+				callback(msgCreated);
         });
     }
 
     function getTotalMsgSent(callback) {
         common.db.collection("messages").find({}, {"result":1}).toArray(function (err, messages) {
-            var sentMsgCount = 0;
-
-            for (var i = 0; i < messages.length; i++) {
-                if (messages[i] && messages[i]["result"] && messages[i]["result"]["sent"]) {
-                    sentMsgCount += messages[i]["result"]["sent"];
-                }
-            }
-
-            callback(sentMsgCount);
+			if (err || !messages)
+                callback(0);
+			else{
+				var sentMsgCount = 0;
+	
+				for (var i = 0; i < messages.length; i++) {
+					if (messages[i] && messages[i]["result"] && messages[i]["result"]["sent"]) {
+						sentMsgCount += messages[i]["result"]["sent"];
+					}
+				}
+	
+				callback(sentMsgCount);
+			}
         });
     }
 
     function getUserCountForApp(app, callback) {
         common.db.collection("app_users" + app._id).find({}).count(function (err, count) {
-            callback(err, count);
+			if (err || !count)
+                callback(0);
+			else
+				callback(err, count);
         });
     }
 
