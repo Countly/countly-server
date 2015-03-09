2f07ec9e4812ebe984019bca214c5c360101258c
Arturs Sosins <ar2rsawseen@gmail.com>
2015-03-09 14:50:53 +0200

    [plugins] Added systemlogs plugin

---
+++ plugins/systemlogs/frontend/public/javascripts/countly.views.js
@@ -0,0 +1,72 @@
+window.SystemLogsView = countlyView.extend({
+	initialize:function () {
+		
+    },
+    beforeRender: function() {
+		if(this.template)
+			return $.when(countlySystemLogs.initialize()).then(function () {});
+		else{
+			var self = this;
+			return $.when($.get(countlyGlobal["path"]+'/systemlogs/templates/logs.html', function(src){
+				self.template = Handlebars.compile(src);
+			}), countlySystemLogs.initialize()).then(function () {});
+		}
+    },
+    renderCommon:function (isRefresh) {
+        var data = countlySystemLogs.getData();
+        this.templateData = {
+            "page-title":jQuery.i18n.map["systemlogs.title"]
+        };
+		var self = this;
+        if (!isRefresh) {
+            $(this.el).html(this.template(this.templateData));
+
+			this.dtable = $('#systemlogs-table').dataTable($.extend({}, $.fn.dataTable.defaults, {
+                "aaData": data,
+                "aoColumns": [
+                    { "mData": function(row, type){
+						if(type == "display"){
+							return moment(row.ts*1000).format("MMMM Do YYYY, hh:mm:ss");
+						}else return row.ts;}, "sType":"string", "sTitle": jQuery.i18n.map["systemlogs.timestamp"] },
+					{ "mData": function(row, type){return row.u;}, "sType":"string", "sTitle": jQuery.i18n.map["systemlogs.user"]},
+					{ "mData": function(row, type){return row.a;}, "sType":"string", "sTitle": jQuery.i18n.map["systemlogs.action"]},
+                    { "mData": function(row, type){return row.ip;}, "sType":"string", "sTitle": jQuery.i18n.map["systemlogs.ip-address"]},
+                    { "mData": function(row, type){
+						if(typeof row.i == "object")
+							return "<pre style='white-space:pre-wrap;'>"+JSON.stringify(row.i, null, 2)+"</pre>";
+						else
+							return row.i;}, "sType":"string", "sTitle": jQuery.i18n.map["systemlogs.info"]}
+                ]
+            }));
+			this.dtable.stickyTableHeaders();
+			this.dtable.fnSort( [ [0,'desc'] ] );
+        }
+    },
+    refresh:function () {
+        var self = this;
+        $.when(countlyLogger.initialize()).then(function () {
+            if (app.activeView != self) {
+                return false;
+            }
+            var data = countlyLogger.getData();
+			CountlyHelpers.refreshTable(self.dtable, data);
+            app.localize();
+        });
+    }
+});
+
+//register views
+app.systemLogsView = new SystemLogsView();
+
+app.route('/manage/systemlogs', 'systemlogs', function () {
+	this.renderWhenReady(this.systemLogsView);
+});
+
+$( document ).ready(function() {
+	var menu = '<a href="#/manage/systemlogs" class="item">'+
+		'<div class="logo-icon fa fa-user-secret"></div>'+
+		'<div class="text" data-localize="systemlogs.title"></div>'+
+	'</a>';
+	if($('#management-submenu .help-toggle').length)
+		$('#management-submenu .help-toggle').before(menu);
+});
