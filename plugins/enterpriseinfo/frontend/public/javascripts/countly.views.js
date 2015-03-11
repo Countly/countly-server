ff592c05fe6aa5b4a758974c73c962f5b29145aa
Arturs Sosins <ar2rsawseen@gmail.com>
2015-03-11 19:03:41 +0200

    [enterpriseinfo] proper undefined check

--- plugins/enterpriseinfo/frontend/public/javascripts/countly.views.js
+++ plugins/enterpriseinfo/frontend/public/javascripts/countly.views.js
@@ -52,7 +52,7 @@ $( document ).ready(function() {
     '</a>';
 	$('#sidebar-menu').append(menu);
 	
-	if(countlyGlobalEE && countlyGlobalEE["discount"]){
+	if(typeof countlyGlobalEE != "undefined" && countlyGlobalEE["discount"]){
 		var msg = {title:"5000+ users reached", message: "<a href='https://count.ly/enterprise-edition/' target='_blank'>To get 20% off Enterprise edition contact us with code:<br/><strong>"+countlyGlobalEE["discount"]+"</strong></a>", info:"Thank you for being with us", sticky:true, closeOnClick:false};
 		CountlyHelpers.notify(msg);
     }
