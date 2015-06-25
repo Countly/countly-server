50cd80091398f8f54f4bee688e5ce454c7fcd178
Arturs Sosins <ar2rsawseen@gmail.com>
2015-06-25 22:45:10 +0300

    [systemlogs] make use of new frontend plugin methods

--- plugins/systemlogs/frontend/app.js
+++ plugins/systemlogs/frontend/app.js
@@ -3,97 +3,66 @@ var plugin = {},
 	countlyConfig = require('../../../frontend/express/config');
 
 (function (plugin) {
+    var countlyDb;
 	function getIpAddress(req) {
 		var ipAddress = req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
 	
 		/* Since x-forwarded-for: client, proxy1, proxy2, proxy3 */
 		return ipAddress.split(',')[0];
 	};
+    
+    function getTimestamp(){
+        return Math.round(new Date().getTime()/1000);
+    }
 	
 	function sha1Hash(str, addSalt) {
 		var salt = (addSalt) ? new Date().getTime() : "";
 		return crypto.createHmac('sha1', salt + "").update(str + "").digest('hex');
 	};
-	plugin.init = function(app, countlyDb, express){
-		app.post(countlyConfig.path+'/reset', function (req, res, next) {
-			if (req.body.password && req.body.again && req.body.prid) {
-				var now = Math.round(new Date().getTime()/1000);
-				countlyDb.collection('password_reset').findOne({prid:req.body.prid}, function (err, passwordReset) {
-					countlyDb.collection('members').findOne({_id:passwordReset.user_id}, function (err, member) {
-						countlyDb.collection('systemlogs').insert({ts:now, a:"Password reset", u:member.email, ip:getIpAddress(req), i:""}, function () {});
-					});
-				});
-			}
-			next();
-		});
-		
-		app.post(countlyConfig.path+'/forgot', function (req, res, next) {
-			if (req.body.email) {
-				var now = Math.round(new Date().getTime()/1000);
-				countlyDb.collection('systemlogs').insert({ts:now, a:"Password request", u:req.body.email, ip:getIpAddress(req), i:""}, function () {});
-			}
-			next();
-		});
-		
-		app.post(countlyConfig.path+'/login', function (req, res, next) {
-			if (req.body.username && req.body.password) {
-				var now = Math.round(new Date().getTime()/1000);
-				var password = sha1Hash(req.body.password);
-				countlyDb.collection('members').findOne({$or: [ {"username":req.body.username}, {"email":req.body.username} ], "password":password}, function (err, member) {
-					if (member) {
-						countlyDb.collection('systemlogs').insert({ts:now, a:"Login Successful", u:member.email, ip:getIpAddress(req), i:""}, function () {});
-					}
-					else{
-						countlyDb.collection('systemlogs').insert({ts:now, a:"Login Unsuccessful", u:req.body.username, ip:getIpAddress(req), i:""}, function () {});
-					}
-				});
-			}
-			next();
-		});
-		
-		app.post(countlyConfig.path+'/mobile/login', function (req, res, next) {
-			if (req.body.username && req.body.password) {
-				var password = sha1Hash(req.body.password);
-				var now = Math.round(new Date().getTime()/1000);
-				countlyDb.collection('members').findOne({$or: [ {"username":req.body.username}, {"email":req.body.username} ], "password":password}, function (err, member) {
-					if (member) {
-						countlyDb.collection('systemlogs').insert({ts:now, a:"Login Successful", u:member.email, ip:getIpAddress(req), i:""}, function () {});
-					}
-					else{
-						countlyDb.collection('systemlogs').insert({ts:now, a:"Login Unsuccessful", u:req.body.username, ip:getIpAddress(req), i:""}, function () {});
-					}
-				});
-			} 
-			next();
-		});
-		
-		app.post(countlyConfig.path+'/user/settings', function (req, res, next) {
-			if (req.session.uid) {
-				var now = Math.round(new Date().getTime()/1000);
-				countlyDb.collection('systemlogs').insert({ts:now, a:"Settings changed", u:req.session.email, ip:getIpAddress(req), i:""}, function () {});
-			}
-			next();
-		});
-		app.get(countlyConfig.path+'/api-key', express.basicAuth(function(user, pass, callback){
-			var password = sha1Hash(pass);
-			countlyDb.collection('members').findOne({$or: [ {"username":user}, {"email":user} ], "password":password}, function (err, member) {
-				if(member)
-					callback(null, member);
-				else
-					callback(null, user);
-			});
-		}), 
-		function (req, res, next) {
-			var now = Math.round(new Date().getTime()/1000);
-			if (req.user && req.user._id) {
-				countlyDb.collection('systemlogs').insert({ts:now, a:"API KEY Successful", u:req.user.email, ip:getIpAddress(req), i:""}, function () {});
-			}
-			else{
-				countlyDb.collection('systemlogs').insert({ts:now, a:"API KEY Unsuccessful", u:req.user, ip:getIpAddress(req), i:""}, function () {});
-			}
-			next();
-		});
-	};
+	plugin.init = function(app, db, express){
+        countlyDb = db;
+    };
+    
+    plugin.userLogout = function(ob){
+        countlyDb.collection('systemlogs').insert({ts:getTimestamp(), a:"Settings changed", u:ob.data.email, ip:getIpAddress(ob.req), i:""}, function () {});
+    };
+    
+    plugin.passwordReset = function(ob){
+        countlyDb.collection('systemlogs').insert({ts:getTimestamp(), a:"Password reset", u:ob.data.email, ip:getIpAddress(ob.req), i:""}, function () {});
+    };
+    
+    plugin.passwordRequest = function(ob){
+        countlyDb.collection('systemlogs').insert({ts:getTimestamp(), a:"Password request", u:ob.data.email, ip:getIpAddress(ob.req), i:""}, function () {});
+    };
+    
+    plugin.loginSuccessful = function(ob){
+        countlyDb.collection('systemlogs').insert({ts:getTimestamp(), a:"Login Successful", u:ob.data.email+" ("+ob.data.username+")", ip:getIpAddress(ob.req), i:""}, function () {});
+    };
+    
+    plugin.loginFailed = function(ob){
+        countlyDb.collection('systemlogs').insert({ts:getTimestamp(), a:"Login Unsuccessful", u:ob.data.username, ip:getIpAddress(ob.req), i:""}, function () {});
+    };
+    
+    plugin.apikeySuccessful = function(ob){
+        countlyDb.collection('systemlogs').insert({ts:getTimestamp(), a:"API KEY Successful", u:ob.data.email+" ("+ob.data.username+")", ip:getIpAddress(ob.req), i:""}, function () {});
+    };
+    
+    plugin.apikeyFailed = function(ob){
+        countlyDb.collection('systemlogs').insert({ts:getTimestamp(), a:"API KEY Unsuccessful", u:ob.data.username, ip:getIpAddress(ob.req), i:""}, function () {});
+    };
+    
+    plugin.mobileloginSuccessful = function(ob){
+        countlyDb.collection('systemlogs').insert({ts:getTimestamp(), a:"Mobile Login Successful", u:ob.data.email+" ("+ob.data.username+")", ip:getIpAddress(ob.req), i:""}, function () {});
+    };
+    
+    plugin.mobileloginFailed = function(ob){
+        countlyDb.collection('systemlogs').insert({ts:getTimestamp(), a:"Mobile Login Unsuccessful", u:ob.data.username, ip:getIpAddress(ob.req), i:""}, function () {});
+    };
+    
+    plugin.userSettings = function(ob){
+        countlyDb.collection('systemlogs').insert({ts:getTimestamp(), a:"Settings changed", u:ob.data.email+" ("+ob.data.username+")", ip:getIpAddress(ob.req), i:""}, function () {});
+    };
+    
 }(plugin));
 
 module.exports = plugin;
