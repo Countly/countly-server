34ebde22eff53c915347701a5750f19bbd2b7ee9
Arturs Sosins <ar2rsawseen@gmail.com>
2015-07-22 14:59:32 +0300

    [scripts] added block.connections.sh to optionally block connections

---
+++ bin/scripts/block.connections.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#drop packages coming from 0/0 going through mongodb port
+#allow those coming from localhost
+iptables -A INPUT -m state --state NEW -p tcp --destination-port 27019 -s localhost -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp --destination-port 27019 -s 0/0 -j DROP
+
+#save rules
+iptables-save > /etc/iptables/rules.v4
+
+#install iptables-persistent
+apt-get -y install iptables-persistent
