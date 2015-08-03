57f57e428b36fc1ec574b485927ce25bf7b77431
Arturs Sosins <ar2rsawseen@gmail.com>
2015-08-03 19:51:34 +0300

    [plugins] cache plugin static files

--- plugins/pluginManager.js
+++ plugins/pluginManager.js
@@ -164,7 +164,7 @@ var pluginManager = function pluginManager(){
 			try{
 				var plugin = require("./"+plugins[i]+"/frontend/app");
                 plugs.push(plugin);
-                app.use(countlyConfig.path+'/'+plugins[i], express.static(__dirname + '/'+plugins[i]+"/frontend/public"));
+                app.use(countlyConfig.path+'/'+plugins[i], express.static(__dirname + '/'+plugins[i]+"/frontend/public"), { maxAge:31557600000 });
                 if(plugin.staticPaths)
                     plugin.staticPaths(app, countlyDb, express);
 			} catch (ex) {
