2efa8ce0e5d66a7ec89fe0eef0b2f0f31765cc51
Arturs Sosins <ar2rsawseen@gmail.com>
2015-08-06 11:55:28 +0300

    [mail] make private methods public

--- api/parts/mgmt/mail.js
+++ api/parts/mgmt/mail.js
@@ -40,12 +40,12 @@ mail.sendMail = function(message) {
 }
 
 mail.sendToNewMember = function (member, memberPassword) {
-    lookup(function(err, host) {
+    mail.lookup(function(err, host) {
         var message = {
             to:member.email,
             from:"Countly",
             subject:'Your Countly Account',
-            html:'Hi ' + getUserFirstName(member) + ',<br/><br/>' +
+            html:'Hi ' + mail.getUserFirstName(member) + ',<br/><br/>' +
                 'Your Countly account on <a href="' + host + '">' + host + '</a> is created with the following details;<br/><br/>' +
                 'Username: ' + member.username + '<br/>' +
                 'Password: ' + memberPassword + '<br/><br/>' +
@@ -58,12 +58,12 @@ mail.sendToNewMember = function (member, memberPassword) {
 };
 
 mail.sendToUpdatedMember = function (member, memberPassword) {
-    lookup(function(err, host) {
+    mail.lookup(function(err, host) {
         var message = {
             to:member.email,
             from:"Countly",
             subject:'Countly Account - Password Change',
-            html:'Hi ' + getUserFirstName(member) + ',<br/><br/>' +
+            html:'Hi ' + mail.getUserFirstName(member) + ',<br/><br/>' +
                 'Your password for your Countly account on <a href="' + host + '">' + host + '</a> has been changed. Below you can find your updated account details;<br/><br/>' +
                 'Username: ' + member.username + '<br/>' +
                 'Password: ' + memberPassword + '<br/><br/>' +
@@ -76,12 +76,12 @@ mail.sendToUpdatedMember = function (member, memberPassword) {
 };
 
 mail.sendPasswordResetInfo = function (member, prid) {
-    lookup(function(err, host) {
+    mail.lookup(function(err, host) {
         var message = {
             to:member.email,
             from:"Countly",
             subject:'Countly Account - Password Reset',
-            html:'Hi ' + getUserFirstName(member) + ',<br/><br/>' +
+            html:'Hi ' + mail.getUserFirstName(member) + ',<br/><br/>' +
                 'You can reset your Countly account password by following ' +
                 '<a href="' + host + '/reset/' + prid + '">this link</a>.<br/><br/>' +
                 'If you did not request to reset your password ignore this email.<br/><br/>' +
@@ -93,7 +93,7 @@ mail.sendPasswordResetInfo = function (member, prid) {
     });
 };
 
-function getUserFirstName(member) {
+mail.getUserFirstName = function(member) {
     var userName = (member.full_name).split(" "),
         userFirstName = "";
 
@@ -106,9 +106,10 @@ function getUserFirstName(member) {
     return userFirstName;
 }
 
-function lookup(callback) {
+mail.lookup = function(callback) {
     // If host is set in config.js use that, otherwise get the external IP from ifconfig.me
-    if (plugins.getConfig("api").domain) {
+    var domain = plugins.getConfig("api").domain;
+    if (typeof domain != "undefined" && domain != "") {
         callback(false, plugins.getConfig("api").domain);
     } else {
         request('http://ifconfig.me/ip', function(error, response, body) {
