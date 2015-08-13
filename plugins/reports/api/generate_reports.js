a484e71921a1cece546efb7388758fcb9f40b743
Arturs Sosins <ar2rsawseen@gmail.com>
2015-08-13 20:50:56 +0300

    [reports] generate report for each user

---
+++ plugins/reports/api/generate_reports.js
@@ -0,0 +1,63 @@
+//start db connection
+var mongo = require('mongoskin'),
+    countlyConfig = require('../../../frontend/express/config'),
+    plugins = require('../../pluginManager.js'),
+    async = require("async"),
+    reports = require("./reports");
+var dbName;
+var dbOptions = {
+    server:{auto_reconnect:true, poolSize: countlyConfig.mongodb.max_pool_size, socketOptions: { keepAlive: 30000, connectTimeoutMS: 0, socketTimeoutMS: 0 }},
+    replSet:{socketOptions: { keepAlive: 30000, connectTimeoutMS: 0, socketTimeoutMS: 0 }},
+    mongos:{socketOptions: { keepAlive: 30000, connectTimeoutMS: 0, socketTimeoutMS: 0 }}
+};
+
+if (typeof countlyConfig.mongodb === "string") {
+    dbName = countlyConfig.mongodb;
+} else{
+    countlyConfig.mongodb.db = countlyConfig.mongodb.db || 'countly';
+    if ( typeof countlyConfig.mongodb.replSetServers === 'object'){
+        //mongodb://db1.example.net,db2.example.net:2500/?replicaSet=test
+        dbName = countlyConfig.mongodb.replSetServers.join(",")+"/"+countlyConfig.mongodb.db;
+        if(countlyConfig.mongodb.replicaName){
+            dbOptions.replSet.rs_name = countlyConfig.mongodb.replicaName;
+        }
+    } else {
+        dbName = (countlyConfig.mongodb.host + ':' + countlyConfig.mongodb.port + '/' + countlyConfig.mongodb.db);
+    }
+}
+if(countlyConfig.mongodb.username && countlyConfig.mongodb.password){
+    dbName = countlyConfig.mongodb.username + ":" + countlyConfig.mongodb.password +"@" + dbName;
+}
+if(dbName.indexOf("mongodb://") !== 0){
+    dbName = "mongodb://"+dbName;
+}
+var countlyDb = mongo.db(dbName, dbOptions);
+countlyDb._emitter.setMaxListeners(0);
+if(!countlyDb.ObjectID)
+    countlyDb.ObjectID = mongo.ObjectID;
+//load configs
+plugins.loadConfigs(countlyDb, function(){
+    var cache = {};
+    countlyDb.collection("members").find().toArray(function(err, res){
+        var arr = [];
+        for(var i = 0; i <  res.length; i++){
+            if(!res[i].global_admin)
+            arr.push({emails:[res[i].email], apps:res[i].admin_of || [], metrics:{"analytics":true, "revenue":true, "push":true, "crash":true }, frequency:"daily", hour:0, minute:0, day:1, timezone:"Etc/GMT", user:res[i]._id});
+        }
+        async.map(arr, function(report, done){
+            countlyDb.collection("reports").insert(report, function(){
+                console.log("created", report);
+                done(null, null);
+            });
+        }, function(err, results) {
+            console.log("all reports generated");
+            countlyDb.close();
+            process.exit();
+        });
+    })
+    /*//send report
+    reports.sendReport(countlyDb, myArgs[0], function(err, res){
+        //close db to stop process
+        countlyDb.close();
+    });*/
+});
