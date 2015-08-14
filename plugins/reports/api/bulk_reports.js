59b04ff94a77b21c3f46866766a4e0277e489c78
Arturs Sosins <ar2rsawseen@gmail.com>
2015-08-14 16:34:52 +0300

    [reports] bulk process report for each hour

--- plugins/reports/api/bulk_reports.js
+++ plugins/reports/api/bulk_reports.js
@@ -38,20 +38,32 @@ if(!countlyDb.ObjectID)
 //load configs
 plugins.loadConfigs(countlyDb, function(){
     var cache = {};
-    countlyDb.collection("reports").find({}).toArray(function(err, res){
+    var date = new Date();
+    var hour = date.getHours();
+    var dow = date.getDay();
+    if(dow == 0)
+        dow = 7;
+    
+    console.log(hour, dow);
+    countlyDb.collection("reports").find({r_hour:hour}).toArray(function(err, res){
         async.eachSeries(res, function(report, done){
-            reports.getReport(countlyDb, report, function(err, ob){
-                if(!err){
-                    reports.send(ob.report, ob.message, function(){
-                        console.log("sent to", ob.report.emails[0]);
+            if(report.frequency == "daily" || (report.frequency == "weekly" && report.r_day == dow)){
+                reports.getReport(countlyDb, report, function(err, ob){
+                    if(!err){
+                        reports.send(ob.report, ob.message, function(){
+                            console.log("sent to", ob.report.emails[0]);
+                            done(null, null);
+                        });
+                    }
+                    else{
+                        console.log(err, ob.report.emails[0]);
                         done(null, null);
-                    });
-                }
-                else{
-                    console.log(err, ob.report.emails[0]);
-                    done(null, null);
-                }
-            }, cache);
+                    }
+                }, cache);
+            }
+            else{
+               done(null, null); 
+            }
         }, function(err, results) {
             console.log("all reports sent");
             countlyDb.close();
