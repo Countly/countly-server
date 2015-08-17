0ebe1b05e82022675ac440c05ff21f509786c923
Arturs Sosins <ar2rsawseen@gmail.com>
2015-08-17 17:24:35 +0300

    [scripts] launchd add mongo installing (need to make version independent way)

--- bin/commands/launchd/install.sh
+++ bin/commands/launchd/install.sh
@@ -2,8 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
+sudo cp /usr/local/Cellar/mongodb/2.2.0-x86_64/homebrew.mxcl.mongodb.plist /Library/LaunchDaemons/
 sudo cp com.countly.dashboard.plist /Library/LaunchDaemons/
 sudo cp com.countly.api.plist /Library/LaunchDaemons/
 
+sudo launchctl load homebrew.mxcl.mongodb.plist
 sudo launchctl load com.countly.dashboard.plist
 sudo launchctl load com.countly.api.plist
