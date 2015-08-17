48e8f6883c1ee32049ca5a6c80f50113e6f4d3d2
Arturs Sosins <ar2rsawseen@gmail.com>
2015-08-17 19:44:42 +0300

    [scripts] systemd enable services on boot

--- bin/commands/systemd/install.sh
+++ bin/commands/systemd/install.sh
@@ -9,6 +9,7 @@ pkill -SIGTERM supervisord
 #create supervisor service script
 (cat $DIR/countly.service ; echo "ExecStart=/usr/bin/supervisord --nodaemon --configuration $BINDIR/config/supervisord.conf") > /etc/systemd/system/countly.service
 
+
 if [ ! -f /etc/systemd/system/mongod.service ]; then
     #create mongodb service script
     (cat $DIR/mongod.service ; echo "ExecStart=/bin/bash $BINDIR/commands/systemd/mongodb.sh") > /etc/systemd/system/mongod.service
@@ -16,3 +17,7 @@ fi
 
 #reload services
 systemctl daemon-reload
+
+#enable services on boot
+systemctl enable countly.service
+systemctl enable mongod.service
