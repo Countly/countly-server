ab08076eb0d1e642f6bd8c3e3073aa3806728e0a
Arturs Sosins <ar2rsawseen@gmail.com>
2015-08-17 15:37:24 +0300

    [scripts] using init systems as scripts and not as functions

--- bin/commands/launchd/countly.sh
+++ bin/commands/launchd/countly.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../.." && pwd )"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-function countly() {
-    if [ "$1" = "start" ]; then
-        launchctl start com.countly.api;
-        launchctl start com.countly.dashboard;
-    elif [ "$1" = "stop" ]; then
-        launchctl stop com.countly.api;
-        launchctl stop com.countly.dashboard;
-    elif [ "$1" = "restart" ]; then
-        countly start;
-        countly stop;
-    elif [ "$1" = "upgrade" ]; then
-        (cd $DIR ;
-        npm install ;
-        grunt dist-all;
-        countly restart;
-        )
-    else
-        echo "";
-        echo "usage:";
-        echo "    countly start   # starts countly process";
-        echo "    countly stop    # stops countly process";
-        echo "    countly restart # restarts countly process";
-        echo "    countly upgrade # standard upgrade process (install dependencies, minify files, restart countly)";
-        echo "";
-    fi
-    
-    return 0;
-}
+if [ "$1" = "start" ]; then
+    launchctl start com.countly.api;
+    launchctl start com.countly.dashboard;
+elif [ "$1" = "stop" ]; then
+    launchctl stop com.countly.api;
+    launchctl stop com.countly.dashboard;
+elif [ "$1" = "restart" ]; then
+    countly start;
+    countly stop;
+elif [ "$1" = "upgrade" ]; then
+    (cd $DIR ;
+    npm install ;
+    grunt dist-all;
+    countly restart;
+    )
+else
+    echo "";
+    echo "usage:";
+    echo "    countly start   # starts countly process";
+    echo "    countly stop    # stops countly process";
+    echo "    countly restart # restarts countly process";
+    echo "    countly upgrade # standard upgrade process (install dependencies, minify files, restart countly)";
+    echo "";
+fi
--- bin/commands/upstart/countly.sh
+++ bin/commands/upstart/countly.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../.." && pwd )"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-function countly() {
-    if [ "$1" = "start" ]; then
-        start countly-supervisor;
-    elif [ "$1" = "stop" ]; then
-        stop countly-supervisor;
-    elif [ "$1" = "restart" ]; then
-        restart countly-supervisor;
-    elif [ "$1" = "upgrade" ]; then
-        (cd $DIR ;
-        npm install ;
-        grunt dist-all;
-        restart countly-supervisor;
-        )
-    else
-        echo "";
-        echo "usage:";
-        echo "    countly start   # starts countly process";
-        echo "    countly stop    # stops countly process";
-        echo "    countly restart # restarts countly process";
-        echo "    countly upgrade # standard upgrade process (install dependencies, minify files, restart countly)";
-        echo "";
-    fi
-    
-    return 0;
-}
+if [ "$1" = "start" ]; then
+    start countly-supervisor;
+elif [ "$1" = "stop" ]; then
+    stop countly-supervisor;
+elif [ "$1" = "restart" ]; then
+    restart countly-supervisor;
+elif [ "$1" = "upgrade" ]; then
+    (cd $DIR ;
+    npm install ;
+    grunt dist-all;
+    restart countly-supervisor;
+    )
+else
+    echo "";
+    echo "usage:";
+    echo "    countly start   # starts countly process";
+    echo "    countly stop    # stops countly process";
+    echo "    countly restart # restarts countly process";
+    echo "    countly upgrade # standard upgrade process (install dependencies, minify files, restart countly)";
+    echo "";
+fi
