23cba384a2f85a87101e8030c93fbe5e484b1fd9
Arturs Sosins <ar2rsawseen@gmail.com>
2015-08-18 12:28:45 +0300

    [scripts] upstart add countly user to sudoers

--- bin/commands/upstart/install.sh
+++ bin/commands/upstart/install.sh
@@ -8,3 +8,5 @@ pkill -SIGTERM supervisord
 
 #create supervisor upstart script
 (cat $DIR/countly-supervisor.conf ; echo "exec /usr/bin/supervisord --nodaemon --configuration $BINDIR/config/supervisord.conf") > /etc/init/countly-supervisor.conf
+
+echo "countly ALL=(ALL) NOPASSWD: /sbin/start countly-supervisor, /sbin/stop countly-supervisor, /sbin/restart countly-supervisor, /sbin/status countly-supervisor" > /etc/sudoers.d/countly
