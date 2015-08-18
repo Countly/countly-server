a8ec58d0c6d8588568217b38766b7d52d2f134ff
Arturs Sosins <ar2rsawseen@gmail.com>
2015-08-18 14:15:15 +0300

    [scripts] change existing init sys commands to functions

--- bin/commands/launchd/countly.sh
+++ bin/commands/launchd/countly.sh
@@ -1,34 +1,16 @@
 #!/bin/bash
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
-if [ "$1" = "start" ]; then
+function countly_start { 
     launchctl start com.countly.api;
     launchctl start com.countly.dashboard;
-elif [ "$1" = "stop" ]; then
+}
+
+function countly_stop { 
     launchctl stop com.countly.api;
     launchctl stop com.countly.dashboard;
-elif [ "$1" = "restart" ]; then
+}
+
+function countly_restart {
     countly start;
     countly stop;
-elif [ "$1" = "upgrade" ]; then
-    (cd $DIR ;
-    npm install ;
-    grunt dist-all;
-    countly restart;
-    )
-else
-    echo "";
-    echo "usage:";
-    echo "    countly start   # starts countly process";
-    echo "    countly stop    # stops countly process";
-    echo "    countly restart # restarts countly process";
-    echo "    countly upgrade # standard upgrade process (install dependencies, minify files, restart countly)";
-    echo "";
-fi
+}
--- bin/commands/upstart/countly.sh
+++ bin/commands/upstart/countly.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
-if [ "$1" = "start" ]; then
+function countly_start { 
     start countly-supervisor;
-elif [ "$1" = "stop" ]; then
+} 
+
+function countly_stop { 
     stop countly-supervisor;
-elif [ "$1" = "restart" ]; then
+} 
+
+function countly_restart { 
     restart countly-supervisor;
-elif [ "$1" = "upgrade" ]; then
-    (cd $DIR ;
-    npm install ;
-    grunt dist-all;
-    countly restart;
-    )
-else
-    echo "";
-    echo "usage:";
-    echo "    countly start   # starts countly process";
-    echo "    countly stop    # stops countly process";
-    echo "    countly restart # restarts countly process";
-    echo "    countly upgrade # standard upgrade process (install dependencies, minify files, restart countly)";
-    echo "";
-fi
+} 
