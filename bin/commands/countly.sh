c3de5cb0f2106ef646e9942079e62437dd1c1417
Arturs Sosins <ar2rsawseen@gmail.com>
2015-08-24 11:55:52 +0300

    [cmd] backup/restore commands

--- bin/commands/countly.sh
+++ bin/commands/countly.sh
@@ -35,7 +35,77 @@ countly_upgrade (){
 }
 
 countly_version (){
-     echo $VERSION;
+    echo $VERSION;
+}
+
+countly_backup (){
+    if [ $# -eq 0 ]
+    then
+        echo "Please provide path" ;
+        return 0;
+    fi
+    (cd $1 ;
+    mongodump --db countly > /dev/null;
+    mongodump --db countly_drill > /dev/null;
+    mkdir -p files/frontend/express/public/appimages ;
+    mkdir -p files/frontend/express/public/userimages ;
+    mkdir -p files/frontend/express/certificates ;
+    mkdir -p files/frontend/express/public/javascripts/countly ;
+    mkdir -p files/api ;
+    cp $DIR/../../frontend/express/config.js files/frontend/express/config.js
+    cp $DIR/../../frontend/express/public/javascripts/countly/countly.config.js files/frontend/express/public/javascripts/countly/countly.config.js
+    cp $DIR/../../api/config.js files/api/config.js
+    cp -a $DIR/../../frontend/express/public/appimages/. files/frontend/express/public/appimages/
+    cp -a $DIR/../../frontend/express/public/userimages/. files/frontend/express/public/userimages/
+    cp -a $DIR/../../frontend/express/certificates/. files/frontend/express/certificates/
+    
+    for d in $DIR/../../plugins/*; do
+        PLUGIN=$(basename $d);
+        if [ -f $d/config.js ]; then
+            mkdir -p files/plugins/$PLUGIN ;
+            cp $d/config.js files/plugins/$PLUGIN/config.js ;
+        fi
+    done
+    )
+}
+
+countly_restore (){
+    if [ $# -eq 0 ]
+    then
+        echo "Please provide path" ;
+        return 0;
+    fi
+    mongorestore --db countly $1/dump/countly > /dev/null;
+    mongorestore --db countly_drill $1/dump/countly_drill > /dev/null;
+    (cd $1 ;
+    mkdir -p $DIR/../../frontend/express/public/appimages ;
+    mkdir -p $DIR/../../frontend/express/public/userimages ;
+    mkdir -p $DIR/../../frontend/express/certificates ;
+    mkdir -p $DIR/../../frontend/express/public/javascripts/countly ;
+    mkdir -p $DIR/../../api ;
+    if [ -f files/frontend/express/config.js ]; then
+        cp files/frontend/express/config.js $DIR/../../frontend/express/config.js
+    fi
+    if [ -f files/frontend/express/public/javascripts/countly/countly.config.js ]; then
+        cp files/frontend/express/public/javascripts/countly/countly.config.js $DIR/../../frontend/express/public/javascripts/countly/countly.config.js
+    fi
+    if [ -f files/api/config.js ]; then
+        cp files/api/config.js $DIR/../../api/config.js
+    fi
+    cp -a files/frontend/express/public/appimages/. $DIR/../../frontend/express/public/appimages/ 
+    cp -a files/frontend/express/public/userimages/. $DIR/../../frontend/express/public/userimages/
+    cp -a files/frontend/express/certificates/. $DIR/../../frontend/express/certificates/
+    
+    for d in files/plugins/*; do
+        PLUGIN=$(basename $d);
+        if [ -f $d/config.js ]; then
+            mkdir -p $DIR/../../plugins/$PLUGIN ;
+            cp $d/config.js $DIR/../../plugins/$PLUGIN/config.js ;
+        fi
+    done
+    
+    countly restart;
+    )
 }
 
 #load real platform/init sys file to overwrite stubs
@@ -57,6 +127,8 @@ else
     echo "    countly restart # restarts countly process";
     echo "    countly upgrade # standard upgrade process (install dependencies, minify files, restart countly)";
     echo "    countly version # outputs current countly version";
+    echo "    countly backup path/to/backup # backups countly db and config files";
+    echo "    countly restore path/to/backup # restores countly db and config files from provided backup";
     echo "    countly usage   # prints this out, but so as basically everything else does";
     echo "";
 fi
