e36197d1a5b98a143b7d33cf35582605d1fd9d75
Artem Salpagarov <a@ahoy.li>
2015-10-07 13:18:20 +0300

    [jobs] Removing console.log in favour of log

--- api/parts/mgmt/jobsRunner.js
+++ api/parts/mgmt/jobsRunner.js
@@ -1,9 +1,10 @@
-var jobs = require('./jobs.js');
+var jobs = require('./jobs.js'),
+	log = require('../../utils/log.js')('jobs:runner');
 
 jobs.startWorker(null, {
 	test: function(job, done){
 		done();
 	}
-}, true, function(){
-	console.log('Jobs started', arguments);
+}, true, function(err, worker){
+	log.i('Jobs started', err ? err : worker.types);
 });
