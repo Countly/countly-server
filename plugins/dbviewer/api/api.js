e65224083e4477cbe023a39ad72cb045737adeef
Arturs Sosins <ar2rsawseen@gmail.com>
2015-11-08 15:48:12 +0200

    [dbviewer] display decoded collection names

--- plugins/dbviewer/api/api.js
+++ plugins/dbviewer/api/api.js
@@ -1,6 +1,7 @@
 var plugin = {},
 	common = require('../../../api/utils/common.js'),
 	async = require('async'),
+	crypto = require('crypto'),
     plugins = require('../../pluginManager.js'),
 	connections = {};
 
@@ -49,47 +50,89 @@ var plugin = {},
 				}
 			}
 			else{
-				async.map(Object.keys(dbs), getCollections, function (err, results) {
-					if(err) {
-						console.error(err);
-					}
-					if(results){
-						results = results.filter(function(val) { return val !== null; });
-					}
-					common.returnOutput(params, results || []);
-				});
-					
-				function getCollections(name, callback) {
-					if(dbs[name]){
-						dbs[name].collectionNames(function (err, results) {
-							var db = {name:name, collections:[]};
-							for (var r in results) {
-								if(results[r].name.indexOf("system.indexes") == -1 && results[r].name.indexOf("sessions_") == -1){
-									var col = parseCollectionName(results[r].name);
-									db.collections.push(col.name);
-								}
-							}
-							db.collections.sort();
-							callback(err, db);
-						});
-					}
-					else
-						callback(null, null);
-				}
+                common.db.collection('apps').find({}).toArray(function (err, apps) {
+                    if(err) {
+                        console.error(err);
+                    }
+                    var lookup = {};
+                    for (var i = 0; i < apps.length ;i++) {
+                        lookup[apps[i]._id+""] = apps[i].name;
+                    }
+                    
+                    function getEvents(app, callback){
+                        var result = {};
+                        common.db.collection('events').findOne({'_id': common.db.ObjectID(app._id+"")}, function(err, events) {
+                            if (!err && events && events.list) {
+                                for (var i = 0; i < events.list.length; i++) {
+                                    result[crypto.createHash('sha1').update(events.list[i] + app._id + "").digest('hex')] = "("+app.name+": "+events.list[i]+")";
+                                }
+                            }
+                            callback(null, result);
+                        });
+                    }
+                    
+                    async.map(apps, getEvents, function (err, events) {
+                        var eventList = {};
+                        for(var i = 0; i < events.length; i++){
+                            for(var j in events[i]){
+                                eventList[j] = events[i][j];
+                            }
+                        }
+                        async.map(Object.keys(dbs), getCollections, function (err, results) {
+                            if(err) {
+                                console.error(err);
+                            }
+                            if(results){
+                                results = results.filter(function(val) { return val !== null; });
+                            }
+                            common.returnOutput(params, results || []);
+                        });
+                            
+                        function getCollections(name, callback) {
+                            if(dbs[name]){
+                                dbs[name].collectionNames(function (err, results) {
+                                    var db = {name:name, collections:{}};
+                                    for (var r in results) {
+                                        if(results[r].name.indexOf("system.indexes") == -1 && results[r].name.indexOf("sessions_") == -1){
+                                            var col = parseCollectionName(results[r].name, lookup, eventList);
+                                            db.collections[col.pretty] = col.name;
+                                        }
+                                    }
+                                    callback(err, db);
+                                });
+                            }
+                            else
+                                callback(null, null);
+                        }
+                    });
+                });
 			}
 		});		
 		return true;
 	});
 	
-	var parseCollectionName = function parseCollectionName(full_name) {
+	var parseCollectionName = function parseCollectionName(full_name, apps, events) {
 		var coll_parts = full_name.split('.');
-		
-		if (coll_parts.length <= 1) {
-			return { name: coll_parts.join('.'), database: "countly" };
+		var database = "countly";
+		if (coll_parts.length > 1) {
+            database = coll_parts.splice(0,1);
 		}
-		
-		var database = coll_parts.splice(0,1);
-		return { name: coll_parts.join('.'), database: database.toString() };
+		var name = coll_parts.join('.');
+        var pretty = name;
+        
+        for(var i in apps){
+            if(name.indexOf(i, name.length - i.length) !== -1){
+                pretty = name.replace(i, "("+apps[i]+")");
+            }
+        }
+        
+        for(var i in events){
+            if(name.indexOf(i, name.length - i.length) !== -1){
+                pretty = name.replace(i, events[i]);
+            }
+        }
+           
+		return { name: name, pretty: pretty, database: database.toString() };
 	};
 	var checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$");
 	var isObjectId = function(id) {
