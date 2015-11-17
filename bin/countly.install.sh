95137e7f822dcb68a73bdd53a3d76a683e3051ce
Arturs Sosins <ar2rsawseen@gmail.com>
2015-11-17 10:36:21 +0200

    update installation scripts

--- bin/countly.install.sh
+++ bin/countly.install.sh
@@ -32,8 +32,12 @@ fi
 wget -qO- https://deb.nodesource.com/setup | bash -
 
 #add mongodb repo
-echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" > /etc/apt/sources.list.d/mongodb-10gen-countly.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+#echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" > /etc/apt/sources.list.d/mongodb-10gen-countly.list
+#apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
 
 #update once more after adding new repos
 apt-get update
--- bin/countly.install_rhel.sh
+++ bin/countly.install_rhel.sh
@@ -22,11 +22,11 @@ curl -sL https://rpm.nodesource.com/setup | bash -
 yum install -y nodejs
 
 #install mongodb
-echo "[10gen]
-name=10gen Repository
-baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64
+echo "[mongodb-org-3.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.0/x86_64/
 gpgcheck=0
-enabled=1" > /etc/yum.repos.d/10gen.repo
+enabled=1" > /etc/yum.repos.d/mongodb-org-3.0.repo
 
 sudo yum -y install mongodb-org
 sudo service mongod start
