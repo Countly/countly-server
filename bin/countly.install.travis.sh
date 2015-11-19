01b5baffafced7f61338501c5db7cfcf4f25f575
Arturs Sosins <ar2rsawseen@gmail.com>
2015-11-19 14:55:23 +0200

    [travis] trying g++ version

--- bin/countly.install.travis.sh
+++ bin/countly.install.travis.sh
@@ -24,7 +24,7 @@ bash $DIR/scripts/make.swap.sh
 #update package index
 apt-get update
 
-apt-get -y install python-software-properties g++ build-essential
+apt-get -y install python-software-properties build-essential
 
 if !(command -v apt-add-repository >/dev/null) then
     apt-get -y install software-properties-common
