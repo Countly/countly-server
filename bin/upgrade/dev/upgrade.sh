b3326f812b9e24bf3320a6d541b107723dd8b956
Arturs Sosins <ar2rsawseen@gmail.com>
2015-11-23 20:43:06 +0200

    [scripts] provisionary upgrade file for next release

---
+++ bin/upgrade/dev/upgrade.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+
+#bash $DIR/scripts/detect.init.sh
+
+#force mobile plugin for default mobile dashboard
+countly plugin install mobile ;
+
+#replace stores with sources
+STORESTATE=$(countly plugin status stores);
+if [ "$STORESTATE" == "installed" ] 
+then
+    countly plugin uninstall stores ;
+    countly plugin install sources ;
+fi
+
+#update crashes dependencies
+CRASHSTATE=$(countly plugin status crashes);
+if [ "$CRASHSTATE" == "installed" ] 
+then
+    nodejs $DIR/../plugins/crashes/install.js
+fi
+
+#update repos
+wget -qO- https://deb.nodesource.com/setup_4.x | bash -
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+apt-get update
+
+#install new node.js
+apt-get -y --force-yes install nodejs || (echo "Failed to install nodejs." ; exit)
+
+#install new mongodb
+apt-get -y --force-yes install mongodb-org || (echo "Failed to install mongodb." ; exit)
+
+#install dependencies, process files and restart countly
+countly upgrade
