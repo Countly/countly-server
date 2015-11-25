0fd383289b5bfdaeaaad29b864a04f536b6e38a2
Arturs Sosins <ar2rsawseen@gmail.com>
2015-11-25 14:22:19 +0200

    [config] remove old mongodb and install nodejs dependencies

--- bin/upgrade/dev/upgrade.sh
+++ bin/upgrade/dev/upgrade.sh
@@ -23,6 +23,8 @@ then
 fi
 
 #uninstall mognodb
+apt-get remove -y mongodb-org
+apt-get autoremove -y
 rm /etc/apt/sources.list.d/mongodb-10gen-countly.list
 
 #update repos
@@ -31,11 +33,17 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 apt-get update
 
+#install new nodejs dependencies
+apt-get install -y build-essential libkrb5-dev
+
 #install new node.js
 apt-get -y --force-yes install nodejs || (echo "Failed to install nodejs." ; exit)
 
 #install new mongodb
 apt-get -y --force-yes install mongodb-org || (echo "Failed to install mongodb." ; exit)
+
+#remove previous dependencies, as they need to be rebuild for new nodejs version
+rm -rf $DIR/../node_modules
 
 #install dependencies, process files and restart countly
 countly upgrade
