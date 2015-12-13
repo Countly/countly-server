4a83619f793dd34ecdc5661fb7d7e75754b3db65
Arturs Sosins <ar2rsawseen@gmail.com>
2015-12-13 18:12:52 +0200

    [tests] do not output logs

--- test/1.frontend/1.setup.test.js
+++ test/1.frontend/1.setup.test.js
@@ -166,30 +166,3 @@ describe('Setting Up', function(){
 		})
 	})
 })
-
-var fs = require('fs');
-
-describe('Outputing logs', function(){
-	describe('countly-dashboard.log', function(){
-		it('should output', function(done){
-			fs.readFile('log/countly-dashboard.log', 'utf8', function (err,data) {
-				if (err) {
-					console.log(err);
-				}
-				console.log(data);
-				done();
-			});
-		});
-	});
-	describe('countly-api.log', function(){
-		it('should output', function(done){
-			fs.readFile('log/countly-api.log', 'utf8', function (err,data) {
-				if (err) {
-					console.log(err);
-				}
-				console.log(data);
-				done();
-			});
-		});
-	});
-});
--- test/1.frontend/2.login.test.js
+++ test/1.frontend/2.login.test.js
@@ -322,30 +322,3 @@ describe('Getting API KEY', function(){
 		})
 	})
 })
-
-var fs = require('fs');
-
-describe('Outputing logs', function(){
-	describe('countly-dashboard.log', function(){
-		it('should output', function(done){
-			fs.readFile('log/countly-dashboard.log', 'utf8', function (err,data) {
-				if (err) {
-					console.log(err);
-				}
-				console.log(data);
-				done();
-			});
-		});
-	});
-	describe('countly-api.log', function(){
-		it('should output', function(done){
-			fs.readFile('log/countly-api.log', 'utf8', function (err,data) {
-				if (err) {
-					console.log(err);
-				}
-				console.log(data);
-				done();
-			});
-		});
-	});
-});
