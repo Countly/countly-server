069de5e069d70984b6d761849094fd76a3698151
Arturs Sosins <ar2rsawseen@gmail.com>
2016-01-31 22:38:52 +0200

    [cmd] show wget output for updating components

--- bin/commands/scripts/update.sh
+++ bin/commands/scripts/update.sh
@@ -14,11 +14,11 @@ if [ "$1" = "translations" ]; then
 elif [ "$1" = "geoip" ]; then
     (cd $DIR/../../../node_modules/geoip-lite ; npm run-script updatedb ;)
 elif [ "$1" = "devices" ]; then
-    wget -q https://raw.githubusercontent.com/Countly/countly-localization/master/data/countly.device.list.js -O $DIR/../../../frontend/express/public/javascripts/countly/countly.device.list.js
+    wget -nv https://raw.githubusercontent.com/Countly/countly-localization/master/data/countly.device.list.js -O $DIR/../../../frontend/express/public/javascripts/countly/countly.device.list.js
 elif [ "$1" = "sdk-web" ]; then
     mkdir -p $DIR/../../../frontend/express/public/sdk/web;
-    wget -q https://raw.githubusercontent.com/Countly/countly-sdk-web/master/lib/countly.min.js -O $DIR/../../../frontend/express/public/sdk/web/countly.min.js
-    wget -q https://raw.githubusercontent.com/Countly/countly-sdk-web/master/lib/countly.js -O $DIR/../../../frontend/express/public/sdk/web/countly.js
+    wget -nv https://raw.githubusercontent.com/Countly/countly-sdk-web/master/lib/countly.min.js -O $DIR/../../../frontend/express/public/sdk/web/countly.min.js
+    wget-nv https://raw.githubusercontent.com/Countly/countly-sdk-web/master/lib/countly.js -O $DIR/../../../frontend/express/public/sdk/web/countly.js
 else
     usage ;
 fi
