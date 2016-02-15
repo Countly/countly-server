6c0426cdd909772a61a039687cf4b05931f3b9a6
Artem Salpagarov <a@ahoy.li>
2016-02-15 19:12:57 +0300

    [push] Adding indexes during installation

--- plugins/push/install.js
+++ plugins/push/install.js
@@ -1,8 +1,45 @@
-var fs = require('fs'),
-	path = require("path");
+'use strict';
 
-console.log("Installing push plugin");
-console.log("Creating certificates directory");
+var fs = require('fs'),
+	path = require('path'),
+	async = require('async'),
+	pluginManager = require('../pluginManager.js'),
+	db = pluginManager.dbConnection();
+
+console.log('Installing push plugin');
+
+console.log('Creating certificates directory');
 var dir = path.resolve(__dirname, '');
 fs.mkdir(dir+'/../../frontend/express/certificates', function(){});
-console.log("Push plugin installation finished");
+
+console.log('Adding messages indexes');
+db.collection('messages').ensureIndex({'apps': 1, deleted: 1}, function(){
+	
+	console.log('Adding token indexes');
+	db.collection('apps').find({}).toArray(function(err, apps){
+		if (err || !apps) {
+			return db.close();
+		}
+
+		function upgrade(app, done){
+			var cnt = 0;
+			console.log('Adding push token indexes to ' + app.name);
+			function cb(){
+				cnt++;
+				if (cnt == 2) {
+					done();
+				}
+			}        
+			db.collection('app_users' + app._id).ensureIndex({'tk.ip': 1}, {sparse: true}, cb);
+			db.collection('app_users' + app._id).ensureIndex({'tk.ap': 1}, {sparse: true}, cb);
+		}
+
+		async.forEach(apps, upgrade, function(){
+			console.log('Push plugin installation finished');
+			db.close();
+		});
+
+	});
+
+});
+
