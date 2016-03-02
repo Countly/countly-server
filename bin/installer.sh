16fa275cda9a1a0091f3b7865b5ac93d3839123e
Arturs Sosins <ar2rsawseen@gmail.com>
2016-03-02 23:44:36 +0200

    [scripts] installer, allow to install release or development version

--- bin/installer.sh
+++ bin/installer.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-#use it as
-# wget -qO- https://raw.githubusercontent.com/Countly/countly-server/master/bin/installer.sh | bash
+#   use this to get latest stable release
+#       wget -qO- https://raw.githubusercontent.com/Countly/countly-server/master/bin/installer.sh | bash
+#   
+#   use this to get latest development version from repo
+#       wget -qO- https://raw.githubusercontent.com/Countly/countly-server/master/bin/installer.sh | bash -s dev
 
-LATEST=$(wget -qO- https://api.github.com/repos/countly/countly-server/releases/latest | grep browser_download_url | head -n 1 | cut -d '"' -f 4) ;
-wget -nv $LATEST -O ./countly.zip ;
 apt-get install -y zip </dev/null ;
-unzip countly.zip ;
+if [ "$1" = "dev" ]; then
+    wget -nv https://github.com/Countly/countly-server/archive/master.zip -O ./countly.zip ;
+    unzip countly.zip ;
+    mv countly-server-master countly
+else
+    LATEST=$(wget -qO- https://api.github.com/repos/countly/countly-server/releases/latest | grep browser_download_url | head -n 1 | cut -d '"' -f 4) ;
+    wget -nv $LATEST -O ./countly.zip ;
+    unzip countly.zip ;
+fi
 bash countly/bin/countly.install.sh ;
