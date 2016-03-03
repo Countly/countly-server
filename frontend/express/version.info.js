dc9106378c27fbd605a6d44ddb7b780e394b25ec
Arturs Sosins <ar2rsawseen@gmail.com>
2016-03-03 13:38:08 +0200

    [version] 16.02.01

--- frontend/express/version.info.js
+++ frontend/express/version.info.js
@@ -1,6 +1,6 @@
 var plugins = require('../../plugins/pluginManager.js');
 var versionInfo = {
-    version: "16.02",
+    version: "16.02.01",
     type: "777a2bf527a18e0fffe22fb5b3e322e68d9c07a6"
 };
 plugins.extendModule("version.info", versionInfo);
