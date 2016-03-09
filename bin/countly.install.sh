829ffd30081b49f3332797c52d0c2bdd17e7416c
Igor Klopov <igor@klopov.com>
2016-03-09 17:12:59 +0300

    One more `sudo`

--- bin/countly.install.sh
+++ bin/countly.install.sh
@@ -36,7 +36,7 @@ fi
 #apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
 
 #update once more after adding new repos
