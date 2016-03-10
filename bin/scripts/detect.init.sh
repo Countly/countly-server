93a1df71108e8696339a15217b383a0572938fdc
Arturs Sosins <ar2rsawseen@gmail.com>
2016-03-10 13:29:13 +0200

    [cmd] use symlink instead of copying file

--- bin/scripts/detect.init.sh
+++ bin/scripts/detect.init.sh
@@ -14,7 +14,7 @@ then
 fi
 
 bash $DIR/commands/$INITSYS/install.sh
-cp $DIR/commands/$INITSYS/countly.sh $DIR/commands/enabled/countly.sh
+ln -sf $DIR/commands/$INITSYS/countly.sh $DIR/commands/enabled/countly.sh
 
 chmod +x $DIR/commands/countly.sh
 ln -sf $DIR/commands/countly.sh /usr/bin/countly
