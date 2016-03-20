197f4c432615f214c32486b912d39311a57d4723
Onur Alp Soner <oas@count.ly>
2016-03-20 23:45:52 +0200

    [js] Update Flot to the latest available version

--- frontend/express/public/javascripts/visualization/flot/jquery.flot.symbol.js
+++ frontend/express/public/javascripts/visualization/flot/jquery.flot.symbol.js
@@ -1,28 +1,17 @@
-/*
-Flot plugin that adds some extra symbols for plotting points.
+/* Flot plugin that adds some extra symbols for plotting points.
 
-The symbols are accessed as strings through the standard symbol
-choice:
+Copyright (c) 2007-2014 IOLA and Ole Laursen.
+Licensed under the MIT license.
 
-  series: {
-      points: {
-          symbol: "square" // or "diamond", "triangle", "cross"
-      }
-  }
+The symbols are accessed as strings through the standard symbol options:
+
+	series: {
+		points: {
+			symbol: "square" // or "diamond", "triangle", "cross"
+		}
+	}
 
 */
-
-function roundRect(ctx, x, y, w, h, r) {
-  if (w < 2 * r) r = w / 2;
-  if (h < 2 * r) r = h / 2;
-  ctx.beginPath();
-  ctx.moveTo(x+r, y);
-  ctx.arcTo(x+w, y,   x+w, y+h, r);
-  ctx.arcTo(x+w, y+h, x,   y+h, r);
-  ctx.arcTo(x,   y+h, x,   y,   r);
-  ctx.arcTo(x,   y,   x+w, y,   r);
-  ctx.closePath();
-}
 
 (function ($) {
     function processRawData(plot, series, datapoints) {
@@ -32,22 +21,8 @@ function roundRect(ctx, x, y, w, h, r) {
         var handlers = {
             square: function (ctx, x, y, radius, shadow) {
                 // pi * r^2 = (2s)^2  =>  s = r * sqrt(pi)/2
-                var size = radius * Math.sqrt(Math.PI) * 1.2;
-                
-                var fillText="5";
-								ctx.textBaseline="top";
-								ctx.font="14px Arial";
-								ctx.fillStyle="#333";
-								ctx.lineWidth = 1;
-								//ctx.strokeStyle = '#333';
-								ctx.shadowColor = "#FFF";
-								ctx.shadowOffsetX = 1;
-								
-								ctx.fillText(ctx.measureText(fillText).width, x - (ctx.measureText(fillText).width / 2), y - 5);
-								ctx.fillStyle="#999999";
-                //ctx.rect(x - size, y - size, size + size, size + size);
-                roundRect(ctx, x - size, y - size, size + size, size + size, 4);
-                
+                var size = radius * Math.sqrt(Math.PI) / 2;
+                ctx.rect(x - size, y - size, size + size, size + size);
             },
             diamond: function (ctx, x, y, radius, shadow) {
                 // pi * r^2 = 2s^2  =>  s = r * sqrt(pi/2)
@@ -77,7 +52,7 @@ function roundRect(ctx, x, y, w, h, r) {
                 ctx.moveTo(x - size, y + size);
                 ctx.lineTo(x + size, y - size);
             }
-        }
+        };
 
         var s = series.points.symbol;
         if (handlers[s])
