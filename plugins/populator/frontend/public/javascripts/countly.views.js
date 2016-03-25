94a84cefd2feddf6f9a168b54c70b5f0ddd8c14a
Artem Salpagarov <a@ahoy.li>
2016-03-25 21:16:09 +0300

    [populator] More consistent behaviour in accordance to flows
    - consecutive execution of requests;
    - less variativity of events to limit number of flows generated;
    - wating for actual data population to complete before showing "OK" status;
    - wating for jobs to complete when flows plugin is enabled;

--- plugins/populator/frontend/public/javascripts/countly.views.js
+++ plugins/populator/frontend/public/javascripts/countly.views.js
@@ -1,37 +1,53 @@
 window.PopulatorView = countlyView.extend({
-	initialize:function () {
-	},
+    initialize:function () {
+    },
     beforeRender: function() {
-		if(!this.template){
-			var self = this;
-			return $.when($.get(countlyGlobal["path"]+'/populator/templates/populate.html', function(src){
-				self.template = Handlebars.compile(src);
-			})).then(function () {});
-		}
+        if(!this.template){
+            var self = this;
+            return $.when($.get(countlyGlobal["path"]+'/populator/templates/populate.html', function(src){
+                self.template = Handlebars.compile(src);
+            })).then(function () {});
+        }
     },
     renderCommon:function (isRefresh) {
-		this.templateData = {
-			"page-title":jQuery.i18n.map["populator.title"]
-		};
-		var now = new Date();
-		var fromDate = new Date(now.getTime()-1000*60*60*24*30);
-		var toDate = now;
+        this.templateData = {
+            "page-title":jQuery.i18n.map["populator.title"]
+        };
+        var now = new Date();
+        var fromDate = new Date(now.getTime()-1000*60*60*24*30);
+        var toDate = now;
         var maxTime = 60;
         var maxTimeout;
-		
-		
-		$(this.el).html(this.template(this.templateData));
-		$("#start-populate").on('click', function() {
+        
+        
+        $(this.el).html(this.template(this.templateData));
+        $("#start-populate").on('click', function() {
             CountlyHelpers.confirm(jQuery.i18n.map["populator.warning1"]+" ("+countlyGlobal["apps"][countlyCommon.ACTIVE_APP_ID].name+").<br/>"+jQuery.i18n.map["populator.warning2"], "red", function (result) {
                 if (!result) {
-					return true;
-				}
-                maxTime = $( "#populate-maxtime" ).val() || maxTime;
+                    return true;
+                }
+                maxTime = parseInt($( "#populate-maxtime" ).val()) || maxTime;
                 maxTimeout = setTimeout(function(){
-                    countlyPopulator.stopGenerating();
+                    $("#populator-status").fadeOut().text(jQuery.i18n.map["populator.processing"]).fadeIn();
+                    countlyPopulator.stopGenerating(function(done){
+                        if (done === true) {
                     $("#stop-populate").hide();
                     $("#start-populate").show();
+                            $("#populator-status").fadeOut().text(jQuery.i18n.map["populator.done"]).fadeIn().delay(2000).text('');
                     CountlyHelpers.alert(jQuery.i18n.map["populator.success"], "green");
+                            $("#populate-bar div").css({width: 0});
+                        } else if (done === false) {
+                            $("#populator-status").html(jQuery.i18n.map["populator.jobs"]);
+                            $("#stop-populate").hide();
+                            // do nothing for now
+                        } else {
+                            CountlyHelpers.alert(done, "red");
+                            $("#stop-populate").hide();
+                            $("#start-populate").show();
+                            $("#populator-status").hide();
+                            $("#populate-bar div").css({width: 0});
+                        }
+                    });
                 }, maxTime*1000);
                 
                 fromDate = $( "#populate-from" ).datepicker( "getDate" ) || fromDate;
@@ -41,43 +57,44 @@ window.PopulatorView = countlyView.extend({
                 countlyPopulator.generateUsers(parseInt($("#populate-users").val()));
                 $("#start-populate").hide();
                 $("#stop-populate").show();
+                $("#populator-status").fadeOut().text(jQuery.i18n.map["populator.generating"]).fadeIn();
                 $("#populate-bar div").animate({width:"100%"}, 60000);
             });
-		});
-		$("#stop-populate").on('click', function() {
+        });
+        $("#stop-populate").on('click', function() {
             if(maxTimeout){
                 clearTimeout(maxTimeout);
                 maxTimeout = null;
             }
-			countlyPopulator.stopGenerating();
-			$("#stop-populate").hide();
-			$("#start-populate").show();
+            countlyPopulator.stopGenerating();
+            $("#stop-populate").hide();
+            $("#start-populate").show();
             $("#populate-bar div").stop(true);
             $("#populate-bar div").width(0);
             CountlyHelpers.alert(jQuery.i18n.map["populator.success"], "green");
-		});
-		
-		$("#populate-explain").on('click', function() {
-			CountlyHelpers.alert(jQuery.i18n.map["populator.help"], "green");
-		});
-		
-		if(countlyPopulator.isGenerating()){
-			$("#start-populate").hide();
-			$("#stop-populate").show();
-			countlyPopulator.generateUI();
-			$( "#populate-users" ).val(countlyPopulator.getUserAmount());
-			$( "#populate-from" ).val(moment(countlyPopulator.getStartTime()*1000).format("YYYY-MM-DD"));
-			$( "#populate-to" ).val(moment(countlyPopulator.getEndTime()*1000).format("YYYY-MM-DD"));
-			$( "#populate-from" ).datepicker({dateFormat: "yy-mm-dd", defaultDate:new Date(countlyPopulator.getStartTime()*1000), constrainInput:true, maxDate: now });
-			$( "#populate-to" ).datepicker({dateFormat: "yy-mm-dd", defaultDate:new Date(countlyPopulator.getEndTime()*1000), constrainInput:true, maxDate: now });
-		}
-		else{
-			$( "#populate-from" ).val(moment(fromDate).format("YYYY-MM-DD"));
-			$( "#populate-to" ).val(moment(toDate).format("YYYY-MM-DD"));
-			$( "#populate-from" ).datepicker({dateFormat: "yy-mm-dd", defaultDate:-30, constrainInput:true, maxDate: now });
-			$( "#populate-to" ).datepicker({dateFormat: "yy-mm-dd", constrainInput:true, maxDate: now });
-		}
-		app.localize();
+        });
+        
+        $("#populate-explain").on('click', function() {
+            CountlyHelpers.alert(jQuery.i18n.map["populator.help"], "green");
+        });
+        
+        if(countlyPopulator.isGenerating()){
+            $("#start-populate").hide();
+            $("#stop-populate").show();
+            countlyPopulator.generateUI();
+            $( "#populate-users" ).val(countlyPopulator.getUserAmount());
+            $( "#populate-from" ).val(moment(countlyPopulator.getStartTime()*1000).format("YYYY-MM-DD"));
+            $( "#populate-to" ).val(moment(countlyPopulator.getEndTime()*1000).format("YYYY-MM-DD"));
+            $( "#populate-from" ).datepicker({dateFormat: "yy-mm-dd", defaultDate:new Date(countlyPopulator.getStartTime()*1000), constrainInput:true, maxDate: now });
+            $( "#populate-to" ).datepicker({dateFormat: "yy-mm-dd", defaultDate:new Date(countlyPopulator.getEndTime()*1000), constrainInput:true, maxDate: now });
+        }
+        else{
+            $( "#populate-from" ).val(moment(fromDate).format("YYYY-MM-DD"));
+            $( "#populate-to" ).val(moment(toDate).format("YYYY-MM-DD"));
+            $( "#populate-from" ).datepicker({dateFormat: "yy-mm-dd", defaultDate:-30, constrainInput:true, maxDate: now });
+            $( "#populate-to" ).datepicker({dateFormat: "yy-mm-dd", constrainInput:true, maxDate: now });
+        }
+        app.localize();
         if(this.state == "/autostart"){
             $("#start-populate").click();
         }
@@ -100,16 +117,16 @@ app.route('/manage/populate*state', 'populate', function (state) {
 
 var start_populating = false;
 app.addPageScript("/manage/apps", function(){
-	var populateApp = '<tr class="populate-demo-data">'+
-		'<td>'+
-			'<span data-localize="populator.demo-data"></span>'+
-		'</td>'+
-		'<td>'+
-			'<input type="checkbox" id="populate-app-after"/>&nbsp;&nbsp;&nbsp;<span data-localize="populator.tooltip"></span>'+
-		'</td>'+
-	'</tr>';
-	
-	$("#add-new-app table .table-add").before(populateApp);
+    var populateApp = '<tr class="populate-demo-data">'+
+        '<td>'+
+            '<span data-localize="populator.demo-data"></span>'+
+        '</td>'+
+        '<td>'+
+            '<input type="checkbox" id="populate-app-after"/>&nbsp;&nbsp;&nbsp;<span data-localize="populator.tooltip"></span>'+
+        '</td>'+
+    '</tr>';
+    
+    $("#add-new-app table .table-add").before(populateApp);
     
     var appId = countlyCommon.ACTIVE_APP_ID;
     if(countlyGlobal["apps"][appId] && (countlyGlobal["apps"][appId].type == "mobile" || countlyGlobal["apps"][appId].type == "web")) {
@@ -118,15 +135,15 @@ app.addPageScript("/manage/apps", function(){
     else{
         $(".populate-demo-data").hide();
     }
-	
-	$("#save-app-add").click(function () {
-		if($("#add-new-app table #populate-app-after").is(':checked')){
+    
+    $("#save-app-add").click(function () {
+        if($("#add-new-app table #populate-app-after").is(':checked')){
             start_populating = true;
             setTimeout(function(){
                 start_populating = false;
             }, 5000);
         }
-	});
+    });
 });
 
 app.addAppManagementSwitchCallback(function(appId, type){
