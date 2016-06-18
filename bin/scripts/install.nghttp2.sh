60d3e730e64c05d6130879dfcd300da5e766aa02
Onur Alp Soner <oas@count.ly>
2016-06-18 03:17:18 +0300

    Add --force-yes to install.nghttp2.sh

--- bin/scripts/install.nghttp2.sh
+++ bin/scripts/install.nghttp2.sh
@@ -8,7 +8,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 add-apt-repository -y ppa:ondrej/apache2
-apt-get update && apt-get install -y openssl git g++ make binutils autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libjansson-dev libjemalloc-dev cython python3-dev python-setuptools
+apt-get update && apt-get install -y --force-yes openssl git g++ make binutils autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libjansson-dev libjemalloc-dev cython python3-dev python-setuptools
 git clone https://github.com/nghttp2/nghttp2.git
 cd nghttp2
 export CFLAGS="-g -O2 -fPIC" && export CPPFLAGS="-fPIC" && autoreconf -i && automake && autoconf && ./configure --disable-examples --disable-app && make && make install
