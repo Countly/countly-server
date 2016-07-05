46875064e1f5119adb880c5a4a6730e7b99d041b
Arturs Sosins <ar2rsawseen@gmail.com>
2016-07-05 16:16:04 +0300

    [frontend] use sendStatus instead of sent(status)

--- frontend/express/app.js
+++ frontend/express/app.js
@@ -718,7 +718,7 @@ app.post(countlyConfig.path+'/login', function (req, res, next) {
 app.get(countlyConfig.path+'/api-key', function (req, res, next) {
     function unauthorized(res) {
         res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-        return res.send(401);
+        return res.sendStatus(401);
     };
     var user = basicAuth(req);
     
