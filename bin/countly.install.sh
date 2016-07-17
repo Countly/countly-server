af0e89d39c818bdad044a8e21924f66ac127101d
Arturs Sosins <ar2rsawseen@gmail.com>
2016-07-17 14:27:36 +0300

    [scripts] more consistency in naming

--- bin/countly.install.sh
+++ bin/countly.install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ -f /etc/lsb-release ]; then
-    bash $DIR/countly.install_deb.sh
+    bash $DIR/countly.install_ubuntu.sh
 elif [ -f /etc/redhat-release ]; then
     bash $DIR/countly.install_rhel.sh
 fi
