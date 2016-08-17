693fb15e489c2853df7037c4863d926a0badcc84
Arturs Sosins <ar2rsawseen@gmail.com>
2016-08-17 11:52:49 +0300

    [web] parse user agent on server to get missing information

--- plugins/web/api/api.js
+++ plugins/web/api/api.js
@@ -1,8 +1,32 @@
 var plugin = {},
+    useragent = require('useragent'),
 	common = require('../../../api/utils/common.js'),
     plugins = require('../../pluginManager.js');
 
 (function (plugin) {
+    plugins.register("/session/metrics", function(ob){
+        var params = ob.params;
+        if(params.app.type == "web"){
+            if(!params.qstring.metrics)
+                params.qstring.metrics = {};
+
+            //if some metrics are not provided, parse them from user agent
+            var agent = useragent.parse(params.req.headers['user-agent'], params.qstring.metrics._ua);
+            
+            if(!params.qstring.metrics._browser)
+                params.qstring.metrics._browser = agent.family;
+            
+            if(!params.qstring.metrics._os)
+                params.qstring.metrics._os = agent.os.family;
+            
+            if(!params.qstring.metrics._os_version && (agent.os.major != 0 || agent.os.minor != 0 || agent.os.patch != 0))
+                params.qstring.metrics._os_version = agent.os.toVersion();
+            
+            if(!params.qstring.metrics._device && agent.device.family != "Other")
+                params.qstring.metrics._device = agent.device.family;
+        }
+	});
+    
 	plugins.register("/o", function(ob){
 		var params = ob.params;
 		var validateUserForDataReadAPI = ob.validateUserForDataReadAPI;
