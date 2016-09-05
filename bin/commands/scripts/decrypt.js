59d57c99043eda308343c1c1646865e1b90d6f17
Arturs Sosins <ar2rsawseen@gmail.com>
2016-09-05 15:53:42 +0300

    [cmd] provide encrypt/decrypt commands

---
+++ bin/commands/scripts/decrypt.js
@@ -0,0 +1,9 @@
+var utils = require('../../../api/utils/utils.js');
+    
+var myArgs = process.argv.slice(2);
+//check if we have a command
+if(myArgs[0]){
+    console.log(utils.decrypt(myArgs[0]));
+}
+else
+    console.log("No text provided to decrypt");
