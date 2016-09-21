fed0a76b4bd74c669363cdeee74c8ac8f3dd8cc0
Arturs Sosins <ar2rsawseen@gmail.com>
2016-09-21 20:29:55 +0300

    [api] set security default confis also on api side

--- api/api.js
+++ api/api.js
@@ -26,6 +26,13 @@ plugins.setConfigs("apps", {
     timezone: "Europe/Istanbul",
     category: "6"
 });
+
+plugins.setConfigs("security", {
+    login_tries: 3,
+    login_wait: 5*60,
+    dashboard_additional_headers: "X-Frame-Options:deny\nX-XSS-Protection:1; mode=block\nX-Content-Type-Options:nosniff\nStrict-Transport-Security:max-age=31536000 ; includeSubDomains",
+    api_additional_headers: "X-Frame-Options:deny\nX-XSS-Protection:1; mode=block\nX-Content-Type-Options:nosniff"
+});
     
 plugins.setConfigs('logs', {
     debug:      (countlyConfig.logging && countlyConfig.logging.debug)     ?  countlyConfig.logging.debug.join(', ')    : '',
