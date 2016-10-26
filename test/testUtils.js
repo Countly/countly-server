b01549508a4be4390021147ac3d26918ac5ea200
Arturs Sosins <ar2rsawseen@gmail.com>
2016-10-26 15:20:40 +0300

    [test] decrease cooldowns between tests

--- test/testUtils.js
+++ test/testUtils.js
@@ -193,7 +193,7 @@ var testUtils = function testUtils(){
                 }
             }
 		}
-		setTimeout(done, 10000)
+		setTimeout(done, 100)
 	};
 	
 	this.validateDashboard = function(err, res, done, correct){
@@ -235,7 +235,7 @@ var testUtils = function testUtils(){
 			period.should.have.property("period");
 		}
 		
-		setTimeout(done, 10000);
+		setTimeout(done, 100);
 	};
 	
 	this.validateCountries = function(err, res, done, correct){
@@ -256,7 +256,7 @@ var testUtils = function testUtils(){
 				ob[key][i].should.have.property("n", correct.n);
 			}
 		}
-		setTimeout(done, 10000)
+		setTimeout(done, 100)
 	};
 	
 	this.validateMetrics = function(err, res, done, correct){
@@ -311,7 +311,7 @@ var testUtils = function testUtils(){
 				}
 			}
 		}
-		setTimeout(done, 10000);
+		setTimeout(done, 100);
 	};
 	
 	this.validateEvents = function(err, res, done, correct, refresh){
@@ -391,7 +391,7 @@ var testUtils = function testUtils(){
 				}
 			}
 		}
-		setTimeout(done, 10000)
+		setTimeout(done, 100)
 	};
 }
 
