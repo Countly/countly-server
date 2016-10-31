bd01eb0fe4c462b5f0847067333b98eea1a7d8ac
Arturs Sosins <ar2rsawseen@gmail.com>
2016-10-31 12:08:06 +0200

    [systemlogs] record user _id when it is possible

--- plugins/systemlogs/api/api.js
+++ plugins/systemlogs/api/api.js
@@ -19,60 +19,94 @@ var plugin = {},
 			return true;
 		}
 	});
+    
+    plugins.register("/i/systemlogs", function(ob){
+		var params = ob.params;
+        if(typeof params.qstring.data === "string"){
+            try{
+                params.qstring.data = JSON.parse(params.qstring.data);
+            }
+            catch(ex){
+                console.log("Error parsing systemlogs data", params.qstring.data);
+            }
+        }
+        if(typeof params.qstring.action == "string")
+            recordAction(params, {}, params.qstring.action, params.qstring.data);
+	});
 	
 	plugins.register("/i/apps/create", function(ob){
-		var appId = ob.appId;
-		var params = ob.params;
-		var data = {_id:appId, name:ob.data.name};
-		var now = Math.round(new Date().getTime()/1000);
-		common.db.collection('systemlogs').insert({ts:now, a:"App Created", u:params.member.email, ip:common.getIpAddress(params.req), i:data}, function () {});
+        ob.data._id = ob.appId;
+        recordAction(ob.params, params.member, "App Created", ob.data);
 	});
 	
 	plugins.register("/i/apps/update", function(ob){
 		var appId = ob.appId;
-		var params = ob.params;
-		var data = ob.data;
-		data._id = appId;
-		var now = Math.round(new Date().getTime()/1000);
-		common.db.collection('systemlogs').insert({ts:now, a:"App Updated", u:params.member.email, ip:common.getIpAddress(params.req), i:data}, function () {});
+		ob.data._id = appId;
+        recordAction(ob.params, params.member, "App Updated", ob.data);
 	});
 	
 	plugins.register("/i/apps/delete", function(ob){
-		var appId = ob.appId;
-		var params = ob.params;
-		var now = Math.round(new Date().getTime()/1000);
-		var data = ob.data;
-		common.db.collection('systemlogs').insert({ts:now, a:"App Deleted", u:params.member.email, ip:common.getIpAddress(params.req), i:data}, function () {});
+        recordAction(ob.params, params.member, "App Deleted", ob.data);
 	});
 	
 	plugins.register("/i/apps/reset", function(ob){
 		var appId = ob.appId;
-		var params = ob.params;
-		var now = Math.round(new Date().getTime()/1000);
-		var data = {_id:appId, name:ob.data.name};
-		common.db.collection('systemlogs').insert({ts:now, a:"App Reset", u:params.member.email, ip:common.getIpAddress(params.req), i:data}, function () {});
+        ob.data._id = appId;
+        recordAction(ob.params, params.member, "App Reset", ob.data);
 	});
 	
 	plugins.register("/i/users/create", function(ob){
-		var params = ob.params;
-		var data = ob.data;
-		var now = Math.round(new Date().getTime()/1000);
-		common.db.collection('systemlogs').insert({ts:now, a:"User Created", u:params.member.email, ip:common.getIpAddress(params.req), i:data}, function () {});
+        recordAction(ob.params, params.member, "User Created", ob.data);
 	});
 	
 	plugins.register("/i/users/update", function(ob){
-		var params = ob.params;
-		var data = ob.data;
-		var now = Math.round(new Date().getTime()/1000);
-		common.db.collection('systemlogs').insert({ts:now, a:"User Updated", u:params.member.email, ip:common.getIpAddress(params.req), i:data}, function () {});
+        recordAction(ob.params, params.member, "User Updated", ob.data);
 	});
 	
 	plugins.register("/i/users/delete", function(ob){
-		var params = ob.params;
-		var data = ob.data;
-		var now = Math.round(new Date().getTime()/1000);
-		common.db.collection('systemlogs').insert({ts:now, a:"User Deleted", u:params.member.email, ip:common.getIpAddress(params.req), i:data}, function () {});
+        recordAction(ob.params, params.member, "User Deleted", ob.data);
 	});
+    
+    plugins.register("/systemlogs", function(ob){
+        var user = ob.user || params.member;
+        recordAction(ob.params, user, ob.action, ob.data);
+    });
+    
+    function recordAction(params, user, action, data){
+        var log = {};
+        log.a = action;
+        log.i = data;
+        log.ts = Math.round(new Date().getTime()/1000);
+        log.u = user.email || user.username || "";
+        log.ip = common.getIpAddress(params.req);
+        if(user._id){
+            log.user_id = user._id;
+            common.db.collection('systemlogs').insert(log, function () {});
+        }
+        else{
+            var query = {};
+            if(user.username)
+                query.username = user.username;
+            else if(user.email)
+                query.email = user.email;
+            else if (params.qstring.api_key)
+                query.api_key = params.qstring.api_key;
+            if(Object.keys(query).length){
+                common.db.collection('members').findOne(query, function(err, res){
+                    if(!err && res){
+                        log.user_id = res._id;
+                        if(log.u == ""){
+                            log.u = res.email || res.username;
+                        }
+                    }
+                     common.db.collection('systemlogs').insert(log, function () {});
+                });
+            }
+            else{
+                common.db.collection('systemlogs').insert(log, function () {});
+            }
+        }
+    };
 }(plugin));
 
 module.exports = plugin;
