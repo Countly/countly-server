f8767392db210d879ecbcb2fed054f72cd5111a4
Arturs Sosins <ar2rsawseen@gmail.com>
2016-11-03 11:17:20 +0200

    [scripts] create install logs

--- bin/countly.install.sh
+++ bin/countly.install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DATE=`date +%Y-%m-%d:%H:%M:%S`
 if [ -f /etc/lsb-release ]; then
-    bash $DIR/countly.install_ubuntu.sh
+    bash $DIR/countly.install_ubuntu.sh 2>&1 | tee $DIR/../log/countly-install-$DATE.log
 elif [ -f /etc/redhat-release ]; then
-    bash $DIR/countly.install_rhel.sh
+    bash $DIR/countly.install_rhel.sh 2>&1 | tee $DIR/../log/countly-install-$DATE.log
 fi
