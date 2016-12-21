6b35b2f9892500f9e2122c94a24ee5dc22ed03e7
Arturs Sosins <ar2rsawseen@gmail.com>
2016-12-21 23:18:23 +0200

    Revert "[mongodb] added logrotation option"
    This reverts commit 19f63fe1e0cad1b08628c240602d54ef6961888a.

--- bin/scripts/configure_mongodb.js
+++ bin/scripts/configure_mongodb.js
@@ -17,11 +17,6 @@ if(myArgs.length && fs.existsSync(myArgs[0])){
     if(!ob.net)
         ob.net = {};
     ob.net.bindIp = "127.0.0.1";
- 
-    if(!ob.systemLog)
-        ob.systemLog = {};
-    ob.systemLog.logAppend = true;
-    ob.systemLog.logRotate = "reopen";
     
     fs.writeFileSync(myArgs[0], yaml.stringify(ob, 4));
 }
