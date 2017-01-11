3ad545b4a57f4a294b86a0b669d930d0d631a67e
Arturs Sosins <ar2rsawseen@gmail.com>
2017-01-11 15:16:23 +0200

    [momentjs] provide formatting for not iso dates

--- api/lib/countly.common.js
+++ api/lib/countly.common.js
@@ -188,16 +188,16 @@ var countlyCommon = {},
                 if (!countlyCommon.periodObj.isSpecialPeriod) {
 
                     if (countlyCommon.periodObj.periodMin == 0) {
-                        formattedDate = moment((activeDate + " " + i + ":00:00").replace(/\./g, "/"));
+                        formattedDate = moment((activeDate + " " + i + ":00:00").replace(/\./g, "/"), "YYYY/MM/DD HH:mm:ss");
                     } else if (("" + activeDate).indexOf(".") == -1) {
-                        formattedDate = moment((activeDate + "/" + i + "/1").replace(/\./g, "/"));
+                        formattedDate = moment((activeDate + "/" + i + "/1").replace(/\./g, "/"), "YYYY/MM/DD");
                     } else {
-                        formattedDate = moment((activeDate + "/" + i).replace(/\./g, "/"));
+                        formattedDate = moment((activeDate + "/" + i).replace(/\./g, "/"), "YYYY/MM/DD");
                     }
 
                     dataObj = countlyCommon.getDescendantProp(db, activeDate + "." + i);
                 } else {
-                    formattedDate = moment((activeDateArr[i]).replace(/\./g, "/"));
+                    formattedDate = moment((activeDateArr[i]).replace(/\./g, "/"), "YYYY/MM/DD");
                     dataObj = countlyCommon.getDescendantProp(db, activeDateArr[i]);
                 }
 
