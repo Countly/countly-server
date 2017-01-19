0c75f1d62f4f242ff8e84a2a32ab589b1e2cefea
Arturs Sosins <ar2rsawseen@gmail.com>
2017-01-19 12:48:31 +0200

    [upgrade] 16.12.2

---
+++ bin/upgrade/16.12.2/upgrade.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+
+#enable command line
+bash $DIR/scripts/detect.init.sh
+
+#upgrade plugins
+countly plugin upgrade push
+
+countly update sdk-web
+
+#add indexes
+nodejs $DIR/scripts/add_indexes.js
+
+#install dependencies, process files and restart countly
+countly upgrade
