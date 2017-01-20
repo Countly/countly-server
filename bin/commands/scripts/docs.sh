73d46bfd19b0d6ca40c1361e4e6d1ade6052cae6
Arturs Sosins <ar2rsawseen@gmail.com>
2017-01-20 18:46:38 +0200

    [cmd] generate docs

---
+++ bin/commands/scripts/docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+usage (){
+    echo "";
+    echo "countly docs usage:";
+    echo "    countly docs generate # regenerate documentation";
+} 
+if [ -z "$1" ]
+then
+    usage ;
+elif [ "$1" = "generate" ]; then
+    $DIR/../../../node_modules/.bin/jsdoc $DIR/../../../frontend/express/public/javascripts/countly -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../docs/browser ;
+    #$DIR/../../../node_modules/.bin/jsdoc $DIR/../../../api -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../docs/api ;
+fi
