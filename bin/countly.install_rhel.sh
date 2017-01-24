ad0ff21fd81c71bf247a17ce0803e8918043de21
Waiter Zen <waiter.james1991@gmail.com>
2017-01-24 18:40:38 +0800

    add google services checking in install scripts

--- bin/countly.install_rhel.sh
+++ bin/countly.install_rhel.sh
@@ -132,6 +132,15 @@ cd $DIR/.. && grunt dist-all
 #finally start countly api and dashboard
 countly start
 
+# close google services for China area
+if ping -c 1 google.com >> /dev/null 2>&1; then
+    echo "enable google services"
+else
+    echo "should close google services"
+    countly config "frontend.use_google" false
+fi
+
+
 ENABLED=`getenforce`
 if [ "$ENABLED" == "Enforcing" ]; then
   echo -e "\e[31mSELinux is enabled, please disable it or add nginx to exception for Countly to work properly\e[0m"
