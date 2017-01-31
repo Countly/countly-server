042c6856a4a432ca8afa20ddbbaa43bebbe6081e
Arturs Sosins <ar2rsawseen@gmail.com>
2017-01-31 12:22:29 +0200

    [views] reused common function to reduce code

--- plugins/views/frontend/public/javascripts/countly.models.js
+++ plugins/views/frontend/public/javascripts/countly.models.js
@@ -1,29 +1,29 @@
-(function (countlyMetric, _name, $) {
-	//Private Properties
-	var _periodObj = {},
-		_Db = {},
-		_metrics = [],
-        _frequency = [],
+(function () {
+    window.countlyViews = window.countlyViews || {};
+    CountlyHelpers.createMetricModel(window.countlyViews, {name: "views"}, jQuery);
+    //Private Properties
+    var _periodObj = {},
         _actionData = {},
-		_activeAppKey = 0,
-		_initialized = false,
+        _activeAppKey = 0,
+        _initialized = false,
         _segment = null,
         _segments = [],
-		_period = null;
+        _name = "views",
+        _period = null;
 
-	//Public Methods
-	countlyMetric.initialize = function () {
-		if (_initialized &&  _period == countlyCommon.getPeriodForAjax() && _activeAppKey == countlyCommon.ACTIVE_APP_KEY) {
-			return this.refresh();
-		}
+    //Public Methods
+    countlyViews.initialize = function () {
+        if (_initialized &&  _period == countlyCommon.getPeriodForAjax() && _activeAppKey == countlyCommon.ACTIVE_APP_KEY) {
+            return this.refresh();
+        }
 
-		_period = countlyCommon.getPeriodForAjax();
+        _period = countlyCommon.getPeriodForAjax();
 
-		if (!countlyCommon.DEBUG) {
-			_activeAppKey = countlyCommon.ACTIVE_APP_KEY;
-			_initialized = true;
+        if (!countlyCommon.DEBUG) {
+            _activeAppKey = countlyCommon.ACTIVE_APP_KEY;
+            _initialized = true;
 
-			return $.when(
+            return $.when(
                 $.ajax({
                     type:"GET",
                     url:countlyCommon.API_PARTS.data.r,
@@ -55,33 +55,32 @@
                     },
                     dataType:"jsonp",
                     success:function (json) {
-                        _Db = json;
-                        setMeta();
+                        countlyViews.setDb(json);
                     }
                 })
             ).then(function(){
                 return true;
             });
-		} else {
-			_Db = {"2012":{}};
-			return true;
-		}
-	};
+        } else {
+            _Db = {"2012":{}};
+            return true;
+        }
+    };
 
-	countlyMetric.refresh = function () {
-		_periodObj = countlyCommon.periodObj;
+    countlyViews.refresh = function () {
+        _periodObj = countlyCommon.periodObj;
 
-		if (!countlyCommon.DEBUG) {
+        if (!countlyCommon.DEBUG) {
 
-			if (_activeAppKey != countlyCommon.ACTIVE_APP_KEY) {
-				_activeAppKey = countlyCommon.ACTIVE_APP_KEY;
-				return this.initialize();
-			}
+            if (_activeAppKey != countlyCommon.ACTIVE_APP_KEY) {
+                _activeAppKey = countlyCommon.ACTIVE_APP_KEY;
+                return this.initialize();
+            }
             
             if(!_initialized)
                 return this.initialize();
 
-			return $.when(
+            return $.when(
                 $.ajax({
                     type:"GET",
                     url:countlyCommon.API_PARTS.data.r,
@@ -114,41 +113,39 @@
                     },
                     dataType:"jsonp",
                     success:function (json) {
-                        countlyCommon.extendDbObj(_Db, json);
-                        extendMeta();
+                        countlyViews.extendDb(json);
                     }
                 })
             ).then(function(){
                 return true;
             });
-		} else {
-			_Db = {"2012":{}};
+        } else {
+            _Db = {"2012":{}};
 
-			return true;
-		}
-	};
+            return true;
+        }
+    };
 
-	countlyMetric.reset = function () {
-		_Db = {};
-        _frequency = [];
+    countlyViews._reset = countlyViews.reset;
+    countlyViews.reset = function () {
         _actionData = {};
         _segment - null;
         _initialized = false;
-		setMeta();
-	};
+        countlyViews._reset();
+    };
     
-    countlyMetric.setSegment = function(segment){
+    countlyViews.setSegment = function(segment){
         _segment = countlyCommon.decode(segment);
     };
     
-    countlyMetric.getSegments = function(){
+    countlyViews.getSegments = function(){
         return _segments;
     };
     
-    countlyMetric.loadActionsData = function (view) {
-		_period = countlyCommon.getPeriodForAjax();
+    countlyViews.loadActionsData = function (view) {
+        _period = countlyCommon.getPeriodForAjax();
 
-		return $.when(
+        return $.when(
             $.ajax({
                 type:"GET",
                 url:countlyCommon.API_PARTS.data.r,
@@ -186,9 +183,9 @@
         ).then(function(){
             return true;
         });
-	};
+    };
     
-    countlyMetric.testUrl = function(url, callback){
+    countlyViews.testUrl = function(url, callback){
         $.ajax({
             type:"GET",
             url:countlyCommon.API_PARTS.data.r+"/urltest",
@@ -203,12 +200,12 @@
         });
     };
     
-    countlyMetric.getActionsData = function (view) {
+    countlyViews.getActionsData = function (view) {
         return _actionData;
     };
     
-    countlyMetric.getChartData = function(path, metric, name){
-		var chartData = [
+    countlyViews.getChartData = function(path, metric, name){
+        var chartData = [
                 { data:[], label:name, color:'#DDDDDD', mode:"ghost" },
                 { data:[], label:name, color:'#333933' }
             ],
@@ -223,100 +220,62 @@
                 { name:metric}
             ];
 
-        return countlyCommon.extractChartData(_Db, countlyMetric.clearObject, chartData, dataProps, path);
-	};
+        return countlyCommon.extractChartData(countlyViews.getDb(), countlyViews.clearObject, chartData, dataProps, path);
+    };
 
-	countlyMetric.getData = function (clean) {
+    countlyViews.getData = function (clean) {
 
-		var chartData = countlyCommon.extractTwoLevelData(_Db, _metrics, this.clearObject, [
-			{
-				name:_name,
-				func:function (rangeArr, dataObj) {
+        var chartData = countlyCommon.extractTwoLevelData(countlyViews.getDb(), countlyViews.getMeta(), countlyViews.clearObject, [
+            {
+                name:_name,
+                func:function (rangeArr, dataObj) {
                     return countlyCommon.decode(rangeArr);
-				}
-			},
-			{ "name":"u" },
-			{ "name":"t" },
-			{ "name":"s" },
-			{ "name":"b" },
-			{ "name":"e" },
-			{ "name":"d" },
-			{ "name":"n" }
-		]);
+                }
+            },
+            { "name":"u" },
+            { "name":"t" },
+            { "name":"s" },
+            { "name":"b" },
+            { "name":"e" },
+            { "name":"d" },
+            { "name":"n" }
+        ]);
 
         chartData.chartData = countlyCommon.mergeMetricsByName(chartData.chartData, _name);
-
-		return chartData;
-	};
-
-	countlyMetric.clearObject = function (obj) {
-		if (obj) {
-			if (!obj["u"]) obj["u"] = 0;
-			if (!obj["t"]) obj["t"] = 0;
-			if (!obj["n"]) obj["n"] = 0;
-			if (!obj["s"]) obj["s"] = 0;
-			if (!obj["e"]) obj["e"] = 0;
-			if (!obj["b"]) obj["b"] = 0;
-			if (!obj["d"]) obj["d"] = 0;
-		}
-		else {
-			obj = {"u":0, "t":0, "n":0, "s":0, "e":0, "b":0, "d":0};
-		}
-
-		return obj;
-	};
-
-	countlyMetric.getBars = function () {
-		return countlyCommon.extractBarData(_Db, _metrics, this.clearObject, fetchValue);
-	};
-    
-    countlyMetric.getViewFrequencyData = function () {
-        var _sessionDb = countlySession.getDb();
-        if(_frequency.length){
-            if (_sessionDb['meta']) {
-                _frequency = countlyCommon.union(_frequency, _sessionDb['meta']['v-ranges']);
-            }
-        }
-        else{
-            if (_sessionDb['meta']) {
-                _frequency = (_sessionDb['meta']['v-ranges']) ? _sessionDb['meta']['v-ranges'] : [];
-            } else {
-                _frequency = [];
-            }
-        }
-        var chartData = {chartData:{}, chartDP:{dp:[], ticks:[]}};
-
-        chartData.chartData = countlyCommon.extractRangeData(_sessionDb, "vc", _frequency, countlyMetric.explainFrequencyRange);
-
-        var durations = _.pluck(chartData.chartData, "vc"),
-            durationTotals = _.pluck(chartData.chartData, "t"),
-            chartDP = [
-                {data:[]}
-            ];
-
-        chartDP[0]["data"][0] = [-1, null];
-        chartDP[0]["data"][durations.length + 1] = [durations.length, null];
-
-        chartData.chartDP.ticks.push([-1, ""]);
-        chartData.chartDP.ticks.push([durations.length, ""]);
-
-        for (var i = 0; i < durations.length; i++) {
-            chartDP[0]["data"][i + 1] = [i, durationTotals[i]];
-            chartData.chartDP.ticks.push([i, durations[i]]);
-        }
-
-        chartData.chartDP.dp = chartDP;
-
-        for (var i = 0; i < chartData.chartData.length; i++) {
-            chartData.chartData[i]["percent"] = "<div class='percent-bar' style='width:" + (2 * chartData.chartData[i]["percent"]) + "px;'></div>" + chartData.chartData[i]["percent"] + "%";
-        }
 
         return chartData;
     };
     
-    countlyMetric.explainFrequencyRange = function (index) {
+    countlyViews.clearObject = function (obj) {
+        if (obj) {
+            if (!obj["u"]) obj["u"] = 0;
+            if (!obj["t"]) obj["t"] = 0;
+            if (!obj["n"]) obj["n"] = 0;
+            if (!obj["s"]) obj["s"] = 0;
+            if (!obj["e"]) obj["e"] = 0;
+            if (!obj["b"]) obj["b"] = 0;
+            if (!obj["d"]) obj["d"] = 0;
+        }
+        else {
+            obj = {"u":0, "t":0, "n":0, "s":0, "e":0, "b":0, "d":0};
+        }
+        return obj;
+    };
+    
+    countlyViews.getViewFrequencyData = function () {
+        var _Db = countlyViews.getDb();
+        countlyViews.setDb(countlySession.getDb());
+        
+        var data = countlyViews.getRangeData("vc", "v-ranges", countlyViews.explainFrequencyRange);
+        
+        countlyViews.setDb(_Db);
+        
+        return data;
+    };
+    
+    var getRange = function(){
         var visits = jQuery.i18n.map["views.visits"].toLowerCase();
-        var range = [
+        return [
             "1 - 2 " + visits,
             "3 - 5 " + visits,
             "6 - 10 " + visits,
@@ -326,39 +285,14 @@
             "51 - 100 " + visits,
             "> 100 " + visits
         ];
-
-        return range[index];
+    };
+    
+    countlyViews.explainFrequencyRange = function (index) {
+        return getRange()[index];
     };
 
-    countlyMetric.getFrequencyIndex = function (value) {
-        var visits = jQuery.i18n.map["views.visits"].toLowerCase();
-
-        var range = [
-            "1 - 2 " + visits,
-            "3 - 5 " + visits,
-            "6 - 10 " + visits,
-            "11 - 15 " + visits,
-            "16 - 30 " + visits,
-            "31 - 50 " + visits,
-            "51 - 100 " + visits,
-            "> 100 " + visits
-        ];
-
-        return range.indexOf(value);
+    countlyViews.getFrequencyIndex = function (value) {
+        return getRange().indexOf(value);
     };
 
-	function setMeta() {
-		if (_Db['meta']) {
-			_metrics = (_Db['meta'][_name]) ? _Db['meta'][_name] : [];
-		} else {
-			_metrics = [];
-		}
-	}
-
-	function extendMeta() {
-		if (_Db['meta']) {
-			_metrics = countlyCommon.union(_metrics, _Db['meta'][_name]);
-		}
-	}
-
-})(window.countlyViews = window.countlyViews || {}, "views", jQuery);
+})();
