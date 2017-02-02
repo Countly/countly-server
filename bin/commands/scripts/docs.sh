a1693bdf60d74f87179db472ed8e247540424aca
Arturs Sosins <ar2rsawseen@gmail.com>
2017-02-02 13:48:22 +0200

    [cmd] generate docs in public folder and generate main index file linking docs

--- bin/commands/scripts/docs.sh
+++ bin/commands/scripts/docs.sh
@@ -10,6 +10,7 @@ if [ -z "$1" ]
 then
     usage ;
 elif [ "$1" = "generate" ]; then
-    $DIR/../../../node_modules/.bin/jsdoc $DIR/../../../frontend/express/public/javascripts/countly $DIR/../../../frontend/express/public/javascripts/pre-login.js $DIR/../../../frontend/express/views/dashboard.html -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../docs/browser ;
-    $DIR/../../../node_modules/.bin/jsdoc $DIR/../../../api -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../docs/api ;
+    $DIR/../../../node_modules/.bin/jsdoc $DIR/maindoc.jsdoc -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../frontend/express/public/docs ;
+    $DIR/../../../node_modules/.bin/jsdoc $DIR/../../../frontend/express/public/javascripts/countly $DIR/../../../frontend/express/public/javascripts/pre-login.js -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../frontend/express/public/docs/browser ;
+    $DIR/../../../node_modules/.bin/jsdoc $DIR/../../../api -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../frontend/express/public/docs/api ;
 fi
