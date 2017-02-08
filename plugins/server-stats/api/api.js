16b8ffb747acb322cb09641241e4fdbd8d78a69f
Arturs Sosins <ar2rsawseen@gmail.com>
2017-02-08 18:13:55 +0200

    [server-stats] fixing check for last end session timestamp

--- plugins/server-stats/api/api.js
+++ plugins/server-stats/api/api.js
@@ -53,7 +53,7 @@ var plugin = {},
 
             // If the last end_session is received less than 15 seconds ago we will ignore
             // current begin_session request and mark this user as having an ongoing session
-            var lastEndSession = params.app_user[common.dbUserMap['last_end_session_timestamp']];
+            var lastEndSession = params.app_user && params.app_user[common.dbUserMap['last_end_session_timestamp']] || 0;
 
             if (params.qstring.begin_session && !params.qstring.ignore_cooldown && lastEndSession && (params.time.timestamp - lastEndSession) < plugins.getConfig("api").session_cooldown) {
                 sessionCount++;
