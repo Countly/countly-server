5576efdcf8ea552e38152333925b1ce3f41b454a
Cihangir SAVAS <cihangir@koding.com>
2017-02-16 00:27:55 +0300

    bin/config: update nginx config before starting nginx in docker

--- bin/commands/docker/nginx.sh
+++ bin/commands/docker/nginx.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+/usr/bin/nodejs /opt/countly/bin/scripts/create_nginx_conf.js
 exec /usr/sbin/nginx
---
+++ bin/scripts/create_nginx_conf.js
@@ -0,0 +1,17 @@
+// adds PATH config into nginx if it exists in ENV vars
+
+var fs = require("fs");
+var path  = require('path');
+
+var nginxConfTargetPath = "/etc/nginx/sites-enabled/default";
+var nginxConfSourcePath = path.join(__dirname, '../config/nginx.server.conf');
+
+var nginxConf = fs.readFileSync(nginxConfSourcePath, { encoding: 'utf8' });
+var apiPath = process.env.COUNTLY_PATH || "";
+
+if (apiPath !== "") {
+    var searchRegexp = /location.+?\//g;
+    var replaceRegexp = "$&" + apiPath +"\/";
+    nginxConf = nginxConf.replace(searchRegexp, replaceRegexp);
+}
+fs.writeFileSync(nginxConfTargetPath, nginxConf);
