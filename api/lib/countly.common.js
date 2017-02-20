460e537bc16cae2aa6becc464719483f190337ed
Arturs Sosins <ar2rsawseen@gmail.com>
2017-02-20 19:30:12 +0200

    [api] fix estimation

--- api/lib/countly.common.js
+++ api/lib/countly.common.js
@@ -378,7 +378,6 @@ var countlyCommon = {},
                             if (typeof tmpCheckVal !== 'string') {
                                 propertySum += tmpCheckVal;
                                 tmpUniqValCheck += tmpCheckVal;
-                                tmpPropertyObj["u"] += tmpCheckVal;
                             }
                         }
 
