e701dcdcacdda745f71ea597829682b75035c228
Arturs Sosins <ar2rsawseen@gmail.com>
2017-03-09 16:46:38 +0200

    [api] creating server side models for some plugins

---
+++ plugins/density/api/lib/countly.model.js
@@ -0,0 +1,25 @@
+var countlyModel = require('../../../../api/lib/countly.model.js'),
+    countlyCommon = require('../../../../api/lib/countly.common.js'),
+    common = require('../../../../api/utils/common.js'),
+    underscore = require('underscore');
+
+/**
+* This module defines default model to handle devices data
+* @module "plugins/density/api/lib/countly.model"
+* @extends module:api/lib/countly.model~countlyMetric
+*/
+
+var countlyDensity = countlyModel.create(function(rangeArr, dataObj){
+    var stripped = false;
+    for(var os in common.os_mapping){
+        if(rangeArr.startsWith(common.os_mapping[os])){
+            rangeArr = rangeArr.replace(common.os_mapping[os], "");
+            stripped = true;
+            break;
+        }
+    }
+    if(!stripped)
+        rangeArr = rangeArr.substr(1);
+    return rangeArr.replace(/:/g, ".");
+});
+module.exports = countlyDensity;
