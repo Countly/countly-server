f762fc8d3c3a0516498be31721d921c728b3f327
Arturs Sosins <ar2rsawseen@gmail.com>
2017-04-09 09:41:17 +0300

    [web] record highest sdk version

--- plugins/web/api/api.js
+++ plugins/web/api/api.js
@@ -8,6 +8,9 @@ var plugin = {},
     plugins.register("/sdk", function(ob){
         var params = ob.params;
         if(params.app.type == "web"){
+            if(params.qstring.sdk_version && (!params.app.sdk_version || common.versionCompare(params.qstring.sdk_version, params.app.sdk_version) === 1)){
+                common.db.collection("apps").update({_id:params.app._id}, {$set:{sdk_version:params.qstring.sdk_version}});
+            }
             var agent = useragent.parse(params.req.headers['user-agent'], (params.qstring.metrics) ? params.qstring.metrics._ua : undefined);
             var data = getOSFromAgent(agent);
             if(params.qstring.begin_session){
