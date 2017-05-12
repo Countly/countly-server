e7e801c2226479a5deaca03004cdb990c4b22bb8
ArtursK <kadikis.arturs@gmail.com>
2017-05-12 15:39:41 +0300

    [Assistant] Adding index to assistant app_id field.

--- plugins/assistant/install.js
+++ plugins/assistant/install.js
@@ -9,5 +9,7 @@ const db_name_notifs = "assistant_notifs";
 // 60 seconds * 60 minutes * 24 hours * 31 days * 2 months
 // 60 * 60 * 24 * 31 * 2 = 5 356 800
 countlyDb.collection(db_name_notifs).ensureIndex({cd: 1}, {expireAfterSeconds: 5356800}, function () {
-    countlyDb.close();
+    countlyDb.collection(db_name_notifs).ensureIndex({app_id: 1}, {}, function () {
+        countlyDb.close();
+    });
 });
