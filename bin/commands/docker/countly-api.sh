1a55216e7b0c15678b3b490120ecf05a2caf82a9
Artem <Salpagarov>
2017-07-10 15:21:52 +0300

    [docker] Wait for mongo to start before starting Countly

--- bin/commands/docker/countly-api.sh
+++ bin/commands/docker/countly-api.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+until mongo localhost/countly --eval "db.stats()" | grep -q "collections"
+do
+    echo
+    echo "[api] waiting for MongoDB to allocate files ..."
+    sleep 1
+done
+sleep 3
+echo "[api] MongoDB started"
+
 exec /sbin/setuser countly /usr/bin/nodejs /opt/countly/api/api.js
--- bin/commands/docker/countly-dashboard.sh
+++ bin/commands/docker/countly-dashboard.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+until mongo localhost/countly --eval "db.stats()" | grep "collections"
+do
+    echo
+    echo "[dashboard] waiting for MongoDB to allocate files ..."
+    sleep 1
+done
+sleep 3
+echo "[dashboard] MongoDB started"
+
 exec /sbin/setuser countly /usr/bin/nodejs /opt/countly/frontend/express/app.js
