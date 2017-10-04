e082bb0d36621cdfcce29544288daa3690d9c295
Arturs Sosins <ar2rsawseen@gmail.com>
2017-10-04 19:05:57 +0300

    [cmd] command to migrate files from one storage system to another

---
+++ bin/commands/scripts/fs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+usage (){
+    echo "";
+    echo "countly fs usage:";
+    echo "    countly fs migrate <from> <to> # from fs to gridfs or vice versa";
+} 
+if [ -z "$1" ]
+then
+    usage ;
+else
+    nodejs $DIR/fs.js $1 $2 $3;
+fi
