e8e24d93e57d2df57be8b91066fb2b2ab529ed41
Arturs Sosins <ar2rsawseen@gmail.com>
2017-10-06 13:48:05 +0300

    [scripts] fixed making swap additionally with optional amount Gb param, which defaults to 4

--- bin/scripts/make.swap.sh
+++ bin/scripts/make.swap.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
+set -e
 # This helper script creates a 4 GB swap file on /swapfile, activates it, 
 # and writes to /etc/fstab so that swap is enabled each time your instance is rebooted.
 # Run it if your instance has a small memory (e.g less than 2GB) so it doesn't 
 # go out of RAM.
 
+#amount in GB
+AMOUNT=${1:-4}
+echo "Attempting to create $AMOUNT Gb swap file"
+echo "Reserving space, may take a while"
+
 #create file
-if hash fallocate 2>/dev/null; then
-    fallocate -l 4G /swapfile
-else
-    dd if=/dev/zero of=/swapfile bs=1G count=4
-fi
+dd if=/dev/zero of=/swapfile bs=1G count=$AMOUNT
 chmod 600 /swapfile
 
 #make file a swap file
