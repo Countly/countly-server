48ce09ccb54ca635e9533cb1157342dd9cf21850
Arturs Sosins <ar2rsawseen@gmail.com>
2017-12-05 13:23:46 +0200

    [cmd] additional checks on user management

--- bin/commands/scripts/user_mgmt.js
+++ bin/commands/scripts/user_mgmt.js
@@ -14,20 +14,28 @@ function md5Hash(str) {
 }
 
 if(myArgs[0] == "register" && myArgs[1] && myArgs[2]){
-    var doc = {"full_name":myArgs[1], "username":myArgs[1], "password":sha1Hash(myArgs[2]), "email":myArgs[1], "global_admin":true};
-    db.collection('members').insert(doc, {safe:true}, function (err, member) {
-        if(err){
-            console.log(err);
-             db.close();
+    db.collection('members').findOne({$or:[{username:myArgs[1], email:myArgs[1]}]}, function (err, member) {
+        if(member){
+            console.log("User "+myArgs[1]+" already exists");
+            db.close();
         }
         else{
-            member = member.ops;
-            var a = {};
-            a.api_key = md5Hash(member[0]._id + (new Date).getTime());
-            member[0].api_key =  a.api_key;
-            db.collection("members").update({_id:member[0]._id}, {$set:a}, function() {
-                console.log(member[0]);
-                db.close();
+            var doc = {"full_name":myArgs[1], "username":myArgs[1], "password":sha1Hash(myArgs[2]), "email":myArgs[1], "global_admin":true};
+            db.collection('members').insert(doc, {safe:true}, function (err, member) {
+                if(err){
+                    console.log(err);
+                    db.close();
+                }
+                else{
+                    member = member.ops;
+                    var a = {};
+                    a.api_key = md5Hash(member[0]._id + (new Date).getTime());
+                    member[0].api_key =  a.api_key;
+                    db.collection("members").update({_id:member[0]._id}, {$set:a}, function() {
+                        console.log(member[0]);
+                        db.close();
+                    });
+                }
             });
         }
     });
