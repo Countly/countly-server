4904ebe647a32fee8f4f7d6027de15dcf2034ee6
Melih Korkmaz <mk@count.ly>
2018-01-05 16:51:00 +0300

    Some issues have been fixed.

--- plugins/times-of-day/api/api.js
+++ plugins/times-of-day/api/api.js
@@ -11,9 +11,8 @@ var plugin = {},
 			return false;
 		}
 
-
 		var appId = params.app_id;
-		
+
 		var events = [];
 
 		if (!appId) return false;
@@ -37,15 +36,20 @@ var plugin = {},
 		var options = {};
 
 		if (hasSession && params.qstring.hour !== undefined && params.qstring.dow !== undefined) {
+			var sessionDate = common.getDate(params.qstring.timestamp);
+			var month = sessionDate.getFullYear() + ":" + (sessionDate.getMonth() + 1);
+
 			criteria = {
-				"_id": "tod_" + appId + ":[CLY]_session"
+				"_id": "[CLY]_session" + "_" + month,
 			};
 
 			var incData = {};
-			incData[params.qstring.dow + "." + params.qstring.hour + ".count"] = 1;
+			incData['d.' + params.qstring.dow + "." + params.qstring.hour + ".count"] = 1;
 			var setData = {};
-			setData["_id"] = "tod_" + appId + ":[CLY]_session";
-			setData['app'] = appId;
+			setData["_id"] = "[CLY]_session" + "_" + month;
+			setData['m'] = month;
+			setData['s'] = "[CLY]_session";
+			setData['app_id'] = appId;
 
 			update = {
 				$set: setData,
@@ -66,22 +70,30 @@ var plugin = {},
 		}
 
 		if (hasEvents) {
+
 			for (var i = 0; i < events.length; i++) {
+				if (events[i].key === undefined) continue;
+
+				var eventDate = events[i].timestamp ? common.getDate(events[i].timestamp) : common.getDate(params.qstring.timestamp);
+				var month = eventDate.getFullYear() + ":" + (eventDate.getMonth() + 1);
 				criteria = {
-					"_id": "tod_" + appId + ":" + events[i].key
+					"_id": events[i].key + "_" + month
 				};
 
-				var dow = events[i].dow || params.qstring.dow;
-				var hour = events[i].hour || params.qstring.hour;
-				
-				if(dow === undefined || hour === undefined)
+				var dow = events[i].dow === undefined ? params.qstring.dow : events[i].dow;
+				var hour = events[i].hour === undefined ? params.qstring.hour : events[i].hour;
+
+
+				if (dow === undefined || hour === undefined)
 					continue;
-				
+
 				var incData = {};
-				incData[dow + "." + hour + ".count"] = 1;
+				incData['d.' + dow + "." + hour + ".count"] = 1;
 				var setData = {};
-				setData["_id"] = "tod_" + appId + ":" + events[i].key;
-				setData['app'] = appId;
+				setData["_id"] = events[i].key + "_" + month;
+				setData['m'] = month;
+				setData['s'] = events[i].key;
+				setData['app_id'] = appId;
 
 				update = {
 					$set: setData,
@@ -97,25 +109,48 @@ var plugin = {},
 					"update": update,
 					"options": options
 				};
-
 				queryArray.push(query);
 			}
 		}
-        
-        if(queryArray.length){
-            common.db.onOpened(function(){
-                var bulk = common.db._native.collection("timesofday").initializeUnorderedBulkOp();
-                for(var i = 0; i < queryArray.length; i++){
-                    bulk.find(queryArray[i].criteria).upsert().updateOne(queryArray[i].update);
-                }
-                bulk.execute(function(err, updateResult) {
-                    if(err){
-                        //there was an error
-                    }
-                    //all done
-                });
-            });
-        }
+
+		var collectionName = "timesofday" + appId;
+
+		
+		if (queryArray.length) {
+			common.db.collection('events').findOne({ _id: common.db.ObjectID(appId) }, { list: 1 }, function (err, eventData) {
+
+				if (err) {
+					console.log("err", err);
+					return true;
+				}
+
+				eventData = eventData == undefined ? { list : []} : eventData;
+				
+				var limit = plugins.getConfig("api").event_limit;
+				var overLimit = eventData.list.count > limit;
+
+				common.db.onOpened(function () {
+					var bulk = common.db._native.collection(collectionName).initializeUnorderedBulkOp();
+
+					for (var i = 0; i < queryArray.length; i++) {
+
+						var s = queryArray[i].update.$set.s;
+						if (s === "[CLY]_session" || !overLimit || (overLimit && eventData.list.indexOf(s) >= 0))
+							bulk.find(queryArray[i].criteria).upsert().updateOne(queryArray[i].update);
+
+					}
+
+					if (bulk.length > 0) {
+						bulk.execute(function (err, updateResult) {
+							if (err) {
+								//there was an error
+							}
+							//all done
+						});
+					}
+				});
+			})
+		}
 
 		return true;
 	});
@@ -128,27 +163,34 @@ var plugin = {},
 			var todType = params.qstring.tod_type;
 
 			var criteria = {
-				"_id": "tod_" + appId + ":" + todType
+				"s": todType
 			}
 
-			common.db.collection('timesofday').find(criteria).toArray(function (err, result) {
+			if (params.qstring.date_range)
+				criteria.m = { $in: params.qstring.date_range.split(',') }
+
+			var collectionName = "timesofday" + appId;
+			common.db.collection(collectionName).find(criteria).toArray(function (err, results) {
 				if (err) {
 					console.log("Error while fetching times of day data: ", err.message);
 					common.returnMessage(params, 400, "Something went wrong");
 					return false;
 				}
 
-				result = result[0] || {};
+				var timesOfDay = [0, 1, 2, 3, 4, 5, 6].map(function (x) {
+					return Array(24).fill(0)
+				});
 
-				var timesOfDay = [];
-				for (var i = 0; i < 7; i++) {
-					timesOfDay[i] = [];
-					for (var j = 0; j < 24; j++) {
-						timesOfDay[i][j] = result[i] ?
-							(result[i][j] ? result[i][j]["count"] : 0)
-							: 0;
+				results.forEach(result => {
+					for (var i = 0; i < 7; i++) {
+						for (var j = 0; j < 24; j++) {
+							timesOfDay[i][j] += result["d"][i] ?
+								(result["d"][i][j] ? result["d"][i][j]["count"] : 0)
+								: 0;
+						}
 					}
-				}
+				});
+
 				common.returnOutput(params, timesOfDay);
 				return true;
 			})
@@ -159,24 +201,27 @@ var plugin = {},
 
 	plugins.register("/i/apps/clear_all", function (ob) {
 		var appId = ob.appId;
-		common.db.collection('timesofday').remove({ app: common.db.ObjectID(appId) }, function (res) {
+		var collectionName = "timesofday" + appId;
+		common.db.collection(collectionName).remove({}, function (res) {
 			console.log("appId", res);
 		})
 	});
 
-	plugins.register("/i/apps/reset", function(ob){
-        var appId = ob.appId;
-		common.db.collection('timesofday').remove({ app: common.db.ObjectID(appId) }, function (res) {
+	plugins.register("/i/apps/reset", function (ob) {
+		var appId = ob.appId;
+		var collectionName = "timesofday" + appId;
+		common.db.collection(collectionName).remove({}, function (res) {
 			console.log("appId", res);
 		})
 	});
-	
-	plugins.register("/i/apps/delete", function(ob){
-        var appId = ob.appId;
-		common.db.collection('timesofday').remove({ app: common.db.ObjectID(appId) }, function (res) {
+
+	plugins.register("/i/apps/delete", function (ob) {
+		var appId = ob.appId;
+		var collectionName = "timesofday" + appId;
+		common.db.collection(collectionName).remove({}, function (res) {
 			console.log("appId", res);
 		})
-    });
+	});
 }(plugin));
 
 module.exports = plugin;
---
+++ plugins/times-of-day/tests.js
@@ -0,0 +1,158 @@
+var request = require('supertest');
+var should = require('should');
+var testUtils = require("../../test/testUtils");
+request = request.agent(testUtils.url);
+
+var APP_KEY = "";
+var API_KEY_ADMIN = "";
+var APP_ID = "";
+var DEVICE_ID = "1234567890";
+
+
+describe('Testing Times Of Day', function () {
+
+    it('Should reset app data', function(done){
+        API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
+        APP_ID = testUtils.get("APP_ID");
+
+        var params = {app_id:APP_ID, period:"reset"};
+        request
+        .get('/i/apps/reset?api_key='+API_KEY_ADMIN+"&args="+JSON.stringify(params))
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            var ob = JSON.parse(res.text);
+            ob.should.have.property('result', 'Success');
+            setTimeout(done, 5000)
+        });
+    })
+
+    it('Should get empty data', function (done) {
+        API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
+        APP_ID = testUtils.get("APP_ID");
+        APP_KEY = testUtils.get("APP_KEY");
+        request
+            .get('/o?api_key=' + API_KEY_ADMIN + "&app_key=" + API_KEY_ADMIN + "&app_id=" + APP_ID + "&method=times-of-day&tod_type=[CLY]_session")
+            .expect(200)
+            .end(function (err, res) {
+                
+                if (err) return done(err);
+
+                var ob = JSON.parse(res.text);
+                ob.length.should.eql(7);
+
+                ob.forEach(element => {
+                    element.length.should.eql(24);
+
+                    var allItemZero = element.every(function(i){ return i === 0});
+                    allItemZero.should.eql(true);
+                });
+
+                setTimeout(done, 1000)
+            });
+    });
+
+    it('Should add data', function(done){
+        APP_ID = testUtils.get("APP_ID");
+        APP_KEY = testUtils.get("APP_KEY");
+
+        var url = '/i?app_key=' + APP_KEY 
+                + '&app_id=' + APP_ID 
+                + '&device_id=' + DEVICE_ID 
+                + '&begin_session=true'
+                + '&dow=0'
+                + '&hour=0'
+                + '&events=[{"key" : "Login", "count" : 1, "timestamp" : 1513389337}, {"key" : "Login", "count" : 1, "timestamp" : 1513389337, "hour" : 1, "dow" : 1}]' 
+
+        request
+            .get(url)
+            .expect(200)
+            .end(function(err, res){
+                if(err)
+                    done(err);
+                
+                var ob = JSON.parse(res.text);
+                ob.result.should.eql("Success");
+                done();
+            })
+    });
+
+    it('Should validate session data', function(done){
+        API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
+        APP_ID = testUtils.get("APP_ID");
+        APP_KEY = testUtils.get("APP_KEY");
+        request
+            .get('/o?api_key=' + API_KEY_ADMIN + "&app_key=" + API_KEY_ADMIN + "&app_id=" + APP_ID + "&method=times-of-day&tod_type=[CLY]_session")
+            .expect(200)
+            .end(function (err, res) {
+                
+                if (err) return done(err);
+
+                var ob = JSON.parse(res.text);
+                ob.length.should.eql(7);
+
+                ob.forEach((element, index) => {
+                    element.length.should.eql(24);
+
+                    if(index === 0){
+                        element[0].should.eql(1);
+                    }else{
+                        var allItemZero = element.every(function(i){ return i === 0});
+                        allItemZero.should.eql(true);
+                    }
+                });
+
+                setTimeout(done, 1000)
+            });
+    })
+
+    it('Should validate login data', function(done){
+        API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
+        APP_ID = testUtils.get("APP_ID");
+        APP_KEY = testUtils.get("APP_KEY");
+        request
+            .get('/o?api_key=' + API_KEY_ADMIN + "&app_key=" + API_KEY_ADMIN + "&app_id=" + APP_ID + "&method=times-of-day&tod_type=Login")
+            .expect(200)
+            .end(function (err, res) {
+                
+                if (err) return done(err);
+
+                var ob = JSON.parse(res.text);
+                ob.length.should.eql(7);
+
+                ob.forEach((element, index) => {
+                    element.length.should.eql(24);
+
+                    if(index === 0){
+                        element[0].should.eql(1); //First event has no dow and hour so it should get it from session level
+                    }else if(index === 1){
+                        element[1].should.eql(1); //Second event has own dow and hour parameter.
+                    }
+                    else{
+                        var allItemZero = element.every(function(i){ return i === 0});
+                        allItemZero.should.eql(true);
+                    }
+                });
+
+                setTimeout(done, 1000)
+            });
+    })
+
+    after(function(){
+        
+        //Clear app data;
+        API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
+        APP_ID = testUtils.get("APP_ID");
+
+        var params = {app_id:APP_ID, period:"reset"};
+        request
+        .get('/i/apps/reset?api_key='+API_KEY_ADMIN+"&args="+JSON.stringify(params))
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            var ob = JSON.parse(res.text);
+            ob.should.have.property('result', 'Success');
+            setTimeout(done, 5000)
+        });
+    })
+})
