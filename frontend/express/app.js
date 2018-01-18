b4f2bd742e1c61260e39da83cb30426dd97db07b
Arturs Sosins <ar2rsawseen@gmail.com>
2018-01-18 12:07:56 +0200

    [server-stats] move server stats request to plugin

--- frontend/express/app.js
+++ frontend/express/app.js
@@ -852,64 +852,33 @@ app.post(countlyConfig.path+'/login', function (req, res, next) {
                     }
                     if (!countlyConfig.web.track || countlyConfig.web.track == "GA" && member['global_admin'] || countlyConfig.web.track == "noneGA" && !member['global_admin']) {
                         countlyStats.getUser(countlyDb, member, function(statsObj){
-                            countlyDb.collection("server_stats_data_points").aggregate([
-                                {
-                                    $group: {
-                                        _id: "$m",
-                                        e: { $sum: "$e"},
-                                        s: { $sum: "$s"}
-                                    }
-                                }
-                            ], { allowDiskUse:true }, function(error, allData) {
-                                var custom = {
-                                    apps: (member.user_of) ? member.user_of.length : 0,
-                                    platforms:{"$addToSet":statsObj["total-platforms"]},
-                                    events:statsObj["total-events"],
-                                    pushes:statsObj["total-msg-sent"],
-                                    crashes:statsObj["total-crash-groups"],
-                                    users:statsObj["total-users"]
-                                };
-                                if(!error && allData && allData.length && !versionInfo.trial){
-                                    var data = {};
-                                    data.all = 0;
-                                    data.month3 = [];
-                                    var utcMoment = moment.utc();
-                                    var months = {};
-                                    for(var i = 0; i < 3; i++){
-                                        months[utcMoment.format("YYYY:M")] = true;
-                                        utcMoment.subtract(1, 'months');
-                                    }
-                                    for(var i = 0; i < allData.length; i++){
-                                        data.all += allData[i].e + allData[i].s;
-                                        if(months[allData[i]._id]){
-                                            data.month3.push(allData[i]._id + " - " + (allData[i].e + allData[i].s));
+                            var custom = {
+                                apps: (member.user_of) ? member.user_of.length : 0,
+                                platforms:{"$addToSet":statsObj["total-platforms"]},
+                                events:statsObj["total-events"],
+                                pushes:statsObj["total-msg-sent"],
+                                crashes:statsObj["total-crash-groups"],
+                                users:statsObj["total-users"]
+                            };
+                            var date = new Date();
+                            request({
+                                uri:"https://stats.count.ly/i",
+                                method:"GET",
+                                timeout:4E3,
+                                qs:{
+                                    device_id:member.email,
+                                    app_key:"386012020c7bf7fcb2f1edf215f1801d6146913f",
+                                    timestamp: Math.round(date.getTime()/1000),
+                                    hour: date.getHours(),
+                                    dow: date.getDay(),
+                                    user_details:JSON.stringify(
+                                        {
+                                            custom:custom
                                         }
-                                    }
-                                    data.avg = Math.round((data.all/allData.length)*100)/100;
-                                    custom.dataPointsAll = data.all;
-                                    custom.dataPointsMonthlyAvg = data.avg;
-                                    custom.dataPointsLast3Months = data.month3;
+                                    )
                                 }
-                                var date = new Date();
-                                request({
-                                    uri:"https://stats.count.ly/i",
-                                    method:"GET",
-                                    timeout:4E3,
-                                    qs:{
-                                        device_id:member.email,
-                                        app_key:"386012020c7bf7fcb2f1edf215f1801d6146913f",
-                                        timestamp: Math.round(date.getTime()/1000),
-                                        hour: date.getHours(),
-                                        dow: date.getDay(),
-                                        user_details:JSON.stringify(
-                                            {
-                                                custom:custom
-                                            }
-                                        )
-                                    }
-                                }, function(a, c, b) {});
-                            }); 
-                        });
+                            }, function(a, c, b) {});
+                        }); 
                     }
                     req.session.regenerate(function(err) {
                         // will have a new session here
--- plugins/server-stats/frontend/app.js
+++ plugins/server-stats/frontend/app.js
@@ -1,8 +1,72 @@
 var plugin = {};
+var countlyConfig = require('../../../frontend/express/config');
+var versionInfo = require('../../../frontend/express/version.info');
+var request = require('request');
 
 (function (plugin) {
     plugin.init = function(app, countlyDb){
-
+        plugin.loginSuccessful = function(ob){
+            var member = ob.data;
+            if (!countlyConfig.web.track || countlyConfig.web.track == "GA" && member['global_admin'] || countlyConfig.web.track == "noneGA" && !member['global_admin']) {
+                var match = {};
+                if(versionInfo.trial){
+                    match.a = {$in:member.user_of || []};
+                }
+                countlyDb.collection("server_stats_data_points").aggregate([
+                    {
+                        $match: match
+                    },
+                    {
+                        $group: {
+                            _id: "$m",
+                            e: { $sum: "$e"},
+                            s: { $sum: "$s"}
+                        }
+                    }
+                ], { allowDiskUse:true }, function(error, allData) {
+                    var custom = {};
+                    if(!error && allData && allData.length){
+                        var data = {};
+                        data.all = 0;
+                        data.month3 = [];
+                        var utcMoment = moment.utc();
+                        var months = {};
+                        for(var i = 0; i < 3; i++){
+                            months[utcMoment.format("YYYY:M")] = true;
+                            utcMoment.subtract(1, 'months');
+                        }
+                        for(var i = 0; i < allData.length; i++){
+                            data.all += allData[i].e + allData[i].s;
+                            if(months[allData[i]._id]){
+                                data.month3.push(allData[i]._id + " - " + (allData[i].e + allData[i].s));
+                            }
+                        }
+                        data.avg = Math.round((data.all/allData.length)*100)/100;
+                        custom.dataPointsAll = data.all;
+                        custom.dataPointsMonthlyAvg = data.avg;
+                        custom.dataPointsLast3Months = data.month3;
+                        var date = new Date();
+                        request({
+                            uri:"https://stats.count.ly/i",
+                            method:"GET",
+                            timeout:4E3,
+                            qs:{
+                                device_id:member.email,
+                                app_key:"386012020c7bf7fcb2f1edf215f1801d6146913f",
+                                timestamp: Math.round(date.getTime()/1000),
+                                hour: date.getHours(),
+                                dow: date.getDay(),
+                                user_details:JSON.stringify(
+                                    {
+                                        custom:custom
+                                    }
+                                )
+                            }
+                        }, function(a, c, b) {});
+                    }
+                }); 
+            }
+        };
     };
 }(plugin));
 
