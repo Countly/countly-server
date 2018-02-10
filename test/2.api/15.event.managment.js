ebee013fcee2f57640dea6adeb7b369fccaadae3
Cookiezaurs <sosina.anna@gmail.com>
2018-02-10 13:09:35 +0200

    event tests

--- test/2.api/15.event.managment.js
+++ test/2.api/15.event.managment.js
@@ -10,31 +10,11 @@ var APP_KEY = "";
 var DEVICE_ID = "1234567890";
 
 describe('Testing event settings', function(){
-    describe('setting test data', function(){
-    
-        API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
-        APP_ID = testUtils.get("APP_ID");
-        APP_KEY = testUtils.get("APP_KEY");  
-        
-		it('should create app', function(done){
-			var appName = "My test App";
-			var params = {name:appName};
-			request
-			.get('/i/apps/create?api_key='+API_KEY_ADMIN+"&args="+JSON.stringify(params))
-			.expect(200)
-			.end(function(err, res){
-				if (err) return done(err);
-				var ob = JSON.parse(res.text);
-				ob.should.have.property('name', appName);
-				APP_ID = ob._id;
-				APP_KEY = ob.key;
-				testUtils.set("APP_ID", APP_ID);
-				testUtils.set("APP_KEY", APP_KEY);
-				done()
-			});
-		});
-        
+    describe('setting test data', function(){      
         it('create test events', function(done){
+            API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
+            APP_ID = testUtils.get("APP_ID");
+            APP_KEY = testUtils.get("APP_KEY");  
             var params = [
                 {"key": "test1","count": 1},
                 {"key": "test2","count": 1,"sum":5,"dur":10}];
@@ -182,10 +162,10 @@ describe('Testing event settings', function(){
     });
     
     describe('cleanup',function(){
-        it('should delete app', function(done){
+        it('should reset app', function(done){
                 var params = {app_id:APP_ID};
                 request
-                .get('/i/apps/delete?api_key='+API_KEY_ADMIN+"&args="+JSON.stringify(params))
+                .get('/i/apps/reset?api_key='+API_KEY_ADMIN+"&args="+JSON.stringify(params))
                 .expect(200)
                 .end(function(err, res){
                     if (err) return done(err);
