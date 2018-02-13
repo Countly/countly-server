1bc154ccf1845e84802554c3b03f3e234edeeb38
Arturs Sosins <ar2rsawseen@gmail.com>
2018-02-13 17:40:31 +0200

    [request] add task manager object

--- api/utils/requestProcessor.js
+++ api/utils/requestProcessor.js
@@ -8,6 +8,7 @@ const url = require('url');
 const common = require('./common.js');
 const {validateUser, validateUserForRead, validateUserForWrite, validateGlobalAdmin} = require('./rights.js');
 const authorize = require('./authorizer.js');
+const taskmanager = require('./taskmanager.js');
 const plugins = require('../../plugins/pluginManager.js');
 const versionInfo = require('../../frontend/express/version.info');
 const log = require('./log.js')('core:api');
