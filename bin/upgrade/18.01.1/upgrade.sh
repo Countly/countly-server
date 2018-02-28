3425f63c0999f4b4fb40e2b608f1ad096b02a3ce
Arturs Sosins <ar2rsawseen@gmail.com>
2018-02-28 13:26:19 +0200

    [upgrade] add more plugins to update

--- bin/upgrade/18.01.1/upgrade.sh
+++ bin/upgrade/18.01.1/upgrade.sh
@@ -7,12 +7,14 @@ bash $DIR/scripts/detect.init.sh
 
 #upgrade existing plugins
 countly plugin upgrade crashes
-
-#add indexes
-nodejs $DIR/scripts/add_indexes.js
+countly plugin upgrade views
+countly plugin upgrade users
 
 #remove stuck push collections
 nodejs $DIR/upgrade/18.01.1/scripts/push_clear.js
+
+#add indexes
+nodejs $DIR/scripts/add_indexes.js
 
 #install dependencies, process files and restart countly
 countly upgrade
