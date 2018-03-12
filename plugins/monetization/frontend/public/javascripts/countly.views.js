3e95b90200c4e44d4e8b32e88b343fca284893fd
can-keklikm <cncncn1@windowslive.com>
2018-03-12 12:34:52 +0000

    [bugfix] datatable bug is fixed

--- plugins/monetization/frontend/public/javascripts/countly.views.js
+++ plugins/monetization/frontend/public/javascripts/countly.views.js
@@ -1,105 +1,124 @@
 window.MonetizationMetricsView = countlyView.extend({
-  templateData: {
-  },
+  templateData: {},
   beforeRender: function() {
     var self = this;
     return $.when(
       $.get(countlyGlobal["path"] + '/monetization/templates/metrics.html', function(src) {
         self.template = Handlebars.compile(src);
       }),
-      $.get(countlyGlobal["path"] + '/monetization/templates/bignumbers.html', function (src) {
+      $.get(countlyGlobal["path"] + '/monetization/templates/bignumbers.html', function(src) {
         self.bigNumbersTemplate = Handlebars.compile(src);
       }),
       countlyMonetization.initialize()
     );
   },
-  initializeEventSelector:function () {
+  initializeEventSelector: function() {
     var self = this;
-    $("#big-numbers-container").on("click",".check",function () {
-        var classes = $(this).attr('class');
-        var id = $(this).attr('data-id');
-        
-        if (classes.indexOf('selected') >= 0) {
-            if(countlyMonetization.tryDisableEvent(id)){
-              $(this).removeClass("selected");
-            }
-        } else {
-            $(this).addClass("selected");
-            countlyMonetization.enableEvent(id)
-        }
+    $("#big-numbers-container").on("click", ".check", function() {
+      var classes = $(this).attr('class');
+      var id = $(this).attr('data-id');
 
-        self.updateChart();
+      if (classes.indexOf('selected') >= 0) {
+        if (countlyMonetization.tryDisableEvent(id)) {
+          $(this).removeClass("selected");
+        }
+      } else {
+        $(this).addClass("selected");
+        countlyMonetization.enableEvent(id)
+      }
+
+      self.updateChart();
     });
   },
   renderCommon: function(isRefresh) {
-      var self = this;
-      if (!isRefresh) {
-          $(this.el).html(self.template(this.templateData));
-          self.initializeEventSelector()
-          self.updateView(false)
-      }
+    var self = this;
+    if (!isRefresh) {
+      $(this.el).html(self.template(this.templateData));
+      self.initializeEventSelector()
+      self.updateView(false)
+    }
   },
-  updateChart:function(fetchData){
+  updateChart: function(fetchData) {
     var self = this;
     var data = self.getData();
     self.renderChart(data.chartDP);
   },
-  updateView:function(isRefresh){
-        var self = this;
-        var data = self.getData();
-        self.renderChart(data.chartDP);
-        self.renderNumbers(data.bigNumbersData);
-        self.renderTable(data.tableData, isRefresh);
-    },
-    refresh: function () {
-        var self = this;
-        $.when(
-            countlyMonetization.requestMetricData()
-        ).done(function () {
-            self.updateView(true)
-        });
-    },
-    renderChart: function (data) {
-        var enabledLines = countlyMonetization.getEnabledEvents();
-        var dataToBeRendered=[];
-        enabledLines.forEach(function(key){
-          dataToBeRendered.push(data[key]);
-        })
-        return countlyCommon.drawTimeGraph(dataToBeRendered, "#chartContainer");
-    },
-    renderNumbers:function(data){
-        var self = this;
-        var compiled =self.bigNumbersTemplate({
-          "bignumbers":data
-        });
+  updateView: function(isRefresh) {
+    var self = this;
+    var data = self.getData();
+    self.renderChart(data.chartDP);
+    self.renderNumbers(data.bigNumbersData);
+    self.renderTable(data.tableData, isRefresh);
+  },
+  refresh: function() {
+    var self = this;
+    $.when(
+      countlyMonetization.requestMetricData()
+    ).done(function() {
+      self.updateView(true)
+    });
+  },
+  renderChart: function(data) {
+    var enabledLines = countlyMonetization.getEnabledEvents();
+    var dataToBeRendered = [];
+    enabledLines.forEach(function(key) {
+      dataToBeRendered.push(data[key]);
+    })
+    return countlyCommon.drawTimeGraph(dataToBeRendered, "#chartContainer");
+  },
+  renderNumbers: function(data) {
+    var self = this;
+    var compiled = self.bigNumbersTemplate({
+      "bignumbers": data
+    });
 
-        $("#big-numbers-container").html(compiled);
-    },
-    renderTable: function (data, isRefresh) {
-        var self = this;
+    $("#big-numbers-container").html(compiled);
+  },
+  renderTable: function(data, isRefresh) {
+    var self = this;
 
-        var aoColumns=  [
-            { "mData": "date", "sType":"customDate", "sTitle": jQuery.i18n.map["common.date"] }
-        ];
+    var tableData = data
+    if (isRefresh) {
+      CountlyHelpers.refreshTable($('#dataTable').dataTable(), tableData);
+    } else {
+      self.createTable(data);
+    }
+  },
+  createTable: function(data, destroy) {
+    var aoColumns = [{
+      "mData": "date",
+      "sType": "customDate",
+      "sTitle": jQuery.i18n.map["common.date"]
+    }];
 
-        var eventColumns = countlyMonetization.getColumns();
+    var eventColumns = countlyMonetization.getColumns();
 
-        for (var ekey in eventColumns){
-          aoColumns.push(
-            { "mData": ekey, sType:"formatted-num", "mRender":function(d) { return countlyCommon.formatNumber(d); }, "sTitle": jQuery.i18n.map[eventColumns[ekey].localize] },
-          )
-        }
-        this.dtable = $('#dataTable').dataTable($.extend({}, $.fn.dataTable.defaults, {
-            "aaData": data,
-            "aoColumns": aoColumns
-        }));
+    for (var ekey in eventColumns) {
+      aoColumns.push({
+        "mData": ekey,
+        sType: "formatted-num",
+        "mRender": function(d) {
+          return countlyCommon.formatNumber(d);
+        },
+        "sTitle": jQuery.i18n.map[eventColumns[ekey].localize]
+      }, )
+    }
 
-        $("#dataTable").stickyTableHeaders();
-    },
+    if (destroy && $.fn.dataTable.fnIsDataTable('#dataTable')) {
+      $("#dataTable").dataTable().fnDestroy();
+      $("#dataTable").html("");
+    }
 
-    getData: function () {
-      return countlyMonetization.getMetricData();
-    },
+    this.dtable = $('#dataTable').dataTable($.extend({}, $.fn.dataTable.defaults, {
+      "aaData": data,
+      "aoColumns": aoColumns
+    }));
+
+    $("#dataTable").stickyTableHeaders();
+  },
+  getData: function() {
+    return countlyMonetization.getMetricData();
+  },
 });
 
 window.MonetizationSignupView = countlyView.extend({
