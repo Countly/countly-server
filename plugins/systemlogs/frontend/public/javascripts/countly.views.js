525f554bff8ab289eefe75b4b8ece67deb11d9b0
Arturs Sosins <ar2rsawseen@gmail.com>
2018-03-16 15:09:41 +0200

    [systemlogs] add consent action table and support new app_users actions

--- plugins/systemlogs/frontend/public/javascripts/countly.views.js
+++ plugins/systemlogs/frontend/public/javascripts/countly.views.js
@@ -73,7 +73,6 @@ window.SystemLogsView = countlyView.extend({
                     if(self._query){
                         aoData.push({ "name": "query", "value": JSON.stringify(self._query) });
                     }
-                    aoData.push({ "name": "period", "value": countlyCommon.getPeriodForAjax() });
                 },
                 "fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
 					$(nRow).attr("id", aData._id);
@@ -307,6 +306,111 @@ if(countlyGlobal["member"].global_admin){
         this.systemLogsView._query = query;
         this.systemLogsView._back = true;
         this.renderWhenReady(this.systemLogsView);
+    });
+    
+    app.addPageScript("/manage/consents#", function(){
+        if(app.activeView && app.activeView.tabs){
+            app.activeView.tabs.tabs('add','#consent-actionlogs', jQuery.i18n.map["consent.export-history"]);
+            app.activeView.tabs.tabs("refresh");
+            $.when(countlySystemLogs.initialize()).then(function () {
+                var meta = countlySystemLogs.getMetaData();
+                var html = "<div class='widget-header'><div class='left'><div style='overflow: auto'><div class='title small'>"+jQuery.i18n.map["consent.export-history"]+"</div></div>"+
+                    "<div style='width:400px; display: block;'>"+
+                    "<div class='cly-select float filter_actions-segmentation'>"+
+                        "<div class='select-inner'>"+
+                            "<div class='text-container'>"+
+                                "<div class='text'>"+jQuery.i18n.map["common.all"]+"</div>"+
+                            "</div>"+
+                            "<div class='right combo'></div>"+
+                        "</div>"+
+                        "<div class='select-items square' style='width:300px;'>"+
+                            "<div>"+
+                                "<div data-value='all' class='segmentation-option item'>"+jQuery.i18n.map["common.all"]+"</div>"+
+                                "<div data-value='export_app_user' class='segmentation-option item'>"+jQuery.i18n.map["systemlogs.action.export_app_user"]+"</div>"+
+                                "<div data-value='app_user_deleted' class='segmentation-option item'>"+jQuery.i18n.map["systemlogs.action.app_user_deleted"]+"</div>"+
+                                "<div data-value='export_app_user_deleted' class='segmentation-option item'>"+jQuery.i18n.map["systemlogs.action.export_app_user_deleted"]+"</div>"+
+                            "</div>"+
+                        "</div>"+
+                    "</div>"+
+                    "</div>"+
+                "</div></div><table id='d-table-actionlogs' class='d-table sortable help-zone-vb' cellpadding='0' cellspacing='0'></table>";
+                $("#consent-actionlogs").append(html);
+                $(".filter_actions-segmentation .segmentation-option").on("click", function () {
+                    var val = $(this).data("value");
+                    if(val && val !== "all"){
+                        app.activeView.action_query = {a:val};
+                    }
+                    else{
+                        app.activeView.action_query = {a:{$in:["export_app_user","app_user_deleted","export_app_user_deleted"]}};
+                    }
+                    app.activeView.dtableactionlogs.fnDraw(false);
+                });
+                app.activeView.dtableactionlogs = $('#d-table-actionlogs').dataTable($.extend({}, $.fn.dataTable.defaults, {
+                    "iDisplayLength": 30,
+                    "aaSorting": [[ 2, "desc" ]],
+                    "bServerSide": true,
+                    "bFilter": false,
+                    "sAjaxSource": countlyCommon.API_PARTS.data.r + "?api_key="+countlyGlobal.member.api_key+"&app_id="+countlyCommon.ACTIVE_APP_ID+"&method=systemlogs",
+                    "fnServerData": function ( sSource, aoData, fnCallback ) {
+                        self.request = $.ajax({
+                            "dataType": 'json',
+                            "type": "POST",
+                            "url": sSource,
+                            "data": aoData,
+                            "success": function(data){
+                                fnCallback(data);
+                            }
+                        });
+                    },
+                    "fnServerParams": function ( aoData ) {
+                        aoData.push({ "name": "query", "value": JSON.stringify(app.activeView.action_query || {a:{$in:["export_app_user","app_user_deleted","export_app_user_deleted"]}}) });
+                    },
+                    "aoColumns": [
+                        { "mData": function(row, type){
+                            if(type == "display"){
+                                return moment(new Date(row.ts*1000)).format("ddd, D MMM YYYY HH:mm:ss");
+                            }else return row.ts;}, "sType":"string", "sExport":"systemlogs", "sTitle": jQuery.i18n.map["systemlogs.timestamp"]},
+                        { "mData": function(row, type){if(row.user_id && type === "display"){return "<a href='#/manage/users/"+row.user_id+"' class='table-link-user green' title='"+row.u+"'>"+row.u+"</a>";} else return "<span title='"+row.u+"'>"+row.u+"</span>";}, "sType":"string", "sTitle": jQuery.i18n.map["systemlogs.user"], bSortable: false, "sClass":"trim"},
+                        { "mData": function(row, type){return row.ip;}, "sType":"string", "sTitle": jQuery.i18n.map["systemlogs.ip-address"], bSortable: false},
+                        { "mData": function(row, type){
+                            var ret = "<p>"+((jQuery.i18n.map["systemlogs.action."+row.a]) ? jQuery.i18n.map["systemlogs.action."+row.a] : row.a)+"</p>";
+                            if(typeof row.i == "object"){
+                                if(typeof row.i.app_id != "undefined" && countlyGlobal["apps"][row.i.app_id]){
+                                    ret += "<p title='"+row.i.app_id+"'>"+jQuery.i18n.map["systemlogs.for-app"]+": "+countlyGlobal["apps"][row.i.app_id].name+"</p>";
+                                }
+                                if(typeof row.i.user_id != "undefined"){
+                                    var id = row.i.user_id;
+                                    for(var i = 0; i < meta.users.length; i++){
+                                        if(meta.users[i]._id == row.i.user_id){
+                                            id = meta.users[i].full_name;
+                                            break;
+                                        }
+                                    }
+                                    ret += "<p title='"+id+"'>"+jQuery.i18n.map["systemlogs.for-user"]+": "+id+"</p>";
+                                }
+                                if(typeof row.i.campaign_id != "undefined" && typeof countlyAttribution != "undefined"){
+                                    ret += "<p title='"+row.i.campaign_id+"'>"+jQuery.i18n.map["systemlogs.for-campaign"]+": "+countlyAttribution.getCampaignName(row.i.campaign_id)+"</p>";
+                                }
+                                if(typeof row.i.crash_id != "undefined" && typeof countlyCrashes != "undefined"){
+                                    ret += "<p title='"+row.i.crash_id+"'>"+jQuery.i18n.map["systemlogs.for-crash"]+": "+countlyCrashes.getCrashName(row.i.crash_id)+"</p>";
+                                }
+                                if(typeof row.i.appuser_id != "undefined"){
+                                    ret += "<p title='"+row.i.appuser_id+"'>"+jQuery.i18n.map["systemlogs.for-appuser"]+": "+row.i.appuser_id+"</p>";
+                                }
+                                if(typeof row.i.before != "undefined" && typeof row.i.after != "undefined"){
+                                    if(!jQuery.isEmptyObject(row.i.before)){
+                                        if(typeof row.i._id != "undefined"){
+                                            ret += "<p title='"+row.i._id+"'>"+jQuery.i18n.map["systemlogs.for-id"]+": "+row.i._id+"</p>";
+                                        }
+                                    }
+                                }
+                            }
+                            return ret;
+                        }, "sType":"string", "sTitle": jQuery.i18n.map["systemlogs.action"], bSortable: false}
+                    ]
+                }));
+            });
+        }
     });
 }
 
--- plugins/systemlogs/install.js
+++ plugins/systemlogs/install.js
@@ -13,10 +13,11 @@ console.log("Adding systemlogs indexes");
 var cnt = 0;
 function cb(){
 	cnt++;
-	if(cnt == 3)
+	if(cnt == 4)
 		done();
 }
 
 countlyDb.collection('systemlogs').ensureIndex({"ts":1},cb);
+countlyDb.collection('systemlogs').ensureIndex({"a":1},cb);
 countlyDb.collection('systemlogs').ensureIndex({"user_id":1},cb);
 countlyDb.collection('systemlogs').ensureIndex({"app_id":1},cb);
