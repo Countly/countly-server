b80362b57a35a6fbfb76796e16dec121bd568556
Arturs Sosins <ar2rsawseen@gmail.com>
2018-04-06 22:45:16 +0300

    [deploy] forking remote deployment

--- bin/scripts/deploy.sh
+++ bin/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-    ssh -oStrictHostKeyChecking=no countly@128.199.45.30 'bash /home/countly/deploy.sh'
+    ssh -oStrictHostKeyChecking=no countly@128.199.45.30 'bash /home/countly/deploy.sh > /home/countly/logs/countly-deploy-${TRAVIS_BUILD_NUMBER}-${TRAVIS_COMMIT}.log 2>&1 &'
 fi
