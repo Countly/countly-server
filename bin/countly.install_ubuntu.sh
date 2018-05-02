7a8afb5f1266db1664a28c9f88ac804a323076ce
Arturs Sosins <ar2rsawseen@gmail.com>
2018-05-02 12:11:15 +0300

    [scripts] fix permission issue for npm5+

--- bin/countly.install_ubuntu.sh
+++ bin/countly.install_ubuntu.sh
@@ -52,7 +52,7 @@ fi
 apt-get -y install sendmail
 
 #install grunt & npm modules
-( cd $DIR/.. ; npm install -g grunt-cli --unsafe-perm ; npm install )
+( cd $DIR/.. ; npm install -g grunt-cli --unsafe-perm ; sudo npm install )
 
 #install mongodb
 bash $DIR/scripts/mongodb.install.sh
