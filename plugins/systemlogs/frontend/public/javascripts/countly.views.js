d99fb4e67c8203fa31d54245edbad8ffc37829af
Waiter Zen <waiter.james1991@gmail.com>
2018-05-09 23:42:20 +0800

    [core] add util func to fix  table metadata bug in datatable 1.9

--- plugins/systemlogs/frontend/public/javascripts/countly.views.js
+++ plugins/systemlogs/frontend/public/javascripts/countly.views.js
@@ -60,7 +60,8 @@ window.SystemLogsView = countlyView.extend({
                 "aaSorting": [[ 0, "desc" ]],
                 "bServerSide": true,
                 "sAjaxSource": countlyCommon.API_PARTS.data.r + "?api_key="+countlyGlobal.member.api_key+"&app_id="+countlyCommon.ACTIVE_APP_ID+"&method=systemlogs",
-                "fnServerData": function ( sSource, aoData, fnCallback ) {
+                "fnServerData": function (sSource, aoData, fnCallback, oSettings) {
+                    $(oSettings.oInstance).trigger('preXhr', [oSettings, aoData]);
                     $.ajax({
                         "dataType": 'json',
                         "type": "POST",
