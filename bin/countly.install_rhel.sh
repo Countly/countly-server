e59b0d2db389d385125b237a2c8c95103426176f
prikshittekta <prikshittekta@gmail.com>
2018-06-21 21:21:45 +0530

    headless chrome init scripts

--- bin/countly.install_rhel.sh
+++ bin/countly.install_rhel.sh
@@ -12,6 +12,10 @@ bash $DIR/scripts/logo.sh;
 
 #install nginx
 yum -y install wget openssl-devel gcc-c++-4.8.5 make git sqlite unzip
+
+#Install dependancies required by the puppeteer
+yum -y pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc
+
 if grep -q -i "release 6" /etc/redhat-release ; then
 	echo "[nginx]
 name=nginx repo
