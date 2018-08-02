ada8bf0a71b253f05e5996127808902aac576036
Furkan BASARAN <frknbasaran@gmail.com>
2018-08-02 15:34:56 +0300

    [feedback] x-frame-options header removed.
    - x-frame-options header removed from iframe serve endpoint.

--- plugins/star-rating/frontend/app.js
+++ plugins/star-rating/frontend/app.js
@@ -2,7 +2,7 @@ var plugin = {};
 (function (plugin) {
 	plugin.init = function(app, countlyDb) {
 		app.get(countlyConfig.path+'/feedback', function (req, res, next) {
-            res.setHeader('X-Frame-Options','ALLOW-FROM *');
+            res.removeHeader('X-Frame-Options');
             res.render('../../../plugins/star-rating/frontend/public/templates/feedback-popup', {});
         });
 	};
