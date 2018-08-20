a05c8fd2ac3ff1e562a442e74d8f37d012f60d0d
Arturs Sosins <ar2rsawseen@gmail.com>
2018-08-20 22:09:10 +0300

    [star-rating] plugin upgrade scripts

---
+++ plugins/star-rating/install.js
@@ -0,0 +1,25 @@
+var async = require('async'),
+    pluginManager = require('../pluginManager.js'),
+	countlyDb = pluginManager.dbConnection();
+console.log("Installing ratings plugin");
+countlyDb.collection('apps').find({}).toArray(function (err, apps) {
+
+    if (!apps || err) {
+        return;
+    }
+	function upgrade(app, done){
+		var cnt = 0;
+		console.log("Adding ratings collections to " + app.name);
+		function cb(){
+			cnt++;
+			if(cnt == 2)
+				done();
+		}        
+		countlyDb.collection('feedback' + app._id).ensureIndex({"uid":1}, {background:true}, cb);
+		countlyDb.collection('feedback' + app._id).ensureIndex({"ts":1}, {background:true}, cb);
+	}
+	async.forEach(apps, upgrade, function(){
+		console.log("Ratings plugin installation finished");
+		countlyDb.close();
+	});
+});
