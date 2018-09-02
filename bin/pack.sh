ca4a9518cbf0002300df0163d5cec40896f51791
FoysalOsmany <foysalo@gmail.com>
2018-09-02 15:14:42 +0600

    Pack shell script for organizing files before packaging

---
+++ bin/pack.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -d "$DIR/../dist" ]; then
+    mkdir $DIR/../dist/
+fi
+
+if [ ! -d "$DIR/../api" ]; then
+    mkdir $DIR/../api/
+fi
+
+if [ ! -d "$DIR/../frontend" ]; then
+    mkdir $DIR/../frontend/
+fi
+
+if [ ! -d "$DIR/../plugins" ]; then
+    mkdir $DIR/../plugins/
+fi
+
+if [ ! -d "$DIR/../node_modules" ]; then
+    mkdir $DIR/../node_modules/
+fi
+
+rsync -avL --progress $DIR/../api/ $DIR/../dist/api/
+rsync -avL --progress $DIR/../frontend/ $DIR/../dist/frontend/
+rsync -avL --progress $DIR/../plugins/ $DIR/../dist/plugins/
+rsync -avL --progress $DIR/../node_modules/ $DIR/../dist/node_modules/
+
+rsync -avL --progress $DIR/../Gruntfile.js $DIR/../dist/
+rsync -avL --progress $DIR/../package.json $DIR/../dist/
+rsync -avL --progress $DIR/../frontend/express/config.sample.js $DIR/../dist/frontend/express/config.js
+rsync -avL --progress $DIR/../plugins/plugins.default.json $DIR/../dist/plugins/plugins.json
+
+rsync -avL --progress $DIR/package/install_plugins.js $DIR/../dist/install_plugins.js
+
+rsync -avL --progress $DIR/package/countly_marked_version.json $DIR/../dist/countly_marked_version.json
+
+cd $DIR/../dist/
+
+sudo npm --prefix $DIR/../dist/ install;
+
+node $DIR/../dist/install_plugins;
+
+pkg $DIR/../dist/package.json;
