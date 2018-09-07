e20833726ea12cf46b172f4984835501b33f7b0e
Cookiezaurs <sosina.anna@gmail.com>
2018-09-07 15:27:25 +0300

    fix

--- api/utils/authorizer.js
+++ api/utils/authorizer.js
@@ -4,12 +4,12 @@
 */
 
 /** @lends module:api/utils/authorizer */
-var authorizerOb = {};
+var authorizer = {};
 var common = require("./common.js");
 var crypto = require("crypto");
 
-(function(authorizer) {
-    /**
+
+/**
     * Store token for later authentication
     * @param {object} options - options for the task
     * @param {object} options.db - database connection
@@ -21,106 +21,106 @@ var crypto = require("crypto");
     * @param {string} options.endpoint - regexp of endpoint(any string - is used as substring,to mach exact ^{yourpath}$)
     * @param {function} options.callback - function called when saving was completed or errored, providing error object as first param and token string as second
     */
-    authorizer.save = function(options) {
-        options.db = options.db || common.db;
-        options.token = options.token || authorizer.getToken();
-        options.ttl = options.ttl || 0;
-        options.multi = options.multi || false;
-        options.app = options.app || "";
-        options.endpoint = options.endpoint || "";
-        options.purpose = options.purpose || "";
+authorizer.save = function(options) {
+    options.db = options.db || common.db;
+    options.token = options.token || authorizer.getToken();
+    options.ttl = options.ttl || 0;
+    options.multi = options.multi || false;
+    options.app = options.app || "";
+    options.endpoint = options.endpoint || "";
+    options.purpose = options.purpose || "";
 
-        if (options.endpoint !== "" && !Array.isArray(options.endpoint)) {
-            options.endpoint = [options.endpoint];
-        }
+    if (options.endpoint !== "" && !Array.isArray(options.endpoint)) {
+        options.endpoint = [options.endpoint];
+    }
 
-        if (options.app !== "" && !Array.isArray(options.app)) {
-            options.app = [options.app];
-        }
+    if (options.app !== "" && !Array.isArray(options.app)) {
+        options.app = [options.app];
+    }
 
-        if (options.owner && options.owner !== "") {
-            options.owner = options.owner + "";
-            options.db.collection('members').findOne({'_id': options.db.ObjectID(options.owner)}, function(err, member) {
-                if (err) {
-                    if (typeof options.callback === "function") {
-                        options.callback(err, "");
-                        return;
-                    }
+    if (options.owner && options.owner !== "") {
+        options.owner = options.owner + "";
+        options.db.collection('members').findOne({'_id': options.db.ObjectID(options.owner)}, function(err, member) {
+            if (err) {
+                if (typeof options.callback === "function") {
+                    options.callback(err, "");
+                    return;
                 }
-                else if (member) {
-                    options.db.collection("auth_tokens").insert({
-                        _id: options.token,
-                        ttl: options.ttl,
-                        ends: options.ttl + Math.round(Date.now() / 1000),
-                        multi: options.multi,
-                        owner: options.owner,
-                        app: options.app,
-                        endpoint: options.endpoint,
-                        purpose: options.purpose
-                    }, function(err1) {
-                        if (typeof options.callback === "function") {
-                            options.callback(err1, options.token);
-                        }
-                    });
-                }
-                else {
-                    if (typeof options.callback === "function") {
-                        options.callback("Token must have owner. Please provide correct user id", "");
-                    }
-                }
-            });
-        }
-        else {
-            if (typeof options.callback === "function") {
-                options.callback("Token must have owner. Please provide correct user id", "");
             }
+            else if (member) {
+                options.db.collection("auth_tokens").insert({
+                    _id: options.token,
+                    ttl: options.ttl,
+                    ends: options.ttl + Math.round(Date.now() / 1000),
+                    multi: options.multi,
+                    owner: options.owner,
+                    app: options.app,
+                    endpoint: options.endpoint,
+                    purpose: options.purpose
+                }, function(err1) {
+                    if (typeof options.callback === "function") {
+                        options.callback(err1, options.token);
+                    }
+                });
+            }
+            else {
+                if (typeof options.callback === "function") {
+                    options.callback("Token must have owner. Please provide correct user id", "");
+                }
+            }
+        });
+    }
+    else {
+        if (typeof options.callback === "function") {
+            options.callback("Token must have owner. Please provide correct user id", "");
         }
-    };
+    }
+};
 
-    /**
+/**
     * Get whole token information from database
     * @param {object} options - options for the task
     * @param {object} options.db - database connection
     * @param {string} options.token - token to read
     * @param {function} options.callback - function called when reading was completed or errored, providing error object as first param and token object from database as second
     */
-    authorizer.read = function(options) {
-        options.db = options.db || common.db;
-        if (!options.token || options.token === "") {
-            options.callback(Error('Token not given'), null);
-        }
-        else {
-            options.db.collection("auth_tokens").findOne({_id: options.token}, options.callback);
-        }
-    };
+authorizer.read = function(options) {
+    options.db = options.db || common.db;
+    if (!options.token || options.token === "") {
+        options.callback(Error('Token not given'), null);
+    }
+    else {
+        options.db.collection("auth_tokens").findOne({_id: options.token}, options.callback);
+    }
+};
 
-    /**
+/**
     * Checks if token is not expired yet
     * @param {object} options - options for the task
     * @param {object} options.db - database connection
     * @param {string} options.token - token to rvalidate
     * @param {function} options.callback - function called when reading was completed or errored, providing error object as first param, true or false if expired as second, seconds till expiration as third.(-1 if never expires, 0 - if expired) 
     */ 
-    authorizer.check_if_expired = function(options) {
-        options.db = options.db || common.db;
-        options.db.collection("auth_tokens").findOne({_id: options.token}, function(err, res) {
-            var expires_after = 0;
-            var valid = false;
-            if (res) {
-                if (res.ttl > 0 && res.ends >= Math.round(Date.now() / 1000)) {
-                    valid = true;
-                    expires_after = res.ends - Math.round(Date.now() / 1000);
-                }
-                else if (res.ttl === 0) {
-                    valid = true;
-                    expires_after = -1;
-                }
+authorizer.check_if_expired = function(options) {
+    options.db = options.db || common.db;
+    options.db.collection("auth_tokens").findOne({_id: options.token}, function(err, res) {
+        var expires_after = 0;
+        var valid = false;
+        if (res) {
+            if (res.ttl > 0 && res.ends >= Math.round(Date.now() / 1000)) {
+                valid = true;
+                expires_after = res.ends - Math.round(Date.now() / 1000);
             }
-            options.callback(err, valid, expires_after);
-        });
-    };
+            else if (res.ttl === 0) {
+                valid = true;
+                expires_after = -1;
+            }
+        }
+        options.callback(err, valid, expires_after);
+    });
+};
 
-    /**
+/**
     * extent token life spas
     * @param {object} options - options for the task
     * @param {object} options.db - database connection
@@ -129,39 +129,39 @@ var crypto = require("crypto");
     * @param {string} options.extendTill - extend till given timestamp. (optional) You have to provide extedBy or extendTill
     * @param {function} options.callback - function called when reading was completed or errored, providing error object as first param and true as second if extending successful
     */
-    authorizer.extend_token = function(options) {
-        if (!options.token || options.token === "") {
-            if (typeof options.callback === "function") {
-                options.callback(Error("Token not provided"), null);
-            }
-            return;
+authorizer.extend_token = function(options) {
+    if (!options.token || options.token === "") {
+        if (typeof options.callback === "function") {
+            options.callback(Error("Token not provided"), null);
         }
-        options.db = options.db || common.db;
-        var updateArr = {
-            ttl: 0,
-            ends: 0
-        };
-        if (options.extendBy) {
-            updateArr.ends = Math.round((options.extendBy + Date.now()) / 1000);
-            updateArr.ttl = options.extendBy;
-        }
-        else if (options.extendTill) {
-            updateArr.ends = Math.round(options.extendTill / 1000);
-            updateArr.ttl = 1;
-        }
-        else {
-            if (typeof options.callback === "function") {
-                options.callback(Error("Please provide extendTill or extendBy"), null);
-            }
-            return;
-        }
-        options.db.collection("auth_tokens").update({_id: options.token}, {$set: updateArr}, function(err) {
-            if (typeof options.callback === "function") {
-                options.callback(err, true);
-            }
-        });
+        return;
+    }
+    options.db = options.db || common.db;
+    var updateArr = {
+        ttl: 0,
+        ends: 0
     };
-    /**
+    if (options.extendBy) {
+        updateArr.ends = Math.round((options.extendBy + Date.now()) / 1000);
+        updateArr.ttl = options.extendBy;
+    }
+    else if (options.extendTill) {
+        updateArr.ends = Math.round(options.extendTill / 1000);
+        updateArr.ttl = 1;
+    }
+    else {
+        if (typeof options.callback === "function") {
+            options.callback(Error("Please provide extendTill or extendBy"), null);
+        }
+        return;
+    }
+    options.db.collection("auth_tokens").update({_id: options.token}, {$set: updateArr}, function(err) {
+        if (typeof options.callback === "function") {
+            options.callback(err, true);
+        }
+    });
+};
+/**
     * Token validation function called from verify and verify return
     * @param {object} options - options for the task
     * @param {object} options.db - database connection
@@ -169,73 +169,73 @@ var crypto = require("crypto");
     * @param {function} options.callback - function called when verifying was completed or errored, providing error object as first param and true as second if extending successful
     * @param {boolean} return_owner states if in callback owner shold be returned. If return_owner==false, returns true or false.
     */
-    var verify_token = function(options, return_owner) {
-        options.db = options.db || common.db;
-        if (!options.token || options.token === "") {
-            if (typeof options.callback === "function") {
-                options.callback(false);
+var verify_token = function(options, return_owner) {
+    options.db = options.db || common.db;
+    if (!options.token || options.token === "") {
+        if (typeof options.callback === "function") {
+            options.callback(false);
+        }
+        return;
+    }
+    else {
+        options.db.collection("auth_tokens").findOne({_id: options.token}, function(err, res) {
+            var valid = false;
+            if (res) {
+                var valid_endpoint = true;
+                if (res.endpoint && res.endpoint !== "") {
+                    //keep backwards compability
+                    if (!Array.isArray(res.endpoint)) {
+                        res.endpoint = [res.endpoint];
+                    }
+                    valid_endpoint = false;
+                    if (options.req_path !== "") {
+                        for (var p = 0; p < res.endpoint.length; p++) {
+                            var my_regexp = new RegExp(res.endpoint[p]);
+                            if (my_regexp.test(options.req_path)) {
+                                valid_endpoint = true;
+                            }
+                        }
+                    }
+                }
+                var valid_app = true;
+                if (res.app && res.app !== "") {
+                    //keep backwards compability
+                    if (!Array.isArray(res.app)) {
+                        res.app = [res.app];
+                    }
+                    if (options.qstring && options.qstring.app_id) {
+                        if (res.app.indexOf(options.qstring.app_id) === -1) {
+                            valid_app = false;
+                        }
+                    }
+                }
+
+                if (valid_endpoint && valid_app) {
+                    if (res.ttl === 0) {
+                        valid = true;
+                        if (return_owner) {
+                            valid = res.owner;
+                        }
+                    }
+                    else if (res.ends >= Math.round(Date.now() / 1000)) {
+                        valid = true;
+                        if (return_owner) {
+                            valid = res.owner;
+                        }
+                    }
+
+                    //consume token if expired or not multi
+                    if (!res.multi || (res.ttl > 0 && res.ends < Math.round(Date.now() / 1000))) {
+                        options.db.collection("auth_tokens").remove({_id: options.token});
+                    }
+                }
             }
-            return;
-        }
-        else {
-            options.db.collection("auth_tokens").findOne({_id: options.token}, function(err, res) {
-                var valid = false;
-                if (res) {
-                    var valid_endpoint = true;
-                    if (res.endpoint && res.endpoint !== "") {
-                        //keep backwards compability
-                        if (!Array.isArray(res.endpoint)) {
-                            res.endpoint = [res.endpoint];
-                        }
-                        valid_endpoint = false;
-                        if (options.req_path !== "") {
-                            for (var p = 0; p < res.endpoint.length; p++) {
-                                var my_regexp = new RegExp(res.endpoint[p]);
-                                if (my_regexp.test(options.req_path)) {
-                                    valid_endpoint = true;
-                                }
-                            }
-                        }
-                    }
-                    var valid_app = true;
-                    if (res.app && res.app !== "") {
-                        //keep backwards compability
-                        if (!Array.isArray(res.app)) {
-                            res.app = [res.app];
-                        }
-                        if (options.qstring && options.qstring.app_id) {
-                            if (res.app.indexOf(options.qstring.app_id) === -1) {
-                                valid_app = false;
-                            }
-                        }
-                    }
-
-                    if (valid_endpoint && valid_app) {
-                        if (res.ttl === 0) {
-                            valid = true;
-                            if (return_owner) {
-                                valid = res.owner;
-                            }
-                        }
-                        else if (res.ends >= Math.round(Date.now() / 1000)) {
-                            valid = true;
-                            if (return_owner) {
-                                valid = res.owner;
-                            }
-                        }
-
-                        //consume token if expired or not multi
-                        if (!res.multi || (res.ttl > 0 && res.ends < Math.round(Date.now() / 1000))) {
-                            options.db.collection("auth_tokens").remove({_id: options.token});
-                        }
-                    }
-                }
-                if (typeof options.callback === "function") {
-                    options.callback(valid);
-                }
-            });
-        }
-    };
+            if (typeof options.callback === "function") {
+                options.callback(valid);
+            }
+        });
+    }
+};
     /**
     * Verify token and expire it
     * @param {object} options - options for the task
@@ -244,11 +244,11 @@ var crypto = require("crypto");
     * @param {string} options.req_path - current request path
     * @param {function} options.callback - function called when verifying was completed, providing 1 argument, true if could verify token and false if couldn't
     */
-    authorizer.verify = function(options) {
-        verify_token(options, false);
-    };
+authorizer.verify = function(options) {
+    verify_token(options, false);
+};
 
-    /** 
+/** 
     * Similar to authorizer.verify. Only difference - return token owner if valid.
     * @param {object} options - options for the task
     * @param {object} options.db - database connection
@@ -256,31 +256,29 @@ var crypto = require("crypto");
     * @param {string} options.req_path - current request path
     * @param {function} options.callback - function called when verifying was completed, providing 1 argument, true if could verify token and false if couldn't
     */
-    authorizer.verify_return = function(options) {
-        verify_token(options, true);
-    };
-    /**
+authorizer.verify_return = function(options) {
+    verify_token(options, true);
+};
+/**
     * Generates auhtentication ID
     * @returns {string} id to be used when saving the task
     */
-    authorizer.getToken = function() {
-        return crypto.createHash('sha1').update(crypto.randomBytes(16).toString("hex") + "" + new Date().getTime()).digest('hex');
-    };
+authorizer.getToken = function() {
+    return crypto.createHash('sha1').update(crypto.randomBytes(16).toString("hex") + "" + new Date().getTime()).digest('hex');
+};
 
-    /**
+/**
     * Clean all expired tokens
     * @param {object} options - options for the task
     * @param {object} options.db - database connection
     * @param {function} options.callback - function called when cleaning completed
     */
-    authorizer.clean = function(options) {
-        options.db = options.db || common.db;
-        options.db.collection("auth_tokens").remove({
-            ends: {$lt: Math.round(Date.now() / 1000)},
-            ttl: {$ne: 0}
-        }, options.callback);
-    };
+authorizer.clean = function(options) {
+    options.db = options.db || common.db;
+    options.db.collection("auth_tokens").remove({
+        ends: {$lt: Math.round(Date.now() / 1000)},
+        ttl: {$ne: 0}
+    }, options.callback);
+};
 
-}(authorizerOb));
-
-module.exports = authorizerOb;
+module.exports = authorizer;
