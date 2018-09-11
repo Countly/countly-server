b77f79c9a414e3a8c5e3ee1ed8d6e2ec0a710b59
Cookiezaurs <sosina.anna@gmail.com>
2018-09-11 18:13:31 +0300

    guidelines fixes: countly.carrier.js, countly.app.version.js, countly.city.js

--- frontend/express/public/javascripts/countly/countly.carrier.js
+++ frontend/express/public/javascripts/countly/countly.carrier.js
@@ -1,6 +1,10 @@
+/*global CountlyHelpers*/
 (function() {
     var _carrierCodeMap = {"46000": "中国移动(GSM)", "46001": "中国联通(GSM)", "46002": "中国移动(TD-S)", "46003": "中国电信(CDMA)", "46005": "中国电信(CDMA)", "46006": "中国联通(WCDMA)", "46007": "中国移动(TD-S)", "46011": "中国电信(FDD-LTE)", "460 11": "中国电信(FDD-LTE)"};
-
+    /** function returns carrier code name
+    * @param {string} code - carrier code
+    * @returns {string} carrier name
+    */
     function getCarrierCodeName(code) {
         return _carrierCodeMap[code] ? _carrierCodeMap[code] : code;
     }
