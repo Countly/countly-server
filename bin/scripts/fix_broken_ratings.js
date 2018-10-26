c8932483381067441520d81cbb82a2c29bc7af0f
Furkan Başaran <frknbasaran@gmail.com>
2018-10-26 17:42:51 +0300

    [feedback] device_id fix script for broken records.
    - find all broken app_users
    - check is there any correct user for fixed_device_id
    - map it correct uid and did to feedback and drill collections, and -remove broken app_user record
    - if there is not correct user for fixed_device_id, use it as user and map it to feedback and drill collections

---
+++ bin/scripts/fix_broken_ratings.js
@@ -0,0 +1,91 @@
+var pluginManager = require('../../plugins/pluginManager.js'),
+    async = require('async'),
+    crypto = require('crypto'),
+    countlyDb = pluginManager.dbConnection(),
+    countlyDrillDB = pluginManager.dbConnection("countly_drill");
+
+countlyDb.collection('apps').find({}).toArray(function(err, apps) {
+    if (!apps || err) {
+        console.log('No apps to fix');
+        countlyDb.close();
+        countlyDrillDB.close();
+        return;
+    }
+
+    function fixBrokenDeviceIDs(app, done) {
+        // find all broken app_user record containts sdk_name
+        countlyDb.collection('app_users' + app._id).find({did: /.*sdk_name=javascript_feedback_popup/}).toArray(function(err, users) {
+            if (!err) {
+                async.forEach(users, function(brokenUser, doneUser) {
+                    let fixed_device_id = brokenUser.did.substr(0, 36);
+                    // check is there correct app_user for this fixed_device_id
+                    countlyDb.collection('app_users' + app._id).findOne({did: fixed_device_id}, function(err, correctUser) {
+                        if (!err) {
+                            // if there is correct app_user for fixed_device_id, use that values, otherwise use brokenUser uid and fixed did
+                            var correctReference = (correctUser) ? correctUser : {
+                                did: fixed_device_id,
+                                uid: brokenUser.uid
+                            };
+                            // map correct uid and did values to drill events records
+                            countlyDrillDB.collection("drill_events" + crypto.createHash('sha1').update("[CLY]_star_rating" + app._id).digest('hex'))
+                                .updateMany({did: brokenUser.did}, {
+                                    $set: {
+                                        did: correctReference.did,
+                                        uid: correctReference.uid
+                                    }
+                                }, function(err) {
+                                    if (!err) {
+                                        console.log("Drill event fixed for " + app.name);
+                                        // map correct uid and did values to broken feedback records
+                                        countlyDb.collection("feedback" + app._id)
+                                            .updateMany({device_id: brokenUser.did}, {
+                                                $set: {
+                                                    device_id: correctReference.did,
+                                                    uid: correctReference.uid
+                                                }
+                                            }, function(err) {
+                                                if (!err) {
+                                                    console.log("Feedback fixed for " + app.name);
+                                                    // if there is correct user for this fixed device_id, remove broken app_user record.
+                                                    if (correctUser) {
+                                                        countlyDb.collection('app_users' + app._id).remove({_id: brokenUser._id}, function(err) {
+                                                            doneUser();
+                                                        });
+                                                    }
+                                                    // if there is no correct user for this fixed device_id, fix this broken app_user record and use it as user.
+                                                    else {
+                                                        countlyDb.collection('app_users' + app._id).findAndModify({_id: brokenUser._id}, {}, {$set: {did: fixed_device_id}}, function(err) {
+                                                            doneUser();
+                                                        });
+                                                    }
+                                                }
+                                                else {
+                                                    console.log(err);
+                                                    doneUser();
+                                                }
+                                            });
+                                    }
+                                    else {
+                                        console.log(err);
+                                        doneUser();
+                                    }
+                                });
+                        }
+                    });
+                }, function() {
+                    done();
+                });
+            }
+            else {
+                console.log(err);
+                done();
+            }
+        });
+    }
+
+
+    async.forEach(apps, fixBrokenDeviceIDs, function() {
+        countlyDb.close();
+        countlyDrillDB.close();
+    });
+});
