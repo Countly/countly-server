9e9696cbfae86c3edf0be2fbaa99cd284ec7e812
Arturs Sosins <ar2rsawseen@gmail.com>
2018-11-07 16:43:38 +0200

    [api] use default period 30days

--- api/parts/data/fetch.js
+++ api/parts/data/fetch.js
@@ -1498,7 +1498,7 @@ function getPeriodObj(params) {
         }
         catch (SyntaxError) {
             console.log('Parse period JSON failed');
-            return false;
+            params.qstring.period = "30days";
         }
     }
 
