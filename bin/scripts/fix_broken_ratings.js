c49c1b1e1789b01a260ebcf12e9769ea2f2e0b46
Furkan Başaran <frknbasaran@gmail.com>
2018-11-09 23:05:42 +0300

    [ratings-fix-script] eslint changes.
    - eslint changes.

--- bin/scripts/fix_broken_ratings.js
+++ bin/scripts/fix_broken_ratings.js
@@ -4,23 +4,29 @@ var pluginManager = require('../../plugins/pluginManager.js'),
     countlyDb = pluginManager.dbConnection(),
     countlyDrillDB = pluginManager.dbConnection("countly_drill");
 
-countlyDb.collection('apps').find({}).toArray(function(err, apps) {
-    if (!apps || err) {
+countlyDb.collection('apps').find({}).toArray(function(errAppFind, apps) {
+    if (!apps || errAppFind) {
         console.log('No apps to fix');
         countlyDb.close();
         countlyDrillDB.close();
         return;
     }
 
+    /**
+     * fix device id's of broken records
+     * @method fixBrokenDeviceIds
+     * @param {object} app - application object
+     * @param {callback} done - callback function
+     */
     function fixBrokenDeviceIDs(app, done) {
         // find all broken app_user record containts sdk_name
-        countlyDb.collection('app_users' + app._id).find({did: /.*sdk_name=javascript_feedback_popup/}).toArray(function(err, users) {
-            if (!err) {
+        countlyDb.collection('app_users' + app._id).find({did: /.*sdk_name=javascript_feedback_popup/}).toArray(function(errBrokenUserFind, users) {
+            if (!errBrokenUserFind) {
                 async.forEach(users, function(brokenUser, doneUser) {
                     let fixed_device_id = brokenUser.did.substr(0, brokenUser.did.length - 34);
                     // check is there correct app_user for this fixed_device_id
-                    countlyDb.collection('app_users' + app._id).findOne({did: fixed_device_id}, function(err, correctUser) {
-                        if (!err) {
+                    countlyDb.collection('app_users' + app._id).findOne({did: fixed_device_id}, function(errFindCorrectUser, correctUser) {
+                        if (!errFindCorrectUser) {
                             // if there is correct app_user for fixed_device_id, use that values, otherwise use brokenUser uid and fixed did
                             var correctReference = (correctUser) ? correctUser : {
                                 did: fixed_device_id,
@@ -33,8 +39,8 @@ countlyDb.collection('apps').find({}).toArray(function(err, apps) {
                                         did: correctReference.did,
                                         uid: correctReference.uid
                                     }
-                                }, function(err) {
-                                    if (!err) {
+                                }, function(errUpdateDrill) {
+                                    if (!errUpdateDrill) {
                                         console.log("Drill event fixed for " + app.name);
                                         // map correct uid and did values to broken feedback records
                                         countlyDb.collection("feedback" + app._id)
@@ -43,35 +49,42 @@ countlyDb.collection('apps').find({}).toArray(function(err, apps) {
                                                     device_id: correctReference.did,
                                                     uid: correctReference.uid
                                                 }
-                                            }, function(err) {
-                                                if (!err) {
+                                            }, function(errUpdateFeedback) {
+                                                if (!errUpdateFeedback) {
                                                     console.log("Feedback fixed for " + app.name);
                                                     // if there is correct user for this fixed device_id, remove broken app_user record.
-                                                    if (correctUser) {
-                                                        countlyDb.collection('app_users' + app._id).remove({_id: brokenUser._id}, function(err) {
+                                                    countlyDb.collection('app_users' + app._id).remove({_id: brokenUser._id}, function(errRemoveAppUser) {
+                                                        if (errRemoveAppUser) {
+                                                            console.log(errRemoveAppUser);
                                                             doneUser();
-                                                        });
-                                                    }
-                                                    // if there is no correct user for this fixed device_id, fix this broken app_user record and use it as user.
-                                                    else {
-                                                        brokenUser._id = crypto.createHash('sha1')
-                                                            .update(app.app_key + fixed_device_id + "")
-                                                            .digest('hex');
-                                                        brokenUser.did = fixed_device_id;
-                                                        countlyDb.collection('app_users' + app._id).insert(brokenUser, function(err) {
-                                                            if (err) console.log(err);
-                                                            doneUser();
-                                                        });
-                                                    }
+                                                        }
+                                                        else {
+                                                            if (!correctUser) {
+                                                                brokenUser._id = crypto.createHash('sha1')
+                                                                    .update(app.app_key + fixed_device_id + "")
+                                                                    .digest('hex');
+                                                                brokenUser.did = fixed_device_id;
+                                                                countlyDb.collection('app_users' + app._id).insert(brokenUser, function(err) {
+                                                                    if (err) {
+                                                                        console.log(err);
+                                                                    }
+                                                                    doneUser();
+                                                                });
+                                                            }
+                                                            else {
+                                                                doneUser();
+                                                            }
+                                                        }
+                                                    });
                                                 }
                                                 else {
-                                                    console.log(err);
+                                                    console.log(errUpdateFeedback);
                                                     doneUser();
                                                 }
                                             });
                                     }
                                     else {
-                                        console.log(err);
+                                        console.log(errUpdateDrill);
                                         doneUser();
                                     }
                                 });
@@ -82,13 +95,11 @@ countlyDb.collection('apps').find({}).toArray(function(err, apps) {
                 });
             }
             else {
-                console.log(err);
+                console.log(errBrokenUserFind);
                 done();
             }
         });
     }
-
-
     async.forEach(apps, fixBrokenDeviceIDs, function() {
         countlyDb.close();
         countlyDrillDB.close();
