3a70951cffaba65dd14185834efb47b2338924a0
Arturs Sosins <ar2rsawseen@gmail.com>
2019-01-14 18:32:14 +0200

    [scripts] fixes for relative paths

--- bin/upgrade/19.01/scripts/clear_jobs.js
+++ bin/upgrade/19.01/scripts/clear_jobs.js
@@ -1,4 +1,4 @@
-var pluginManager = require('../../../plugins/pluginManager.js'),
+var pluginManager = require('../../../../plugins/pluginManager.js'),
     countlyDb = pluginManager.dbConnection();
 
 countlyDb.collection('jobs').deleteMany({}, function(err, res) {});
--- bin/upgrade/19.01/scripts/fix_broken_ratings.js
+++ bin/upgrade/19.01/scripts/fix_broken_ratings.js
@@ -1,4 +1,4 @@
-var pluginManager = require('../../../plugins/pluginManager.js'),
+var pluginManager = require('../../../../plugins/pluginManager.js'),
     async = require('async'),
     crypto = require('crypto'),
     countlyDb = pluginManager.dbConnection(),
