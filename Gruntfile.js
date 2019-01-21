eb2afa3c1a214543ed168f9dc745744d0c006122
Furkan Başaran <frknbasaran@gmail.com>
2019-01-21 23:34:53 +0300

    [overview-bars] created getTop() method.
    - created countly.analytics.js file and generic getTop() method for top bars on the overview screen.
    - added dependency links to related places.

--- Gruntfile.js
+++ Gruntfile.js
@@ -97,7 +97,8 @@ module.exports = function(grunt) {
                     'frontend/express/public/javascripts/countly/countly.views.js',
                     'frontend/express/public/javascripts/countly/countly.vue.components.js',
                     'frontend/express/public/javascripts/countly/countly.token.manager.js',
-                    'frontend/express/public/javascripts/countly/countly.version.history.js'
+                    'frontend/express/public/javascripts/countly/countly.version.history.js',
+                    'frontend/express/public/javascripts/countly/countly.analytics.js'
                 ],
                 dest: 'frontend/express/public/javascripts/min/countly.lib.concat.js'
             }
---
+++ frontend/express/public/javascripts/countly/countly.analytics.js
@@ -0,0 +1,30 @@
+/*global countlyAnalyticsAPI, $, countlyCommon, countlyGlobal */
+(function() {
+    window.countlyAnalyticsAPI = window.countlyAnalyticsAPI || {};
+
+    countlyAnalyticsAPI.data = {};
+    countlyAnalyticsAPI.metrics = {
+        "mobile": '["platforms", "devices", "carriers"]',
+        "desktop": '["platforms", "resolutions", "languages"]',
+        "web": '["platforms", "sources", "browsers"]'
+    };
+
+    countlyAnalyticsAPI.initialize = function() {
+        return $.ajax({
+            type: "GET",
+            url: countlyCommon.API_PARTS.data.r + "/analytics/tops",
+            data: {
+                "app_id": countlyCommon.ACTIVE_APP_ID,
+                "metrics": countlyAnalyticsAPI.metrics[countlyGlobal.admin_apps[countlyCommon.ACTIVE_APP_ID].type]
+            },
+            dataType: "json",
+            success: function(json) {
+                countlyAnalyticsAPI.data = json;
+            }
+        });
+    };
+
+    countlyAnalyticsAPI.getTop = function(metric) {
+        return countlyAnalyticsAPI.data[metric];
+    };
+}());
