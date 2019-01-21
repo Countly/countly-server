cc5bbfeb0a3bfae65924e77bc841f6cc510db156
Furkan Başaran <frknbasaran@gmail.com>
2019-01-21 23:38:56 +0300

    [overview-bars] mobile plugin integration.
    - new library integrated to mobile plugin.

--- plugins/mobile/frontend/public/javascripts/countly.views.js
+++ plugins/mobile/frontend/public/javascripts/countly.views.js
@@ -1,4 +1,4 @@
-/*globals MobileDashboardView,_,CountlyHelpers, countlyDevice, countlyTotalUsers,Handlebars,countlyView,jQuery,$,app,countlyGlobal,countlySession,countlyCommon,countlyLocation,countlyDeviceDetails,countlyCarrier */
+/*globals countlyAnalyticsAPI, MobileDashboardView,_,CountlyHelpers, countlyDevice, countlyTotalUsers,Handlebars,countlyView,jQuery,$,app,countlyGlobal,countlySession,countlyCommon,countlyLocation,countlyDeviceDetails,countlyCarrier */
 window.MobileDashboardView = countlyView.extend({
     selectedView: "#draw-total-sessions",
     selectedMap: "#map-list-sessions",
@@ -13,7 +13,7 @@ window.MobileDashboardView = countlyView.extend({
             "map-list-users": {id: 'total', label: jQuery.i18n.map["sidebar.analytics.users"], type: 'number', metric: "u"},
             "map-list-new": {id: 'total', label: jQuery.i18n.map["common.table.new-users"], type: 'number', metric: "n"}
         };
-        return $.when(countlySession.initialize(), countlyDevice.initialize(), countlyCarrier.initialize(), countlyDeviceDetails.initialize(), countlyTotalUsers.initialize("users"), countlyTotalUsers.initialize("countries")).then(function() {});
+        return $.when(countlyAnalyticsAPI.initialize(), countlySession.initialize(), countlyDevice.initialize(), countlyCarrier.initialize(), countlyDeviceDetails.initialize(), countlyTotalUsers.initialize("users"), countlyTotalUsers.initialize("countries")).then(function() {});
     },
     afterRender: function() {
         if (countlyGlobal.config.use_google) {
@@ -137,17 +137,17 @@ window.MobileDashboardView = countlyView.extend({
         sessionData.bars = [
             {
                 "title": jQuery.i18n.map["common.bar.top-platform"],
-                "data": countlyDeviceDetails.getBarsWPercentageOfTotal("os"),
+                "data": countlyAnalyticsAPI.getTop('platforms'),
                 "help": "dashboard.top-platforms"
             },
             {
                 "title": jQuery.i18n.map["common.bar.top-devices"],
-                "data": countlyDevice.getBarsWPercentageOfTotal("devices"),
+                "data": countlyAnalyticsAPI.getTop('devices'),
                 "help": "dashboard.top-devices"
             },
             {
                 "title": jQuery.i18n.map["common.bar.top-carrier"],
-                "data": countlyCarrier.getBarsWPercentageOfTotal(),
+                "data": countlyAnalyticsAPI.getTop('carriers'),
                 "help": "dashboard.top-carriers"
             },
             {
