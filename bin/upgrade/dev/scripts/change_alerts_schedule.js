c8eadb29d714a26d7808ab1bdd8ee1d5e640e362
Arturs Sosins <ar2rsawseen@gmail.com>
2019-01-28 17:37:21 +0200

    [upgrade] update upgrade script

---
+++ bin/upgrade/dev/scripts/change_alerts_schedule.js
@@ -0,0 +1,18 @@
+// 'every 5 minutes';every 59 mins starting on the 59 min
+var pluginManager = require('../../../../plugins/pluginManager'),
+  countlyDb = pluginManager.dbConnection();
+
+countlyDb.collection('alerts').updateMany({
+    // 'period': 'every 5 minutes',
+  }, {
+    "$set": {
+      "period": 'every 1 hour on the 59th min'
+    }
+  }, function (err, result) {
+    if (err) {
+      console.log('Change alerts schedule time failed.')
+    } else {
+      console.log(`Changed alerts schedule time records count: ${result.modifiedCount}`)
+    }
+    countlyDb.close();
+  });
--- bin/upgrade/dev/upgrade.sh
+++ bin/upgrade/dev/upgrade.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+CUR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#enable command line
+bash $DIR/scripts/detect.init.sh
+
+#upgrade config
+mv $DIR/../frontend/express/public/javascripts/countly/countly.config.js $DIR/../frontend/express/public/javascripts/countly/countly.config.backup.19.01.js
+cp -n $DIR/../frontend/express/public/javascripts/countly/countly.config.sample.js $DIR/../frontend/express/public/javascripts/countly/countly.config.js
+
+#remove predefined locale file, it should fallback to default one
+rm -rf $DIR/../frontend/express/public/localization/min/locale_en.properties 
+
+#remove outdated connect-mongoskin
+rm -rf $DIR/../node_modules/connect-mongoskin/
+
+#upgrade plugins
+countly upgrade
 countly plugin upgrade retention_segments
+countly plugin upgrade alerts
+
+nodejs $CUR/scripts/change_alerts_schedule.js
+nodejs $CUR/scripts/clear_jobs.js
+nodejs $CUR/scripts/fix_report_manager.js
+
+#add indexes
+nodejs $DIR/scripts/add_indexes.js
+
+#install dependencies, process files and restart countly
+countly upgrade
