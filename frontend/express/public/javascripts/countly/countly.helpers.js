0e493d8061bb6c8208e8094fa2934e67d55eb432
Cookiezaurs <sosina.anna@gmail.com>
2019-02-11 18:15:08 +0200

    eslint

--- frontend/express/public/javascripts/countly/countly.helpers.js
+++ frontend/express/public/javascripts/countly/countly.helpers.js
@@ -1212,7 +1212,7 @@
         }
         dtable.CoultyColumnSel.maxCol = maxCol;
         $(dtable[0]).parent().find(".select-column-table-data").first().after('<div class="data-table-column-selector" tabindex="1"><div class="title" ><span style="margin-left: 15px;">Select columns to display</span><span class="columncounter" style="margin-right: 15px;">' + selectedC + '/' + maxCol + '</span></div><div class="all_columns scrollable"><table>' + str + '</table></div></div>');
-        if(maxCol>8) {
+        if (maxCol > 8) {
             $(dtable[0]).parent().find('.scrollable').slimScroll({
                 height: '100%',
                 start: 'top',
