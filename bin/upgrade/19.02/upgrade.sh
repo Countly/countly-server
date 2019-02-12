b8fecb0931f76c5ac1dee3f76ce40ac1d077d69f
Arturs Sosins <ar2rsawseen@gmail.com>
2019-02-12 18:09:39 +0200

    [upgrade] more plugin upgrades

--- bin/upgrade/19.02/upgrade.sh
+++ bin/upgrade/19.02/upgrade.sh
@@ -21,9 +21,19 @@ countly upgrade
 countly plugin upgrade retention_segments
 countly plugin upgrade alerts
 countly plugin upgrade push
+countly plugin upgrade assistant
+countly plugin upgrade attribution
+countly plugin upgrade crashes
+countly plugin upgrade flows
+countly plugin upgrade plugin-upload
+countly plugin upgrade views
+
+#enable new plugins
+countly plugin enable remote-config
 
 nodejs $CUR/scripts/change_alerts_schedule.js
 nodejs $CUR/scripts/clear_jobs.js
+nodejs $CUR/scripts/drop_sessions.js
 nodejs $CUR/scripts/fix_report_manager.js
 nodejs $CUR/scripts/updateViews.js
 
