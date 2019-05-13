6ee782c10fabcafc293fac11815542e30d56ca82
Arturs Sosins <ar2rsawseen@gmail.com>
2019-05-13 09:10:06 +0300

    [api] allow to force updating multiple users

--- api/utils/requestProcessor.js
+++ api/utils/requestProcessor.js
@@ -332,7 +332,7 @@ const processRequest = (params) => {
                                 common.returnMessage(params, 400, 'No users matching criteria');
                                 return false;
                             }
-                            if (count > 1) {
+                            if (count > 1 && !params.qstring.force) {
                                 common.returnMessage(params, 400, 'This query would update more than one user');
                                 return false;
                             }
