2f89c29c1d53efd6f318003722cafec41a59b8ca
Arturs Sosins <ar2rsawseen@gmail.com>
2019-05-17 21:57:37 +0300

    [docs] add separate app.js docs

--- bin/commands/scripts/docs.sh
+++ bin/commands/scripts/docs.sh
@@ -10,7 +10,8 @@ if [ -z "$1" ]
 then
     usage ;
 elif [ "$1" = "generate" ]; then
-    $DIR/../../../node_modules/.bin/jsdoc $DIR/maindoc.jsdoc -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../frontend/express/public/docs ;
+    $DIR/../../../node_modules/.bin/jsdoc $DIR/../countly.sh -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../frontend/express/public/docs ;
     $DIR/../../../node_modules/.bin/jsdoc $DIR/../../../frontend/express/public/javascripts/countly $DIR/../../../frontend/express/public/javascripts/pre-login.js -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../frontend/express/public/docs/browser ;
     $DIR/../../../node_modules/.bin/jsdoc $DIR/../../../api -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../frontend/express/public/docs/api ;
+    $DIR/../../../node_modules/.bin/jsdoc $DIR/../../../frontend/express/app.js $DIR/../../../frontend/express/config.sample.js $DIR/../../../frontend/express/version.info.js $DIR/../../../frontend/express/locale.conf.js $DIR/../../../frontend/express/libs/ -R $DIR/../../../README.md -c  $DIR/../../../jsdoc_conf.json -d  $DIR/../../../frontend/express/public/docs/app ;
 fi
