bb008b78c839d975331b03cc9f8e9813e0e3856f
Kaan Kölköy <kaanklky@gmail.com>
2019-06-18 02:18:30 +0300

    Remove trailing whitespace

--- bin/countly.install_rhel.sh
+++ bin/countly.install_rhel.sh
@@ -28,7 +28,7 @@ gpgcheck=0
 enabled=1" > /etc/yum.repos.d/nginx.repo
     yum -y install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc
 else
-    echo "Unsupported OS version, only support RHEL/Centos 7 and 6" 
+    echo "Unsupported OS version, only support RHEL/Centos 7 and 6"
     exit 1
 fi
 
@@ -73,7 +73,7 @@ service sendmail start
 if grep -q -i "release 6" /etc/redhat-release ; then
     echo "updating gcc to devtoolset-2..."
     sudo rpm --import http://ftp.riken.jp/Linux/cern/centos/7.1/os/x86_64/RPM-GPG-KEY-cern
-    yum install -y wget 
+    yum install -y wget
     wget -O /etc/yum.repos.d/slc6-devtoolset.repo http://linuxsoft.cern.ch/cern/devtoolset/slc6-devtoolset.repo
     yum install -y devtoolset-2-gcc devtoolset-2-gcc-c++ devtoolset-2-binutils
     source /opt/rh/devtoolset-2/enable
