939a8d78c5322f0ab95ff36f21f8d004eebcf8d1
Bram Verdonck <bram@exsconsult.com>
2019-06-26 05:59:33 +0200

    Check if swap already exists + smaller bs size

--- bin/scripts/make.swap.sh
+++ bin/scripts/make.swap.sh
@@ -4,14 +4,18 @@ set -e
 # and writes to /etc/fstab so that swap is enabled each time your instance is rebooted.
 # Run it if your instance has a small memory (e.g less than 2GB) so it doesn't 
 # go out of RAM.
+if free | awk '/^Swap:/ {exit !$2}'; then
+    echo "Swap space already exists, skipping make.swap.sh"
+    exit 1
+fi
 
 #amount in GB
 AMOUNT=${1:-4}
 echo "Attempting to create $AMOUNT Gb swap file"
 echo "Reserving space, may take a while"
 
-#create file
-dd if=/dev/zero of=/swapfile bs=1G count=$AMOUNT
+#create file with small bs setting to allow the dd on low mem instances
+dd if=/dev/zero of=/swapfile bs=1024 count=$(($AMOUNT*1048576))
 chmod 600 /swapfile
 
 #make file a swap file
