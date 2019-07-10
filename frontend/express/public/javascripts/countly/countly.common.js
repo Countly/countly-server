2d4992f97608e47b04663b1a3f2b9b4669c25902
Ata Kuyumcu <ata.kuyumcu@yandex.ru>
2019-07-10 09:28:25 +0300

    [frontend] fixed eslint errors for levenshtein

--- frontend/express/public/javascripts/countly/countly.common.js
+++ frontend/express/public/javascripts/countly/countly.common.js
@@ -44,7 +44,7 @@
         };
 
         // Public Properties
-        /** 
+        /**
          * Set user persistent settings to store local storage
          * @param {object} data - Object param for set new data
         */
@@ -73,22 +73,22 @@
         };
         /**
         * App Key of currently selected app or 0 when not initialized
-        * @type {string|number} 
+        * @type {string|number}
         */
         countlyCommon.ACTIVE_APP_KEY = 0;
         /**
         * App ID of currently selected app or 0 when not initialized
-        * @type {string|number} 
+        * @type {string|number}
         */
         countlyCommon.ACTIVE_APP_ID = 0;
         /**
         * Current user's selected language in form en-EN, by default will use browser's language
-        * @type {string} 
+        * @type {string}
         */
         countlyCommon.BROWSER_LANG = countlyCommon.browserLang() || "en-US";
         /**
         * Current user's browser language in short form as "en", by default will use browser's language
-        * @type {string} 
+        * @type {string}
         */
         countlyCommon.BROWSER_LANG_SHORT = countlyCommon.BROWSER_LANG.split("-")[0];
 
@@ -115,7 +115,7 @@
         /**
         * Change currently selected period
         * @param {string|array} period - new period, supported values are (month, 60days, 30days, 7days, yesterday, hour or [startMiliseconds, endMiliseconds] as [1417730400000,1420149600000])
-        * @param {int} timeStamp - timeStamp for the period based 
+        * @param {int} timeStamp - timeStamp for the period based
         * @param {boolean} noSet - if set  - updates countly_date
         */
         countlyCommon.setPeriod = function(period, timeStamp, noSet) {
@@ -211,7 +211,7 @@
 
 
         /**
-        * Encode html 
+        * Encode html
         * @param {string} html - value to encode
         * @returns {string} encode string
         */
@@ -1143,7 +1143,7 @@
         * @param {string} propertyName - name of the property to extract
         * @param {object} rangeArray - array of all metrics/segments to extract (usually what is contained in meta)
         * @param {function} explainRange - function to convert range/bucket index to meaningful label
-        * @param {array} myorder - arrays of preferred order for give keys. Optional. If not passed - sorted by values 
+        * @param {array} myorder - arrays of preferred order for give keys. Optional. If not passed - sorted by values
         * @returns {array} array containing extracted ranged data as [{"f":"First session","t":352,"percent":"88.4"},{"f":"2 days","t":46,"percent":"11.6"}]
         * @example <caption>Extracting session frequency from users collection</caption>
         *    //outputs [{"f":"First session","t":352,"percent":"88.4"},{"f":"2 days","t":46,"percent":"11.6"}]
@@ -3710,13 +3710,13 @@
 
         /**
         * add one more column in chartDP[index].data to show string in dp
-        * for example: 
+        * for example:
         *     chartDPs = [
         *          {color:"#88BBC8", label:"duration", data:[[0, 23], [1, 22]}],
         *          {color:"#88BBC8", label:"count", data:[[0, 3], [1, 3]}],
         *     }
         *     lable = 'duration',
-        *      
+        *
         * will return
         *     chartDPs = [
         *          {color:"#88BBC8", label:"duration", data:[[0, 23, "00:00:23"], [1, 22, "00:00:22"]}],
@@ -3832,7 +3832,8 @@
         var collator;
         try {
             collator = (typeof Intl !== "undefined" && typeof Intl.Collator !== "undefined") ? Intl.Collator("generic", { sensitivity: "base" }) : null;
-        } catch (err) {
+        }
+        catch (err) {
             // console.log("Failed to initialize collator for Levenshtein\n" + err.stack);
         }
 
@@ -3847,11 +3848,11 @@
             /**
             * Calculate levenshtein distance of the two strings.
             *
-            * @param str1 String the first string.
-            * @param str2 String the second string.
-            * @param [options] Additional options.
-            * @param [options.useCollator] Use `Intl.Collator` for locale-sensitive string comparison.
-            * @return Integer the levenshtein distance (0 and above).
+            * @param {string} str1 String the first string.
+            * @param {string} str2 String the second string.
+            * @param {object} [options] Additional options.
+            * @param {boolean} [options.useCollator] Use `Intl.Collator` for locale-sensitive string comparison.
+            * @return {number} Integer the levenshtein distance (0 and above).
             */
             get: function(str1, str2, options) {
                 var useCollator = (options && collator && options.useCollator);
@@ -3860,14 +3861,19 @@
                     str2Len = str2.length;
 
                 // base cases
-                if (str1Len === 0) return str2Len;
-                if (str2Len === 0) return str1Len;
+                if (str1Len === 0) {
+                    return str2Len;
+                }
+
+                if (str2Len === 0) {
+                    return str1Len;
+                }
 
                 // two rows
                 var curCol, nextCol, i, j, tmp;
 
                 // initialise previous row
-                for (i=0; i<str2Len; ++i) {
+                for (i = 0; i < str2Len; ++i) {
                     prevRow[i] = i;
                     str2Char[i] = str2.charCodeAt(i);
                 }
