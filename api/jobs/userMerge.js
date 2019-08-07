62ddd70ae207be07e9a548dc11b1c4ada0641794
Arturs Sosins <ar2rsawseen@gmail.com>
2019-08-07 17:33:58 +0300

    [jobs] make user merge job less paralel, more sequential to reduce effect on DB

--- api/jobs/userMerge.js
+++ api/jobs/userMerge.js
@@ -33,32 +33,45 @@ class UserMergeJob extends job.Job {
                 if (!err && res && res.length) {
                     log.d('Found merges for ' + app._id + ' ', res);
                     var merged = [];
-                    for (var i = 0; i < res.length; i++) {
-                        if (res[i].merged_to) {
-                            merged.push(res[i]._id);
+                    async.eachSeries(res, function(user, done2) {
+                        if (user.merged_to) {
+                            merged.push(user._id);
                             log.d('Dispatching', {
                                 app_id: app._id + "",
-                                oldUser: {uid: res[i]._id},
-                                newUser: {uid: res[i].merged_to}
+                                oldUser: {uid: user._id},
+                                newUser: {uid: user.merged_to}
                             });
                             plugins.dispatch("/i/device_id", {
                                 app_id: app._id + "",
-                                oldUser: {uid: res[i]._id},
-                                newUser: {uid: res[i].merged_to}
+                                oldUser: {uid: user._id},
+                                newUser: {uid: user.merged_to}
+                            }, function() {
+                                done2();
                             });
                         }
-                    }
-                    //delete merged users if they still exist
-                    if (merged.length) {
-                        db.collection("app_users" + app._id).remove({uid: {$in: merged}}, function() {});
-                    }
+                        else {
+                            done2();
+                        }
+                    }, function() {
+                        //delete merged users if they still exist
+                        if (merged.length) {
+                            db.collection("app_users" + app._id).remove({uid: {$in: merged}}, function() {
+                                callback();
+                            });
+                        }
+                        else {
+                            callback();
+                        }
+                    });
                 }
-                callback();
+                else {
+                    callback();
+                }
             });
         }
-        db.collection('apps').find({}).toArray(function(err, apps) {
+        db.collection('apps').find({}, {_id: 1}).toArray(function(err, apps) {
             if (!err && apps && apps.length) {
-                async.forEach(apps, handleMerge, function() {
+                async.eachSeries(apps, handleMerge, function() {
                     log.d('Merging users finished ...');
                     done();
                 });
