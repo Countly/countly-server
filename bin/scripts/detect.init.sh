39ac6c8a2e5294e3a0319c54ad6acd9bb07ef5da
Artem <a@ahoy.li>
2019-09-04 17:37:47 +0300

    [docker] docker-compose & docker stack

--- bin/scripts/detect.init.sh
+++ bin/scripts/detect.init.sh
@@ -5,13 +5,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 # use available init system
 INITSYS="systemd"
 
-if [ "$INSIDE_DOCKER" = "1" ]
+if [ -z "$COUNTLY_CONTAINER" ]
 then
+	if [[ `/sbin/init --version` =~ upstart ]];
+	then
+	    INITSYS="upstart"
+	fi 2> /dev/null
+else
 	INITSYS="docker" 
-elif [[ `/sbin/init --version` =~ upstart ]];
-then
-    INITSYS="upstart"
-fi 2> /dev/null
+fi
 
 bash $DIR/commands/$INITSYS/install.sh
 ln -sf $DIR/commands/$INITSYS/countly.sh $DIR/commands/enabled/countly.sh
