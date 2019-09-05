46e3de267b0cf90c0431659a57726ec0568e8bde
Artem <a@ahoy.li>
2019-09-05 19:10:22 +0300

    [docker] Formatting, correct docker images

--- bin/docker/k8s/gce-cluster-init.sh
+++ bin/docker/k8s/gce-cluster-init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Kubernentes setup
 
+# IGNORE IF YOU HAVE KUBERNETES SET UP ALREADY
+# ---------------------------------------------------------------------------------------------------
+# -------------------------- IGNORE IF YOU HAVE ALREADY SET UP KUBECTL ------------------------------
+# ---------------------------------------------------------------------------------------------------
+# Kubernentes setup
 export PROJECT_NAME=countly-playground
 export CLUSTER_NAME=countly
 export ZONE_NAME=europe-west3-b
@@ -12,33 +16,35 @@ gcloud config set compute/zone "${ZONE_NAME}"
 gcloud components update
 gcloud container clusters get-credentials ${CLUSTER_NAME}
 kubectl config set-cluster ${CLUSTER_NAME}
+# ---------------------------------------------------------------------------------------------------
 
-# Cluster setup
+
+
+# ---------------------------------------------------------------------------------------------------
+# ---------------------------- IGNORE IF YOU HAVE ALREADY SET UP HELM -------------------------------
+# ---------------------------------------------------------------------------------------------------
+# Cluster & Helm setup
 # give user rights to give helm rights
 kubectl create clusterrolebinding user-cluster-admin-binding   --clusterrole=cluster-admin   --user=${GCE_USER}
 kubectl create clusterrolebinding tiller-cluster-admin-binding --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-
 # give helm the rights
 kubectl create -f rbac-config.yaml
 kubectl --namespace kube-system create serviceaccount tiller
 helm init
 kubectl --namespace kube-system patch deploy tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}' 
+# ---------------------------------------------------------------------------------------------------
 
 
-# Install MongoDB
 
-## Create static IP address
-gcloud compute addresses create countly-static-ip --global
-
-## Create a namespace
+# Create a namespace
 kubectl create ns countly
 kubectl config set-context --current --namespace=countly
 
-## MongoDB helm
+# Install MongoDB
 kubectl apply -f mongo/storage-class.yaml
 helm install --name db -f mongo/values.yaml stable/mongodb-replicaset
 
-## Wait ~ 3 minutes until all 3 replica set containers spin up
+## Wait ~ 3 minutes until all 3 replica set pods spin up
 kubectl get po
 
 
@@ -49,13 +55,14 @@ kubectl apply -f countly-api.yaml
 kubectl apply -f countly-api-service.yaml
 
 # Install Countly ingress
+gcloud compute addresses create countly-static-ip --global
 kubectl apply -f countly-ingress-gce.yaml
 # Alternatively install ingress without static address created above
-kubectl apply -f countly-ingress.yaml
+#kubectl apply -f countly-ingress.yaml
 
 ## Wait ~ 3 minutes until all Countly nodes spin up
 kubectl get po
 
-## Wait ~ 5-10 minutes until Ingress sets up and open static Countly IP address:
+## Wait ~ 5-10 minutes until Ingress sets up and open static Countly IP address
 # Check Ingress status at GCE console in the meantime, then wait 10 more minutes
 kubectl get ing
