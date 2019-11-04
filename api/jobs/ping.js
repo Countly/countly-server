d588b0f21b55aee07c0c1a614fe56d7d126ffd5c
Mert Koseoglu <info@mkoseoglu.com>
2019-11-04 10:24:06 +0300

    [offline_mode] loadConfigs

--- api/jobs/ping.js
+++ api/jobs/ping.js
@@ -5,8 +5,7 @@ const job = require('../parts/jobs/job.js'),
     countlyConfig = require("../../frontend/express/config.js"),
     versionInfo = require('../../frontend/express/version.info'),
     request = require('request'),
-    plugins = require('../../plugins/pluginManager.js'),
-    offlineMode = plugins.getConfig("api").offline_mode;
+    plugins = require('../../plugins/pluginManager.js');
 
 /** Class for the job of pinging servers **/
 class PingJob extends job.Job {
@@ -24,67 +23,70 @@ class PingJob extends job.Job {
         if (versionInfo.type !== "777a2bf527a18e0fffe22fb5b3e322e68d9c07a6") {
             url = "https://count.ly/configurations/ee/tracking";
         }
-        if (!offlineMode) {
-            request(url, function(err, response, body) {
-                if (typeof body === "string") {
-                    try {
-                        body = JSON.parse(body);
-                    }
-                    catch (ex) {
-                        body = null;
-                    }
-                }
-                if (body) {
-                    if (countlyConfigOrig.web.use_intercom && typeof body.intercom !== "undefined") {
-                        countlyConfig.web.use_intercom = body.intercom;
-                    }
-                    if (typeof countlyConfigOrig.web.track === "undefined" && typeof body.stats !== "undefined") {
-                        if (body.stats) {
-                            countlyConfig.web.track = null;
+        plugins.loadConfigs(db, function() {
+            const offlineMode = plugins.getConfig("api").offline_mode;
+            if (!offlineMode) {
+                request(url, function(err, response, body) {
+                    if (typeof body === "string") {
+                        try {
+                            body = JSON.parse(body);
                         }
-                        else {
-                            countlyConfig.web.track = "none";
+                        catch (ex) {
+                            body = null;
                         }
                     }
-                }
-                log.d(err, body, countlyConfigOrig, countlyConfig);
-                if (countlyConfig.web.track !== "none") {
-                    db.collection("members").findOne({global_admin: true}, function(err2, member) {
-                        if (!err2 && member) {
-                            var date = new Date();
-                            request({
-                                uri: "https://stats.count.ly/i",
-                                method: "GET",
-                                timeout: 4E3,
-                                qs: {
-                                    device_id: member.email,
-                                    app_key: "386012020c7bf7fcb2f1edf215f1801d6146913f",
-                                    timestamp: Math.floor(date.getTime() / 1000),
-                                    hour: date.getHours(),
-                                    dow: date.getDay(),
-                                    no_meta: true,
-                                    events: JSON.stringify([
-                                        {
-                                            key: "PING",
-                                            count: 1
-                                        }
-                                    ])
-                                }
-                            }, function(a/*, c, b*/) {
-                                log.d('Done running ping job: %j', a);
+                    if (body) {
+                        if (countlyConfigOrig.web.use_intercom && typeof body.intercom !== "undefined") {
+                            countlyConfig.web.use_intercom = body.intercom;
+                        }
+                        if (typeof countlyConfigOrig.web.track === "undefined" && typeof body.stats !== "undefined") {
+                            if (body.stats) {
+                                countlyConfig.web.track = null;
+                            }
+                            else {
+                                countlyConfig.web.track = "none";
+                            }
+                        }
+                    }
+                    log.d(err, body, countlyConfigOrig, countlyConfig);
+                    if (countlyConfig.web.track !== "none") {
+                        db.collection("members").findOne({global_admin: true}, function(err2, member) {
+                            if (!err2 && member) {
+                                var date = new Date();
+                                request({
+                                    uri: "https://stats.count.ly/i",
+                                    method: "GET",
+                                    timeout: 4E3,
+                                    qs: {
+                                        device_id: member.email,
+                                        app_key: "386012020c7bf7fcb2f1edf215f1801d6146913f",
+                                        timestamp: Math.floor(date.getTime() / 1000),
+                                        hour: date.getHours(),
+                                        dow: date.getDay(),
+                                        no_meta: true,
+                                        events: JSON.stringify([
+                                            {
+                                                key: "PING",
+                                                count: 1
+                                            }
+                                        ])
+                                    }
+                                }, function(a/*, c, b*/) {
+                                    log.d('Done running ping job: %j', a);
+                                    done();
+                                });
+                            }
+                            else {
                                 done();
-                            });
-                        }
-                        else {
-                            done();
-                        }
-                    });
-                }
-                else {
-                    done();
-                }
-            });
-        }
+                            }
+                        });
+                    }
+                    else {
+                        done();
+                    }
+                });
+            }
+        });
     }
 }
 
