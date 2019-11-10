cdd38af5478658f28d6d22deb9377d629c75ed7e
Mert Koseoglu <info@mkoseoglu.com>
2019-11-10 17:56:00 +0300

    [SERVER-1625] some improments and changed display condition

--- api/jobs/topEvents.js
+++ api/jobs/topEvents.js
@@ -10,8 +10,64 @@ const countlyApi = {
 };
 const countlyEvents = countlyApi.data.lib.load("event");
 const countlyCommon = countlyApi.data.common;
+
 /** Class for job of top events widget **/
 class TopEventsJob extends job.Job {
+
+    /**
+     * Events collections.
+     * @param {string} eventName - event name.
+     * @param {ObjectId} appId - app id.
+     * @return {string} event collection.
+     */
+    eventsCollentions(eventName, appId) {
+        return "events" + crypto.createHash("sha1").update(eventName + appId).digest("hex");
+    }
+
+    /**
+     * If the event's name include [CLY], removed them.
+     * @param {array} eventsData - events list.
+     * @return {array} filtered data.
+     */
+    eventsFilter(eventsData) {
+        return eventsData.filter(l => !l.includes('[CLY]'));
+    }
+
+    /**
+     * Get events count.
+     * @param {string} colName - event collection name.
+     * @param {Object} ob - it contains all necessary info.
+     * @param {Object} data - dummy event data.
+     * @param {string} event - event name.
+     * @param {function} doneCallback - callback.
+     * @returns {Object} topEvents data.
+     */
+    getEventsCount(colName, ob, data, event, doneCallback) {
+        return countlyApi.data.fetch.getTimeObjForEvents(colName, ob, (doc)=> {
+            countlyEvents.setDb(doc || {});
+            const countProp = countlyEvents.getNumber("c");
+            data[event] = {};
+            data[event].data = {
+                count: countProp
+            };
+            doneCallback();
+        });
+    }
+
+    /**
+     * Encode event name.
+     * @param {Object} data - event data.
+     * @return {Object} encoded data.
+     */
+    encodeEvents(data) {
+        const _data = {};
+        Object.keys(data).forEach(key => {
+            const encodeKey = countlyCommon.encode(key);
+            _data[encodeKey] = data[key];
+        });
+        return _data;
+    }
+
     /**
      * Run the job
      * @param {Db} db connection
@@ -23,63 +79,32 @@ class TopEventsJob extends job.Job {
                 const {_id, timezone} = givenApps;
                 db.collection("events").findOne({_id}, (errorEvents, result) =>{
                     if (result && 'list' in result) {
-                        result.list = result.list.filter(l => !l.includes('[CLY]'));
-                        const eventMap = [];
-                        for (const mapKey of result.list) {
-                            if (result.map) {
-                                if (typeof result.map[mapKey] === "undefined") {
-                                    result.map[mapKey] = true;
-                                }
-                                if (typeof result.map[mapKey] !== "object" && result.map[mapKey]) {
-                                    eventMap.push(mapKey);
-                                }
-                            }
-                            else {
-                                eventMap.push(mapKey);
-                            }
-                        }
-                        if (eventMap instanceof Array) {
-                            const collectionName = "top_events";
-                            db.collection(collectionName).remove({app_id: _id});
-                            const arrPeriod = ["30days", "hour"];
-                            async.eachSeries(arrPeriod, (givenPeriod, eachCallback) => {
+                        const eventMap = this.eventsFilter(result.list);
+                        if (eventMap && eventMap instanceof Array && eventMap.length >= TopEventsJob.TOTAL_EVENT_COUNT) {
+                            db.collection(TopEventsJob.COLLENTION_NAME).remove({app_id: _id});
+                            async.eachSeries(TopEventsJob.PERIODS, (givenPeriod, eachCallback) => {
                                 const data = {};
-                                const ob = {
-                                    app_id: _id,
-                                    appTimezone: timezone,
-                                    qstring: {
-                                        period: givenPeriod
-                                    }
-                                };
+                                const ob = { app_id: _id, appTimezone: timezone, qstring: { period: givenPeriod } };
                                 async.each(eventMap, (event, doneEventMap) => {
-                                    const collectionNameEvents = "events" + crypto.createHash("sha1").update(event + _id).digest("hex");
-                                    countlyApi.data.fetch.getTimeObjForEvents(collectionNameEvents, ob, (doc)=> {
-                                        countlyEvents.setDb(doc || {});
-                                        const countProp = countlyEvents.getNumber("c");
-                                        data[event] = {};
-                                        data[event].data = {
-                                            count: countProp
-                                        };
-                                        doneEventMap();
-                                    });
+                                    const collectionNameEvents = this.eventsCollentions(event, _id);
+                                    this.getEventsCount(collectionNameEvents, ob, data, event, doneEventMap);
                                 }, () => {
-                                    const _data = {};
-                                    Object.keys(data).forEach(key => {
-                                        const encodeKey = countlyCommon.encode(key);
-                                        _data[encodeKey] = data[key];
-                                    });
+                                    const encodedData = this.encodeEvents(data);
                                     const ts = Math.round(new Date().getTime() / 1000);
                                     const period = givenPeriod === "hour" ? "today" : givenPeriod;
-                                    db.collection(collectionName).insert({app_id: _id, ts, period, data: _data}, (error, records) => {
+                                    db.collection(TopEventsJob.COLLENTION_NAME).insert({app_id: _id, ts, period, data: encodedData}, (error, records) => {
                                         if (!error && records) {
                                             eachCallback();
                                         }
                                     });
                                 });
-
                             }, ()=>{
                                 resultEachCallback();
                             });
+                        }
+                        else {
+                            db.collection(TopEventsJob.COLLENTION_NAME).remove({app_id: _id});
+                            resultEachCallback();
                         }
                     }
                 });
@@ -88,5 +113,23 @@ class TopEventsJob extends job.Job {
         done();
     }
 }
+
+/**
+ * The total count of events for calculating TopEvents widget must be 5 or more.
+ * @property {number}
+ */
+TopEventsJob.TOTAL_EVENT_COUNT = 5;
+
+/**
+ * TopEvents collection name.
+ * @property {string}
+ */
+TopEventsJob.COLLENTION_NAME = "top_events";
+
+/**
+ * Events periods.
+ * @property {array} ["30days", "hour", "month", "60days", "yesterday", "7days"]
+ */
+TopEventsJob.PERIODS = ["30days", "hour"];
 
 module.exports = TopEventsJob;
