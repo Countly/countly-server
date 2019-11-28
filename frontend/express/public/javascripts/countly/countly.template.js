47f54a6b9f55d282767958ddbdd106c29b7aa953
Furkan Başaran <frknbasaran@gmail.com>
2019-11-28 14:34:04 +0300

    [server-1570] review changes.

--- frontend/express/public/javascripts/countly/countly.template.js
+++ frontend/express/public/javascripts/countly/countly.template.js
@@ -585,25 +585,21 @@ $.expr[":"].contains = $.expr.createPseudo(function(arg) {
 * @global
 */
 function setMenuItems() {
-    // hide empty section headers for types listed below
-    var types = ['web', 'mobile', 'desktop'], categories;
-
-    for (var i = 0; i < types.length; i++) {
-        categories = $('#' + types[i] + '-type .menu-category');
-        for (var j = 0; j < categories.length; j++) {
-            var children = categories[j].children;
-            var isEmpty = true;
-            for (var k = 0; k < children.length; k++) {
-                if (children[k].className.indexOf('restrict') === -1 && children[k].className.indexOf('item') !== -1) {
-                    isEmpty = false;
-                }
+    // hide empty section headers
+    var categories = $('#' + countlyGlobal.apps[countlyCommon.ACTIVE_APP_ID].type + '-type .menu-category');
+    for (var j = 0; j < categories.length; j++) {
+        var children = categories[j].children;
+        var isEmpty = true;
+        for (var k = 0; k < children.length; k++) {
+            if (children[k].className.indexOf('restrict') === -1 && children[k].className.indexOf('item') !== -1) {
+                isEmpty = false;
             }
-            if (isEmpty) {
-                $(categories[j]).hide();
-            }
-            else {
-                $(categories[j]).show();
-            }
+        }
+        if (isEmpty) {
+            $(categories[j]).hide();
+        }
+        else {
+            $(categories[j]).show();
         }
     }
 }
