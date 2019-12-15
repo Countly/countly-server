54bd4b05dfc0bd147d29a4c7b7399f770f0a2cdc
Cookiezaurs <sosina.anna@gmail.com>
2019-12-15 18:38:59 +0200

    upgrade scipt bugfix

--- bin/upgrade/dev/scripts/upgradeReports.js
+++ bin/upgrade/dev/scripts/upgradeReports.js
@@ -4,7 +4,13 @@ var pluginManager = require('../../../../plugins/pluginManager.js'),
     Promise = require("bluebird"),
     countlyDb = pluginManager.dbConnection();
 
-countlyDb.collection('long_tasks').update({autoRefresh: true,type:"formulas"},{$set:{"period_desc":"732days",taskgroup:true}},{multi:true},function (err,res){
+countlyDb.collection('long_tasks').update({autoRefresh: true,type:"formulas", taskgtoup:{$exists:false},subtask:{$exists:false}},{$set:{"period_desc":"732days",taskgroup:true}},{multi:true},function (err,res){
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log(res.result);
+    }
     countlyDb.collection('long_tasks').find({autoRefresh: true,type:"drill"}).toArray(function(err, tasks) {
         Promise.each(tasks, function(task) {
             return new Promise(function(resolve, reject) {
@@ -16,19 +22,25 @@ countlyDb.collection('long_tasks').update({autoRefresh: true,type:"formulas"},{$
                         request.json.period_desc = "732days";
                         request.json.bucket = "";
                         request.method = "createReport";
-                        countlyDb.collection("long_tasks").update({_id: task.id}, {
+                        countlyDb.collection("long_tasks").update({_id: task._id}, {
                             $set: {
                                 subtasks:{},
                                 data: {},
                                 taskgroup:true,
                                 period_desc:"732days",
-                                request:JSON.encode(request)
+                                request:JSON.stringify(request)
                             }
                         }, {'upsert': false}, function(err,res){
+                            if(err) {
+                                console.log(err);
+                            }
+                            console.log("Report "+task._id +": upgraded");
                             resolve();
                         });
                     }
-                    resolve();
+                    else {
+                        resolve();
+                    }
                 }
                 else {
                     resolve();
