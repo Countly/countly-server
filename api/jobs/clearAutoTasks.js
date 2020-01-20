89ee9a0b58c3eb18769b938e9f294ed925d93df7
Waiter Zen <waiter.james1991@gmail.com>
2020-01-20 19:15:01 +0800

    [jobs] add job for clearing old auto created tasks.

---
+++ api/jobs/clearAutoTasks.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const job = require("../parts/jobs/job.js");
+const log = require('../utils/log.js')('job:clearAutoTasks');
+
+/** Class for job of clearing auto tasks created long time ago **/
+class ClearAutoTasks extends job.Job {
+    /**
+     * Run the job
+     * @param {Db} db connection
+     * @param {done} done callback
+     */
+    run(db, done) {
+        const beforeTime = new Date().getTime() - 1000 * 60 * 60 * 24 * 90;  //90 days ago
+        const query = {
+            manually_create: false,
+            ts: { $lt: beforeTime }
+        };
+        db.collection("long_tasks").remove(query, function(error) {
+            if (error) {
+                log.e("Error deleting auto tasks.");
+            }
+            done();
+        });
+    }
+}
+
+module.exports = ClearAutoTasks;
