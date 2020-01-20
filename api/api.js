fcb9d7a622f0920588972ed6f4699d20b36a5434
Waiter Zen <waiter.james1991@gmail.com>
2020-01-20 19:15:01 +0800

    [jobs] add job for clearing old auto created tasks.

--- api/api.js
+++ api/api.js
@@ -243,6 +243,7 @@ if (cluster.isMaster) {
         jobs.job('api:ping').replace().schedule('every 1 day');
         jobs.job('api:clear').replace().schedule('every 1 day');
         jobs.job('api:clearTokens').replace().schedule('every 1 day');
+        jobs.job('api:clearAutoTasks').replace().schedule('every 1 day');
         jobs.job('api:task').replace().schedule('every 5 minutes');
         jobs.job('api:userMerge').replace().schedule('every 1 hour on the 10th min');
     }, 10000);
