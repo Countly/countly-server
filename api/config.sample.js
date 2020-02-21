a1dc07a2425d2c9171c26858c087d686d6f6180e
Artem <a@ahoy.li>
2020-02-21 17:15:17 +0300

    [core] Unused configuration option

--- api/config.sample.js
+++ api/config.sample.js
@@ -151,7 +151,4 @@ var countlyConfig = {
     */
 };
 
-// Set your host IP or domain to be used in the emails sent
-// countlyConfig.host = "YOUR_IP_OR_DOMAIN";
-
 module.exports = require('./configextender')('API', countlyConfig, process.env);
