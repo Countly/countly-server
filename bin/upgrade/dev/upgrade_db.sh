c871ec969f8cda8eddb7f50a7a7b45a6c49fcd7e
Arturs Sosins <ar2rsawseen@gmail.com>
2020-03-14 08:13:21 +0200

    [upgrade] delete old drill meta

--- bin/upgrade/dev/upgrade_db.sh
+++ bin/upgrade/dev/upgrade_db.sh
@@ -16,6 +16,7 @@ nodejs "$CUR/scripts/encrypt_2fa_secrets.js"
 nodejs "$CUR/scripts/set_additional_api_configs.js"
 nodejs "$CUR/scripts/clearOldTokens.js"
 nodejs "$CUR/scripts/remove_drill_index.js"
+nodejs "$CUR../18.01/scripts/delete_drill_meta.js"
 
 #add indexes
 nodejs "$DIR/scripts/add_indexes.js"
