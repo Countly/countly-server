9dfca2a9a75c67e83f8991a5bece061e5afe50f0
Travis CI <travis@travis-ci.org>
2020-03-14 11:03:51 +0000

    Deploy code docs to GitHub Pages Travis build: 6161
    Commit: c7a890c969246d65135f3d212153f7a81a7b6f95

--- browser/scripts/collapse.js
+++ browser/scripts/collapse.js
@@ -1,11 +1,20 @@
 function hideAllButCurrent(){
     //by default all submenut items are hidden
-    $("nav > ul > li > ul li").hide();
+    //but we need to rehide them for search
+    document.querySelectorAll("nav > ul > li > ul li").forEach(function(parent) {
+        parent.style.display = "none";
+    });
     
     //only current page (if it exists) should be opened
-    var file = window.location.pathname.split("/").pop();
-    $("nav > ul > li > a[href^='"+file+"']").parent().find("> ul li").show();
+    var file = window.location.pathname.split("/").pop().replace(/\.html/, '');
+    document.querySelectorAll("nav > ul > li > a").forEach(function(parent) {
+        var href = parent.attributes.href.value.replace(/\.html/, '');
+        if (file === href) {
+            parent.parentNode.querySelectorAll("ul li").forEach(function(elem) {
+                elem.style.display = "block";
+            });
+        }
+    });
 }
-$( document ).ready(function() {
-    hideAllButCurrent();
-});
+
+hideAllButCurrent();
