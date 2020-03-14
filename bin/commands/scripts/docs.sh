0c01b580238b44d8be511d426f409d5920403ddb
Arturs Sosins <ar2rsawseen@gmail.com>
2020-03-14 11:49:07 +0200

    [docs] add redirect file after folder is created

--- bin/commands/scripts/docs.sh
+++ bin/commands/scripts/docs.sh
@@ -10,8 +10,9 @@ if [ -z "$1" ]
 then
     usage ;
 elif [ "$1" = "generate" ]; then
-    echo "<html><head><meta http-equiv='Refresh' content='0; url=./api/index.html'/><script type='javascript'>window.location = './api/index,html';</script></head></html>" > "$DIR/../../../frontend/express/public/docs/index.html"
     "$DIR/../../../node_modules/.bin/jsdoc" "$DIR/../../../frontend/express/public/javascripts/countly" -R "$DIR/../../../frontend/express/public/javascripts/pre-login.js" -R "$DIR/../../../README.md" -c  "$DIR/../../../jsdoc_conf.json" -d "$DIR/../../../frontend/express/public/docs/browser" ;
     "$DIR/../../../node_modules/.bin/jsdoc" "$DIR/../../../api" -R "$DIR/../../../README.md" -c  "$DIR/../../../jsdoc_conf.json" -d  "$DIR/../../../frontend/express/public/docs/api" ;
     "$DIR/../../../node_modules/.bin/jsdoc" "$DIR/../../../frontend/express/app.js" "$DIR/../../../frontend/express/config.sample.js" "$DIR/../../../frontend/express/version.info.js" "$DIR/../../../frontend/express/locale.conf.js" "$DIR/../../../frontend/express/libs/" -R "$DIR/../../../README.md" -c  "$DIR/../../../jsdoc_conf.json" -d  "$DIR/../../../frontend/express/public/docs/app" ;
+    #add redirect for main folder
+    echo "<html><head><meta http-equiv='Refresh' content='0; url=./api/index.html'/><script type='javascript'>window.location = './api/index,html';</script></head></html>" > "$DIR/../../../frontend/express/public/docs/index.html"
 fi
