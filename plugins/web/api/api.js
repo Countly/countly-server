37487b56025e3c99fa523771f0db9e32b25a228e
Arturs Sosins <ar2rsawseen@gmail.com>
2020-03-15 22:40:25 +0200

    [web] populate field for feedback event

--- plugins/web/api/api.js
+++ plugins/web/api/api.js
@@ -78,6 +78,9 @@ var pluginOb = {},
                 if (currEvent.key === "[CLY]_view" && currEvent.segmentation && currEvent.segmentation.name && (!currEvent.segmentation.segment && !currEvent.segmentation.platform)) {
                     currEvent.segmentation.segment = data.os;
                 }
+                else if (currEvent.key === "[CLY]_star_rating" && currEvent.segmentation && !currEvent.segmentation.platform) {
+                    currEvent.segmentation.platform = data.os;
+                }
                 return currEvent;
             });
         }
