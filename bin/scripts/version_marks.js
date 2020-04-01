26ceed2a5c36d8db25ca77ffd62f854c4ea5a79c
Vedat Can Keklik <vcankeklik@gmail.com>
2020-04-01 19:03:36 +0300

    [SCRIPTS][SERVER-1603] Version marks db set latest

--- bin/scripts/version_marks.js
+++ bin/scripts/version_marks.js
@@ -1,4 +1,7 @@
-const fs = require('fs');
+const fs = require('fs'),
+    pluginManager = require('../../plugins/pluginManager.js'),
+    countlyDb = pluginManager.dbConnection();
+
 const fsMarkedVersionPath = __dirname + "/../../countly_marked_version.json";
 
 function writeMsg(type, msg){
@@ -38,11 +41,58 @@ function doMarkFsVersion(targetVersion) {
         });
         try {
             fs.writeFileSync(fsMarkedVersionPath, JSON.stringify(olderVersions));
+            writeMsg("info", "1");
         }
         catch (error) {
             writeMsg("error", error);
         }
     }
+    else {
+        writeMsg("info", "0");
+    }
+}
+
+function doMarkDbVersion(targetVersion) {
+    countlyDb.collection('plugins').find({'_id':'version'}).toArray(function(err, versionDocs) {
+        if (err) {
+            writeMsg("error", err);
+            countlyDb.close();
+            return;
+        }
+        var versionDoc = {};
+        if (!versionDocs[0]) {
+            versionDoc.history = [];
+        }
+        else {
+            versionDoc = versionDocs[0];
+        }
+        var lastVersion = "",
+            olderVersions = versionDoc.history;
+
+        if (olderVersions.length > 0) {
+            lastVersion = olderVersions[olderVersions.length - 1].version
+        }
+
+        if (lastVersion === "" || lastVersion !== targetVersion) {
+            versionDoc.history.push({
+                version: targetVersion,
+                updated: Date.now()
+            });
+            countlyDb.collection('plugins').update({'_id': 'version'}, {
+                $set: {
+                    "history": versionDoc.history,
+                    "current": targetVersion
+                }
+            }, {'upsert': true}, function() {
+                writeMsg("info", "1");
+                countlyDb.close();
+            });
+        }
+        else {
+            writeMsg("info", "0");
+            countlyDb.close();
+        }
+    });
 }
 
 var myArgs = process.argv.slice(2),
@@ -54,5 +104,8 @@ if (myArgs.length === 2) {
     targetVersion = myArgs[1];
     if (namespace === 'fs') {
         doMarkFsVersion(targetVersion);
+    }
+    else if (namespace === 'db') {
+        doMarkDbVersion(targetVersion);
     }
 }
