a3b977a20f316c084dde35532437c910f6dabbdc
Vedat Can Keklik <vcankeklik@gmail.com>
2020-04-18 11:56:14 +0300

    [SCRIPTS] Script for app_nxret fix

---
+++ bin/upgrade/dev/scripts/fix_nxret.js
@@ -0,0 +1,108 @@
+var pluginManager = require('../../../../plugins/pluginManager.js'),
+    asyncjs = require('async'),
+    countlyDb = pluginManager.dbConnection();
+
+function printMessage(messageType, ...message) {
+    console[messageType]('[nxret._id <- nxret.uid][' + messageType + '] ', ...message);
+}
+
+printMessage("log", "Started...");
+
+countlyDb.collection('apps').find({}).toArray(function(appsErr, apps) {
+
+    if (!appsErr && apps) {
+        asyncjs.eachSeries(apps, upgrade, function() {
+            printMessage("log", "Finished.");
+            countlyDb.close();
+        });
+    }
+    else {
+        printMessage("error", "Error at app fetch. Stopped. ", appsErr);
+        countlyDb.close();
+    }
+
+    function upgrade(app, done) {
+
+        printMessage("log", "(" + app.name + ") Fixing...");
+        var cursor = countlyDb.collection('app_nxret' + app._id).find({$expr: {$ne: ["$_id", "$uid"] } });
+        var requests = [];
+        var bulkWritePromises = [];
+
+        cursor.forEach(function(nxret) {
+            var oldId = nxret._id;
+            nxret._id = nxret.uid;
+            requests.push({
+                insertOne: { "document": nxret }
+            });
+            requests.push({
+                deleteOne: { "filter": { _id: oldId } }
+            });
+            if (requests.length === 1000) {
+                bulkWritePromises.push(getBulkWritePromise(requests));
+                requests = [];
+            }
+        }, function(err) {
+            if (err) {
+                printMessage("error", "cursor.forEach stopped execution: ", err);
+            }
+            if (requests.length > 0) {
+                bulkWritePromises.push(getBulkWritePromise(requests));
+                requests = [];
+            }
+            Promise.all(bulkWritePromises).catch(finalizeApp).then(finalizeApp);
+        });
+
+        function getBulkWritePromise(reqArr) {
+            return new Promise(function(resolve/*, reject*/) {
+                countlyDb.collection('app_nxret' + app._id).bulkWrite(reqArr, function(err, response) {
+                    if (err) {
+                        printMessage("error", "Bulk write completed with errors: ", err);
+                    }
+                    if (response && response.result) {
+                        resolve({err: err, result: response.result});
+                    }
+                    else {
+                        resolve({err: err, result: null});
+                    }
+                });
+            }).catch(function(err) {
+                printMessage("error", "(" + app.name + ")", "error in Promise:", err);
+                return Promise.resolve({err: err, result: null});
+            });
+        }
+
+        function finalizeApp(responses) {
+            var hasError = false;
+            if (Array.isArray(responses)) {
+                var nInserted = 0;
+                var nRemoved = 0;
+                responses.forEach(function(response) {
+                    if (!response) {
+                        return;
+                    }
+                    hasError = hasError || response.err;
+                    if (response.result) {
+                        if (response.result.nInserted) {
+                            nInserted += response.result.nInserted;
+                        }
+                        if (response.result.nRemoved) {
+                            nRemoved += response.result.nRemoved;
+                        }
+                    }
+                });
+            }
+            else {
+                hasError = true;
+            }
+            if (hasError) {
+                printMessage("error", "----------------------------------------------");
+                printMessage("error", "(" + app.name + ")", "ERRORS, see previous", "\n");
+            }
+            else {
+                printMessage("log", "(" + app.name + ")", "/", "nInserted =", nInserted, "/", "nRemoved =", nRemoved);
+                printMessage("log", "(" + app.name + ")", "Successful.", "\n");
+            }
+            return done();
+        }
+    }
+});
