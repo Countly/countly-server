e8f4cef92201ec534a7209e47b58bf1ae9db7d49
Artem <a@ahoy.li>
2020-04-28 11:32:59 +0300

    [docker] Unsafe-perm everywhere

--- bin/docker/preinstall.sh
+++ bin/docker/preinstall.sh
@@ -5,7 +5,7 @@ source /etc/os-release
 while IFS= read -r -d '' plugin
 do
     echo "Installing $plugin..."
-	(cd "$plugin" && HOME=/tmp npm install --unsafe-perm)
+	(cd "$plugin" && HOME=/tmp npm install --unsafe-perm=true --allow-root)
 	echo "done"
 done <   <(find /opt/countly/plugins -mindepth 1 -maxdepth 1 -type d -print0)
 
