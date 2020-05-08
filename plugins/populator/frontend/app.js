9e6394948e85a95929b791550a808c8804d0224c
Furkan Başaran <frknbasaran@gmail.com>
2020-05-08 23:27:52 +0300

    [heatmap-pages] covered case that for custom populator.
    - moved assets into populator plugin.

--- plugins/populator/frontend/app.js
+++ plugins/populator/frontend/app.js
@@ -5,8 +5,12 @@ var exported = {},
 
 (function(plugin) {
     plugin.init = function(app) {
+        const predefinedTypes = ["demo-banking.html", "demo-gaming.html", "demo-navigation.html", "demo-healthcare.html", "demo-ecommerce.html"];
         app.get(countlyConfig.path + '/:id/:page', function(req, res) {
-            var url = 'public/templates/' + req.params.page;
+            let url = 'public/templates/demo-page.html';
+            if (predefinedTypes.includes(req.params.page)) {
+                url = 'public/templates/' + req.params.page;
+            }
             var pageSourceCode = fs.readFileSync(path.resolve(__dirname, url));
             res.writeHead(200, { 'Content-Type': 'text/html'});
             res.end(pageSourceCode);
