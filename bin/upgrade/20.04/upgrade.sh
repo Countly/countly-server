c907ebe0964e503767fd5e5913fba24271cb5ff3
Glenn L. Austin <glenn@austinsoft.com>
2020-05-23 12:02:12 -0700

    Remove file transfer from old to new Countly to keep PR only to Mac-specific changes.

--- bin/upgrade/20.04/upgrade.sh
+++ bin/upgrade/20.04/upgrade.sh
@@ -1,38 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-NEW_COUNTLY_DIR="$( cd "$DIR/../../.." && pwd )"
 DATE=$(date +%Y-%m-%d:%H:%M:%S)
 VERSION="$(basename "${DIR}")"
-
-# Get the paths to the old and new installations
-if [ $# -gt 0 -a -d "$1"/bin/commands ]
-then
-    OLD_COUNTLY_DIR="$1"
-    OLD_COUNTLY="$(OLD_COUNTLY_DIR)/bin/commands/countly.sh"
-else
-    OLD_COUNTLY="$(which countly)"
-    if [ -z "$OLD_COUNTLY" ]
-    then
-        echo "The countly command-line tool isn't available, you'll need to specify where the previous countly installation was"
-        exit 1
-    fi
-    while [ -L "$OLD_COUNTLY" ]
-    do
-        OLD_COUNTLY="$(readlink ${OLD_COUNTLY})"
-    done
-    # Now that we have the executable file used for the countly command line, find the old installation directory
-    OLD_COUNTLY_DIR="$(dirname ${OLD_COUNTLY})"
-    while [ ! -d "$OLD_COUNTLY_DIR"/api -o ! -d "${OLD_COUNTLY_DIR}"/bin -o ! -d "${OLD_COUNTLY_DIR}"/extend -o ! -d "${OLD_COUNTLY_DIR}"/frontend -o ! -d "${OLD_COUNTLY_DIR}"/plugins ]
-    do
-        if [ "$OLD_COUNTLY_DIR" == "/" ]
-        then
-            # Can't find the old countly installation!
-            echo "We can't find the previous installation, you'll need to specify where the previous countly installation was"
-            exit 1
-        fi
-        OLD_COUNTLY_DIR="$( cd "$OLD_COUNTLY_DIR"/.. && pwd )"
-    done
-fi
 
 countly stop
 if [ -d "$DIR/.nodeenv" ]; then
@@ -44,12 +13,6 @@ if [ -f "$DIR/upgrade_fs.sh" ]; then
 fi
 if [ -f "$DIR/upgrade_db.sh" ]; then
     bash "$DIR/upgrade_db.sh" combined 2>&1 | tee -a "$DIR/../../../log/countly-upgrade-db-$VERSION-$DATE.log"
-fi
-
-# See if there are any user-added files that we should copy over to the new installation
-if [ "$OLD_COUNTLY_DIR" != "$NEW_COUNTLY_DIR" ]
-then
-    rsync -av "${OLD_COUNTLY_DIR}/frontend/express/public/appimages/" "${NEW_COUNTLY_DIR}/frontend/express/public/appimages"
 fi
 
 countly start
