e06ad640a5d1a4613e62a45d804ecdade959adb3
Arturs Sosins <ar2rsawseen@gmail.com>
2020-07-01 20:21:29 +0300

    [assistant] handle error on getting notifications

--- plugins/assistant/api/assistant.js
+++ plugins/assistant/api/assistant.js
@@ -338,7 +338,11 @@ const _ = require('underscore');
      */
     assistant.getAssistantConfig = function(db, callback) {
         db.collection(db_name_config).find({}, {}).toArray(function(err, result) {
-            //log.i('Assistant plugin getAssistantConfig: [%j][%j][%j][%j]', 18, err, result, typeof result);
+            
+            if (err || !result) {
+                log.e('Assistant plugin getAssistantConfig: [%j][%j][%j][%j]', 18, err, result, typeof result);
+                callback([]);
+            }
 
             //optimize config info before returning it
             result.forEach(function(elem) {
