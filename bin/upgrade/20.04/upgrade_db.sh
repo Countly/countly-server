1706d22987617832e952c0a64df78343256c094e
Arturs Sosins <ar2rsawseen@gmail.com>
2020-07-09 16:26:17 +0300

    [scripts] more output on upgrades version marking handling

--- bin/upgrade/20.04/upgrade_db.sh
+++ bin/upgrade/20.04/upgrade_db.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-echo "Running database modifications"
-
 VER="20.04"
 
 CONTINUE="$(countly check before upgrade db "$VER")"
 
+if [ "$CONTINUE" != "1" ] && [ "$1" != "combined" ]
+then
+    echo "Database is already up to date with $VER"
+    read -r -p "Are you sure you want to run this script? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+    then
+        CONTINUE=1
+    fi
+fi
+
 if [ "$CONTINUE" == "1" ]
 then
+    echo "Running database modifications"
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
     CUR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -31,4 +40,12 @@ then
 
     #call after check
     countly check after upgrade db "$VER"
+elif [ "$CONTINUE" == "0" ]
+then
+    echo "Database is already upgraded to $VER"
+elif [ "$CONTINUE" == "-1" ]
+then
+    echo "Database is upgraded to higher version"
+else
+    echo "Unknown ugprade state: $CONTINUE"
 fi
