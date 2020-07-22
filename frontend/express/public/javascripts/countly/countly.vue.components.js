d0476a28ae7a6f86f49a54dea46640ee8136a686
Vedat Can Keklik <vcankeklik@gmail.com>
2020-07-22 11:56:40 +0300

    [extended-datepicker] Moved Vue directive

--- frontend/express/public/javascripts/countly/countly.vue.components.js
+++ frontend/express/public/javascripts/countly/countly.vue.components.js
@@ -1,4 +1,4 @@
-/* global countlyCommon, moment, jQuery */
+/* global countlyCommon, moment, jQuery, Vue */
 (function(CountlyVueComponents, $) {
 
     /**
@@ -348,5 +348,18 @@
         }
     };
 
+    Vue.directive('click-outside', {
+        bind: function(el, binding, vnode) {
+            el.clickOutsideEvent = function(event) {
+                if (!(el === event.target || el.contains(event.target))) {
+                    vnode.context[binding.expression](event);
+                }
+            };
+            document.body.addEventListener('click', el.clickOutsideEvent);
+        },
+        unbind: function(el) {
+            document.body.removeEventListener('click', el.clickOutsideEvent);
+        }
+    });
 
 }(window.CountlyVueComponents = window.CountlyVueComponents || {}, jQuery));
