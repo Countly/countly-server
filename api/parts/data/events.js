48faf675b0b07ac6b08ea0bbb87140ebcaee3a31
Cookiezaurs <sosina.anna@gmail.com>
2020-08-12 11:40:49 +0300

    fix

--- api/parts/data/events.js
+++ api/parts/data/events.js
@@ -453,7 +453,9 @@ function processEvents(appEvents, appSegments, appSgValues, params, omitted_segm
         for (var k = 0; k < eventDocs.length; k++) {
             common.writeBatcher.add(eventDocs[k].collection, eventDocs[k]._id, eventDocs[k].updateObj);
         }
-        common.returnMessage(params, 200, 'Success');
+		if (!params.bulk) {
+			common.returnMessage(params, 200, 'Success');
+		}
 
         /*async.map(eventDocs, updateEventDb, function(err, eventUpdateResults) {
             var needRollback = false;
