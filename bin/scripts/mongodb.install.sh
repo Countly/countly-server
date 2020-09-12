2b22c37b8037e254491d6e15edd1476cf6232d20
Kaan Kölköy <kaanklky@gmail.com>
2020-09-12 05:43:43 +0300

    Set more system configs for MongoDB

--- bin/scripts/mongodb.install.sh
+++ bin/scripts/mongodb.install.sh
@@ -277,7 +277,8 @@ function mongodb_check() {
     update_sysctl "fs.file-max" "392000"
     update_sysctl "kernel.pid_max" "256000"
     update_sysctl "kernel.threads-max" "256000"
-    update_sysctl "vm.max_map_count" "512000"
+    update_sysctl "vm.max_map_count" "2048000"
+    update_sysctl "net.ipv4.ip_local_port_range" "1024 65535"
 
     message_ok "Configured file handle kernel limits"
 
