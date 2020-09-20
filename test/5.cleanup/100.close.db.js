2db6ba4f507dceea53dd430bf12a633ec8ea7777
Arturs Sosins <ar2rsawseen@gmail.com>
2020-09-20 21:59:22 +0300

    [tests] separate open and close db tests

--- test/5.cleanup/100.close.db.js
+++ test/5.cleanup/100.close.db.js
@@ -2,11 +2,7 @@ var plugins = require("../../plugins/pluginManager");
 var should = require('should');
 var testUtils = require("../testUtils");
 
-describe('Create DB connection for tests', function() {
-    before('Open db connection', async function() {
-        testUtils.db = await plugins.dbConnection("countly");
-        testUtils.client = testUtils.db.client;
-    });
+describe('Close DB connection after tests', function() {
     it('Check db Connection', function(done) {
         testUtils.db.collection("plugins").findOne({_id: "plugins"}, function(err, res) {
             if (err) {
@@ -17,5 +13,8 @@ describe('Create DB connection for tests', function() {
                 done();
             }
         });
+    });
+    after('Close db connection', async function() {
+        testUtils.client.close();
     });
 });
