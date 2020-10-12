2dcc8d7fcc576dea1765675b32cdb301748ed7b6
Arturs Sosins <ar2rsawseen@gmail.com>
2020-10-12 12:19:04 +0300

    [eslint] fixes for plugin.js

--- bin/commands/scripts/plugin.js
+++ bin/commands/scripts/plugin.js
@@ -26,16 +26,64 @@ function save_changes(data, finalList) {
     });
 }
 
+function runInstall(installList) {
+    var data = {},
+        pluginsCopy = plugins.slice();
+
+    async.eachSeries(installList, function(toBeInstalled, callback) {
+        manager.installPlugin(toBeInstalled, function(pluginErr) {
+            if (!pluginErr) {
+                data[toBeInstalled] = true;
+                pluginsCopy.push(toBeInstalled);
+                callback();
+            }
+            else {
+                console.log("Could not be enabled:", toBeInstalled);
+                callback(pluginErr);
+            }
+        });
+    }, function(generalErr) {
+        save_changes(data, pluginsCopy);
+        if (generalErr) {
+            console.log("Install completed with errors:", generalErr);
+        }
+    });
+}
+
+function runUninstall(uninstallList) {
+    var data = {},
+        pluginsCopy = plugins.slice();
+
+    async.eachSeries(uninstallList, function(toBeUninstalled, callback) {
+        manager.uninstallPlugin(toBeUninstalled, function(pluginErr) {
+            if (!pluginErr) {
+                data[toBeUninstalled] = false;
+                pluginsCopy.splice(pluginsCopy.indexOf(toBeUninstalled), 1);
+                callback();
+            }
+            else {
+                console.log("Could not be disabled:", toBeUninstalled);
+                callback(pluginErr);
+            }
+        });
+    }, function(generalErr) {
+        save_changes(data, pluginsCopy);
+        if (generalErr) {
+            console.log("Uninstall completed with errors:", generalErr);
+        }
+    });
+}
+
 //check if we have a command
 if (myArgs[0] == "enable" && myArgs[1]) {
-    var pluginName = myArgs[1];
+    let pluginName = myArgs[1];
     if (plugins.indexOf(pluginName) == -1) {
-        var {dpcs} = dependencies.getDependencies(plugins.concat(pluginName), {
+        let {dpcs} = dependencies.getDependencies(plugins.concat(pluginName), {
             discoveryStrategy: "enableParents",
             env: "cli"
         });
 
-        if (!dpcs[pluginName]){
+        if (!dpcs[pluginName]) {
             console.log("Plugin not found.");
             return;
         }
@@ -48,35 +96,11 @@ if (myArgs[0] == "enable" && myArgs[1]) {
             }
         });
 
-        function runInstall(installList){
-            var data = {},
-                pluginsCopy = plugins.slice();
-
-            async.eachSeries(installList, function(toBeInstalled, callback) {
-                manager.installPlugin(toBeInstalled, function(pluginErr) {
-                    if (!pluginErr) {
-                        data[toBeInstalled] = true;
-                        pluginsCopy.push(toBeInstalled);
-                        callback();
-                    }
-                    else {
-                        console.log("Could not be enabled:", toBeInstalled);
-                        callback(pluginErr);
-                    }
-                });
-            }, function(generalErr) {
-                save_changes(data, pluginsCopy);
-                if (generalErr) {
-                    console.log("Install completed with errors:", generalErr);
-                }
-            });
-        }
-
         if (ancestorsNeedToBeEnabled.length > 0) {
-            var message = `> Enabling '${pluginName}' will cause depended plugin(s) (${ancestorsNeedToBeEnabled.join(', ')}) to be enabled as well.`;
+            let message = `> Enabling '${pluginName}' will cause depended plugin(s) (${ancestorsNeedToBeEnabled.join(', ')}) to be enabled as well.`;
             message += "\n> Do you want to continue (y/N)? ";
 
-            var rl = readline.createInterface({
+            let rl = readline.createInterface({
                 input: process.stdin,
                 output: process.stdout
             });
@@ -90,8 +114,8 @@ if (myArgs[0] == "enable" && myArgs[1]) {
                     console.log("Install aborted.");
                 }
                 rl.close();
-            });         
-                
+            });
+
         }
         else {
             runInstall([pluginName]);
@@ -103,14 +127,14 @@ if (myArgs[0] == "enable" && myArgs[1]) {
     }
 }
 else if (myArgs[0] == "disable" && myArgs[1]) {
-    var pluginName = myArgs[1];
+    let pluginName = myArgs[1];
     if (plugins.indexOf(pluginName) > -1) {
-        var {dpcs} = dependencies.getDependencies(plugins.concat(pluginName), {
+        let {dpcs} = dependencies.getDependencies(plugins.concat(pluginName), {
             discoveryStrategy: "enableParents",
             env: "cli"
         });
 
-        if (!dpcs[pluginName]){
+        if (!dpcs[pluginName]) {
             console.log("Plugin not found.");
             return;
         }
@@ -123,35 +147,11 @@ else if (myArgs[0] == "disable" && myArgs[1]) {
             }
         });
 
-        function runUninstall(uninstallList) {
-            var data = {},
-                pluginsCopy = plugins.slice();
-
-            async.eachSeries(uninstallList, function(toBeUninstalled, callback) {
-                manager.uninstallPlugin(toBeUninstalled, function(pluginErr) {
-                    if (!pluginErr) {
-                        data[toBeUninstalled] = false;
-                        pluginsCopy.splice(pluginsCopy.indexOf(toBeUninstalled), 1);
-                        callback();
-                    }
-                    else {
-                        console.log("Could not be disabled:", toBeUninstalled);
-                        callback(pluginErr);
-                    }
-                });
-            }, function(generalErr) {
-                save_changes(data, pluginsCopy);
-                if (generalErr) {
-                    console.log("Uninstall completed with errors:", generalErr);
-                }
-            });
-        }
-
         if (descendantsNeedToBeDisabled.length > 0) {
-            var message = `> Disabling '${pluginName}' will cause dependent plugin(s) (${descendantsNeedToBeDisabled.join(', ')}) to be disabled as well.`;
+            let message = `> Disabling '${pluginName}' will cause dependent plugin(s) (${descendantsNeedToBeDisabled.join(', ')}) to be disabled as well.`;
             message += "\n> Do you want to continue (y/N)? ";
 
-            var rl = readline.createInterface({
+            let rl = readline.createInterface({
                 input: process.stdin,
                 output: process.stdout
             });
@@ -165,7 +165,7 @@ else if (myArgs[0] == "disable" && myArgs[1]) {
                     console.log("Uninstall aborted.");
                 }
                 rl.close();
-            });            
+            });
         }
         else {
             runUninstall([pluginName]);
