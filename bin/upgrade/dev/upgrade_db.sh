cb3e299678faa2c77c93220103bb6d70d9dd0de2
Arturs Sosins <ar2rsawseen@gmail.com>
2020-10-16 09:04:49 +0300

    [upgrade] add consolidate plugin upgrade for users who were already using it

--- bin/upgrade/dev/upgrade_db.sh
+++ bin/upgrade/dev/upgrade_db.sh
@@ -23,6 +23,7 @@ then
     if [ "$1" != "combined" ]; then
         #upgrade plugins
         countly plugin upgrade star-rating
+        countly plugin upgrade consolidate
         countly plugin upgrade users
     fi
 
