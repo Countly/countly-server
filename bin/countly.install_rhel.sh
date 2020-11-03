c8b4b1c713273ee637651869922e69678021cb8b
Artem <a@ahoy.li>
2020-11-03 17:02:13 +0300

    [core] Removing openssl & gcc 4.8 from install scripts

--- bin/countly.install_rhel.sh
+++ bin/countly.install_rhel.sh
@@ -11,7 +11,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bash "$DIR/scripts/logo.sh";
 
 #install nginx
-yum -y install wget openssl-devel gcc-c++-4.8.5 make git sqlite unzip bzip2
+yum -y install wget openssl-devel gcc-c++ make git sqlite unzip bzip2
 
 if grep -q -i "release 6" /etc/redhat-release ; then
 	echo "[nginx]
@@ -27,6 +27,13 @@ baseurl=http://nginx.org/packages/rhel/7/x86_64/
 gpgcheck=0
 enabled=1" > /etc/yum.repos.d/nginx.repo
     yum -y install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc
+elif grep -q -i "release 8" /etc/redhat-release ; then
+	echo "[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/rhel/8/x86_64/
+gpgcheck=0
+enabled=1" > /etc/yum.repos.d/nginx.repo
+    yum -y install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc
 else
     echo "Unsupported OS version, only support RHEL/Centos 7 and 6"
     exit 1
