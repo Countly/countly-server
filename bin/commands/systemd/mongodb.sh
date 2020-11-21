81cde27f77a80ea8d301b09d4ac28c213d900db6
Kaan Kölköy <kaanklky@gmail.com>
2020-11-21 14:23:45 +0300

    Use sed to change line instead of deleting it first

--- bin/commands/systemd/mongodb.sh
+++ bin/commands/systemd/mongodb.sh
@@ -4,14 +4,10 @@ NUMACTL_STATUS=0
 
 SERVICE_FILE_PATH=$(systemctl status mongod | grep "loaded" | awk -F';' '{print $1}' | awk -F'(' '{print $2}')
 
-if grep -q "ExecStart=" "${SERVICE_FILE_PATH}"; then
-    sed -i "/ExecStart=/d" "${SERVICE_FILE_PATH}"
-fi
-
 if [[ $NUMACTL_STATUS -eq 0 ]]; then
-    echo "/usr/bin/mongod --quiet --config /etc/mongod.conf" >> "${SERVICE_FILE_PATH}"
+    sed -i "/ExecStart=/c\ExecStart=\/usr\/bin\/mongod --quiet --config \/etc\/mongod.conf" "${SERVICE_FILE_PATH}"
 else
-    echo "/usr/bin/numactl --interleave=all /usr/bin/mongod --quiet --config /etc/mongod.conf" >> "${SERVICE_FILE_PATH}"
+    sed -i "/ExecStart=/c\ExecStart=\/usr\/bin\/numactl --interleave=all \/usr\/bin/mongod --quiet --config \/etc\/mongod.conf" "${SERVICE_FILE_PATH}"
 fi
 
 systemctl daemon-reload
