83090bd47b14d46679ae54f5b037829eab1897dc
Kaan Kölköy <kaanklky@gmail.com>
2020-11-21 13:34:46 +0300

    Remove countly dir dependency from mongod.service

--- bin/commands/systemd/mongodb.sh
+++ bin/commands/systemd/mongodb.sh
@@ -2,8 +2,16 @@
 
 NUMACTL_STATUS=0
 
-if [[ $NUMACTL_STATUS -eq 0 ]]; then
-    /usr/bin/mongod --quiet --config /etc/mongod.conf
-else
-    /usr/bin/numactl --interleave=all /usr/bin/mongod --quiet --config /etc/mongod.conf
+SERVICE_FILE_PATH=$(systemctl status mongod | grep "loaded" | awk -F';' '{print $1}' | awk -F'(' '{print $2}')
+
+if grep -q "ExecStart=" "${SERVICE_FILE_PATH}"; then
+    sed -i "/ExecStart=/d" "${SERVICE_FILE_PATH}"
 fi
+
+if [[ $NUMACTL_STATUS -eq 0 ]]; then
+    echo "/usr/bin/mongod --quiet --config /etc/mongod.conf" >> "${SERVICE_FILE_PATH}"
+else
+    echo "/usr/bin/numactl --interleave=all /usr/bin/mongod --quiet --config /etc/mongod.conf" >> "${SERVICE_FILE_PATH}"
+fi
+
+systemctl daemon-reload
