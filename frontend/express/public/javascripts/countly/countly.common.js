e801013440b58a8733dd598aa3e534cee3f5aa09
prikshittekta <prikshittekta@gmail.com>
2020-12-03 12:18:32 +0000

    hex to rgba

--- frontend/express/public/javascripts/countly/countly.common.js
+++ frontend/express/public/javascripts/countly/countly.common.js
@@ -4640,6 +4640,28 @@
 
             return obj;
         };
+
+        /**
+		 * Function to change HEX to RGBA
+		 * @param  {String} h - hex code
+		 * @returns {String} rgba string
+		 */
+        countlyCommon.hexToRgba = function(h) {
+            var r = 0, g = 0, b = 0, a = 1;
+
+            if (h.length === 4) {
+                r = "0x" + h[1] + h[1];
+                g = "0x" + h[2] + h[2];
+                b = "0x" + h[3] + h[3];
+            }
+            else if (h.length === 7) {
+                r = "0x" + h[1] + h[2];
+                g = "0x" + h[3] + h[4];
+                b = "0x" + h[5] + h[6];
+            }
+
+            return "rgba(" + +r + "," + +g + "," + +b + "," + a + ")";
+        };
     };
 
     window.CommonConstructor = CommonConstructor;
