02fb7c15a771904ea46896c1a0502fa4cecf9a2e
Vedat Can Keklik <vcankeklik@gmail.com>
2020-12-08 17:01:41 +0300

    [vue] Initial changes for a new drawer variant

--- frontend/express/public/javascripts/countly/vue/components/drawer.js
+++ frontend/express/public/javascripts/countly/vue/components/drawer.js
@@ -204,6 +204,203 @@
         }
     );
 
+
+    var BaseDrawer2 = countlyBaseComponent.extend(
+        // @vue/component
+        {
+            inheritAttrs: false,
+            mixins: [
+                _mixins.i18n
+            ],
+            props: {
+                isOpened: {type: Boolean, required: true},
+                initialEditedObject: {
+                    type: Object,
+                    default: function() {
+                        return {};
+                    }
+                },
+                name: {type: String, required: true},
+                title: {type: String, required: true},
+                saveButtonLabel: {type: String, required: true, default: ""},
+                beforeCopyFn: {type: Function},
+            },
+            data: function() {
+                return {
+                    editedObject: this.copyOfEdited(),
+                    currentStepIndex: 0,
+                    stepContents: [],
+                    sidecarContents: [],
+                    localState: this.getInitialLocalState(),
+                    inScope: [],
+                    isMounted: false
+                };
+            },
+            computed: {
+                activeContentId: function() {
+                    if (this.activeContent) {
+                        return this.activeContent.tId;
+                    }
+                    return null;
+                },
+                currentStepId: function() {
+                    return this.activeContentId;
+                },
+                isCurrentStepValid: function() {
+                    if (!this.stepValidations || !Object.prototype.hasOwnProperty.call(this.stepValidations, this.activeContentId)) {
+                        // No validation scenario defined
+                        return true;
+                    }
+                    return this.stepValidations[this.activeContentId];
+                },
+                isLastStep: function() {
+                    return this.stepContents.length > 1 && this.currentStepIndex === this.stepContents.length - 1;
+                },
+                activeContent: function() {
+                    if (this.currentStepIndex > this.stepContents.length - 1) {
+                        return null;
+                    }
+                    return this.stepContents[this.currentStepIndex];
+                },
+                isMultiStep: function() {
+                    return this.stepContents.length > 1;
+                },
+                hasSidecars: function() {
+                    return this.sidecarContents.length > 0;
+                },
+                passedScope: function() {
+                    var defaultKeys = ["editedObject", "$v", "localState"],
+                        self = this;
+
+                    var passed = defaultKeys.reduce(function(acc, val) {
+                        acc[val] = self[val];
+                        return acc;
+                    }, {});
+
+                    if (this.inScopeReadOnly) {
+                        passed.readOnly = this.inScopeReadOnly.reduce(function(acc, val) {
+                            acc[val] = self[val];
+                            return acc;
+                        }, {});
+                    }
+
+                    return passed;
+                }
+            },
+            watch: {
+                initialEditedObject: function() {
+                    this.editedObject = this.copyOfEdited();
+                    this.reset();
+                    this.$emit("copy", this.editedObject);
+                },
+                isOpened: function(newState) {
+                    if (!newState) {
+                        this.reset();
+                    }
+                }
+            },
+            mounted: function() {
+                this.stepContents = this.$children.filter(function(child) {
+                    return child.isContent && child.role === "default";
+                });
+                this.sidecarContents = this.$children.filter(function(child) {
+                    return child.isContent && child.role === "sidecar";
+                });
+                this.isMounted = true;
+            },
+            methods: {
+                tryClosing: function() {
+                    this.$emit("close", this.name);
+                },
+                copyOfEdited: function() {
+                    var copied = JSON.parse(JSON.stringify(this.initialEditedObject));
+                    if (this.beforeCopyFn) {
+                        return this.beforeCopyFn(copied);
+                    }
+                    else {
+                        return copied;
+                    }
+                },
+                setStep: function(newIndex) {
+                    if (newIndex >= 0 && newIndex < this.stepContents.length) {
+                        this.currentStepIndex = newIndex;
+                    }
+                },
+                prevStep: function() {
+                    this.setStep(this.currentStepIndex - 1);
+                },
+                nextStep: function() {
+                    this.beforeLeavingStep();
+                    if (this.isCurrentStepValid) {
+                        this.setStep(this.currentStepIndex + 1);
+                    }
+                },
+                reset: function() {
+                    this.$v.$reset();
+                    this.resetLocalState();
+                    this.setStep(0);
+                },
+                submit: function() {
+                    this.beforeLeavingStep();
+                    if (!this.$v.$invalid) {
+                        this.$emit("submit", JSON.parse(JSON.stringify(this.editedObject)));
+                        this.tryClosing();
+                    }
+                },
+                getInitialLocalState: function() {
+                    return {};
+                },
+                resetLocalState: function() {
+                    this.localState = this.getInitialLocalState();
+                },
+                beforeLeavingStep: function() { }
+            },
+            template: '<div class="cly-vue-drawer"\n' +
+                            'v-bind:class="{mounted: isMounted, open: isOpened, \'has-sidecars\': hasSidecars}">\n' +
+                            '<div class="title">\n' +
+                                '<span>{{title}}</span>\n' +
+                                '<span class="close" v-on:click="tryClosing">\n' +
+                                    '<i class="ion-ios-close-empty"></i>\n' +
+                                '</span>\n' +
+                            '</div>\n' +
+                            '<div class="sidecars-view" v-show="hasSidecars">\n' +
+                                '<slot name="sidecars"\n' +
+                                    'v-bind="passedScope">\n' +
+                                '</slot>\n' +
+                            '</div>\n' +
+                            '<div class="steps-view">\n' +
+                                '<div class="steps-header" v-show="isMultiStep">\n' +
+                                    '<div class="label" v-bind:class="{active: i === currentStepIndex,  passed: i < currentStepIndex}" v-for="(currentContent, i) in stepContents" :key="i">\n' +
+                                        '<div class="wrapper">\n' +
+                                            '<span class="index">{{i + 1}}</span>\n' +
+                                            '<span class="done-icon"><i class="fa fa-check"></i></span>\n' +
+                                            '<span class="text">{{currentContent.name}}</span>\n' +
+                                        '</div>\n' +
+                                    '</div>\n' +
+                                '</div>\n' +
+                                '<div class="details" v-bind:class="{\'multi-step\':isMultiStep}">\n' +
+                                    '<slot name="default"\n' +
+                                        'v-bind="passedScope">\n' +
+                                    '</slot>\n' +
+                                '</div>\n' +
+                                '<div class="buttons multi-step" v-if="isMultiStep">\n' +
+                                    '<div class="controls-left-container">\n' +
+                                        '<slot name="controls-left"\n' +
+                                            'v-bind="passedScope">\n' +
+                                        '</slot>\n' +
+                                    '</div>\n' +
+                                    '<cly-button @click="nextStep" v-if="!isLastStep" v-bind:disabled="!isCurrentStepValid" skin="green" v-bind:label="i18n(\'common.drawer.next-step\')"></cly-button>\n' +
+                                    '<cly-button @click="submit" v-if="isLastStep" v-bind:disabled="$v.$invalid" skin="green" v-bind:label="saveButtonLabel"></cly-button>\n' +
+                                    '<cly-button @click="prevStep" v-if="currentStepIndex > 0" skin="light" v-bind:label="i18n(\'common.drawer.previous-step\')"></cly-button>\n' +
+                                '</div>\n' +
+                                '<div class="buttons single-step" v-if="!isMultiStep">\n' +
+                                    '<cly-button @click="submit" v-bind:disabled="$v.$invalid" skin="green" v-bind:label="saveButtonLabel"></cly-button>\n' +
+                                '</div>\n' +
+                            '</div>\n' +
+                        '</div>'
+        }
+    );
+
     // @vue/component
     var hasDrawersMixin = function(names) {
         if (!Array.isArray(names)) {
@@ -242,6 +439,7 @@
     };
 
     countlyVue.components.BaseDrawer = BaseDrawer;
+    countlyVue.components.BaseDrawer2 = BaseDrawer2;
     countlyVue.mixins.hasDrawers = hasDrawersMixin;
 
 
