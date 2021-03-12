fee7233ee7edb644899c915dec2704be7a3044db
Vedat Can Keklik <vcankeklik@gmail.com>
2021-03-12 14:30:00 +0300

    [vue] New chart

--- plugins/vue-example/frontend/public/javascripts/countly.views.js
+++ plugins/vue-example/frontend/public/javascripts/countly.views.js
@@ -405,6 +405,24 @@
                             data: [820, 932, 901, 934, 1290, 1330, 1320]
                         }
                     ]
+                },
+                barOptions: {
+                    xAxis: {
+                        type: 'category',
+                        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                    },
+                    yAxis: {
+                        type: 'value'
+                    },
+                    series: [{
+                        data: [120, {
+                            value: 200,
+                            itemStyle: {
+                                color: '#a90000'
+                            }
+                        }, 150, 80, 70, 110, 130],
+                        type: 'bar'
+                    }]
                 }
             };
         },
