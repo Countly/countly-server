a2379167324fb656b51a0369d0b5522663e2b38c
Vedat Can Keklik <vcankeklik@gmail.com>
2021-04-22 20:39:08 +0300

    [vue][chart] Time series preset

--- frontend/express/public/javascripts/countly/vue/components/vis.js
+++ frontend/express/public/javascripts/countly/vue/components/vis.js
@@ -233,9 +233,71 @@
             autoresize: {
                 type: Boolean,
                 default: true
+            },
+            preset: {
+                type: String,
+                default: "manual",
+                validator: function(value) {
+                    return ['manual', 'timeSeries'].indexOf(value) !== -1;
+                }
+            },
+            series: {
+                type: Array,
+                default: function() {
+                    return [];
+                }
+            },
+            option: {
+                type: Object,
+                default: function() {
+                    return {};
+                }
             }
         },
-        template: '<echarts v-bind="$attrs" v-on="$listeners" :autoresize="autoresize"></echarts>'
+        computed: {
+            mergedOption: function() {
+                if (this.preset === "manual") {
+                    return Object.freeze(_.extend({}, this.internalOption, this.option));
+                }
+                else if (this.preset === "timeSeries") {
+                    var tickObj = countlyCommon.getTickObj();
+                    return Object.freeze(_.extend({}, this.internalOption, {
+                        xAxis: {
+                            type: 'category',
+                            data: tickObj.tickTexts
+                        },
+                        series: this.series
+                    }, this.option));
+                }
+            }
+        },
+        data: function() {
+            return {
+                internalOption: {
+                    grid: {
+                        left: 20,
+                        top: 20,
+                        right: 20,
+                        bottom: 40,
+                        containLabel: true
+                    },
+                    yAxis: {
+                        type: 'value'
+                    },
+                    legend: {
+                        bottom: "0%",
+                        itemHeight: 10,
+                        itemWidth: 10,
+                    }
+                }
+            };
+        },
+        template: '<echarts\
+                        v-bind="$attrs"\
+                        v-on="$listeners"\
+                        :option="mergedOption"\
+                        :autoresize="autoresize">\
+                    </echarts>'
     }));
 
 }(window.countlyVue = window.countlyVue || {}, jQuery));
