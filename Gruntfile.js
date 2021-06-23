4442eb86bedb0f2d18da031454668625b8a6c939
Abhishek <abhishek.singla@count.ly>
2021-06-23 11:51:16 +0530

    changes for events detail page

--- Gruntfile.js
+++ Gruntfile.js
@@ -156,7 +156,9 @@ module.exports = function(grunt) {
                     'frontend/express/public/core/session-frequency/javascripts/countly.models.js',
                     'frontend/express/public/core/session-frequency/javascripts/countly.views.js',
                     'frontend/express/public/core/events/javascripts/countly.models.js',
-                    'frontend/express/public/core/events/javascripts/countly.views.js'
+                    'frontend/express/public/core/events/javascripts/countly.views.js',
+                    'frontend/express/public/core/events/javascripts/countly.events.models.js',
+                    'frontend/express/public/core/events/javascripts/countly.events.views.js'
                 ],
                 dest: 'frontend/express/public/javascripts/min/countly.lib.concat.js'
             }
