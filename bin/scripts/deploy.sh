c433d0b692802c9642d8b993e19632be54575c47
Arturs Sosins <ar2rsawseen@gmail.com>
2021-06-29 11:35:21 +0300

    more fixes

--- bin/scripts/deploy.sh
+++ bin/scripts/deploy.sh
@@ -7,7 +7,7 @@ if [ -z "$GITHUB_HEAD_REF" ] && [ "$GITHUB_REPOSITORY" == "Countly/countly-serve
     echo "$GITHUB_BRANCH"
     if [ "$GITHUB_BRANCH" == "master" ] || [ "$GITHUB_BRANCH" == "next" ]; then
         echo "$SSH_PRIVATE_KEY" > deploy-key;
-        mkdir -p ~/.ssh/id_rsa;
+        mkdir -p ~/.ssh;
         mv deploy-key ~/.ssh/id_rsa;
         chmod 600 ~/.ssh/id_rsa;
         ssh -oStrictHostKeyChecking=no "countly@$GITHUB_BRANCH.count.ly" "bash /home/countly/deploy.sh > /home/countly/logs/countly-deploy-github.log 2>&1 &"
