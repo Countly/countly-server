b8ab2ecc063417a30f7a7f3a79199c777ef2e3e0
Arturs Sosins <ar2rsawseen@gmail.com>
2021-06-29 09:55:52 +0300

    [scripts] more debug output

--- bin/scripts/deploy.sh
+++ bin/scripts/deploy.sh
@@ -2,13 +2,16 @@
 
 if [ -z "$GITHUB_HEAD_REF" ] && [ "$GITHUB_REPOSITORY" == "Countly/countly-server" ]; then
     GITHUB_BRANCH=${GITHUB_REF##*/}
+    echo $GITHUB_BRANCH
+    GITHUB_BRANCH=master
+    echo $GITHUB_BRANCH
     if [ "$GITHUB_BRANCH" == "master" ] || [ "$GITHUB_BRANCH" == "next" ]; then
         echo "$SSH_PRIVATE_KEY" > deploy-key;
         chmod 600 deploy-key;
         mv deploy-key ~/.ssh/id_rsa;
-        ssh -oStrictHostKeyChecking=no "countly@$GITHUB_BRANCH.count.ly" "bash /home/countly/deploy.sh > /home/countly/logs/countly-deploy-travis.log 2>&1 &"
+        ssh -oStrictHostKeyChecking=no "countly@$GITHUB_BRANCH.count.ly" "bash /home/countly/deploy.sh > /home/countly/logs/countly-deploy-github.log 2>&1 &"
         if [ "$GITHUB_BRANCH" == "master" ]; then
-            ssh -oStrictHostKeyChecking=no "countly@ce.count.ly" "bash /home/countly/deploy.sh > /home/countly/logs/countly-deploy-travis.log 2>&1 &"
+            ssh -oStrictHostKeyChecking=no "countly@ce.count.ly" "bash /home/countly/deploy.sh > /home/countly/logs/countly-deploy-github.log 2>&1 &"
         fi
     fi
 fi
