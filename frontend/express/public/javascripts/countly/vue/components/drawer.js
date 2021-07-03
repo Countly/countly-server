f6420deb1bdc2932eff16b727468ee00967da68d
Vedat Can Keklik <vcankeklik@gmail.com>
2021-07-03 14:24:46 +0300

    [vue][drawer] Load optimization

--- frontend/express/public/javascripts/countly/vue/components/drawer.js
+++ frontend/express/public/javascripts/countly/vue/components/drawer.js
@@ -35,6 +35,7 @@
             },
             data: function() {
                 return {
+                    isToggleable: true,
                     sidecarContents: []
                 };
             },
