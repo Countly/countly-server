2414a740d52f4ebd88091cc4cdbd2f434b409969
Vedat Can Keklik <vcankeklik@gmail.com>
2021-07-05 14:35:36 +0300

    [vue][donut] New mode

--- frontend/express/public/javascripts/countly/vue/components/progress.js
+++ frontend/express/public/javascripts/countly/vue/components/progress.js
@@ -5,9 +5,31 @@
     Vue.component("cly-progress-donut", countlyBaseComponent.extend({
         props: {
             percentage: {type: Number, default: 42},
-            color: {type: [String, Function, Array], default: '#00C3CA'}
+            color: {type: [String, Function, Array], default: '#00C3CA'},
+            mode: {
+                type: String,
+                default: 'simple',
+                validator: function(val) {
+                    return val === 'simple' || val === 'advanced-tile';
+                }
+            },
+            label: {type: String, required: false, default: ''}
         },
-        template: '<el-progress :color="color" :percentage="percentage" type="circle" :width="56" stroke-linecap="butt" :stroke-width="8" :show-text="false"></el-progress>'
+        computed: {
+            topClasses: function() {
+                if (this.mode === 'advanced-tile') {
+                    return 'bu-p-5 bu-is-flex';
+                }
+                return '';
+            }
+        },
+        template: '<div :class="topClasses">\
+                    <el-progress :color="color" :percentage="percentage" type="circle" :width="56" stroke-linecap="butt" :stroke-width="9" :show-text="false"></el-progress>\
+                    <div v-if="mode === \'advanced-tile\'" class="bu-pl-5 bu-is-flex bu-is-flex-direction-column bu-is-justify-content-space-between">\
+                        <span class="text-medium"><slot>{{label}}</slot></span>\
+                        <h2>{{percentage}} %</h2>\
+                    </div>\
+                </div>'
     }));
 
     Vue.component("cly-progress-bar", countlyBaseComponent.extend({
