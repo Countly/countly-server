7817cd7c85463e8814917b7330e89eed4cb85d89
Cookiezaurs <sosina.anna@gmail.com>
2021-07-15 18:42:20 +0300

    restructured

--- bin/upgrade/dev/scripts/fix_cohorts_appID.js
+++ bin/upgrade/dev/scripts/fix_cohorts_appID.js
@@ -2,24 +2,34 @@ var pluginManager = require('../../../../plugins/pluginManager.js');
 
 console.log("Fixing app_id in cohorts collection.");
 pluginManager.dbConnection().then((countlyDb) => {
-    countlyDb.collection('cohorts').find({}).toArray(function(err, cohorts) {
-        var requests = [];
-        for (var k = 0; k < cohorts.length; k++) {
-            if (typeof cohorts[k].app_id !== 'string') {
-                requests.push({
-                    'updateOne': {
-                        'filter': { '_id': cohorts[k]._id },
-                        'update': { '$set': {"app_id": cohorts[k].app_id + ""} }
-                    }
-                });
-            }
-        }
-        if (requests.length > 0) {
-            console.log("Fixing " + requests.length + " records");
-            countlyDb.collection('cohorts').bulkWrite(requests, {ordered: false}, function(err) {
+
+    var cursor = countlyDb.collection('cohorts').find({});
+    var requests = [];
+    cursor.forEach(function(cohort) {
+		if (typeof cohort.app_id !== 'string') {
+			requests.push({
+				'updateOne': {
+					'filter': { '_id': cohort._id },
+					'update': { '$set': {"app_id": cohort.app_id + ""} }
+				}
+			});
+		}
+        if (requests.length === 500) {
+            //Execute per 500 operations and re-init
+			console.log("Flushing changes:"+requests.length);
+            countlyDb.collection('cohorts').bulkWrite(requests, function(err) {
                 if (err) {
-                    console.log("Error during fixing app_id for cohorts");
-                    console.log(JSON.stringify(err));
+                    console.error(err);
+                }
+            });
+            requests = [];
+        }
+    }, function() {
+        if (requests.length > 0) {
+			console.log("Flushing changes:"+requests.length);
+            countlyDb.collection('cohorts').bulkWrite(requests, function(err) {
+                if (err) {
+                    console.error(err);
                 }
                 console.log("Done");
                 countlyDb.close();
