c726e9f6437f72743bff96e390bb85ff644e0607
Zahid Zafar <zahidzafar51@gmail.com>
2021-09-16 16:33:44 +0500

    changes

--- frontend/express/public/javascripts/countly/vue/components/sidebar.js
+++ frontend/express/public/javascripts/countly/vue/components/sidebar.js
@@ -368,26 +368,7 @@
             computed: {
 
                 allLanguages: function() {
-                    var language = [
-                        { "code": "tr", "name": "Türkçe" },
-                        { "code": "de", "name": "Deutsch" },
-                        { "code": "et", "name": "Eesti" },
-                        { "code": "en", "name": "English" },
-                        { "code": "es", "name": "Español" },
-                        { "code": "el", "name": "Ελληνικά" },
-                        { "code": "fr", "name": "Français" },
-                        { "code": "it", "name": "Italiano" },
-                        { "code": "lv", "name": "Latviski" },
-                        { "code": "hu", "name": "Magyar" },
-                        { "code": "nl", "name": "Nederlands" },
-                        { "code": "pt", "name": "Português" },
-                        { "code": "ru", "name": "Русский язык" },
-                        { "code": "vi", "name": "Tiếng Việt" },
-                        { "code": "zh", "name": "中文" },
-                        { "code": "ja", "name": "日本語" },
-                        { "code": "ko", "name": "한국어" }
-                    ];
-                      return language;
+                      return languages;
                 },
 
                 selLang: function() {
