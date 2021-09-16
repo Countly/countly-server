df7b28f062952bd7258f11785555cdcda29736ca
Zahid Zafar <zahidzafar51@gmail.com>
2021-09-16 15:51:55 +0500

    changes

--- frontend/express/public/javascripts/countly/vue/components/sidebar.js
+++ frontend/express/public/javascripts/countly/vue/components/sidebar.js
@@ -1,5 +1,6 @@
 /* global app, jQuery, CV, Vue, countlyGlobal, _, Backbone*/
-//languages = require('../../../../../locale.conf');
+var pluginObject = {},
+languages = require('../../../../../locale.conf');
 
 (function(countlyVue, $) {
 
@@ -367,29 +368,29 @@
             computed: {
 
                 allLanguages: function() {
-                    var localeConf = [
-                        { "code": "tr", "name": "Türkçe" },
-                        { "code": "de", "name": "Deutsch" },
-                        { "code": "et", "name": "Eesti" },
-                        { "code": "en", "name": "English" },
-                        { "code": "es", "name": "Español" },
-                        { "code": "el", "name": "Ελληνικά" },
-                        { "code": "fr", "name": "Français" },
-                        { "code": "it", "name": "Italiano" },
-                        { "code": "lv", "name": "Latviski" },
-                        { "code": "hu", "name": "Magyar" },
-                        { "code": "nl", "name": "Nederlands" },
-                        { "code": "pt", "name": "Português" },
-                        { "code": "ru", "name": "Русский язык" },
-                        { "code": "vi", "name": "Tiếng Việt" },
-                        { "code": "zh", "name": "中文" },
-                        { "code": "ja", "name": "日本語" },
-                        { "code": "ko", "name": "한국어" }
-                    ];
-                      return localeConf;
+                    // var language = [
+                    //     { "code": "tr", "name": "Türkçe" },
+                    //     { "code": "de", "name": "Deutsch" },
+                    //     { "code": "et", "name": "Eesti" },
+                    //     { "code": "en", "name": "English" },
+                    //     { "code": "es", "name": "Español" },
+                    //     { "code": "el", "name": "Ελληνικά" },
+                    //     { "code": "fr", "name": "Français" },
+                    //     { "code": "it", "name": "Italiano" },
+                    //     { "code": "lv", "name": "Latviski" },
+                    //     { "code": "hu", "name": "Magyar" },
+                    //     { "code": "nl", "name": "Nederlands" },
+                    //     { "code": "pt", "name": "Português" },
+                    //     { "code": "ru", "name": "Русский язык" },
+                    //     { "code": "vi", "name": "Tiếng Việt" },
+                    //     { "code": "zh", "name": "中文" },
+                    //     { "code": "ja", "name": "日本語" },
+                    //     { "code": "ko", "name": "한국어" }
+                    // ];
+                      return languages;
                 },
 
-                langTitle: function() {
+                selLang: function() {
                     return countlyCommon.BROWSER_LANG_SHORT;
                 },
 
