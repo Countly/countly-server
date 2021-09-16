b8627cf9939fce8b60ce4f6e5ba1b7048a263c66
Zahid Zafar <zahidzafar51@gmail.com>
2021-09-16 14:56:56 +0500

    changes

--- frontend/express/public/javascripts/countly/vue/components/sidebar.js
+++ frontend/express/public/javascripts/countly/vue/components/sidebar.js
@@ -366,28 +366,28 @@
             computed: {
 
                 allLanguages: function() {
-                    var options = [{
-                        value: 'Option1',
-                        label: 'Option1'
-                      }, {
-                        value: 'Option2',
-                        label: 'Option2'
-                      }, {
-                        value: 'Option3',
-                        label: 'Option3'
-                      }, {
-                        value: 'Option4',
-                        label: 'Option4'
-                      }, {
-                        value: 'Option5',
-                        label: 'Option5'
-                      }];
+                    // var options = [{
+                    //     value: 'Option1',
+                    //     label: 'Option1'
+                    //   }, {
+                    //     value: 'Option2',
+                    //     label: 'Option2'
+                    //   }, {
+                    //     value: 'Option3',
+                    //     label: 'Option3'
+                    //   }, {
+                    //     value: 'Option4',
+                    //     label: 'Option4'
+                    //   }, {
+                    //     value: 'Option5',
+                    //     label: 'Option5'
+                    //   }];
                       
-                      return options;
+                      return languages;
                 },
 
                 langTitle: function() {
-                    return countlyCommon.BROWSER_LANG_SHORT.toUpperCase()
+                    return '';//countlyCommon.BROWSER_LANG_SHORT.toUpperCase()
                 },
 
                 components: function() {
