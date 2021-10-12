2257ac7c1f4825f18d0a549428ca5c6d62c7382a
Cookiezaurs <sosina.anna@gmail.com>
2021-10-12 09:13:09 +0300

    eslint

--- plugins/views/frontend/public/javascripts/countly.models.js
+++ plugins/views/frontend/public/javascripts/countly.models.js
@@ -1,4 +1,4 @@
-/*global CountlyHelpers, countlyCommon, $, countlySession, jQuery, countlyGlobal, Promise, CV, countlyVue*/
+/*global CountlyHelpers, countlyCommon, $, countlySession, jQuery, countlyGlobal, Promise, CV, countlyVue, app*/
 
 (function(countlyViews) {
 
