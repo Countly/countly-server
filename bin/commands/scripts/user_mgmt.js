e2db555758dd86472f5ed7f07a29dc6ba2511b75
Kaan Kölköy <kaanklky@gmail.com>
2021-10-14 15:48:59 +0300

    Update hash type for user management of CLI

--- bin/commands/scripts/user_mgmt.js
+++ bin/commands/scripts/user_mgmt.js
@@ -1,16 +1,16 @@
 var manager = require('../../../plugins/pluginManager.js'),
-    crypto = require('crypto');
+    countlyConfig = require('../../../frontend/express/config.js'),
+    crypto = require('crypto'),
+    argon2 = require('argon2');
 
 var myArgs = process.argv.slice(2);
 
-function sha1Hash(str, addSalt) {
-    var salt = (addSalt) ? new Date().getTime() : "";
-    return crypto.createHmac('sha1', salt + "").update(str + "").digest('hex');
+function argon2Hash(str) {
+    return argon2.hash(str);
 }
 
-function sha512Hash(str, addSalt) {
-    var salt = (addSalt) ? new Date().getTime() : "";
-    return crypto.createHmac('sha512', salt + "").update(str + "").digest('hex');
+function argon2Verify(hashedStr, str) {
+    return argon2.verify(hashedStr, str);
 }
 
 function md5Hash(str) {
@@ -25,32 +25,51 @@ manager.dbConnection().then((db) => {
                 db.close();
             }
             else {
-                var doc = {"full_name": myArgs[1], "username": myArgs[1], "password": sha512Hash(myArgs[2]), "email": myArgs[1], "global_admin": true};
-                crypto.randomBytes(48, function(errorBuff, buffer) {
-                    doc.api_key = md5Hash(buffer.toString('hex') + Math.random());
-                    db.collection('members').insert(doc, {safe: true}, function(err, member) {
-                        if (err) {
-                            console.log(err);
-                        }
-                        else {
-                            member = member.ops;
-                            console.log(member[0]);
-                        }
-                        db.close();
+                var secret = countlyConfig.passwordSecret || "";
+                argon2Hash(myArgs[2] + secret).then(password_ARGON2 => {
+                    var doc = {"full_name": myArgs[1], "username": myArgs[1], "password": password_ARGON2, "email": myArgs[1], "global_admin": true};
+                    crypto.randomBytes(48, function(errorBuff, buffer) {
+                        doc.api_key = md5Hash(buffer.toString('hex') + Math.random());
+                        db.collection('members').insert(doc, {safe: true}, function(err, member) {
+                            if (err) {
+                                console.log(err);
+                            }
+                            else {
+                                member = member.ops;
+                                console.log(member[0]);
+                            }
+                            db.close();
+                        });
                     });
-                });
+                })
             }
         });
     }
     else if (myArgs[0] == "delete" && myArgs[1] && myArgs[2]) {
-        db.collection('members').remove({$and: [{username: myArgs[1]}, {$or: [{"password": sha512Hash(myArgs[2])}, {"password": sha1Hash(myArgs[2])}]}]}, function(err, member) {
+        var secret = countlyConfig.passwordSecret || "";
+        db.collection('members').findOne({ username: { $eq: myArgs[1] } }, function (err, member) {
             if (err) {
                 console.log(err);
             }
             else {
-                console.log(member.result);
+                argon2Verify(member.password, myArgs[2] + secret).then(isPasswordMatched => {
+                    if (isPasswordMatched) {
+                        db.collection('members').remove({ username: { $eq: myArgs[1] } }, function (err, member) {
+                            if (err) {
+                                console.log(err);
+                            }
+                            else {
+                                console.log(member.result);
+                            }
+                            db.close();
+                        });
+                    }
+                    else {
+                        console.log("Password is wrong!");
+                        db.close();
+                    }
+                });
             }
-            db.close();
         });
     }
 });
