18924be7f76d4d41b9e9e5c2429fe18083a4e5c1
vck <vcankeklik@gmail.com>
2021-10-27 17:08:38 +0300

    [vue][metric-breakdown] New component for metric breakdowns

--- frontend/express/public/javascripts/countly/vue/components/helpers.js
+++ frontend/express/public/javascripts/countly/vue/components/helpers.js
@@ -129,85 +129,8 @@
                   '</div>'
     }));
 
-    Vue.component("cly-breakdown-tile", countlyBaseComponent.extend({
-        mixins: [
-            _mixins.i18n
-        ],
-        props: {
-            name: {
-                type: String
-            },
-            type: {
-                default: 'multi',
-                type: String
-            },
-            values: {
-                type: Array
-            }
-        },
-        template: '<div class="cly-vue-breakdown-tile bu-column bu-is-4">\
-                        <div class="cly-vue-breakdown-tile__wrapper bu-p-5">\
-                            <h4 class="text-uppercase text-small font-weight-bold">{{name}}</h4>\
-                            <div class="cly-vue-breakdown-tile__values-list bu-columns bu-is-gapless bu-is-multiline bu-is-mobile" v-if="type === \'multi\'">\
-                                <div v-for="item in values" class="bu-column bu-is-12">\
-                                    <div class="cly-vue-breakdown-tile__item">\
-                                        <div class="bu-level bu-is-mobile cly-vue-breakdown-tile__item-title">\
-                                            <div class="bu-level-left">\
-                                                <div class="bu-level-item text-medium">\
-                                                    <img v-if="item.icon" :src="item.icon"/>{{item.name}}\
-                                                </div>\
-                                            </div>\
-                                            <div class="bu-level-right">\
-                                                <div class="bu-level-item text-medium">\
-                                                    <a :href="item.link">{{item.description}} ({{item.percent}}%)</a>\
-                                                </div>\
-                                            </div>\
-                                        </div>\
-                                        <div>\
-                                            <progress class="bu-progress bu-is-link" :value="item.percent" max="100">{{item.percent}}%</progress>\
-                                        </div>\
-                                    </div>\
-                                </div>\
-                            </div>\
-                        </div>\
-                    </div>'
-    }));
-
-    Vue.component("cly-tooltip-icon", countlyBaseComponent.extend({
-        props: {
-            icon: {
-                type: String,
-                default: 'ion ion-help-circled'
-            },
-            tooltip: {
-                type: String,
-                default: 'Tooltip here :)'
-            }
-        },
-        template: '<i v-bind:class="\'cly-vue-tooltip-icon \' + icon" v-tooltip="tooltip"></i>'
-    }));
-
-    Vue.component("cly-remover", countlyBaseComponent.extend({
-        props: {
-            disabled: {
-                type: Boolean,
-                default: false
-            }
-        },
-        methods: {
-            remove: function() {
-                if (!this.disabled) {
-                    this.$emit("remove");
-                }
-            }
-        },
-        template: '<div class="cly-vue-remover"\n' +
-                        'v-if="!disabled"\n' +
-                        '@click="remove">\n' +
-                        '<slot>\n' +
-                            '<i class="el-icon-delete"></i>\n' +
-                        '</slot>\n' +
-                    '</div>\n'
+    Vue.component("cly-metric-cards", countlyBaseComponent.extend({
+        template: '<div class="cly-vue-metric-cards bu-columns bu-is-gapless bu-is-mobile bu-is-multiline"><slot></slot></div>',
     }));
 
     Vue.component("cly-metric-card", countlyBaseComponent.extend({
@@ -279,8 +202,65 @@
                     </div>'
     }));
 
-    Vue.component("cly-metric-cards", countlyBaseComponent.extend({
-        template: '<div class="cly-vue-metric-cards bu-columns bu-is-gapless bu-is-mobile bu-is-multiline"><slot></slot></div>',
+    Vue.component("cly-metric-breakdown", countlyVue.components.create({
+        template: countlyVue.T('/javascripts/countly/vue/templates/breakdown.html'),
+        mixins: [
+            _mixins.i18n
+        ],
+        props: {
+            name: {
+                type: String
+            },
+            description: {
+                type: String,
+                default: '',
+                required: false
+            },
+            type: {
+                default: 'multi',
+                type: String
+            },
+            values: {
+                type: Array
+            }
+        }
+    }));
+
+    Vue.component("cly-tooltip-icon", countlyBaseComponent.extend({
+        props: {
+            icon: {
+                type: String,
+                default: 'ion ion-help-circled'
+            },
+            tooltip: {
+                type: String,
+                default: 'Tooltip here :)'
+            }
+        },
+        template: '<i v-bind:class="\'cly-vue-tooltip-icon \' + icon" v-tooltip="tooltip"></i>'
+    }));
+
+    Vue.component("cly-remover", countlyBaseComponent.extend({
+        props: {
+            disabled: {
+                type: Boolean,
+                default: false
+            }
+        },
+        methods: {
+            remove: function() {
+                if (!this.disabled) {
+                    this.$emit("remove");
+                }
+            }
+        },
+        template: '<div class="cly-vue-remover"\n' +
+                        'v-if="!disabled"\n' +
+                        '@click="remove">\n' +
+                        '<slot>\n' +
+                            '<i class="el-icon-delete"></i>\n' +
+                        '</slot>\n' +
+                    '</div>\n'
     }));
 
     var popoverSizes = {
