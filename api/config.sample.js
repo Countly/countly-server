67bfa82e5f4135b07f41ba7c25ef0bef77979b8c
Artem Salpagarov <a@ahoy.li>
2021-11-04 10:26:04 +0300

    [core] update user logging

--- api/config.sample.js
+++ api/config.sample.js
@@ -90,7 +90,7 @@ var countlyConfig = {
     * @property {array=} info - modules to log for information level for {@link logger}
     */
     logging: {
-        info: ["jobs", "push"],
+        info: ["jobs", "push", "core:mgmt.users"],
         default: "warn"
     },
     /**
