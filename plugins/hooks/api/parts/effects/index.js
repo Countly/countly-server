db102a12f8fb3262c0c05f07b7ab4a7df6d7dac0
Waiter Zen <waiter.james1991@gmail.com>
2021-11-11 22:39:17 +0800

    [hooks]remove old ui hooks

--- plugins/hooks/api/parts/effects/index.js
+++
@@ -1,7 +0,0 @@
-const HTTPEffect = require('./http.js');
-const SDKEventEffect = require('./sdk_event.js');
-const EmailEffect = require('./email.js');
-const CustomCodeEffect = require('./custom_code.js');
-
-module.exports = {HTTPEffect, SDKEventEffect, EmailEffect, CustomCodeEffect};
-
--- plugins/hooks/api/parts/triggers/index.js
+++
@@ -1,5 +0,0 @@
-const APIEndPointTrigger = require('./api_endpoint.js');
-const InternalEventTrigger = require('./internal_event.js');
-const IncomingDataTrigger = require('./incoming_data.js');
-
-module.exports = {APIEndPointTrigger, InternalEventTrigger, IncomingDataTrigger};
