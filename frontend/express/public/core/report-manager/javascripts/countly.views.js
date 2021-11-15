eeae92da62e1df8b6d52037dc90440afffbfccb8
vck <vcankeklik@gmail.com>
2021-11-15 13:11:26 +0300

    [UI][report-manager] Tabs

--- frontend/express/public/core/report-manager/javascripts/countly.views.js
+++ frontend/express/public/core/report-manager/javascripts/countly.views.js
@@ -1,10 +1,39 @@
-/*global app, countlyVue, CV */
+/*global app, countlyVue, CV, Vue */
 
 (function() {
 
-    var ReportManagerView = countlyVue.views.create({
-        template: CV.T('/core/report-manager/templates/reportmanager.html')
+    var ManualReportsView = countlyVue.views.create({
+        template: CV.T('/core/report-manager/templates/reportmanager-manual.html')
     });
+
+    var AutomaticReportsView = countlyVue.views.create({
+        template: CV.T('/core/report-manager/templates/reportmanager-auto.html')
+    });
+
+    var ReportManagerView = countlyVue.views.create({
+        template: CV.T('/core/report-manager/templates/reportmanager.html'),
+        data: function() {
+            return {
+                dynamicTab: "manual-reports-tab",
+                localTabs: [
+                    {
+                        title: CV.i18n('report-maanger.manually-created'),
+                        name: "manual-reports-tab",
+                        component: ManualReportsView
+                    },
+                    {
+                        title: CV.i18n('report-maanger.automatically-created'),
+                        name: "automatic-reports-tab",
+                        component: AutomaticReportsView
+                    }
+                ]
+            };
+        }
+    });
+
+    Vue.component("cly-report-manager-table", countlyVue.views.create({
+        template: CV.T('/core/report-manager/templates/reportmanager-table.html')
+    }));
 
     var getMainView = function() {
         return new countlyVue.views.BackboneWrapper({
