883819b84b6c360be9a2d2e7f1e36ac8490652ca
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2021-11-16 13:06:24 +0000

    data manager ce edition

---
+++ plugins/data-manager/api/api.js
@@ -0,0 +1,81 @@
+const common = require('../../../api/utils/common.js');
+const { validateRead } = require('../../../api/utils/rights.js');
+const plugins = require('../../pluginManager.js');
+try {
+    if (plugins.isPluginEnabled('drill')) {
+        require('./api.extended')();
+    }
+}
+catch (e) {
+    console.log('running in basic mode');
+}
+const FEATURE_NAME = 'data_manager';
+
+plugins.register("/o/data-manager/events", function(ob) {
+    validateRead(ob.params, FEATURE_NAME, async function() {
+        try {
+            let appId = ob.params.qstring.app_id;
+
+            let events = await common.db.collection('events')
+                .findOne({'_id': common.db.ObjectID(appId)});
+            let auditLogs = await common.db.collection('systemlogs')
+                .aggregate([
+                    {
+                        '$match': {
+                            'a': {'$in': ['dm-event-edit', 'dm-event-create', 'dm-event-approve']},
+                            'i.ev': {'$in': events?.list || [] }
+                        }
+                    }, {
+                        '$sort': {'ts': -1 }
+                    }, {
+                        '$group': {
+                            '_id': '$i.ev',
+                            'user_id': {'$first': '$user_id'},
+                            'ts': {'$first': '$ts' },
+                            'event': {'$first': '$i.ev' }
+                        }
+                    }
+                ])
+                .toArray();
+
+            let memberIds = [...auditLogs.map(al=>{
+                return al.user_id;
+            }) ];
+
+            let membersMap = {};
+            let members = await common.db.collection('members')
+                .find({_id: {$in: memberIds.map(id=>common.db.ObjectID(id))}}, {projection: {'full_name': 1, '_id': 1}})
+                .toArray();
+            members.forEach(member=>{
+                membersMap[member._id] = member.full_name;
+            });
+
+            events = events?.list
+                .filter(ev=>{
+                    return !ev.startsWith('[CLY]');
+                })
+                .map(ev=>{
+                    let combined = {segments: events.segments[ev] || [], key: ev};
+                    let mapObj = events?.map?.[ev] || {};
+                    return {...combined, ...mapObj};
+                });
+
+            const eventMap = {};
+            events?.forEach(ev=>{
+                eventMap[ev.key] = ev;
+            });
+            auditLogs.forEach(al=>{
+                al.userName = membersMap[al.user_id];
+                let key = al.event;
+                eventMap[key].audit = al;
+            });
+
+            common.returnOutput(ob.params, Object.values(eventMap));
+        }
+        catch (e) {
+            console.error(e);
+            common.returnOutput(ob.params, 500, "Error");
+        }
+    });
+    return true;
+});
