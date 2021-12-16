533bd84579d6c7df29af3bf74ae9d99befc4baee
Waiter Zen <waiter.james1991@gmail.com>
2021-12-16 17:42:17 +0800

    [hooks] fix textarea bug

--- plugins/hooks/frontend/public/javascripts/countly.views.js
+++ plugins/hooks/frontend/public/javascripts/countly.views.js
@@ -1,5 +1,5 @@
 /*global
-   CV, countlyVue, Uint8Array, $, countlyCommon, jQuery,countlyGlobal, app, hooksPlugin, moment, CountlyHelpers,  countlyEvent, countlyAuth
+   CV, _,  countlyVue, Uint8Array, $, countlyCommon, jQuery,countlyGlobal, app, hooksPlugin, moment, CountlyHelpers,  countlyEvent, countlyAuth
  */
 (function() {
     var FEATURE_NAME = "hooks";
