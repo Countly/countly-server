099316f27bf2092dca1e092e44b8c32052525abe
Arturs Sosins <ar2rsawseen@gmail.com>
2021-12-17 11:01:45 +0200

    [old-ui-compatability] plugin for old ui views

---
+++ plugins/old-ui-compatability/frontend/app.js
@@ -0,0 +1,11 @@
+var plugin = {};
+//var countlyConfig = require("../../../frontend/express/config");
+
+plugin.init = function(/*app, countlyDb*/) {
+    //add middleware here
+    /*app.use(countlyConfig.path + '/dashboard', function(req, res, next) {
+        next();
+    });*/
+};
+
+module.exports = plugin;
