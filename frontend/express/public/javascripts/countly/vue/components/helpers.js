0d7a508d47483c915586f12c056d904fa10c6175
itsiprikshit <prikshittekta@gmail.com>
2021-12-27 16:06:34 +0000

    breadcrumbs

--- frontend/express/public/javascripts/countly/vue/components/helpers.js
+++ frontend/express/public/javascripts/countly/vue/components/helpers.js
@@ -915,4 +915,27 @@
 
     Vue.component("cly-empty-datatable", BaseEmptyViewForElements.extend({
     }));
+
+    Vue.component("cly-breadcrumbs", countlyBaseComponent.extend({
+        mixins: [
+            _mixins.i18n
+        ],
+        props: {
+            crumbs: {
+                type: Array,
+                default: function() {
+                    return [];
+                }
+            },
+        },
+        template: "<div class='bu-level cly-breadcrumbs bu-mb-5'>\
+                        <div v-for='(crumb, index) in crumbs'\
+                            :class='[\"bu-level-item text-medium bu-mr-0\",\
+                                    {\"bu-ml-2\": (index !== 0)},\
+                                    {\"color-cool-gray-40\": (index === (crumbs.length - 1))}]'>\
+                            <a :href='crumb.url'>{{crumb.label}}</a>\
+                            <span class='bu-ml-2' v-if='index !== (crumbs.length - 1)'>></span>\
+                        </div>\
+                    </div>"
+    }));
 }(window.countlyVue = window.countlyVue || {}));
