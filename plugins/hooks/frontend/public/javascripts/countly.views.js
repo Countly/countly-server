15de0d1efd592cc1a660fb56b18e79c8b67980af
Waiter Zen <waiter.james1991@gmail.com>
2022-01-25 22:10:07 +0800

    [hooks] update ui

--- plugins/hooks/frontend/public/javascripts/countly.views.js
+++ plugins/hooks/frontend/public/javascripts/countly.views.js
@@ -815,17 +815,17 @@
             },
             downloadLog: function(text, timestamp) {
                 var element = document.createElement('a');
-                var fileName =  'HookError-' + this.hookDetail._id + '-' + timestamp +'.txt';
+                var fileName = 'HookError-' + this.hookDetail._id + '-' + timestamp + '.txt';
                 element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
                 element.setAttribute('download', fileName);
-              
+
                 element.style.display = 'none';
                 document.body.appendChild(element);
-              
+
                 element.click();
-              
+
                 document.body.removeChild(element);
-              
+
             }
         }
     });
