1aeb5ac8f68dc6b03a19afa9bc8bbb079bdf862b
Lumbardh Agaj <agaj.lumbardh@gmail.com>
2022-02-01 10:29:14 +0100

    Revert "feature: Add view registration in global settings"
    This reverts commit a474623f0a3afc361d424e1a5630dd6a9b6ab254.

--- plugins/plugins/frontend/public/javascripts/countly.views.js
+++ plugins/plugins/frontend/public/javascripts/countly.views.js
@@ -295,7 +295,6 @@
                 searchPlaceholder: CV.i18n("common.search"),
                 predefinedLabels: app.configurationsView.predefinedLabels,
                 predefinedInputs: app.configurationsView.predefinedInputs,
-                predefinedViews: app.configurationsView.predefinedViews,
                 predefinedStructure: app.configurationsView.predefinedStructure
             };
         },
@@ -845,19 +844,12 @@
     app.configurationsView = {
         predefinedInputs: {},
         predefinedLabels: {},
-        predefinedViews: {},
         predefinedStructure: {},
         registerInput: function(id, callback) {
             this.predefinedInputs[id] = callback;
         },
         registerLabel: function(id, html) {
             this.predefinedLabels[id] = html;
-        },
-        registerView: function(id, view) {
-            if (!this.predefinedViews[id]) {
-                this.predefinedViews[id] = [];
-            }
-            this.predefinedViews[id].push(view);
         },
         registerStructure: function(id, obj) {
             this.predefinedStructure[id] = obj;
@@ -936,6 +928,8 @@
     app.configurationsView.registerInput("security.robotstxt", {input: "el-input", attrs: {type: "textarea", rows: 5}});
 
     app.configurationsView.registerInput("security.api_additional_headers", {input: "el-input", attrs: {type: "textarea", rows: 5}});
+
+    app.configurationsView.registerInput("push.proxypass", {input: "el-input", attrs: {type: "password"}});
 
     app.configurationsView.registerInput("api.reports_regenerate_interval", {
         input: "el-select",
