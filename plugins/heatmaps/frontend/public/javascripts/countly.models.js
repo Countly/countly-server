a410fe474ca1a71de9246fc370810f17adf8b30c
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2022-02-02 07:09:47 +0000

    fix for non domain heatmaps

--- plugins/heatmaps/frontend/public/javascripts/countly.models.js
+++ plugins/heatmaps/frontend/public/javascripts/countly.models.js
@@ -82,19 +82,18 @@
         onError: onError,
         onReady: function(context, rows) {
             for (var k = 0; k < rows.length; k++) {
-                rows[k].view = rows[k].display || rows[k].view || rows[k]._id;
                 rows[k].u = rows[k].uvalue || rows[k].u || 0;
                 rows[k].actionLink = "unknown";
                 rows[k].useDropdown = true;
                 var url = "#/analytics/views/action-map/";
+                if (url.indexOf("http") !== 0) {
+                    url = "http://" + url;
+                }
+                if (url.substr(url.length - 1) === '/') {
+                    url = url.substr(0, url.length - 1);
+                }
                 if (countlyGlobal.apps[countlyCommon.ACTIVE_APP_ID].app_domain && countlyGlobal.apps[countlyCommon.ACTIVE_APP_ID].app_domain.length > 0) {
                     url = countlyGlobal.apps[countlyCommon.ACTIVE_APP_ID].app_domain;
-                    if (url.indexOf("http") !== 0) {
-                        url = "http://" + url;
-                    }
-                    if (url.substr(url.length - 1) === '/') {
-                        url = url.substr(0, url.length - 1);
-                    }
                     rows[k].useDropdown = false;
                 }
                 var link = rows[k]._id;
@@ -117,7 +116,6 @@
         onError: onError,
         onReady: function(context, rows) {
             for (var k = 0; k < rows.length; k++) {
-                rows[k].view = rows[k].display || rows[k].view || rows[k]._id;
                 rows[k].u = rows[k].uvalue || rows[k].u || 0;
 
                 if (rows[k].t > 0) {
@@ -135,14 +133,14 @@
                 rows[k].actionLink = "unknown";
                 rows[k].useDropdown = true;
                 var url = "#/analytics/views/action-map/";
+                if (url.indexOf("http") !== 0) {
+                    url = "http://" + url;
+                }
+                if (url.substr(url.length - 1) === '/') {
+                    url = url.substr(0, url.length - 1);
+                }
                 if (countlyGlobal.apps[countlyCommon.ACTIVE_APP_ID].app_domain && countlyGlobal.apps[countlyCommon.ACTIVE_APP_ID].app_domain.length > 0) {
                     url = countlyGlobal.apps[countlyCommon.ACTIVE_APP_ID].app_domain;
-                    if (url.indexOf("http") !== 0) {
-                        url = "http://" + url;
-                    }
-                    if (url.substr(url.length - 1) === '/') {
-                        url = url.substr(0, url.length - 1);
-                    }
                     rows[k].useDropdown = false;
                 }
                 var link = rows[k]._id;
