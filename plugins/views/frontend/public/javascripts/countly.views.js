46a1e523ec6c49cb92d1240ddd9ce4a2146e618f
Cookiezaurs <sosina.anna@gmail.com>
2022-02-09 18:15:38 +0200

    Changes for analytics/views dashboard widget

--- plugins/views/frontend/public/javascripts/countly.views.js
+++ plugins/views/frontend/public/javascripts/countly.views.js
@@ -941,7 +941,7 @@
         });
 
         var GridComponent = countlyVue.views.create({
-            template: CV.T('/views/templates/widget.html'),
+            template: CV.T('/dashboards/templates/widgets/analytics/widget.html'),
             mixins: [countlyVue.mixins.commonFormatters],
             props: {
                 data: {
@@ -967,14 +967,11 @@
                     }
                     return "";
                 },
-                period: function() {
-                    /* var pp = countlyWidgets.formatPeriod(this.data.custom_period);
-                    pp = pp || {};
-                    return pp.longName || "";*/
-                    return this.data.custom_period;
+                showBuckets: function() {
+                    return false;
                 },
-                maxTableHeight: function() {
-                    return 200;
+                metricLabels: function() {
+                    return [];
                 },
                 tableStructure: function() {
                     var columns = [{prop: "view", "title": CV.i18n("views.widget-type")}];
@@ -982,7 +979,12 @@
                     this.data = this.data || {};
                     this.data.metrics = this.data.metrics || [];
                     for (var k = 0; k < this.data.metrics.length; k++) {
-                        columns.push({"prop": this.data.metrics[k], "title": CV.i18n("views." + this.data.metrics[k])});
+                        if (this.data.metrics[k] === "d" || this.data.metrics[k] === "src") {
+                            columns.push({"prop": this.data.metrics[k], "title": CV.i18n("views." + this.data.metrics[k])});
+                        }
+                        else {
+                            columns.push({"prop": this.data.metrics[k], "title": CV.i18n("views." + this.data.metrics[k]), "type": "number"});
+                        }
                     }
                     return columns;
                 },
@@ -990,26 +992,40 @@
                     this.data = this.data || {};
                     this.data.dashData = this.data.dashData || {};
                     this.data.dashData.data = this.data.dashData.data || {};
-
-
                     this.data.dashData.data.chartData = this.data.dashData.data.chartData || [];
-                    for (var k = 0; k < this.data.dashData.data.chartData.length; k++) {
-                        if (this.data.dashData.data.chartData[k].t > 0) {
-                            this.data.dashData.data.chartData[k].dCalc = countlyCommon.timeString((this.data.dashData.data.chartData[k].d / this.data.dashData.data.chartData[k].t) / 60);
-                            var vv = parseFloat(this.data.dashData.data.chartData[k].scr) / parseFloat(this.data.dashData.data.chartData[k].t);
-                            if (vv > 100) {
-                                vv = 100;
+
+                    var tableData = [];
+                    for (var z = 0; z < this.data.dashData.data.chartData.length; z++) {
+                        var ob = {"view": this.data.dashData.data.chartData[z].view};
+                        for (var k = 0; k < this.data.metrics.length; k++) {
+                            if (this.data.metrics[k] === "d") {
+                                if (this.data.dashData.data.chartData[z].t > 0) {
+                                    ob[this.data.metrics[k]] = countlyCommon.timeString((this.data.dashData.data.chartData[z].d / this.data.dashData.data.chartData[z].t) / 60);
+                                }
+                                else {
+                                    ob[this.data.metrics[k]] = 0;
+                                }
                             }
-                            this.data.dashData.data.chartData[k].scrCalc = countlyCommon.formatNumber(vv) + "%";
+                            else if (this.data.metrics[k] === "scr") {
+                                if (this.data.dashData.data.chartData[k].t > 0) {
+                                    var vv = parseFloat(this.data.dashData.data.chartData[z].scr) / parseFloat(this.data.dashData.data.chartData[z].t);
+                                    if (vv > 100) {
+                                        vv = 100;
+                                    }
+                                    ob[this.data.metrics[k]] = countlyCommon.formatNumber(vv) + "%";
+                                }
+                                else {
+                                    ob[this.data.metrics[k]] = 0;
+                                }
+                            }
+                            else {
+                                ob[this.data.metrics[k]] = this.data.dashData.data.chartData[z][this.data.metrics[k]];
+                            }
                         }
-                        else {
-                            this.data.dashData.data.chartData[k].dCalc = 0;
-                            this.data.dashData.data.chartData[k].scrCalc = 0;
-                        }
+                        tableData.push(ob);
+
                     }
-
-                    return this.data.dashData.data.chartData;
-
+                    return tableData;
                 }
             }
 
@@ -1020,8 +1036,13 @@
             data: function() {
                 return {
                     useCustomTitle: false,
-                    useCustomPeriod: false,
-                    availableStatsMetric: [
+                    useCustomPeriod: false
+                };
+            },
+            computed: {
+                availableStatsMetric: function() {
+                    var app = this.scope.editedObject.apps[0];
+                    var metrics = [
                         { label: CV.i18n("views.u"), value: "u" },
                         { label: CV.i18n("views.n"), value: "n" },
                         { label: CV.i18n("views.t"), value: "t" },
@@ -1031,45 +1052,16 @@
                         { label: CV.i18n("views.b"), value: "b" },
                         { label: CV.i18n("views.br"), value: "br" },
                         { label: CV.i18n("views.uvc"), value: "uvc" }
-                    ]
-                };
-            },
-            computed: {
+                    ];
+                    if (app && countlyGlobal.apps[app] && countlyGlobal.apps[app].type === "web") {
+                        metrics.push({ label: CV.i18n("views.scr"), value: "scr" });
+                    }
+                    return metrics;
+                }
             },
             mounted: function() {
             },
             methods: {
-            },
-            watch: {
-                'scope.editedObject.selectedApp': function(newVal) {
-                    if (countlyGlobal.apps[newVal] && countlyGlobal.apps[newVal].type === "web") {
-                        this.availableStatsMetric = [
-                            { label: CV.i18n("web.common.table.total-users"), value: "u" },
-                            { label: CV.i18n("web.common.table.new-users"), value: "n" },
-                            { label: CV.i18n("views.t"), value: "t" },
-                            { label: CV.i18n("views.d"), value: "d" },
-                            { label: CV.i18n("views.s"), value: "s" },
-                            { label: CV.i18n("views.e"), value: "e" },
-                            { label: CV.i18n("views.b"), value: "b" },
-                            { label: CV.i18n("views.br"), value: "br" },
-                            { label: CV.i18n("views.uvc"), value: "uvc" },
-                            { label: CV.i18n("views.scr"), value: "scr" }
-                        ];
-                    }
-                    else {
-                        this.availableStatsMetric = [
-                            { label: CV.i18n("views.u"), value: "u" },
-                            { label: CV.i18n("views.n"), value: "n" },
-                            { label: CV.i18n("views.t"), value: "t" },
-                            { label: CV.i18n("views.d"), value: "d" },
-                            { label: CV.i18n("views.s"), value: "s" },
-                            { label: CV.i18n("views.e"), value: "e" },
-                            { label: CV.i18n("views.b"), value: "b" },
-                            { label: CV.i18n("views.br"), value: "br" },
-                            { label: CV.i18n("views.uvc"), value: "uvc" }
-                        ];
-                    }
-                },
             },
             props: {
                 scope: {
@@ -1100,8 +1092,7 @@
                         metrics: [],
                         apps: [],
                         custom_period: "30days",
-                        visualization: "",
-                        breakdowns: []
+                        visualization: "table"
                     };
                 },
                 beforeLoadFn: function(/*doc, isEdited*/) {
@@ -1115,7 +1106,7 @@
                 dimensions: function() {
                     return {
                         minWidth: 6,
-                        minHeight: 6,
+                        minHeight: 4,
                         width: 6,
                         height: 4
                     };
