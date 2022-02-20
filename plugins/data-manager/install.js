2b1aecaf629f1236e8f9e0a722c49bf69e5b3ad5
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2022-02-20 18:46:05 +0000

    fix install script

--- plugins/data-manager/install.js
+++ plugins/data-manager/install.js
@@ -5,12 +5,13 @@ try {
     console.log("Generating schema for events in drill");
 
     pluginManager.dbConnection().then(async(db) => {
-        db.collection('systemlogs').ensureIndex({"i.segment": 1}, {sparse: true, background: true}, function(){});
-        db.collection('systemlogs').ensureIndex({"i.ev": 1}, {sparse: true, background: true}, function(){});
-        db.collection('systemlogs').ensureIndex({"i.id": 1}, {sparse: true, background: true}, function(){});
+        await db.collection('systemlogs').ensureIndex({"i.segment": 1}, {sparse: true, background: true});
+        await db.collection('systemlogs').ensureIndex({"i.ev": 1}, {sparse: true, background: true});
+        await db.collection('systemlogs').ensureIndex({"i.id": 1}, {sparse: true, background: true});
+        require('./install-extension.js')();
+        db.close();
     });
 
-    require('./install-extension.js')();
 }
 catch (e) {
     // suppress
