19e6e690616eba2e8b4315d719d8a0254fdabf08
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2022-02-20 18:51:11 +0000

    fix exception catch block in install script

--- plugins/data-manager/install.js
+++ plugins/data-manager/install.js
@@ -1,18 +1,17 @@
-try {
-    var pluginManager = require('../pluginManager.js');
+var pluginManager = require('../pluginManager.js');
 
-    console.log("Installing data manager plugin");
-    console.log("Generating schema for events in drill");
+console.log("Installing data manager plugin");
+console.log("Generating schema for events in drill");
 
-    pluginManager.dbConnection().then(async(db) => {
-        await db.collection('systemlogs').ensureIndex({"i.segment": 1}, {sparse: true, background: true});
-        await db.collection('systemlogs').ensureIndex({"i.ev": 1}, {sparse: true, background: true});
-        await db.collection('systemlogs').ensureIndex({"i.id": 1}, {sparse: true, background: true});
+pluginManager.dbConnection().then(async(db) => {
+    await db.collection('systemlogs').ensureIndex({"i.segment": 1}, {sparse: true, background: true});
+    await db.collection('systemlogs').ensureIndex({"i.ev": 1}, {sparse: true, background: true});
+    await db.collection('systemlogs').ensureIndex({"i.id": 1}, {sparse: true, background: true});
+    try {
         require('./install-extension.js')();
-        db.close();
-    });
-
-}
-catch (e) {
-    // suppress
-}
+    }
+    catch (e) {
+        // suppress
+    }
+    db.close();
+});
