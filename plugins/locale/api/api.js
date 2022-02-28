c4b9a07b5af24e662724efdc7ec5cb3ce54e1ea5
Cookiezaurs <sosina.anna@gmail.com>
2022-02-28 16:54:42 +0200

    Languages: 1)Added loading 2)Switched sides for graphs 3)Number formatting

--- plugins/locale/api/api.js
+++ plugins/locale/api/api.js
@@ -51,7 +51,9 @@ const FEATURE_NAME = 'locale';
         var params = ob.params;
 
         if (params.qstring.method === "langs") {
+		setTimeout(function(){
             validateRead(params, FEATURE_NAME, fetch.fetchTimeObj, 'langs');
+		},5000);
             return true;
         }
         return false;
