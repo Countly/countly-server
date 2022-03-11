6241fe8018a5e011d3ae911f6b6e176b79aaa9be
Foysal Osmany <foysalo@gmail.com>
2022-03-11 14:48:34 +0000

    added missing index

--- plugins/crashes/install.js
+++ plugins/crashes/install.js
@@ -27,6 +27,8 @@ pluginManager.dbConnection().then((countlyDb) => {
             countlyDb.collection('app_crashgroups' + app._id).ensureIndex({"lastTs": 1}, {background: true}, cb);
             countlyDb.collection('app_crashgroups' + app._id).ensureIndex({"latest_version": 1}, {background: true}, cb);
             countlyDb.collection('app_crashgroups' + app._id).ensureIndex({"groups": 1}, {background: true}, cb);
+            countlyDb.collection('app_crashusers' + app._id).ensureIndex({"_id": 1, "is_hidden": 1}, {background: true}, cb);
+            countlyDb.collection('app_crashusers' + app._id).ensureIndex({"_id": 1, "name": 1}, {background: true}, cb);
             countlyDb.collection('app_crashusers' + app._id).ensureIndex({"group": 1, "uid": 1}, {background: true}, cb);
             countlyDb.collection('app_crashusers' + app._id).ensureIndex({"group": 1, "crashes": 1, "fatal": 1}, {sparse: true, background: true}, cb);
             countlyDb.collection('app_crashusers' + app._id).ensureIndex({"uid": 1}, {background: true}, cb);
