9a15908c1224b7080d98318db46083cd856c1db2
shaharyar-shamshi <shaharyarshamshi@gmail.com>
2022-04-25 11:46:00 +0000

    create consent

--- plugins/compliance-hub/tests.js
+++ plugins/compliance-hub/tests.js
@@ -9,8 +9,8 @@ var APP_ID = "";
 var DEVICE_ID = "1234567890";
 
 describe('Complaince Hub', function() {
-    describe('verify no users', function() {
-        it('should have 0 user', function(done) {
+    describe('verify no result for user consent', function() {
+        it('should have 0 records', function(done) {
             API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
             APP_ID = testUtils.get("APP_ID");
             APP_KEY = testUtils.get("APP_KEY");
@@ -19,12 +19,30 @@ describe('Complaince Hub', function() {
                 .expect(200)
                 .end(function(err, res) {
                     if (err) {
-                        console.log(err);
                         done(err);
                     }
-                    res.text.should.eql("{}");
+                    res.text.should.eql('{"iTotalRecords":0,"iTotalDisplayRecords":0,"aaData":[]}');
                     done();
                 });
         });
+        it('create the consent', function(done) {
+            API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
+            APP_ID = testUtils.get("APP_ID");
+            APP_KEY = testUtils.get("APP_KEY");
+            var consent = {
+            };
+            request
+                .post('/sdk/user_properties?api_key=' + API_KEY_ADMIN + '&app_id=' + APP_ID + '&consent' + JSON.stringify(consent))
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        console.log(err);
+                        done(err);
+                    }
+                    res.text.should.eql('{"iTotalRecords":0,"iTotalDisplayRecords":0,"aaData":[]}');
+                    done();
+                });
+
+        })
     });
 });
