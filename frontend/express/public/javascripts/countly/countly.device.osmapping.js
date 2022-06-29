6ae555397017ed77a5d2c02b8c34570047abf3e6
Cookiezaurs <sosina.anna@gmail.com>
2022-06-29 22:24:59 +0300

    eslint

--- frontend/express/public/javascripts/countly/countly.device.osmapping.js
+++ frontend/express/public/javascripts/countly/countly.device.osmapping.js
@@ -44,7 +44,7 @@ var countlyOsMapping = {
     "nokia": {short: "n", name: "Nokia"},
     "firefox": {short: "f", name: "Firefox OS"},
     "tizen": {short: "t", name: "Tizen"},
-	"arch": {short: "l", name: "Linux"}
+    "arch": {short: "l", name: "Linux"}
 };
 
 /*global module*/
