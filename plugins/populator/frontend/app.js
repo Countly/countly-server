6c5a8c339f11ed32dddbb04e198640cb7f94bb45
Danu Widatama <widatama@gmail.com>
2022-08-12 18:24:31 +0700

    [populator] Add heatmap page routes

--- plugins/populator/frontend/app.js
+++ plugins/populator/frontend/app.js
@@ -7,13 +7,20 @@ var exported = {},
     plugin.init = function(app) {
         const predefinedTypes = [
             "demo-banking.html",
+            "demo-banking-1.html",
+            "demo-banking-2.html",
             "demo-ecommerce.html",
+            "demo-ecommerce-1.html",
+            "demo-ecommerce-2.html",
             "demo-gaming.html",
+            "demo-gaming-1.html",
+            "demo-gaming-2.html",
             "demo-healthcare.html",
-            "demo-healthcare-hospitals.html",
+            "demo-healthcare-1.html",
+            "demo-healthcare-2.html",
             "demo-navigation.html",
-            "demo-navigation-map.html",
-            "demo-navigation-partner.html",
+            "demo-navigation-1.html",
+            "demo-navigation-2.html",
         ];
         app.get(countlyConfig.path + '/populator/:id/:page', function(req, res) {
             let url = 'public/templates/demo-page.html';
