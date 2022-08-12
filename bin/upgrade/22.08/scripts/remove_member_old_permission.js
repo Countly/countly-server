34eb73d6c3fbff7e67d31c4b6c453b5441fc75a0
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2022-08-12 14:56:41 +0000

    remove old keys in single query

--- bin/upgrade/22.08/scripts/remove_member_old_permission.js
+++ bin/upgrade/22.08/scripts/remove_member_old_permission.js
@@ -6,30 +6,11 @@ const pluginManager = require('../../../../plugins/pluginManager.js');
 // New permissions are generated in this script `./member_permission_generator`
 
 pluginManager.dbConnection().then((countlyDb) => {
-    // If member has no permission object then that member permission might not be migrated yet
-    // We will not do anything to that member
-    countlyDb.collection('members').find({ permission: { $exists: true } }).toArray((err, members) => {
-        if (!members || err) {
+    countlyDb.collection('members').updateMany({ permission: { $exists: true } }, { $unset: { admin_of: '', user_of: '' } }, function(err){
+        if (err) {
             console.log(err);
-            countlyDb.close();
-            return;
         }
-
-        function removeOldPermission(member, done) {
-            // Remove old permission 'admin_of' and 'user_of'
-            countlyDb.collection('members').findAndModify({ _id: member._id }, {}, { $unset: { admin_of: '', user_of: '' } }, (err, member) => {
-                if (err || !member) {
-                    console.log('Member not found.');
-                }
-
-                done();
-                return;
-            });
-        }
-
-        async.forEach(members, removeOldPermission, () => {
-            console.log('Finished removing members old permissions');
-            countlyDb.close();
-        });
+        countlyDb.close();
+        return;
     });
 });
