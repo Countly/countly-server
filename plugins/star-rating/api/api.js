1d4e181ac878599542a73799fc3b3f656c4d496a
Furkan Başaran <frknbasaran@gmail.com>
2022-09-08 14:44:21 +0300

    fix: added check for from_survey flag.

--- plugins/star-rating/api/api.js
+++ plugins/star-rating/api/api.js
@@ -994,7 +994,12 @@ function uploadFile(myfile, id, callback) {
                         return true;
                     }
                     else {
-                        common.returnMessage(params, 404, 'Widgets not found.');
+                        if (ob.from_survey) {
+                          common.returnMessage(params, 404, 'Widgets not found.');
+                        }
+                        else {
+                          common.returnOutput(params, docs);
+                        }
                         return true;
                     }
                 }
