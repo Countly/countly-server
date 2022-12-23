bbc17df62d49df9f7e0362716a76080559592544
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2022-12-23 07:24:19 +0000

    script to clear internal user properties exposed in meta_up

---
+++ bin/upgrade/DEV/scripts/clear_internal_up_meta.js
@@ -0,0 +1,49 @@
+const pluginManager = require('../../../../plugins/pluginManager.js');
+var Promise = require("bluebird");
+
+console.log("Started: Clearing internal user properties from meta");
+
+Promise.all(
+    [
+        pluginManager.dbConnection("countly"),
+        pluginManager.dbConnection("countly_drill")
+    ])
+    .spread(async function(countlyDB, countlyDrillDB) {
+        try {
+            let apps = await countlyDB.collection('apps').find({}).project({_id: 1}).toArray();
+            for (const appId of apps.map(a => a._id)) {
+                await countlyDrillDB.collection("drill_meta" + appId).findAndModify(
+                    {_id: 'meta_up'},
+                    {},
+                    {
+                        $unset: {
+                            "up.ingested": "",
+                            "up.cdfs": "",
+                            "up.consent": "",
+                            "up.fsd": "",
+                            "up.fsm": "",
+                            "up.fsw": "",
+                            "up.hadFatalCrash": "",
+                            "up.hadNonfatalCrash": "",
+                            "up.hos": "",
+                            "up.lest": "",
+                            "up.lvt": "",
+                            "up.nxret": "",
+                            "up.tkap": "",
+                            "up.tkip": "",
+                            "up.vc": "",
+                        }
+                    }
+                );
+            }
+            console.log('Complete: Clearing internal user properties from meta');
+            countlyDB.close();
+            countlyDrillDB.close();
+        }
+        catch (e) {
+            console.log('ERROR: Clearing internal user properties from meta');
+            console.error(e);
+            countlyDB.close();
+            countlyDrillDB.close();
+        }
+    });
