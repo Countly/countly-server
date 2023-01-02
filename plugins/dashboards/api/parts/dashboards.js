e31160520710d34bb920a881c2e8bb09dbfc20f3
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2023-01-02 07:57:32 +0000

    remove async lock

--- plugins/dashboards/api/parts/dashboards.js
+++ plugins/dashboards/api/parts/dashboards.js
@@ -9,9 +9,7 @@ var countlyModel = require("../../../../api/lib/countly.model.js"),
     countlyCommon = require('../../../../api/lib/countly.common'),
     fetch = require("../../../../api/parts/data/fetch.js"),
     log = common.log('dashboards:api'),
-    plugins = require("../../../pluginManager.js"),
-    AsyncLock = require('async-lock'),
-    lock = new AsyncLock();
+    plugins = require("../../../pluginManager.js");
 
 /** @lends module:api/parts/data/dashboard */
 var dashboard = {};
@@ -375,17 +373,15 @@ dashboard.fetchAnalyticsData = async function(params, apps, widget) {
             widgetApps = widget.apps || [];
             widgetData = {};
 
-            await lock.acquire(widgetApps, async function() {
-                // return value or promise
-                for (let i = 0; i < widgetApps.length; i++) {
-                    var appId = widgetApps[i];
-                    widgetData[appId] = await getAnalyticsSessionDataForApp(params, apps, appId, widget);
-                }
+            // return value or promise
+            for (let i = 0; i < widgetApps.length; i++) {
+                var appId = widgetApps[i];
+                widgetData[appId] = await getAnalyticsSessionDataForApp(params, apps, appId, widget);
+            }
 
-                dashData.isValid = true;
-                dashData.data = widgetData;
-                widget.dashData = dashData;
-            });
+            dashData.isValid = true;
+            dashData.data = widgetData;
+            widget.dashData = dashData;
         }
         catch (e) {
             log.d("Error while fetching analytics widget data for - ", widget);
