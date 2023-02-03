ae6c4d47e767eb13bb1c3ecd2a2f7de4ff82f3b4
Kaan Kölköy <kaanklky@gmail.com>
2023-02-03 20:54:15 +0300

    Debug GH

--- bin/countly.install.sh
+++ bin/countly.install.sh
@@ -2,11 +2,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATE=$(date +%Y-%m-%d:%H:%M:%S)
 totalm=$(free -m | awk '/^Mem:/{print $2}')
-
+echo "$INSIDE_DOCKER"
 if [ "$INSIDE_DOCKER" == "1" ]; then
     echo -e '#!/bin/bash\n$@' > /usr/bin/sudo
 fi
-
+cat /usr/bin/sudo
 sudo bash "$DIR/scripts/init_countly_user.sh"
 
 if [ "$totalm" -lt "1800" ]; then
