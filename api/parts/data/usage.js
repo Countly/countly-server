54d80af58151d077cb4764dca7bb530d41fc0493
Pınar Genç <gncpin@hotmail.com>
2023-02-08 17:25:10 +0300

    [SER-488] Reporting Windows OS matches OS version as Windows Phone

--- api/parts/data/usage.js
+++ api/parts/data/usage.js
@@ -220,6 +220,11 @@ usage.getPredefinedMetrics = function(params, userProps) {
         if (params.qstring.metrics._os && params.qstring.metrics._os_version) {
             params.qstring.metrics._os += "";
             params.qstring.metrics._os_version += "";
+            var regex = /^windows\s\d+/i;
+
+            if (regex.test(params.qstring.metrics._os.toLowerCase())) {
+                params.qstring.metrics._os = params.qstring.metrics._os.split(" ")[0];
+            }
             if (common.os_mapping[params.qstring.metrics._os.toLowerCase()] && !params.qstring.metrics._os_version.startsWith(common.os_mapping[params.qstring.metrics._os.toLowerCase()])) {
                 params.qstring.metrics._os_version = common.os_mapping[params.qstring.metrics._os.toLowerCase()] + params.qstring.metrics._os_version;
             }
