b3f4733449be1f03bdc78cd96a802bcc888ba520
Umit Coskun Aydinoglu <coskun.aydinoglu@count.ly>
2023-03-22 10:54:30 +0000

    create index in drop callback

--- bin/upgrade/DEV/scripts/update_user_text_index.js
+++ bin/upgrade/DEV/scripts/update_user_text_index.js
@@ -1,30 +1,41 @@
 // Drop the existing index
+// make the following code eslint valid
 const pluginManager = require('../../../../plugins/pluginManager.js'),
-asyncjs = require('async');
+    asyncjs = require('async');
+
 pluginManager.dbConnection().then((countlyDb) => {
-  countlyDb.collection('apps').find({}).toArray(function (err, apps) {
-    function upgrade(app, done) {
-      countlyDb.collection('app_users' + app._id).dropIndex("name_text_email_text");
-      countlyDb.collection('app_users' + app._id).createIndex(
-        { "name": "text", "email": "text", "did": "text", "uid": "text", "username":"text" },
-        { background: true , default_language: "none"  },
-        function (err, result) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(result);
-          }
+    countlyDb.collection('apps').find({}).toArray(function(err, apps) {
+        function upgrade(app, done) {
+            countlyDb.collection('app_users' + app._id).dropIndex("name_text_email_text", function(err) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    countlyDb.collection('app_users' + app._id).createIndex(
+                        { "name": "text", "email": "text", "did": "text", "uid": "text", "username": "text" },
+                        { background: true, default_language: "none" },
+                        function(err, result) {
+                            if (err) {
+                                console.log(err);
+                            }
+                            else {
+                                console.log(result);
+                            }
+                        }
+                    );
+                }
+
+                done();
+            });
+
         }
-      );
-      done();
-    }
 
-    asyncjs.eachSeries(apps, upgrade, function () {
-      console.log("Updating index finished");
-      countlyDb.close();
+        asyncjs.eachSeries(apps, upgrade, function() {
+            console.log("Updating index finished");
+            countlyDb.close();
+        });
+
     });
-
-  });
 
 });
 
