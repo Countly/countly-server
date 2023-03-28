bfce80ab4a20c1319795ff5b39ce1fc98c46ae8c
Kaan Kölköy <kaanklky@gmail.com>
2023-03-28 12:22:35 +0300

    merge countly.sh from next to master

--- bin/commands/systemd/countly.sh
+++ bin/commands/systemd/countly.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+boot_configurations () {
+    LIMIT_NO_FILE=$(($(($(lscpu |grep 'CPU(s):'|head -1|awk -F' ' '{print $2}')*1000)) + 10000))
+    sed -i '/LimitNOFILE/d' /etc/systemd/system/countly.service
+    sed -i "s#\[Service\]#\[Service\]\nLimitNOFILE=${LIMIT_NO_FILE}#g" /etc/systemd/system/countly.service
+    systemctl daemon-reload
+
+    SERVICE_USER=$(grep 'user' "$(countly dir)/bin/config/supervisord.conf"|awk -F'=' '{print $2}')
+    chown -R "$SERVICE_USER:$SERVICE_USER" "$(countly dir)"
+}
+
 countly_start () {
     countly_root ;
+    boot_configurations ;
     systemctl start countly;
 }
 
@@ -12,8 +23,7 @@ countly_stop () {
 
 countly_restart () {
     countly_root ;
-    SERVICE_USER=$(grep 'user' "$(countly dir)/bin/config/supervisord.conf"|awk -F'=' '{print $2}')
-    chown -R "$SERVICE_USER:$SERVICE_USER" "$(countly dir)"
+    boot_configurations ;
     systemctl restart countly;
 }
 
