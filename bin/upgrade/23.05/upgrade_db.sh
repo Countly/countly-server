146d0d9dafc1060abf69a87f86c4f7eb85ccf8d3
Arturs Sosins <as@count.ly>
2023-05-25 11:40:27 +0300

    [version] 23.05

---
+++ bin/upgrade/23.05/upgrade_db.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+VER="23.05"
+
+CONTINUE="$(countly check before upgrade db "$VER")"
+
+if [ "$CONTINUE" != "1" ] && [ "$1" != "combined" ]
+then
+    echo "Database is already up to date with $VER"
+    read -r -p "Are you sure you want to run this script? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+    then
+        CONTINUE=1
+    fi
+fi
+
+if [ "$CONTINUE" == "1" ]
+then
+    echo "Running database modifications"
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+    SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+    if [ "$1" != "combined" ]; then
+        #upgrade plugins
+        countly plugin enable license;
+        nodejs "$DIR/scripts/install_plugins.js"
+    fi
+    
+    #add indexes
+    nodejs "$DIR/scripts/add_indexes.js"
+
+    #run upgrade scripts
+    nodejs "$SCRIPTS/scripts/push_drop_indexes.js"
+    nodejs "$SCRIPTS/scripts/upgrade_user_permission_data-manager.js"
+    nodejs "$SCRIPTS/scripts/rebuild_member_permission.js"
+
+
+    if [ "$1" != "combined" ]; then
+        countly upgrade;
+    fi
+
+    #call after check
+    countly check after upgrade db "$VER"
+elif [ "$CONTINUE" == "0" ]
+then
+    echo "Database is already upgraded to $VER"
+elif [ "$CONTINUE" == "-1" ]
+then
+    echo "Database is upgraded to higher version"
+else
+    echo "Unknown ugprade state: $CONTINUE"
+fi
