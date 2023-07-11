3c1d9c13fd68ecb242725c50fb811d9c52fa5fe1
Arturs Sosins <as@count.ly>
2023-07-11 15:23:26 +0300

    copy test connection script

--- bin/scripts/countly.prepare.ce.tests.sh
+++ bin/scripts/countly.prepare.ce.tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 #prepopulate docker with predefined data
 #bash /opt/countly/bin/backup/run.sh
 
@@ -13,5 +15,5 @@ elif [ ! -f "/usr/bin/node" ]; then
     ln -s "$(which nodejs)" /usr/bin/node
 fi
 
-
+cp -rf "$DIR/test.connection.js" /opt/countly/bin/scripts/test.connection.js
 #until nc -z localhost 3001; do echo Waiting for Countly; sleep 1; done
