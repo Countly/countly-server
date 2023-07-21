f953f8f11f99926c471352f7c9937f102798b850
Danu Widatama <widatama@gmail.com>
2023-07-21 13:33:43 +0700

    Add login_count to member

--- plugins/onboarding/frontend/app.js
+++ plugins/onboarding/frontend/app.js
@@ -1,8 +1,17 @@
+const common = require('../../../api/utils/common');
+
 var exportedPlugin = {};
 
 (function(plugin) {
-    plugin.init = function() {
+    plugin.init = function(_, countlyDb) {
+        plugin.loginSuccessful = function(ob) {
+            const member = ob.data;
 
+            countlyDb.collection('members').update(
+                { _id: common.db.ObjectID(member._id) },
+                { $inc: { login_count: 1 } },
+            );
+        };
     };
 }(exportedPlugin));
 
