995afcbdb334deb1f198e611bbe72effa61fbd68
Danu Widatama <widatama@gmail.com>
2023-07-26 10:00:55 +0700

    Move login count increment to plugin

--- plugins/onboarding/frontend/app.js
+++ plugins/onboarding/frontend/app.js
@@ -1,17 +1,8 @@
-const common = require('../../../api/utils/common');
-
 var exportedPlugin = {};
 
 (function(plugin) {
-    plugin.init = function(_, countlyDb) {
-        plugin.loginSuccessful = function(ob) {
-            const member = ob.data;
+    plugin.init = function() {
 
-            countlyDb.collection('members').update(
-                { _id: common.db.ObjectID(member._id) },
-                { $inc: { login_count: 1 } },
-            );
-        };
     };
 }(exportedPlugin));
 
