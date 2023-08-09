2368051dcffae71ff18af7641202a019c389c8fc
Arturs Sosins <as@count.ly>
2023-08-09 19:17:43 +0300

    add symlink

--- bin/countly.install_ubuntu.sh
+++ bin/countly.install_ubuntu.sh
@@ -64,7 +64,9 @@ set +e
 NODE_JS_CMD=$(which nodejs)
 set -e
 if [[ -z "$NODE_JS_CMD" ]]; then
-    sudo ln -s "$(which node)" /usr/bin/nodejs
+	sudo ln -s "$(which node)" /usr/bin/nodejs
+elif [ ! -f "/usr/bin/node" ]; then
+    sudo ln -s "$(which nodejs)" /usr/bin/node
 fi
 
 #if npm is not installed, install it too
