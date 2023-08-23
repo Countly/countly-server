d159dfd5c29dfdc3dde85317fc86ae684e945948
GC <87137526+LaNinja06@users.noreply.github.com>
2023-08-23 10:30:00 -0600

    Updated getAllEventsSegmentsForApp.js

--- bin/scripts/export-data/getAllEventsSegmentsForApp.js
+++ bin/scripts/export-data/getAllEventsSegmentsForApp.js
@@ -1,5 +1,5 @@
 /**
- * Outputs list of drill collection names for specific app
+ * Output list of app names, event keys, and segments.
  * Server: countly
  * Path: bin/scripts/export-data
  * Command: node getallEventsSegmentsForApp.js
@@ -7,40 +7,40 @@
 
 var plugins = require("../../../plugins/pluginManager.js");
 
-plugins.dbConnection("countly").then(function (db) {
-  db.collection("events").find().toArray(function (err, events) {
-    db.collection("apps").find().toArray(function (err, names) {
+plugins.dbConnection("countly").then(function(db) {
+    db.collection("events").find().toArray(function(err, events) {
+        db.collection("apps").find().toArray(function(err, names) {
 
-      if (err) {
-        console.log("Error fetching data", err);
-        db.close();
-        return;
-      }
-
-      const appMapping = {};
-      for (const app of names) {
-        appMapping[app._id] = app.name;
-      }
-
-      console.log("App Name, Event Key, Segment");
-      
-      for (const event of events) {
-        const appId = event._id;
-        const appName = appMapping[appId] || "Unknown";
-        const segments = event.segments;
-
-        for (const key in segments) {
-          if (segments.hasOwnProperty(key)) {
-            const values = segments[key];
-
-            for (const segmentValue of values) {
-              const row = [appName, key, segmentValue];
-              console.log(row.join(", "))
+            if (err) {
+                console.log("Error fetching data", err);
+                db.close();
+                return;
             }
-          }
-        }
-      }
-      db.close();
+
+            const appMapping = {};
+            for (const app of names) {
+                appMapping[app._id] = app.name;
+            }
+
+            console.log("App Name, Event Key, Segment");
+
+            for (const event of events) {
+                const appId = event._id;
+                const appName = appMapping[appId] || "Unknown";
+                const segments = event.segments;
+
+                for (const key in segments) {
+                    if (Object.hasOwn(segments, key)) {
+                        const values = segments[key];
+
+                        for (const segmentValue of values) {
+                            const row = [appName, key, segmentValue];
+                            console.log(row.join(", "));
+                        }
+                    }
+                }
+            }
+            db.close();
+        });
     });
-  });
 });
