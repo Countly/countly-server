aa75b14725f89e22089850328e0dc93d0b3cca09
can-angun <can1984@gmail.com>
2023-09-04 14:26:25 +0000

    Fixed conflict

--- ui-tests/cypress/support/navigations.js
+++ ui-tests/cypress/support/navigations.js
@@ -9,27 +9,15 @@ const goToLogoutPage = () => {
 };
 
 const goToHomePage = () => {
-<<<<<<< HEAD
-    cy.clickElement(sidebarElements.SIDEBAR_MENU_OPTIONS.MAIN_MENU);
-    cy.clickElement(sidebarElements.SIDEBAR_MAIN_MENU_OPTIONS.HOME);
-};
-
-const goToVisitorLoyalty = () => {
-    cy.clickElement(sidebarElements.SIDEBAR_MENU_OPTIONS.MAIN_MENU);
-    cy.clickElement(sidebarElements.SIDEBAR_MAIN_MENU_OPTIONS.ANALYTICS);
-    cy.clickElement(sidebarElements.SIDEBAR_MAIN_MENU_OPTIONS.ANALYTICS_LIST.VISITOR_LOYALTY);
-};
-=======
 	cy.clickElement(sidebarElements.SIDEBAR_MENU_OPTIONS_LIST.MAIN_MENU)
 	cy.clickElement(sidebarElements.SIDEBAR_MAIN_MENU_OPTIONS.HOME)
-}
+};
 
 const goToVisitorLoyalty = () => {
 	cy.clickElement(sidebarElements.SIDEBAR_MENU_OPTIONS_LIST.MAIN_MENU)
 	cy.clickElement(sidebarElements.SIDEBAR_MAIN_MENU_OPTIONS.ANALYTICS)
 	cy.clickElement(sidebarElements.SIDEBAR_MAIN_MENU_OPTIONS.ANALYTICS_LIST.VISITOR_LOYALTY)
-}
->>>>>>> 5329704f95 (Updated sidebar duplicate defines for lint)
+};
 
 const isNavigatedToDashboard = () => {
     cy.shouldUrlInclude('/dashboard');
