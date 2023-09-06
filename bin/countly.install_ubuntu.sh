a1ba319a02b7126d5d021b6ac600906ac6906e78
Kaan Kölköy <kaanklky@gmail.com>
2023-09-06 11:38:39 +0300

    update ubuntu script

--- bin/countly.install_ubuntu.sh
+++ bin/countly.install_ubuntu.sh
@@ -54,11 +54,14 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 #install node.js
-#add node.js repo
-#echo | apt-add-repository ppa:chris-lea/node.js
-wget -qO- https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get install -y nodejs || (echo "Failed to install nodejs." ; exit)
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 set +e
 NODE_JS_CMD=$(which nodejs)
