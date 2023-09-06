74e49263a00c73903b826b1ce53d4224b6eab20a
Kaan Kölköy <kaanklky@gmail.com>
2023-09-06 13:32:43 +0300

    remove unnecessary mongo imports

--- bin/backup/run.sh
+++ bin/backup/run.sh
@@ -57,9 +57,5 @@ check_connectivity mongosh
 # /usr/bin/mongoexport --db countly  --collection  sessions_                                --out sessions_.json
 
 /usr/bin/mongoimport --db countly --collection app_crashgroups58bf06bd6cba850047ac9f19 --file app_crashgroups58bf06bd6cba850047ac9f19.json --upsert
-/usr/bin/mongoimport --db countly --collection app_users58bf06bd6cba850047ac9f19 --file app_users58bf06bd6cba850047ac9f19.json --upsert
-/usr/bin/mongoimport --db countly --collection app_viewdata58bf06bd6cba850047ac9f19 --file app_viewdata58bf06bd6cba850047ac9f19.json --upsert
 /usr/bin/mongoimport --db countly --collection apps --file apps.json --upsert
-/usr/bin/mongoimport --db countly --collection jobs --file jobs.json --upsert
 /usr/bin/mongoimport --db countly --collection members --file members.json --upsert
-/usr/bin/mongoimport --db countly --collection sessions_ --file sessions_.json --upsert
