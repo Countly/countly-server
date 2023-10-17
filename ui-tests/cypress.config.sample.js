f89f990c8b179c8a57ab2202f8471d95a058df43
can-angun <can1984@gmail.com>
2023-10-17 15:52:40 +0000

    Added video record after fail cases

--- ui-tests/cypress.config.sample.js
+++ ui-tests/cypress.config.sample.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs')
 
 module.exports = defineConfig({
     e2e: {
@@ -9,10 +10,20 @@ module.exports = defineConfig({
         numTestsKeptInMemory: 0,
         projectId: "000000",
         chromeWebSecurity: false,
-        watchForFileChanges: true,
-        retries: {
-            runMode: 0,
-            openMode: 0,
-        },
-    },
-});
+		watchForFileChanges: true,
+		video: true,		
+		setupNodeEvents(on, config) {
+			on('after:spec', (spec, results) => {
+			  if (results && results.video) {
+				const failures = results.tests.some((test) =>
+				  test.attempts.some((attempt) => attempt.state === 'failed')
+				)
+				if (!failures) {
+				  // delete the video if the spec passed and no tests retried
+				  fs.unlinkSync(results.video)
+				}
+			  }
+			})
+		  },
+	},
+})
