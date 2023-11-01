5700e421c29c6c7b59cd98ba58bca6a00043c5c6
Cookiezaurs <sosina.anna@gmail.com>
2023-11-01 15:58:02 +0200

    Omitting  views segments + script for that

--- plugins/views/api/api.js
+++ plugins/views/api/api.js
@@ -170,8 +170,77 @@ const escapedViewSegments = { "name": true, "segment": true, "height": true, "wi
                     });
                 });
             }
+            else if (ob.params.qstring.method === "omit_segments") {
+                validateDelete(ob.params, FEATURE_NAME, function(params) {
+                    if (!params.qstring.app_id) {
+                        common.returnMessage(params, 400, 'Missing request parameter: app_id');
+                        resolve();
+                        return;
+                    }
+                    if (params.qstring.omit_list) {
+                        var omit = [];
+                        try {
+                            omit = JSON.parse(ob.params.qstring.omit_list);
+                        }
+                        catch (SyntaxError) {
+                            log.e('Parsing data failed: ', ob.params.qstring.omit_list);
+                            common.returnMessage(params, 400, 'Cannot parse  parameter: omit_list');
+                            resolve();
+                            return;
+                        }
+                        if (!Array.isArray(omit)) {
+                            common.returnMessage(params, 400, 'Invalid request parameter: omit_list');
+                            resolve();
+                            return;
+                        }
+                        var unset = {};
+                        for (var zz = 0; zz < omit.length; zz++) {
+                            unset["segments." + omit[zz]] = "";
+                        }
+                        common.db.collection('views').updateOne({"_id": common.db.ObjectID(appId)}, {$set: {omit: omit}}, function(err5) {
+                            if (err5) {
+                                log.e(err5);
+                                common.returnMessage(params, 400, "Updating database failed");
+                                resolve();
+                            }
+                            else {
+                                plugins.dispatch("/systemlogs", {params: params, action: "view_segments_ommit", data: { update: omit}});
+
+                                var promises = [];
+                                var errCn = 0;
+                                for (var z = 0; z < omit.length; z++) {
+                                    var colName = "app_viewdata" + crypto.createHash('sha1').update(omit[z] + appId).digest('hex');
+                                    promises.push(new Promise(function(resolve2) {
+                                        common.db.collection(colName).drop(function(err) {
+                                            if (err && err.code !== 26) { //if error is not collection not found.(Because it is possible for it to not exist)
+                                                log.e(JSON.stringify(err));
+                                                errCn++;
+                                            }
+                                            resolve2();
+                                        });
+                                    }));
+                                }
+                                Promise.all(promises).then(function() {
+                                    log.d("Segments omittion compleated  for:" + JSON.stringify(omit));
+                                    if (errCn > 0) {
+                                        plugins.dispatch("/systemlogs", {params: params, action: "view_segments_ommit_complete", data: { update: omit, error: "Failed to delete some(" + errCn + ") collections. Please call omiting again."}});
+                                    }
+                                    else {
+                                        plugins.dispatch("/systemlogs", {params: params, action: "view_segments_ommit_complete", data: { update: omit}});
+                                    }
+                                });
+                                common.returnMessage(params, 200, 'Success');
+                                resolve();
+                            }
+                        });
+                    }
+                    else {
+                        common.returnMessage(params, 400, 'Nothing is passed for omiting');
+                    }
+                });
+            }
             else {
-                common.returnMessage(ob.params, 400, 'Invalid method. Must be one of:delete_view,rename_views ');
+                common.returnMessage(ob.params, 400, 'Invalid method. Must be one of:delete_view,rename_views,omit_segments ');
                 resolve();
             }
         });
@@ -1962,6 +2031,7 @@ const escapedViewSegments = { "name": true, "segment": true, "height": true, "wi
     function recordMetrics(params, currEvent, user, view, viewInfo) {
         viewInfo = viewInfo || {};
         viewInfo.segments = viewInfo.segments || {};
+        viewInfo.omit = viewInfo.omit || [];
 
         //making sure metrics are strings
         // tmpSet["meta_v2." + tmpMetric.set + "." + escapedMetricVal] = true;
@@ -1991,7 +2061,7 @@ const escapedViewSegments = { "name": true, "segment": true, "height": true, "wi
                     segKey = "platform";
                 }
                 let tmpSegVal = currEvent.segmentation[segKey] + "";
-                if (!(escapedViewSegments[segKey] === true)) {
+                if (!(escapedViewSegments[segKey] === true) && !(Array.isArray(viewInfo.omit) && viewInfo.omit.indexOf(segKey) !== -1)) {
                     if (!viewInfo.segments[segKey] && (segKey === 'platform' || Object.keys(viewInfo.segments).length < plugins.getConfig("views").segment_limit)) {
                         viewInfo.segments[segKey] = {};
                     }
