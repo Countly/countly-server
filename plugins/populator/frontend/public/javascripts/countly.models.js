9e927cf1e69dc0f8820cb67b12de45ce043760fe
Emre Üstere <27005957+emreustere@users.noreply.github.com>
2023-11-02 11:03:11 +0300

    Update countly.models.js

--- plugins/populator/frontend/public/javascripts/countly.models.js
+++ plugins/populator/frontend/public/javascripts/countly.models.js
@@ -111,7 +111,7 @@
             },
             "views": ["view1", "view2", "view3"],
             "sequences": ["sequence1", "sequence2", "sequence3"],
-            "generatedOn": "19 May 2023",
+            "generatedOn": Math.round((Date.now().valueOf() || 0) / 1000),
         },
         {
             "_id": "defaultHealthcare",
@@ -170,7 +170,7 @@
             "users": ["user1", "user2", "user3"],
             "views": ["view1", "view2", "view3", "view4", "view5", "view6"],
             "sequences": ["sequence1", "sequence2", "sequence3", "sequence4"],
-            "generatedOn": "25 October 2023",
+            "generatedOn": Math.round((Date.now().valueOf() || 0) / 1000),
         },
         {
             "_id": "defaultEcommerce",
