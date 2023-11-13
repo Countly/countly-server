da2368b48e887e672a252ae64eece79b7759d64a
can-angun <can1984@gmail.com>
2023-11-13 13:13:19 +0000

    Updated data-test-id in quickstart item svg

--- frontend/express/public/core/onboarding/javascripts/countly.models.js
+++ frontend/express/public/core/onboarding/javascripts/countly.models.js
@@ -83,7 +83,7 @@
             var target = item.linkType === 'external' ? 'target="_blank" rel="noreferrer noopener"' : '';
             var icon = item.linkType === 'internal' ? '<i class="ion-arrow-right-c" data-test-id="quickstart-item-arrow-'+item.title.toLowerCase().replace(/\s/g, "-")+'"></i>' : '<i class="ion-android-open" data-test-id="quickstart-item-ios-android-open-'+item.title.toLowerCase().replace(/\s/g, "-")+'"></i>';
             body += '<div class="bu-mt-4 quickstart-item" data-test-id="quickstart-item-'+item.title.toLowerCase().replace(/\s/g, "-")+'">' +
-            '<div class="bu-mr-2"><img src="./images/dashboard/onboarding/light-bulb.svg" data-test-id="quickstart-item-icon-'+item.title.toLowerCase().replace(/\s/g, "-")+'"/></div>' +
+            '<div class="bu-mr-2" data-test-id="quickstart-item-icon-'+item.title.toLowerCase().replace(/\s/g, "-")+'"><img src="./images/dashboard/onboarding/light-bulb.svg" data-test-id="quickstart-item-svg-'+item.title.toLowerCase().replace(/\s/g, "-")+'"/></div>' +
             '<div>' +
             '<a href="' + linkUrl + '" class="quickstart-link bu-is-block bu-has-text-weight-medium" data-test-id="quickstart-item-link-'+item.title.toLowerCase().replace(/\s/g, "-")+'"' + target + '>' +
             title + ' ' + icon +
