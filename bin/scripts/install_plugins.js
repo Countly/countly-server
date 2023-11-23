db9457afb506ca949d70994f49c258edf6144123
Kaan Kölköy <kaanklky@gmail.com>
2023-11-23 17:49:36 +0300

    fix: add offline_mode check

--- bin/scripts/install_plugins.js
+++ bin/scripts/install_plugins.js
@@ -4,16 +4,22 @@ var plugins = manager.getPlugins();
 require('../../api/utils/log').setLevel('db:write', 'mute');
 
 if (plugins.length > 0) {
-    asyncjs.eachSeries(plugins, function(plugin, done) {
-        manager.installPlugin(plugin, function() {
-            done();
-        });
-    }, function() {
+    manager.connectToAllDatabases().then(async () => {
+        if (!manager.getConfig("api").offline_mode) {
+            await asyncjs.eachSeries(plugins, function (plugin, done) {
+                manager.installPlugin(plugin, function () {
+                    done();
+                });
+            });
+        } else {
+            console.log("Server is in offline mode, not installing any plugins.")
+        }
+
         //processing plugin files for production mode
         console.log("Processing plugin files for production mode");
         manager.prepareProduction(() => {
             console.log("Exiting");
             process.exit(0);
         });
-    });
+    })
 }
