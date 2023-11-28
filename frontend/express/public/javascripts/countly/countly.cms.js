d34be9ca7d214f22a6a68d138c9db10a6c1d8439
Danu Widatama <widatama@gmail.com>
2023-11-28 16:05:08 +0700

    [cms] Add entry id check

--- frontend/express/public/javascripts/countly/countly.cms.js
+++ frontend/express/public/javascripts/countly/countly.cms.js
@@ -2,6 +2,7 @@
 
 // frontend client for fetching data from cms.count.ly
 
+var AVAILABLE_API_IDS = ["server-guides", "server-consents", "server-intro-video", "server-quick-start", "server-guide-config"];
 var CMS_TKN = "17fa74a2b4b1524e57e8790250f89f44f364fe567f13f4dbef02ef583e70dcdb700f87a6122212bb01ca6a14a8d4b85dc314296f71681988993c013ed2f6305b57b251af723830ea2aa180fc689af1052dd74bc3f4b9b35e5674d4214a8c79695face42057424f0494631679922a3bdaeb780b522bb025dfaea8d7d56a857dba";
 var CMS_BASE_URL = "https://cms.count.ly/";
 var UPDATE_INTERVAL = 2 * 60 * 60 * 1000; // 2 hours
@@ -135,6 +136,10 @@ var UPDATE_INTERVAL = 2 * 60 * 60 * 1000; // 2 hours
     };
 
     countlyCMS.fetchEntry = function(entryID, options) {
+        if (!AVAILABLE_API_IDS.includes(entryID)) {
+            return Promise.reject({ result: 'Missing or incorrect API _id parameter'});
+        }
+
         var params = {};
         params._id = entryID;
 
