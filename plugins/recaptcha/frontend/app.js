5ddfdf08926ca7c4a0f98c5c69d90f8a56c392d7
Umit Coskun Aydinoglu <coskun.aydinoglu@count.ly>
2023-12-05 10:20:07 +0000

    2fa and recaptcha working togeter

--- plugins/recaptcha/frontend/app.js
+++ plugins/recaptcha/frontend/app.js
@@ -23,14 +23,18 @@ plugins.setConfigs("recaptcha", {
         });
         app.post(countlyConfig.path + '/login', function(req, res, next) {
             if (req.session.fails && plugins.getConfig("recaptcha").enable && plugins.getConfig("recaptcha").site_key !== "" && plugins.getConfig("recaptcha").secret_key !== "") {
-                recaptcha.verify(req, function(error) {
-                    if (!error) {
-                        next();
-                    }
-                    else {
-                        res.redirect(countlyConfig.path + '/login?message=recaptcha.incorrect');
-                    }
-                });
+                if(req.body && req.body.auth_code){ //if coming from two-factor auth skip captcha
+                    next();
+                } else {
+                    recaptcha.verify(req, function(error) {
+                        if (!error) {
+                            next();
+                        }
+                        else {
+                            res.redirect(countlyConfig.path + '/login?message=recaptcha.incorrect');
+                        }
+                    });
+                }        
             }
             else {
                 next();
