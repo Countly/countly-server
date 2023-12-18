6ea2be76d86f1b06087570bbcf868706e90cc479
can-angun <can1984@gmail.com>
2023-12-18 17:21:11 +0000

    Added home page elements control cases

--- ui-tests/cypress/e2e/onboarding/onboarding.cy.js
+++ ui-tests/cypress/e2e/onboarding/onboarding.cy.js
@@ -5,6 +5,7 @@ const initialSetupHelpers = require('../../lib/onboarding/initialSetup');
 const initialConsentHelpers = require('../../lib/onboarding/initialConsent');
 const quickstartPopoeverHelpers = require('../../support/components/quickstartPopover');
 const { APP_TYPE, DATA_TYPE } = require('../../support/constants');
+const homePageHelpers = require('../../lib/home/home')
 
 describe('Complete Onboarding', () => {
     beforeEach(function() {
@@ -119,5 +120,58 @@ describe('Complete Onboarding', () => {
         });
         navigationHelpers.isNavigatedToDashboard();
         quickstartPopoeverHelpers.verifyDefaultPageElements();
+    });
+
+    it('verify home page after complete onboarding with empty data', function() {
+        setupHelpers.completeOnboardingSetup({
+            fullName: user.username,
+            emailAddress: user.email,
+            password: user.password,
+            confirmPassword: user.password,
+            isDemoApp: false
+        });
+
+        initialSetupHelpers.completeOnboardingInitialSetup({
+            isDemoApp: false,
+            appType: APP_TYPE.MOBILE,
+            appName: 'My Mobile App',
+            timezone: 'Istanbul'
+        });
+
+        initialConsentHelpers.completeOnboardingInitialConsent({
+            isEnableTacking: false,
+            isSubscribeToNewsletter: false
+        });
+        navigationHelpers.isNavigatedToDashboard();
+        quickstartPopoeverHelpers.closeQuickStartPopover();
+        homePageHelpers.verifyEmptyPageElements();
+    });
+
+    it('verify home page after complete onboarding with Banking data', function() {
+        setupHelpers.verifyDefaultPageElements;
+        setupHelpers.completeOnboardingSetup({
+            fullName: user.username,
+            emailAddress: user.email,
+            password: user.password,
+            confirmPassword: user.password,
+            isDemoApp: true
+        });
+
+        initialSetupHelpers.verifyDefaultPageElements(true);
+        initialSetupHelpers.completeOnboardingInitialSetup({
+            isDemoApp: true,
+            appType: APP_TYPE.MOBILE,
+            demoAppData: DATA_TYPE.BANKING,
+            timezone: 'Troll'
+        });
+
+        initialConsentHelpers.verifyDefaultPageElements();
+        initialConsentHelpers.completeOnboardingInitialConsent({
+            isEnableTacking: false,
+            isSubscribeToNewsletter: false
+        });
+        navigationHelpers.isNavigatedToDashboard();
+        quickstartPopoeverHelpers.closeQuickStartPopover();
+        homePageHelpers.verifyFullDataPageElements();
     });
 });
