6a7f4d1c1c770f5fe215af5f5d4fc0caebd016b9
umut top <umut.top@count.ly>
2024-01-09 08:36:03 +0000

    eslint fix

--- plugins/alerts/frontend/public/javascripts/countly.models.js
+++ plugins/alerts/frontend/public/javascripts/countly.models.js
@@ -379,7 +379,7 @@
                                     _canUpdate: countlyAuth.validateUpdate(FEATURE_NAME, countlyGlobal.member, list[j].app),
                                     _canDelete: countlyAuth.validateDelete(FEATURE_NAME, countlyGlobal.member, list[j].app),
                                 });
-                                tableData.push(rowData); 
+                                tableData.push(rowData);
                             }
                             context.commit("setInitialized", true);
                             context.commit("setAll", tableData);
