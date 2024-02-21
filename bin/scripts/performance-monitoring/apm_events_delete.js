b7e7666f6cd9017e08df4c94efc7df7fac29af3f
John-Weak <tusharbali11@gmail.com>
2024-02-21 13:31:04 +0000

    switch to drill db

--- bin/scripts/performance-monitoring/apm_events_delete.js
+++ bin/scripts/performance-monitoring/apm_events_delete.js
@@ -8,32 +8,31 @@ var pluginManager = require('../../../plugins/pluginManager.js');
 var crypto = require('crypto');
 
 const dry_run = false; //if set true, there will be only information outputted, but deletion will not be triggered.
-const APP_ID = ''; //YOUR_APP_ID
+const APP_ID = '' //YOUR_APP_ID
 
 if (dry_run) {
     console.log("This is a dry run");
     console.log('Nothing will be deleted');
 }
 
-pluginManager.dbConnection().then(async(countlyDb) => {
+Promise.all([pluginManager.dbConnection("countly"), pluginManager.dbConnection("countly_drill")]).then(async function([countlyDb, drillDb]) {
     try {
-        if (!APP_ID) {
-            throw new Error("APP_ID is not set");
-        }
-        console.log('Deleting APM events for app_id: ' + APP_ID);
+        console.log('Deleting APM events for app_id: ' + APP_ID)
         await Promise.all([
             countlyDb.collection("apm").remove({app_id: APP_ID}),
-            countlyDb.collection("drill_events" + crypto.createHash('sha1').update("[CLY]_apm_network" + APP_ID).digest('hex')).drop(),
-            countlyDb.collection("drill_events" + crypto.createHash('sha1').update("[CLY]_apm_device" + APP_ID).digest('hex')).drop(),
-        ]);
-        console.log("All done");
+            drillDb.collection("drill_events" + crypto.createHash('sha1').update("[CLY]_apm_network" + APP_ID).digest('hex')).drop(),
+            drillDb.collection("drill_events" + crypto.createHash('sha1').update("[CLY]_apm_device" + APP_ID).digest('hex')).drop(),
+        ])
+        console.log("All done")
     }
     catch (error) {
         console.log("ERROR: ");
         console.log(error);
         countlyDb.close();
+        drillDb.close();
     }
     finally {
         countlyDb.close();
+        drillDb.close();
     }
 });
