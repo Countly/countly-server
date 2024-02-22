c79e2381d374200e7591d4e0b111de589b54ede7
Cookiezaurs <sosina.anna@gmail.com>
2024-02-22 13:35:20 +0200

    eslint

--- bin/scripts/data-cleanup/remove_old_flows_collections.js
+++ bin/scripts/data-cleanup/remove_old_flows_collections.js
@@ -5,12 +5,12 @@ console.log("Removing old flows collections");
 pluginManager.dbConnection().then((countlyDb) => {
     countlyDb.collection('apps').find({}).toArray(function(err, apps) {
         function upgrade(app, done) {
-            countlyDb.collection('event_flows' + app._id).drop(function(){
-				console.log('event_flows' + app._id+" dropped");
-				countlyDb.collection('flows_cache' + app._id).drop(function(){
-					console.log('event_cache' + app._id+" dropped");
-					done();
-				});
+            countlyDb.collection('event_flows' + app._id).drop(function() {
+                console.log('event_flows' + app._id + " dropped");
+                countlyDb.collection('flows_cache' + app._id).drop(function() {
+                    console.log('event_cache' + app._id + " dropped");
+                    done();
+                });
             });
         }
         asyncjs.eachSeries(apps, upgrade, function() {
