8d2993229212731ce327abc6a943fd9e7dfdade4
Emre Üstere <emre.ustere@count.ly>
2024-03-08 11:07:13 +0000

    density api changes for unknown platforms

--- plugins/density/api/api.js
+++ plugins/density/api/api.js
@@ -22,14 +22,15 @@ const FEATURE_NAME = 'density';
             params.qstring.metrics._density = parseFloat(params.qstring.metrics._density).toFixed(2);
         }
         if (params.qstring.metrics && params.qstring.metrics._os && params.qstring.metrics._density) {
+            var custom_os = "[" + params.qstring.metrics._os + "]";
             if (common.os_mapping[params.qstring.metrics._os.toLowerCase()]) {
                 //for whatewer reason we go there twice. And on second time _density is already modified. Nested if to prevent error.
                 if (!params.qstring.metrics._density.startsWith(common.os_mapping[params.qstring.metrics._os.toLowerCase()])) {
                     params.qstring.metrics._density = common.os_mapping[params.qstring.metrics._os.toLowerCase()] + params.qstring.metrics._density;
                 }
             }
-            else if (!params.qstring.metrics._density.startsWith(params.qstring.metrics._os[0].toLowerCase())) {
-                params.qstring.metrics._density = params.qstring.metrics._os[0].toLowerCase() + params.qstring.metrics._density;
+            else if (!params.qstring.metrics._density.startsWith(custom_os)) {
+                params.qstring.metrics._density = custom_os + params.qstring.metrics._density;
             }
         }
         var predefinedMetrics = ob.predefinedMetrics;
