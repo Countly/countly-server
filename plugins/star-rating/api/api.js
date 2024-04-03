3a9f834e14b2dd405664d944cfeb2cebf1e6d6a2
Abhishek Singla <abhisheksingla74@gmail.com>
2024-04-03 13:09:22 +0000

    changes for consent

--- plugins/star-rating/api/api.js
+++ plugins/star-rating/api/api.js
@@ -181,7 +181,7 @@ const widgetPropertyPreprocessors = {
                 return links;
             }
             else {
-                return null;
+                return [];
             }
         }
     },
