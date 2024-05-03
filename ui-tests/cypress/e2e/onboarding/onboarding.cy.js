10d7b67647c0c789e069f75cb7d712887dbeffa6
can-angun <can1984@gmail.com>
2024-05-03 15:41:18 +0000

    Added button clicked control

--- ui-tests/cypress/e2e/onboarding/onboarding.cy.js
+++ ui-tests/cypress/e2e/onboarding/onboarding.cy.js
@@ -187,8 +187,7 @@ describe('Complete Onboarding', () => {
             isDemoApp: false,
             appType: APP_TYPE.MOBILE,
             appName: 'My Mobile App',
-            appKey: 'aaaaabe5c377f6ab830890e9d7d416970f5541a3',
-            timezone: 'Harare'
+            timezone: 'Istanbul'
         });
 
         initialConsentHelpers.completeOnboardingInitialConsent({
