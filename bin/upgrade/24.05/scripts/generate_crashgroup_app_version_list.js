9feb9758f9027af128b52efa2e25c9cccb16fbc1
Danu Widatama <widatama@gmail.com>
2024-06-06 08:36:00 +0700

    Update crash app version list script

--- bin/upgrade/24.05/scripts/generate_crashgroup_app_version_list.js
+++ bin/upgrade/24.05/scripts/generate_crashgroup_app_version_list.js
@@ -3,94 +3,62 @@
 // This script will take all of the keys from the object above and store it into an array
 // With the array, it will be possible to query crashgroup with partial app version, e.g. the query '4.2' will get crashgroups that have app version '4.2.0', '4.2.1', etc
 
-var pluginManager = require('../../../../plugins/pluginManager.js');
+const asyncjs = require('async');
+
+const pluginManager = require('../../../../plugins/pluginManager.js');
 
 console.log('Updating crashgroup data');
 
-pluginManager.dbConnection().then(async (countlyDb) => {
-    console.log('Generating updates');
+pluginManager.dbConnection().then((countlyDb) => {
+    const BATCH_SIZE = 200;
 
-    const apps = await countlyDb.collection('apps').find({}).project({_id: 1}).toArray();
-    const updates = {};
+    countlyDb.collection('apps').find({}).toArray(function(err, apps) {
+        function update(app, done) {
+            console.log(`Updating crashes for ${app.name}`);
 
-    for (let idx = 0; idx < apps.length; idx += 1) {
-        const crashgroupCollection = `app_crashgroups${apps[idx]._id}`;
-        const crashgroups = await countlyDb.collection(crashgroupCollection)
-            .find({ _id: { $ne: 'meta' } })
-            .project({ _id: 1, app_version: 1 })
-            .toArray();
+            const cursor = countlyDb.collection(`app_crashgroups${app._id}`)
+                .find({ _id: { $ne: 'meta' } }, { fields: { _id: 1, app_version: 1 } });
+            let requests = [];
 
-        updates[apps[idx]._id] = crashgroups.reduce((acc, crashgroup) => {
-            acc.push({
-                updateOne: {
-                    filter: { _id: crashgroup._id },
-                    update: {
-                        $addToSet: {
-                            app_version_list: {
-                                $each: Object.keys(crashgroup.app_version).map((item) => item.replace(/:/g, '.')),
+            cursor.forEach(function(crashgroup) { 
+                requests.push({ 
+                    updateOne: {
+                        filter: { _id: crashgroup._id },
+                        update: {
+                            $addToSet: {
+                                app_version_list: {
+                                    $each: Object.keys(crashgroup.app_version).map((item) => item.replace(/:/g, '.')),
+                                },
                             },
                         },
                     },
-                },
-            });
+                });
 
-            return acc;
-        }, []);
-    }
-
-    console.log('Applying updates');
-
-    const BATCH_SIZE = 200;
-    let errCount = 0;
-
-    for (let idx = 0; idx < apps.length; idx += 1) {
-        const appId = apps[idx]._id;
-        const crashgroupCollection = `app_crashgroups${appId}`;
-
-        if (updates[appId]) {
-            let buffer = [];
-
-            for (let idy = 0; idy < updates[appId].length; idy += 1) {
-                buffer.push(updates[appId][idy]);
-
-                if (buffer.length === BATCH_SIZE || idy === updates[appId].length - 1) {
-                    try {
-                        await countlyDb.collection(crashgroupCollection).bulkWrite(buffer, { ordered: false });
-                    }
-                    catch (err) {
-                        errCount += 1;
-                        console.error(`Failed updating collection ${crashgroupCollection}`, err);
-                    }
-                    finally {
-                        buffer.length = 0;
-                    }
+                if (requests.length === BATCH_SIZE) {
+                    countlyDb.collection(`app_crashgroups${app._id}`).bulkWrite(requests, function(err){
+                        if (err) {
+                            console.error(err);
+                        }
+                    });
+                    requests = [];
                 }
-            }
+            }, function() {
+                if(requests.length > 0) {
+                    countlyDb.collection(`app_crashgroups${app._id}`).bulkWrite(requests, function(err){
+                        if (err) {
+                            console.error(err);
+                        }
+                        done();
+                    });
+                }
+                else {
+                    done();
+                }
+            });
         }
-
-        if (errCount === 0) {
-            try {
-                await countlyDb.collection(crashgroupCollection).updateOne(
-                    { _id: 'meta' },
-                    { $set: { app_version_list_added: true } },
-                );
-            }
-            catch (err) {
-                console.error(`Failed updating collection ${crashgroupCollection} meta`, err);
-            }
-
-            try {
-                await countlyDb.collection(crashgroupCollection).ensureIndex({ app_version_list: 1 }, { background: true });
-            }
-            catch (err) {
-                console.error(`Failed updating collection ${crashgroupCollection} index`, err);
-            }
-        }
-        else {
-            console.error(`${errCount} batches failed when updating collection ${crashgroupCollection}`);
-        }
-    }
-
-    countlyDb.close();
-    console.log('Crashgroup data update done');
+        asyncjs.eachSeries(apps, update, function() {
+            console.log("Crashgroup data update finished");
+            countlyDb.close();
+        });
+    });
 });
