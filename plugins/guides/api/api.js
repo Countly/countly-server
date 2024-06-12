bc2a1c8134488a7b54530badd97a8222e41ae422
Emre Üstere <emre.ustere@count.ly>
2024-06-12 14:31:05 +0000

    api file changes

--- plugins/guides/api/api.js
+++ plugins/guides/api/api.js
@@ -3,11 +3,6 @@ var exported = {},
     common = require('../../../api/utils/common.js');
 
 (function() {
-    //write api call
-    plugins.register("/i", function(/*ob*/) {
-        //process sdk request here
-    });
-
     plugins.register("/i/guides/viewed", function(ob) {
         var params = ob.params;
         updateMemberViewedGuides(common.db, params.qstring.user_id, params);
@@ -18,17 +13,15 @@ var exported = {},
     * @param {string} user_id - user for which to update settings
     * @param {object} params - params object
     */
-    function updateMemberViewedGuides(db, user_id, params) {
+    function updateMemberViewedGuides(db, user_id) {
         var callback = callback || null;
         var update = {"viewedGuides": true};
-        db.collection("members").update({ _id: db.ObjectID(user_id) }, { $set: update }, { upsert: true }, function(err, /*res*/) {
+        db.collection("members").update({ _id: db.ObjectID(user_id) }, { $set: update }, { upsert: true }, async function(err, res) {
             if (err) {
-                // common.returnMessage(params, 503, 'Save note failed');
-                // return false;
+                common.returnMessage(err, 503, 'Failed to update member');
             }
             else {
-                common.returnMessage(params, 200, 'Succesfuly updated member');
-                return true;
+                common.returnMessage(res, 200, 'Succesfuly updated member');
             }
         });
     }
