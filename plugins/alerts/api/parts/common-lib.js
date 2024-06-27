d1cd4e74745af32ecb34eb74a208b7665d2d1b8d
Yavuz Yilmaz <yavuz.yilmaz1@outlook.com>
2024-06-27 11:35:28 +0300

    SER-1505 number of digits fixed

--- plugins/alerts/api/parts/common-lib.js
+++ plugins/alerts/api/parts/common-lib.js
@@ -170,10 +170,10 @@ async function compileEmail(result) {
     const host = await new Promise((res, rej) => mail.lookup((err, _host) => err ? rej(err) : res(_host)));
     const metrics = [];
     if (metricValue) {
-        metrics.push({ key: "Value now", value: metricValue });
+        metrics.push({ key: "Value now", value: formatMetricValue(metricValue) });
     }
     if (metricValueBefore) {
-        metrics.push({ key: "Value before", value: metricValueBefore });
+        metrics.push({ key: "Value before", value: formatMetricValue(metricValueBefore) });
     }
     return EMAIL_TEMPLATE({
         title: `Countly Alert`,
@@ -188,6 +188,15 @@ async function compileEmail(result) {
             data: metrics
         }]
     });
+}
+
+function formatMetricValue(value) {
+    if (typeof value === 'number' && value === parseFloat(value.toFixed(2))) {
+        return value;
+    } else if (typeof value === 'number') {
+        return parseFloat(value.toFixed(2));
+    }
+    return value;
 }
 
 /**
