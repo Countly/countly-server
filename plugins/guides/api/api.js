caf41f4acdfe10b6879ecf71cb095dd0278bff85
Emre Üstere <27005957+emreustere@users.noreply.github.com>
2024-07-01 14:54:35 +0300

    Update api.js

--- plugins/guides/api/api.js
+++ plugins/guides/api/api.js
@@ -1,23 +1,6 @@
-var exported = {},
-    plugins = require('../../pluginManager.js'),
-    common = require('../../../api/utils/common.js');
+var plugins = require('../../pluginManager.js'),
 
-(function() {
-    plugins.register("/i/guides/viewed", function(ob) {
-        var db = common.db,
-            user_id = ob.params.qstring.user_id,
-            update = {"viewedGuides": true};
-
-        // check for rights and what we do for table updates persistent settings etc in other places
-        db.collection("members").update({ _id: db.ObjectID(user_id) }, { $set: update }, { upsert: true }, function(err, res) {
-            if (err) {
-                common.returnMessage(err, 503, 'Failed to update member');
-            }
-            else {
-                common.returnMessage(res, 200, 'Succesfuly updated member');
-            }
-        });
-    });
-}(exported));
-
-module.exports = exported;
+//write api call
+plugins.register("/i", function(/*ob*/) {
+    //process sdk request here
+});
