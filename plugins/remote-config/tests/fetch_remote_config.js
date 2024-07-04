b75ca494a44654da455f36246ee44d14a6626a84
Danu Widatama <widatama@gmail.com>
2024-07-04 21:27:39 +0700

    [remote-config] Add tests for targetting

--- plugins/remote-config/tests/fetch_remote_config.js
+++ plugins/remote-config/tests/fetch_remote_config.js
@@ -1,6 +1,8 @@
 const spt = require('supertest');
 const should = require('should');
+
 const testUtils = require('../../../test/testUtils');
+const remoteConfig = require('../api/parts/rc');
 
 const request = spt(testUtils.url);
 const API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
@@ -9,7 +11,9 @@ const APP_ID = testUtils.get("APP_ID");
 
 const AMOUNT_OF_KEYS = 5;
 const PARAMETER_PREFIX = 'fetch_remote_config_param_';
+const CONDITION_PREFIX = 'fetchremoteconfigcond';
 const VALUE_PREFIX = 'value_';
+const TARGETTED_USER_ID = 'targetted_user';
 
 before(async() => {
     for (let count = 0; count < AMOUNT_OF_KEYS; count += 1) {
@@ -131,5 +135,121 @@ describe('Fetch remote config', () => {
             should(resp.body).containEql({ [`${PARAMETER_PREFIX}${count}`]: `${VALUE_PREFIX}${count}`});
         }
     });
-});
 
+    describe('Targetting', () => {
+        before(async() => {
+            await request
+                .post('/i/remote-config/add-condition')
+                .send({
+                    api_key: API_KEY_ADMIN,
+                    app_id: APP_ID,
+                    app_key: APP_KEY,
+                    condition: JSON.stringify({
+                        condition_name: `${CONDITION_PREFIX}0`,
+                        condition_color: 1,
+                        condition: { did: { $in: [TARGETTED_USER_ID] } },
+                        condition_definition: `ID = ${TARGETTED_USER_ID}`,
+                        condition_description: '-',
+                        seed_value: '',
+                    }),
+                })
+                .expect('Content-Type', /json/);
+
+            const resp = await request
+                .get('/o')
+                .query({
+                    api_key: API_KEY_ADMIN,
+                    app_id: APP_ID,
+                    app_key: APP_KEY,
+                    method: 'remote-config',
+                })
+                .expect(200);
+
+            const condition_id = resp.body.conditions.find((condition) => condition.condition_name === `${CONDITION_PREFIX}0`)._id;
+
+            await request
+                .post('/i/remote-config/add-parameter')
+                .send({
+                    api_key: API_KEY_ADMIN,
+                    app_id: APP_ID,
+                    app_key: APP_KEY,
+                    parameter: JSON.stringify({
+                        parameter_key: `${PARAMETER_PREFIX}conditioned`,
+                        default_value: `${VALUE_PREFIX}default`,
+                        description: '-',
+                        conditions: [{
+                            condition_id,
+                            value: `${VALUE_PREFIX}conditioned`,
+                        }],
+                        status: 'Running',
+                        expiry_dttm: null,
+                    }),
+                })
+                .expect('Content-Type', /json/);
+        });
+
+        it('Should match targetted user', () => {
+            const targettedUser = {
+                _id: '1c5c91e1dd594d457a656fad1e55d0cf2a3f0601',
+                uid: '13',
+                did: 'targetted_user',
+            };
+            const query = { did: { $in: ['targetted_user'] } };
+
+            should(remoteConfig.processFilter(targettedUser, query)).equal(true);
+        });
+
+        it('Should match targetted user', () => {
+            const targettedUser = {
+                _id: '1c5c91e1dd594d457a656fad1e55d0cf2a3f0601',
+                uid: '13',
+                did: 'targetted_user',
+                cc: 'UK',
+            };
+            const query = { 'up.cc': { $exists: true } };
+
+            should(remoteConfig.processFilter(targettedUser, query)).equal(true);
+        });
+
+        it('Should not match non targetted user', () => {
+            const nonTargettedUser = {
+                _id: '1c5c91e1dd594d457a656fad1e55d0cf2a3f0601',
+                uid: '13',
+                did: 'non_targetted_user',
+            };
+            const query = { did: { $in: ['targetted_user'] } };
+
+            should(remoteConfig.processFilter(nonTargettedUser, query)).equal(false);
+        });
+
+        it('Should fetch remote config with default value', async() => {
+            const resp = await request
+                .get('/o/sdk')
+                .query({
+                    api_key: API_KEY_ADMIN,
+                    app_id: APP_ID,
+                    app_key: APP_KEY,
+                    device_id: 'device_0',
+                    method: 'fetch_remote_config',
+                })
+                .expect(200);
+
+            should(resp.body).containEql({ [`${PARAMETER_PREFIX}conditioned`]: `${VALUE_PREFIX}default`});
+        });
+
+        it('Should fetch remote config with conditioned value', async() => {
+            const resp = await request
+                .get('/o/sdk')
+                .query({
+                    api_key: API_KEY_ADMIN,
+                    app_id: APP_ID,
+                    app_key: APP_KEY,
+                    device_id: TARGETTED_USER_ID,
+                    method: 'fetch_remote_config',
+                })
+                .expect(200);
+
+            should(resp.body).containEql({ [`${PARAMETER_PREFIX}conditioned`]: `${VALUE_PREFIX}conditioned`});
+        });
+    });
+});
