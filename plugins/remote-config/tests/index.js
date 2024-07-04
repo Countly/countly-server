a387c690d09fcf464e7a2cd6b3aa630701041399
John-Weak <tusharbali11@gmail.com>
2024-07-04 11:34:45 +0530

    ab method test

--- plugins/remote-config/tests/index.js
+++ plugins/remote-config/tests/index.js
@@ -2,3 +2,4 @@ require('./add-remote-config.js');
 require('./fetch_remote_config.js');
 require('./remote-config-endpoint.js');
 require('./condition-endpoints.js');
+require('./fetch-params-from-ab.js');
