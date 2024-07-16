a01968e2c52b860177c240dc849cb56795d40350
Emre Üstere <emre.ustere@count.ly>
2024-07-16 11:27:49 +0000

    Converted to covered query, removed app timezone offset for cd field, also querying internal events

--- bin/scripts/export-data/drill_monthly_doc_counts.js
+++ bin/scripts/export-data/drill_monthly_doc_counts.js
@@ -16,8 +16,9 @@ const drillCommon = require('../../../plugins/drill/api/common.js');
 const countlyCommon = require('../../../api/lib/countly.common.js');
 
 const app_list = []; //valid app_ids here. If empty array passed, script will process all apps.
-const path = './'; //path to save csv files
-const period = '60days'; //supported values are 60days, 30days, 7days, yesterday, all, or [startMiliseconds, endMiliseconds] as [1417730400000,1420149600000]
+const internalDrillEvents = ["[CLY]_session", "[CLY]_crash", "[CLY]_view", "[CLY]_action", "[CLY]_push_action", "[CLY]_push_sent", "[CLY]_star_rating", "[CLY]_nps", "[CLY]_survey", "[CLY]_apm_network", "[CLY]_apm_device", "[CLY]_consent"];
+const pathToFile = './'; //path to save csv files
+const period = 'all'; //supported values are 60days, 30days, 7days, yesterday, all, or [startMiliseconds, endMiliseconds] as [1417730400000,1420149600000]
 const headerMap = {
     "app_name": "App Name",
     "event_name": "Event Name",
@@ -33,11 +34,8 @@ Promise.all([pluginManager.dbConnection("countly"), pluginManager.dbConnection("
             return close();
         }
         else {
-            if (period !== 'all') {
-                countlyCommon.setPeriod(period);
-            }
             // CREATE WRITE STREAM
-            const eventDetailsWriteStream = fs.createWriteStream(path + `/monthly_document_counts.csv`);
+            const eventDetailsWriteStream = fs.createWriteStream(pathToFile + `monthly_document_counts.csv`);
             var isFirst = true;
             // CREATE PARSER
             const fileParser = new Parser({fields: Object.keys(headerMap), header: false});
@@ -45,37 +43,31 @@ Promise.all([pluginManager.dbConnection("countly"), pluginManager.dbConnection("
             for (let i = 0; i < apps.length; i++) {
                 var app = apps[i];
                 console.log(i + 1, ") Processing app:", app.name);
-                // SET APP TIMEZONE
-                countlyCommon.setTimezone(app.timezone);
-                var periodObj = countlyCommon.periodObj;
                 try {
-                    // GET EVENTS FOR CURRENT APP
+                    // GET EVENTS FOR CURRENT APP INCLUDING COUNTLY INTERNAL EVENTS
                     var events = await countlyDb.collection("events").findOne({"_id": ObjectId(app._id)});
                     events = events && events.list || [];
+                    if (internalDrillEvents && internalDrillEvents.length) {
+                        events.push(...internalDrillEvents);
+                    }
                     // PROCESS EACH EVENT TO GET COLLECTION NAME
                     for (let j = 0; j < events.length; j++) {
                         var event = events[j];
                         console.log("Processing event:", event);
                         var collectionName = drillCommon.getCollectionName(event, app._id);
-
                         // SET PERIOD AND QUERY
                         let query = {};
                         if (period !== 'all') {
+                            var periodObj = countlyCommon.periodObj;
                             let cd = {};
-                            let tmpArr = periodObj.currentPeriodArr[0].split(".");
 
+                            let tmpArr = periodObj.currentPeriodArr[0].split(".");
                             cd.$gte = moment(new Date(Date.UTC(parseInt(tmpArr[0]), parseInt(tmpArr[1]) - 1, parseInt(tmpArr[2]))));
-                            if (app.timezone) {
-                                cd.$gte.tz(app.timezone);
-                            }
                             cd.$gte = cd.$gte.valueOf() - cd.$gte.utcOffset() * 60000;
                             cd.$gte = moment(cd.$gte).toDate();
 
                             tmpArr = periodObj.currentPeriodArr[periodObj.currentPeriodArr.length - 1].split(".");
                             cd.$lt = moment(new Date(Date.UTC(parseInt(tmpArr[0]), parseInt(tmpArr[1]) - 1, parseInt(tmpArr[2])))).add(1, 'days');
-                            if (app.timezone) {
-                                cd.$lt.tz(app.timezone);
-                            }
                             cd.$lt = cd.$lt.valueOf() - cd.$lt.utcOffset() * 60000;
                             cd.$lt = moment(cd.$lt).toDate();
 
@@ -87,13 +79,7 @@ Promise.all([pluginManager.dbConnection("countly"), pluginManager.dbConnection("
                             const eventName = event;
                             var result = await drillDb.collection(collectionName).aggregate([
                                 {
-                                    $match: query,
-                                },
-                                {
-                                    $set: {
-                                        app_name: appName,
-                                        event_name: eventName,
-                                    }
+                                    $match: { "cd": { $ne: 0 } }
                                 },
                                 {
                                     $group: {
@@ -105,9 +91,13 @@ Promise.all([pluginManager.dbConnection("countly"), pluginManager.dbConnection("
                                         },
                                         count: {
                                             $sum: 1
-                                        },
-                                        app_name: { $first: "$app_name" },
-                                        event_name: { $first: "$event_name" }
+                                        }
+                                    }
+                                },
+                                {
+                                    $set: {
+                                        app_name: appName,
+                                        event_name: eventName,
                                     }
                                 },
                                 {
@@ -120,7 +110,6 @@ Promise.all([pluginManager.dbConnection("countly"), pluginManager.dbConnection("
                                     }
                                 }
                             ], {allowDiskUse: true}).toArray();
-
                             // SAVE TO FILE
                             if (isFirst) {
                                 eventDetailsWriteStream.write(Object.values(headerMap).join(",") + "\n");
