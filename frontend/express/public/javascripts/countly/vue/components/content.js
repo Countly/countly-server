556f6bf77a1b445075792374e45c36d26b12b848
Pınar Genç <gncpin@hotmail.com>
2024-07-18 14:43:17 +0300

    [SER-1601] Global Component - Full screen editing drawer like component

--- frontend/express/public/javascripts/countly/vue/components/content.js
+++ frontend/express/public/javascripts/countly/vue/components/content.js
@@ -236,4 +236,246 @@
                     </div>`,
     }));
 
+    Vue.component("cly-content-steps", countlyVue.components.create({
+        props: {
+            header: {
+                type: String,
+                required: false,
+                default: null
+            },
+        },
+        data() {
+            return {
+                activeSection: ["section"]
+            };
+        },
+        methods: {
+        },
+        template: `
+            <div class="cly-vue-content-builder__layout-steps">
+                <div>
+                    <el-collapse v-model="activeSection">
+                        <el-collapse-item :title="header" name="section">
+                            <slot name="content-builder-layout-steps"></slot>
+                        </el-collapse-item>
+                    </el-collapse>  
+                </div>
+            </div>
+        `,
+    }));
+
+    Vue.component("cly-content-step", countlyVue.components.create({
+        props: {
+            value: {
+                type: [String, Number, Boolean, Object],
+                default: null
+            },
+            subHeader: {
+                type: String,
+                required: false,
+                default: null
+            },
+            label: {
+                type: String,
+                required: false,
+                default: null
+            },
+            inputType: {
+                type: String,
+                required: false,
+                default: 'text'
+            },
+            options: {
+                type: Array,
+                required: false,
+                default: () => []
+            },
+            position: {
+                type: String,
+                required: false,
+                default: 'horizontal'
+            },
+            width: {
+                type: String,
+                required: false,
+                default: null
+            },
+            inputProps: {
+                type: Object,
+                required: false,
+                default: () => ({})
+            }
+        },
+        data() {
+            return {
+                localValue: this.value,
+            };
+        },
+        watch: {
+            value: function(newValue) {
+                this.localValue = newValue;
+            },
+            localValue: function(newValue) {
+                this.$emit('input', newValue);
+            }
+        },
+        methods: {
+            updateValue: function(id, newValue) {
+                this.$set(this.localValues, id, newValue);
+                this.$emit('input', { ...this.localValues });
+            },
+            getComponentType: function(type) {
+                const mapping = {
+                    dropdown: 'el-select',
+                    input: 'el-input',
+                    switch: 'el-switch',
+                    'color-picker': 'cly-colorpicker',
+                    'input-number': 'el-input-number',
+                };
+                return mapping[type] || 'div';
+            }
+        },
+        created: function() {
+        },
+        template: `
+            <div class="cly-vue-content-builder__layout-step">
+                <div v-if="subHeader" class="cly-vue-content-builder__layout-step__sub-header color-cool-gray-50">{{ subHeader }}</div>
+                <div class="cly-vue-content-builder__layout-step__element bu-is-flex bu-is-justify-content-space-between bu-is-align-items-center" :class="{'bu-is-flex-direction-column bu-is-align-items-baseline': position !== 'horizontal' }" :style="[position !== 'horizontal' ? {'gap': '8px'}: {}]">
+                    <div v-if="label" class="cly-vue-content-builder__layout-step__label">{{ label }}</div>
+                    <slot name="content-builder-layout-step">
+                        <component
+                            :is="getComponentType(inputType)"
+                            v-bind="inputProps"
+                            v-model="localValue"
+                            @input="updateValue"
+                            class="cly-vue-content-builder__layout-step__component"
+                            :style="[ position !== 'horizontal' ? {\'width\':  \'100%\'} : {\'width\': width + \'px\'}]"
+                        >
+                        <template v-if="inputProps && inputProps.append" v-slot:append>{{inputProps.append}}</template>
+                        <el-option
+                            v-if="inputType === 'dropdown'"
+                            v-for="option in options"
+                            :key="option.value"
+                            :label="option.label"
+                            :value="option.value"
+                            class="cly-vue-content-builder__layout-step__option"
+                        ></el-option>
+                        </component>
+                    </slot>
+                </div>
+            </div>
+            `,
+    }));
+
+    Vue.component("cly-option-swapper", countlyVue.components.BaseComponent.extend({
+        mixins: [countlyVue.mixins.i18n],
+        props: {
+            value: {
+                type: [String, Number],
+                default: null
+            },
+            items: {
+                type: Array,
+                default: function() {
+                    return [];
+                }
+            },
+            activeColorCode: {
+                type: String,
+                default: '#0166D6'
+            },
+            width: {
+                type: String,
+                default: '100'
+            }
+        },
+        data: function() {
+            return {
+                selectedValue: this.items[0].value || 0
+            };
+        },
+        methods: {
+            numberChange: function(val) {
+                this.selectedValue = val;
+                this.$emit('input', this.selectedValue);
+            }
+        },
+        created: function() {
+            this.selectedValue = this.value || this.items[0].value || 0;
+        },
+        template: '<div>\
+                        <div class="bu-is-flex cly-option-swapper" :style="{\'width\': width + \'px\'}">\
+                            <div v-for="(item, index) in items" :key="item.value" class="cly-option-swapper__each-box-wrapper">\
+                                <div :style="item.value === selectedValue ? {\'background-color\': activeColorCode } : {}" :class="{ \'cly-option-swapper__active \': item.value === selectedValue, \'cly-option-swapper__first\' : index === 0, \'cly-option-swapper__last\' : index === (items.length - 1) }" class="cly-option-swapper__each" @click="numberChange(item.value)">\
+                                    <i v-if="item.icon" :class="item.icon" :style="item.value === selectedValue ? {\'color\': \'#0166d6\' } : {\'color\': \'#000\' }"></i>\
+                                    <span v-else :style="item.value === selectedValue ? {\'color\': \'#0166d6\' } : {\'color\': \'#000\' }" class="text-medium">{{ item.text }}</span>\
+                                </div>\
+                            </div>\
+                        </div>\
+                  </div>'
+    }));
+
+    Vue.component("cly-device-selector", countlyVue.components.BaseComponent.extend({
+        props: {
+            value: {
+                type: String,
+                default: null
+            },
+            selectedBackground: {
+                type: String,
+                default: '#383A3F'
+            },
+            width: {
+                type: String,
+                default: '108px'
+            },
+            showDesktop: {
+                type: Boolean,
+                default: true
+            },
+            showTablet: {
+                type: Boolean,
+                default: true
+            },
+            showMobile: {
+                type: Boolean,
+                default: true
+            }
+        },
+        computed: {
+            selectedDevice: {
+                get() {
+                    return this.value;
+                },
+                set(newValue) {
+                    this.$emit('input', newValue);
+                }
+            }
+        },
+        template: `
+            <div class="cly-vue-device-selector bu-is-flex bu-is-justify-content-space-between" :style="{ width: width }">
+                <div v-if="showMobile" 
+                     @click="selectedDevice = 'mobile'" 
+                     :style="{ backgroundColor: selectedDevice === 'mobile' ? selectedBackground : 'transparent' }"
+                     :class="[selectedDevice === 'mobile' ? 'color-white' : '']"
+                     class="cly-vue-device-selector__device bu-p-2">
+                    <i class="cly-io cly-io-device-mobile"></i>
+                </div>
+                <div v-if="showTablet" 
+                     @click="selectedDevice = 'tablet'" 
+                     :style="{ backgroundColor: selectedDevice === 'tablet' ? selectedBackground : 'transparent' }"
+                     :class="[selectedDevice === 'tablet' ? 'color-white' : '']"
+                     class="cly-vue-device-selector__device bu-p-2">
+                    <i class="cly-io cly-io-device-tablet"></i>
+                </div>
+                <div v-if="showDesktop" 
+                     @click="selectedDevice = 'desktop'" 
+                     :style="{ backgroundColor: selectedDevice === 'desktop' ? selectedBackground : 'transparent' }"
+                     :class="[selectedDevice === 'desktop' ? 'color-white' : '']"
+                     class="cly-vue-device-selector__device bu-p-2">
+                    <i class="cly-io cly-io-desktop-computer"></i>
+                </div>
+            </div>
+        `
+    }));
 }(window.countlyVue = window.countlyVue || {}));
