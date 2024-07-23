854f9377896371c1685df84b4f48b468fa0fbca7
Emre Üstere <emre.ustere@count.ly>
2024-07-23 08:31:45 +0000

    defaults and set values for limits of events and views

--- bin/scripts/export-data/setting_limits_and_real_values.js
+++ bin/scripts/export-data/setting_limits_and_real_values.js
@@ -7,14 +7,14 @@
 
 // const moment = require('moment-timezone');
 const { ObjectId } = require('mongodb');
-const fs = require('fs');
+// const fs = require('fs');
 
 const pluginManager = require('../../../plugins/pluginManager.js');
 // const drillCommon = require('../../../plugins/drill/api/common.js');
 // const countlyCommon = require('../../../api/lib/countly.common.js');
 
 const app_list = []; //valid app_ids here. If empty array passed, script will process all apps.
-const pathToFile = './'; //path to save csv files
+// const pathToFile = './'; //path to save csv files
 // const period = 'all'; //supported values are 60days, 30days, 7days, yesterday, all, or [startMiliseconds, endMiliseconds] as [1417730400000,1420149600000]
 // const MAX_RETRIES = 5;
 
@@ -52,8 +52,6 @@ Promise.all([pluginManager.dbConnection("countly"), pluginManager.dbConnection("
             return close();
         }
         else {
-            // CREATE WRITE STREAM
-            const WriteStream = fs.createWriteStream(pathToFile + `event_keys.csv`);
             // LOOP APPS FOR EACH REQUIREMENT
             for (let i = 0; i < apps.length; i++) {
                 var app = apps[i];
@@ -79,15 +77,13 @@ Promise.all([pluginManager.dbConnection("countly"), pluginManager.dbConnection("
 
                     app_results['Event Keys'] = {"default": defaultVal, "set": currentVal, "real": realVal};
 
-                    // MOST 3 SEGMENTED EVENTS
+                    // SEGMENTS IN ONE EVENT
                     defaultVal = DEFAULTS.event_segment_limit;
 
-                    let currentEventSegmentLimit = await countlyDb.collection("plugins").findOne({"_id": 'plugins'});
-                    currentEventSegmentLimit = currentEventSegmentLimit && currentEventSegmentLimit.api && currentEventSegmentLimit.api.event_segmentation_limit || [];
+                    let currentEventSegmentLimit = pluginsCollectionPlugins && pluginsCollectionPlugins.api && pluginsCollectionPlugins.api.event_segmentation_limit || [];
                     currentVal = currentEventSegmentLimit;
 
                     let eventSegments = eventsCollectionPerApp && eventsCollectionPerApp.segments || {};
-
                     realVal = Object.entries(eventSegments)
                         .sort((a, b) => b[1].length - a[1].length)
                         .map(([key, value]) => {
@@ -97,114 +93,42 @@ Promise.all([pluginManager.dbConnection("countly"), pluginManager.dbConnection("
                     app_results['Event Segments'] = {"default": defaultVal, "set": currentVal, "real": realVal};
 
                     // UNIQUE EVENT SEGMENT VALUES FOR 1 SEGMENT
+                    defaultVal = DEFAULTS.event_segment_value_limit;
+
+                    let currentEventSegmentValueLimit = pluginsCollectionPlugins && pluginsCollectionPlugins.api && pluginsCollectionPlugins.api.event_segmentation_value_limit || [];
+                    currentVal = currentEventSegmentValueLimit;
+
+                    // real values for event segment values
 
                     // UNIQUE VIEV NAMES
+                    defaultVal = DEFAULTS.view_limit;
+
+                    let currentViewLimit = pluginsCollectionPlugins && pluginsCollectionPlugins.api && pluginsCollectionPlugins.api.view_limit || [];
+                    currentVal = currentViewLimit;
 
                     // VIEW NAME LENGTH LIMIT
+                    defaultVal = DEFAULTS.view_name_limit;
+
+                    let currentViewNameLimit = pluginsCollectionPlugins && pluginsCollectionPlugins.api && pluginsCollectionPlugins.api.view_name_limit || [];
+                    currentVal = currentViewNameLimit;
 
                     // SEGMENTS IN ONE VIEW
+                    defaultVal = DEFAULTS.view_segment_limit;
+
+                    let currentViewSegmentLimit = pluginsCollectionPlugins && pluginsCollectionPlugins.api && pluginsCollectionPlugins.api.segment_limit || [];
+                    currentVal = currentViewSegmentLimit;
 
                     // VIEW SEGMENT'S UNIQUE VALUES
+                    defaultVal = DEFAULTS.view_segment_value_limit;
+
+                    let currentViewSegmentValueLimit = pluginsCollectionPlugins && pluginsCollectionPlugins.api && pluginsCollectionPlugins.api.segment_value_limit || [];
+                    currentVal = currentViewSegmentValueLimit;
 
                     // USER PROPERTIES
 
                     // VALUES IN AN ARRAY FOR ONE USER PROPERTY
 
 
-                    // PROCESS EACH EVENT TO GET COLLECTION NAME
-                    // for (let j = 0; j < events.length; j++) {
-                    //     var event = events[j];
-                    //     console.log("Processing event:", event);
-                    //     var collectionName = drillCommon.getCollectionName(event, app._id);
-                    //     // SET PERIOD AND QUERY
-                    //     let query = {};
-                    //     if (period !== 'all') {
-                    //         var periodObj = countlyCommon.periodObj;
-                    //         let cd = {};
-
-                    //         let tmpArr = periodObj.currentPeriodArr[0].split(".");
-                    //         cd.$gte = moment(new Date(Date.UTC(parseInt(tmpArr[0]), parseInt(tmpArr[1]) - 1, parseInt(tmpArr[2]))));
-                    //         cd.$gte = cd.$gte.valueOf() - cd.$gte.utcOffset() * 60000;
-                    //         cd.$gte = moment(cd.$gte).toDate();
-
-                    //         tmpArr = periodObj.currentPeriodArr[periodObj.currentPeriodArr.length - 1].split(".");
-                    //         cd.$lt = moment(new Date(Date.UTC(parseInt(tmpArr[0]), parseInt(tmpArr[1]) - 1, parseInt(tmpArr[2])))).add(1, 'days');
-                    //         cd.$lt = cd.$lt.valueOf() - cd.$lt.utcOffset() * 60000;
-                    //         cd.$lt = moment(cd.$lt).toDate();
-
-                    //         query.cd = cd;
-                    //     }
-                    //     else {
-                    //         query.cd = { $ne: 0 };
-                    //     }
-                    //     // FETCH, TRANSFORM, AND SET DATA
-                    //     try {
-                    //         const appName = app.name;
-                    //         const eventName = event;
-                    //         var isEstablished = false;
-
-                    //         // ESTABLISH CONNECTION WITH DRILL DB BEFORE RUNNING QUERY
-                    //         for (let attempt = 1; attempt <= MAX_RETRIES; attempt++) {
-                    //             try {
-                    //                 let pingResult = await drillDb.command({ ping: 1 });
-                    //                 if (pingResult.ok) {
-                    //                     isEstablished = true;
-                    //                     break;
-                    //                 }
-                    //             }
-                    //             catch (error) {
-                    //                 console.log("Closing and reestablishing database connection");
-                    //                 await drillDb.close();
-                    //                 drillDb = await pluginManager.dbConnection("countly_drill");
-                    //             }
-                    //         }
-
-                    //         // RUN QUERY IF ESTABLISHED, OR SKIP EVENT IF NOT
-                    //         if (isEstablished) {
-                    //             var result = await drillDb.collection(collectionName).aggregate([
-                    //                 {
-                    //                     $match: query
-                    //                 },
-                    //                 {
-                    //                     $group: {
-                    //                         _id: {
-                    //                             $dateToString: {
-                    //                                 format: "%Y-%m",
-                    //                                 date: "$cd"
-                    //                             }
-                    //                         },
-                    //                         count: {
-                    //                             $sum: 1
-                    //                         }
-                    //                     }
-                    //                 },
-                    //                 {
-                    //                     $set: {
-                    //                         app_name: appName,
-                    //                         event_name: eventName,
-                    //                     }
-                    //                 },
-                    //                 {
-                    //                     $project: {
-                    //                         _id: 0,
-                    //                         app_name: "$app_name",
-                    //                         event_name: "$event_name",
-                    //                         month: "$_id",
-                    //                         doc_count: "$count"
-                    //                     }
-                    //                 }
-                    //             ], {allowDiskUse: true}).toArray();
-                    //             // SAVE TO FILE
-                    //             if (result && result.length > 0) {
-                    //                 eventKeysWriteStream.write(fileParser.parse(result));
-                    //                 eventKeysWriteStream.write("\n");
-                    //             }
-                    //         }
-                    //     }
-                    //     catch (err) {
-                    //         console.log("Error aggregating data: ", err);
-                    //     }
-                    // }
                     all_results.push(app_results);
                 }
                 catch (err) {
@@ -212,6 +136,9 @@ Promise.all([pluginManager.dbConnection("countly"), pluginManager.dbConnection("
                 }
             }
             console.log(all_results);
+
+            // CREATE WRITE STREAM
+            // const WriteStream = fs.createWriteStream(pathToFile + `event_keys.csv`);
         }
     }
     catch (err) {
