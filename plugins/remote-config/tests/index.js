4f4e4fa257bf80b00f715a92adbf8435835fd443
John-Weak <tusharbali11@gmail.com>
2024-08-01 13:23:40 +0530

    feedback

--- plugins/remote-config/tests/index.js
+++ plugins/remote-config/tests/index.js
@@ -1,20 +1,5 @@
-const plugins = require('../../pluginManager.js');
-
-try {
-    if (plugins.isPluginEnabled('ab-testing')) {
-        require('./ab_rc-method.js');
-    }
-    else {
-        throw new Error('Plugin ab-testing is not enabled');
-    }
-}
-catch (e) {
-    console.log('running remote-config test in basic mode');
-}
-finally {
-    require('./add-remote-config.js');
-    require('./fetch_remote_config.js');
-    require('./condition-endpoints.js');
-    require('./remote-config-endpoint.js');
-    require('./parameter-crud/parameter-crud.js');
-}
+require('./add-remote-config.js');
+require('./fetch_remote_config.js');
+require('./condition-endpoints.js');
+require('./remote-config-endpoint.js');
+require('./parameter-crud/parameter-crud.js');
