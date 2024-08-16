3609a2db3b36984475930b0dcc279093cc1e3bfe
Danu Widatama <widatama@gmail.com>
2024-08-16 18:29:02 +0700

    [crashes] Update flutter test

--- plugins/crashes/tests.js
+++ plugins/crashes/tests.js
@@ -3111,7 +3111,12 @@ describe('Testing Crashes', function() {
                 .get(`/o?method=crashes&api_key=${API_KEY_ADMIN}&app_id=${APP_ID}&query=${crashGroupQuery}`);
             const crashGroup = crashGroupResponse.body.aaData[0];
 
+            console.warn(crashGroupResponse.body);
+
             should(crashGroup.error).equal(common.escape_html(crashData._error));
+
+            await request
+                .get('/i/crashes/delete?args=' + JSON.stringify({ crash_id: crashGroup._id }) + '&app_id=' + APP_ID + '&api_key=' + API_KEY_ADMIN);
         });
     });
 
