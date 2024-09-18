957c0d20d69dccb1263d43365bef906fa030ebf5
can-angun <can1984@gmail.com>
2024-09-18 10:08:24 +0000

    Added all and compare events pages fulldata cntrl

--- ui-tests/cypress/e2e/onboarding/onboarding.cy.js
+++ ui-tests/cypress/e2e/onboarding/onboarding.cy.js
@@ -362,10 +362,10 @@ describe('Complete Onboarding', () => {
         analyticsGeoCountriesPageHelpers.clickLanguagesTab();
         analyticsGeoLanguagesPageHelpers.verifyFullDataPageElements();
         navigationHelpers.goToAnalyticsEventsOverview();
-        //analyticsEventsOverviewPageHelpers.verifyFullDataPageElements(); //TODO: Data is not being generated with the populator. Need to generate the data 
+        analyticsEventsOverviewPageHelpers.verifyFullDataPageElements(); //TODO: Data is not being generated with the populator. Need to generate the data 
+        navigationHelpers.goToAnalyticsAllEvents();
+        analyticsEventsPageHelpers.verifyFullDataPageElements();
         //TODO: Add the cases of the following pages
-        //navigationHelpers.goToAnalyticsAllEvents();
-        //analyticsEventsPageHelpers.verifyFullDataPageElements();
         //navigationHelpers.goToPushNotifications();
         //messagingPageHelpers.verifyFullDataPageElements();
         //navigationHelpers.goToFeedbackRatingsPage();
