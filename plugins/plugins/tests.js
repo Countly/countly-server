02dfdd1b9c05929e553d446da3712af89d6f243b
Danu Widatama <widatama@gmail.com>
2024-11-11 07:44:06 +0700

    Set up sample test

--- plugins/plugins/tests.js
+++ plugins/plugins/tests.js
@@ -1,16 +1,24 @@
 /*global describe,it */
 var request = require('supertest');
+const should = require('should');
+
+const pluginManager = require('../pluginManager.js');
 var testUtils = require("../../test/testUtils");
 request = request(testUtils.url);
 
-// var APP_KEY = "";
-var API_KEY_ADMIN = "";
-// var APP_ID = "";
-// var DEVICE_ID = "1234567890";
+const APP_ID = testUtils.get('APP_ID');
+const APP_KEY = testUtils.get('APP_KEY');
+const API_KEY_ADMIN = testUtils.get('API_KEY_ADMIN');
+
+const credentials = { api_key: API_KEY_ADMIN, app_id: APP_ID, app_key: APP_KEY };
+
+const sleep = (dur) => {
+    return new Promise(resolve => setTimeout(resolve, dur));
+};
 
 describe('Testing Plugins', function() {
     it('should have plugin', function(done) {
-        API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
+        // API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
         // APP_ID = testUtils.get("APP_ID");
         // APP_KEY = testUtils.get("APP_KEY");
         request
@@ -37,5 +45,95 @@ describe('Testing Plugins', function() {
                 }
                 done();
             });
+    });
+});
+
+describe('Make request without crash', function() {
+    this.timeout(testUtils.testWaitTimeForDrillEvents * testUtils.testScalingFactor * testUtils.testScalingFactor);
+
+    describe('GET request', function() {
+        it('should success', async() => {
+            const events = JSON.stringify([{ key: 'test', count: 1 }]);
+            const resp = await request.get('/i')
+                .query({
+                    ...credentials,
+                    begin_session: 1,
+                    events,
+                    device_id: 'abcd',
+                }).expect(200);
+
+            should(resp.body).not.be.empty();
+
+            await sleep(testUtils.testWaitTimeForDrillEvents * testUtils.testScalingFactor);
+        });
+    });
+
+    describe('verify event', function() {
+        it('should have event', async() => {
+            const resp = await request.get('/o')
+                .query({ ...credentials, method: 'events', event: 'test' })
+                .expect(200);
+
+            should(resp.body).not.be.empty();
+        });
+    });
+});
+
+describe('Make request with crash', function() {
+    this.timeout(testUtils.testWaitTimeForDrillEvents * testUtils.testScalingFactor * 5);
+
+    describe('GET request', function() {
+        it('should success', async() => {
+            const db = await pluginManager.dbConnection();
+            const app = await db.collection('apps').findOne({ _id: db.ObjectID(APP_ID) }, { blocks: 1 });
+            should.not.exists(app.blocks);
+            db.close();
+
+            const events = JSON.stringify([{ key: 'test', count: 1 }]);
+            const crash = JSON.stringify({ _error: 'error', _os: 'android', _name: 'error', _app_version: 'ver', _os_version: '1' });
+            const resp = await request.get('/i')
+                .query({
+                    app_key: APP_KEY,
+                    begin_session: 1,
+                    events,
+                    crash,
+                    device_id: 'abcd',
+                }).expect(200);
+
+            should(resp.body).not.be.empty();
+
+            await sleep(testUtils.testWaitTimeForDrillEvents * testUtils.testScalingFactor * 3);
+        });
+    });
+
+    describe('verify crash by db', function() {
+        it('should have crash', async() => {
+            const db = await pluginManager.dbConnection();
+            const crash = await db.collection(`app_crashes${APP_ID}`).findOne({ app_version: 'ver' });
+            db.close();
+
+            should.exists(crash);
+        });
+    });
+
+    describe('verify crash by req', function() {
+        it('should have crash', async() => {
+            const resp = await request.get('/o')
+                .query({ api_key: API_KEY_ADMIN, app_id: APP_ID, method: 'crashes' })
+                .expect(200);
+
+            should(resp.body).not.be.empty();
+            should(resp.body.aaData.length).equal(1);
+        });
+    });
+
+    describe('verify event', function() {
+        it('should have event', async() => {
+            const resp = await request.get('/o')
+                .query({ ...credentials, method: 'events', event: 'test' })
+                .expect(200);
+
+            should(resp.body).not.be.empty();
+        });
     });
 });
