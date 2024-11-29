e03192da7fe35fdc1df7cce4efd0f6844f7dc06b
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2024-11-29 12:07:52 +0530

    Update compare_drill_aggregated_csv.js

--- bin/scripts/data-reports/compare_drill_aggregated.js
+++ bin/scripts/data-reports/compare_drill_aggregated.js
@@ -6,7 +6,7 @@
  * node compare_drill_aggregated.js
  * 
  */
-var period = "30days"; // Choose any of formats: "Xdays" ("7days", "100days") or  ["1-1-2024", "1-10-2024"], 
+var period = "7days"; // Choose any of formats: "Xdays" ("7days", "100days") or  ["1-1-2024", "1-10-2024"], 
 var app_list = []; // List with apps 
 // Example var eventMap = {"6075f94b7e5e0d392902520c":["Logout","Login"],"6075f94b7e5e0d392902520d":["Logout","Login","Buy"]};
 var eventMap = {}; // If left empty will run for all apps/events.
