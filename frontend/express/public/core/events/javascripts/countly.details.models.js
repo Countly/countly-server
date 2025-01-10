2b83fd071dfce906d80cfc369c6c41c7881eb387
Cihad Tekin <cihad.tekin@count.ly>
2025-01-10 12:44:13 +0300

    [core] Replaced _.unescape with countlyCommon.unescapeHtml

--- frontend/express/public/core/events/javascripts/countly.details.models.js
+++ frontend/express/public/core/events/javascripts/countly.details.models.js
@@ -887,7 +887,7 @@
                     .then(function(res) {
                         if (res) {
                             if (Array.isArray(res.list)) {
-                                res.list = res.list.map(eventName => _.unescape(eventName).replace(/&#39;/g, "'"));
+                                res.list = res.list.map(eventName => countlyCommon.unescapeHtml(eventName));
                             }
                             context.commit("setAllEventsData", res);
                             if ((!context.state.selectedEventName) || (res.map && res.map[context.state.selectedEventName] && !res.map[context.state.selectedEventName].is_visible) || (res.list && res.list.indexOf(context.state.selectedEventName) === -1)) {
