84dbe0321f4afcb6efcf57d40dbdfbdea5262a00
Cookiezaurs <>
2025-01-24 12:34:27 +0200

    Changes for density plugin

--- plugins/density/tests.js
+++ plugins/density/tests.js
@@ -7,6 +7,7 @@ var APP_KEY = "";
 var API_KEY_ADMIN = "";
 var APP_ID = "";
 var DEVICE_ID = "1234567890";
+var drill_db = "";
 
 describe('Testing Density metrics', function() {
     describe('Empty density', function() {
@@ -14,6 +15,7 @@ describe('Testing Density metrics', function() {
             API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
             APP_ID = testUtils.get("APP_ID");
             APP_KEY = testUtils.get("APP_KEY");
+            drill_db = testUtils.client.db("countly_drill");
             request
                 .get('/o?api_key=' + API_KEY_ADMIN + '&app_id=' + APP_ID + '&method=density')
                 .expect(200)
@@ -39,19 +41,14 @@ describe('Testing Density metrics', function() {
                     }
                     var ob = JSON.parse(res.text);
                     ob.should.have.property('result', 'Success');
-                    setTimeout(done, 300 * testUtils.testScalingFactor);
+                    done();
                 });
         });
     });
     //{"2015":{"1":{"15":{"400dpi":{"t":1,"n":1,"u":1}},"400dpi":{"u":1,"t":1,"n":1}},"400dpi":{"u":1,"t":1,"n":1},"w3":{"400dpi":{"u":1}}},"meta":{"density":["400dpi"]}}
     describe('Verify density', function() {
         it('should have density', function(done) {
-            request
-                .get('/o?api_key=' + API_KEY_ADMIN + '&app_id=' + APP_ID + '&method=density')
-                .expect(200)
-                .end(function(err, res) {
-                    testUtils.validateMetrics(err, res, done, {meta: {"density": ['400dpi']}, "400dpi": {"n": 1, "t": 1, "u": 1}});
-                });
+            testUtils.validateTotalsInDrillData(drill_db, {app_id: APP_ID, event: "[CLY]_session", query: {"up.dnst": "400dpi"}, values: {u: 1, t: 1, n: 1}}, done);
         });
     });
     describe('write bulk density', function() {
@@ -68,7 +65,7 @@ describe('Testing Density metrics', function() {
                 {"device_id": DEVICE_ID + "9", "app_key": APP_KEY, "begin_session": 1, "metrics": {"_density": "900dpi"}}
             ];
             request
-                .get('/i/bulk?requests=' + JSON.stringify(params))
+                .get('/i/bulk?safe_api_response=true&requests=' + JSON.stringify(params))
                 .expect(200)
                 .end(function(err, res) {
                     if (err) {
@@ -76,19 +73,51 @@ describe('Testing Density metrics', function() {
                     }
                     var ob = JSON.parse(res.text);
                     ob.should.have.property('result', 'Success');
-                    setTimeout(done, 500 * testUtils.testScalingFactor);
+                    done();
                 });
         });
     });
     describe('Verify bulk density', function() {
         it('should match provided density', function(done) {
+
+            testUtils.validateBreakdownTotalsInDrillData(drill_db, {
+                app_id: APP_ID,
+                event: "[CLY]_session",
+                breakdownKeys: ["up.dnst"],
+                values: {
+                    "100dpi": {"n": 1, "t": 1, "u": 1},
+                    "200dpi": {"n": 1, "t": 1, "u": 1},
+                    "300dpi": {"n": 1, "t": 1, "u": 1},
+                    "400dpi": {"n": 1, "t": 2, "u": 2},
+                    "500dpi": {"n": 1, "t": 1, "u": 1},
+                    "600dpi": {"n": 1, "t": 1, "u": 1},
+                    "700dpi": {"n": 1, "t": 1, "u": 1},
+                    "800dpi": {"n": 1, "t": 1, "u": 1},
+                    "900dpi": {"n": 1, "t": 1, "u": 1}
+                }
+            }, done);
+        });
+    });
+
+    describe("Test density together with platform", function() {
+        it('send some data', function(done) {
+            var params = {"_density": "400dpi", "_os": "Android"};
             request
-                .get('/o?api_key=' + API_KEY_ADMIN + '&app_id=' + APP_ID + '&method=density')
+                .get('/i?device_id=' + DEVICE_ID + '7&app_key=' + APP_KEY + "&begin_session=1&metrics=" + JSON.stringify(params))
                 .expect(200)
                 .end(function(err, res) {
-                    testUtils.validateMetrics(err, res, done, {meta: {"density": ["100dpi", "200dpi", "300dpi", "400dpi", "500dpi", "600dpi", "700dpi", "800dpi", "900dpi"]}, "100dpi": {"n": 1, "t": 1, "u": 1}, "200dpi": {"n": 1, "t": 1, "u": 1}, "300dpi": {"n": 1, "t": 1, "u": 1}, "400dpi": {"n": 2, "t": 2, "u": 2}, "500dpi": {"n": 1, "t": 1, "u": 1}, "600dpi": {"n": 1, "t": 1, "u": 1}, "700dpi": {"n": 1, "t": 1, "u": 1}, "800dpi": {"n": 1, "t": 1, "u": 1}, "900dpi": {"n": 1, "t": 1, "u": 1}});
+                    if (err) {
+                        return done(err);
+                    }
+                    var ob = JSON.parse(res.text);
+                    ob.should.have.property('result', 'Success');
+                    done();
                 });
         });
+        it('should have density and platform(stored density has to pe affected by platform)', function(done) {
+            testUtils.validateTotalsInDrillData(drill_db, {app_id: APP_ID, event: "[CLY]_session", query: {"up.dnst": "a400dpi", "up.p": "Android"}, values: {u: 1, t: 1, n: 1}}, done);
+        });
+
     });
     describe('reset app', function() {
         it('should reset data', function(done) {
@@ -102,23 +131,13 @@ describe('Testing Density metrics', function() {
                     }
                     var ob = JSON.parse(res.text);
                     ob.should.have.property('result', 'Success');
-                    setTimeout(done, 10 * testUtils.testScalingFactor);
+                    setTimeout(done, 20 * testUtils.testScalingFactor);
                 });
         });
     });
     describe('verify empty density', function() {
         it('should have no densities', function(done) {
-            request
-                .get('/o?api_key=' + API_KEY_ADMIN + '&app_id=' + APP_ID + '&method=density')
-                .expect(200)
-                .end(function(err, res) {
-                    if (err) {
-                        return done(err);
-                    }
-                    var ob = JSON.parse(res.text);
-                    ob.should.be.empty;
-                    done();
-                });
+            testUtils.validateTotalsInDrillData(drill_db, {app_id: APP_ID, event: "[CLY]_session", query: {}, values: {u: 0, t: 0, n: 0}}, done);
         });
     });
 });
