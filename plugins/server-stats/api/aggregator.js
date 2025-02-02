d9410fbd74874125fdeb95106a57c838a41bd672
Cookiezaurs <>
2025-02-02 20:10:38 +0200

    Changes for server-stats plugin.

---
+++ plugins/server-stats/api/aggregator.js
@@ -0,0 +1,37 @@
+var plugins = require('../../pluginManager.js'),
+    common = require('../../../api/utils/common.js'),
+    stats = require('./parts/stats.js');
+const { changeStreamReader } = require('../../../api/parts/data/changeStreamReader');
+
+(function() {
+    plugins.register("/aggregator", function() {
+        var changeStream = new changeStreamReader(common.drillDb, {
+            pipeline: [
+                {"$match": {"operationType": "insert"}},
+                {"$project": {"a": "$fullDocument.a", "e": "$fullDocument.e"}}
+            ],
+            "name": "server-stats",
+            "collection": "drill_events"
+        }, (token, next) => {
+            console.log(next.e);
+            if (next.e === "[CLY]_session") {
+                stats.updateDataPoints(common.writeBatcher, next.a, 1, 0, false, token);
+            }
+            else if (stats.internalEventsEnum[next.e]) {
+                var uu = {"e": 1};
+                uu[stats.internalEventsEnum[next.e]] = 1;
+                stats.updateDataPoints(common.writeBatcher, next.a, 0, uu, false, token);
+                //if it is internal event, then we need to update the event count
+            }
+            else {
+                stats.updateDataPoints(common.writeBatcher, next.a, 0, {"e": 1, "ce": 1}, false, token);
+            }
+            // process next document
+        });
+
+        common.writeBatcher.addFlushCallback("server_stats_data_points", function(token) {
+            changeStream.acknowledgeToken(token);
+        });
+
+    });
+}());
