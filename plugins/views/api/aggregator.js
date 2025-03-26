3371326b61af195709619e9df32ce11bafa19158
Cookiezaurs <>
2025-03-26 11:29:33 +0200

    addition for views for fallback processing Fixed job to do not use bluebird promise

--- plugins/views/api/aggregator.js
+++ plugins/views/api/aggregator.js
@@ -49,9 +49,11 @@ const crypto = require('crypto');
                 {"$match": {"operationType": "insert", "fullDocument.e": {"$in": ["[CLY]_view", "[CLY]_action"]}}},
                 {"$addFields": {"__id": "$fullDocument._id", "cd": "$fullDocument.cd"}}
             ],
-            pipeline_process: [{
-                "$match": {"e": {"$in": ["[CLY]_view", "[CLY]_action"]}}
-            }],
+            fallback: {
+                pipeline: [{
+                    "$match": {"e": {"$in": ["[CLY]_view", "[CLY]_action"]}}
+                }]
+            },
             "name": "views",
             "collection": "drill_events",
             "onClose": async function(callback) {
