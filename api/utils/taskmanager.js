ad4c79b6b53d373db3dc04e421dc4bb8de8dd829
Cookiezaurs <>
2025-04-10 13:33:07 +0300

    eslint

--- api/utils/taskmanager.js
+++ api/utils/taskmanager.js
@@ -972,12 +972,12 @@ taskmanager.rerunTask = function(options, callback) {
         });
     }
 
-    var qq = {_id: options.id}
-    if(options.additionalQuery){
+    var qq = {_id: options.id};
+    if (options.additionalQuery) {
         qq = options.additionalQuery;
         qq._id = options.id;
     }
-    log.d("Fetching from long_tasks to rerun: "+ JSON.stringify(qq));
+    log.d("Fetching from long_tasks to rerun: " + JSON.stringify(qq));
     options.db.collection("long_tasks").findOne(qq, function(err, res) {
         if (!err && res && res.request) {
             var reqData = {};
