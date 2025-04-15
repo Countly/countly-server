aac44d731d1e1c5fe645bef6db0e8a1d3d03ce02
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2025-04-15 13:39:00 +0530

    restore original metric parsing

--- api/parts/data/usage.js
+++ api/parts/data/usage.js
@@ -384,9 +384,6 @@ usage.returnAllProcessedMetrics = function(params) {
 
             if (tmpMetric.is_user_prop) {
                 recvMetricValue = params.user[tmpMetric.name];
-                if (recvMetricValue === undefined && params.qstring.metrics && params.qstring.metrics[tmpMetric.name] !== undefined) {
-                    recvMetricValue = params.qstring.metrics[tmpMetric.name];
-                }
             }
             else if (params.qstring.metrics && (tmpMetric.name in params.qstring.metrics)) {
                 recvMetricValue = params.qstring.metrics[tmpMetric.name];
@@ -694,9 +691,6 @@ function processMetrics(user, uniqueLevelsZero, uniqueLevelsMonth, params, done)
                     postfix = null;
                 if (tmpMetric.is_user_prop) {
                     recvMetricValue = params.user[tmpMetric.name];
-                    if (recvMetricValue === undefined && params.qstring.metrics && params.qstring.metrics[tmpMetric.name] !== undefined) {
-                        recvMetricValue = params.qstring.metrics[tmpMetric.name];
-                    }
                 }
                 else if (params.qstring.metrics && params.qstring.metrics[tmpMetric.name]) {
                     recvMetricValue = params.qstring.metrics[tmpMetric.name];
