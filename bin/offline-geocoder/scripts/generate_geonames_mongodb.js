610ef4af6f4067c2f1ba4ad43bf6e21f7a491146
Arturs Sosins <as@count.ly>
2025-06-11 17:48:27 +0300

    try https

--- bin/offline-geocoder/scripts/generate_geonames_mongodb.js
+++ bin/offline-geocoder/scripts/generate_geonames_mongodb.js
@@ -1,2 +1,16 @@
-require("./download_geonames_data.js");
-require("./import_geonames_mongodb.js");
+//require("./download_geonames_data.js");
+//require("./import_geonames_mongodb.js");
+
+const https = require('node:https');
+
+https.get('https://encrypted.google.com/', (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+
+}).on('error', (e) => {
+    console.error(e);
+});
