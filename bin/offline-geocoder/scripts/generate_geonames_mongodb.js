3a61de6c6b373312ec426390f6de3e583ecc4fb5
Arturs Sosins <as@count.ly>
2025-06-11 18:00:00 +0300

    more tests

--- bin/offline-geocoder/scripts/generate_geonames_mongodb.js
+++ bin/offline-geocoder/scripts/generate_geonames_mongodb.js
@@ -1,14 +1,18 @@
 //require("./download_geonames_data.js");
 //require("./import_geonames_mongodb.js");
 
-const https = require('node:https');
-
+const https = require('https');
+const { createWriteStream } = require('fs');
+const file = createWriteStream("cities1000.zip");
 https.get('https://download.geonames.org/export/dump/cities1000.zip', (res) => {
     console.log('statusCode:', res.statusCode);
     console.log('headers:', res.headers);
 
-    res.on('data', (d) => {
-        process.stdout.write(d);
+    res.pipe(file);
+
+    file.on('finish', () => {
+        console.log(`Downloaded`);
+        file.close();
     });
 
 }).on('error', (e) => {
