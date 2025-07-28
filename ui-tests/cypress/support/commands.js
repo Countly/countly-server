edad261ac0fa43a9d2789c66de5b56a5783f1dce
can-angun <can1984@gmail.com>
2025-07-28 13:17:14 +0000

    Updated ensureScrollable is true

--- ui-tests/cypress/support/commands.js
+++ ui-tests/cypress/support/commands.js
@@ -215,7 +215,7 @@ Cypress.Commands.add("scrollPageToTop", (element = '.main-view', index = 0) => {
 });
 
 Cypress.Commands.add("scrollPageToCenter", (element = '.main-view', index = 0) => {
-    cy.get(element).eq(index).scrollTo('center', { ensureScrollable: false });
+    cy.get(element).eq(index).scrollTo('center', { ensureScrollable: true });
 });
 
 Cypress.Commands.add("scrollDataTableToRight", (element = '.el-table__body-wrapper', index = 0) => {
