b08abe2c4d9dd11bcb8a23a98efa92bc8f8fc6ca
Cookiezaurs <>
2025-08-12 16:35:16 +0300

    some changes so tests use mongodb in github

--- api/parts/data/QueryRunner.js
+++ api/parts/data/QueryRunner.js
@@ -10,6 +10,11 @@ const fs = require('fs');
 const path = require('path');
 
 config.queryRunner = config.queryRunner || {};
+config.database = config.database | {};
+config.database.adapterPreference = config.database.adapterPreference || ['mongodb', 'clickhouse'];
+if(config.database.adapterPreference.length === 0){
+    config.database.adapterPreference.push('mongodb');
+}
 if (!config.queryRunner.adapters || Object.keys(config.queryRunner.adapters).length === 0) {
     config.queryRunner.adapters = {
         mongodb: {
