a580118ce31b5f3fce6c319b38356996b0bf924b
John-Weak <tusharbali11@gmail.com>
2025-09-09 00:56:32 +0530

    star rating aggregator

--- plugins/star-rating/api/aggregator.js
+++ plugins/star-rating/api/aggregator.js
@@ -1,63 +1,71 @@
 
 var plugins = require('../../pluginManager.js'),
     common = require('../../../api/utils/common.js');
-const { changeStreamReader } = require('../../../api/parts/data/changeStreamReader');
+const UnifiedEventSource = require('../../../api/eventSource/UnifiedEventSource.js');
 const log = require('../../../api/utils/log.js')('star-rating:aggregator');
-const { WriteBatcher } = require('../../../api/parts/data/batcher.js');
 const usage = require('../../../api/aggregator/usage.js');
 
 (function() {
-    //Single for all surveys+nps. Could divide based on collections 
-    plugins.register("/aggregator", function() {
-        var localBatcher = new WriteBatcher(common.db);
-        var changeStream = new changeStreamReader(common.drillDb, {
-            pipeline: [
-                {"$match": {"operationType": "insert", "fullDocument.e": {"$in": ["[CLY]_star_rating"]}}},
-                {"$addFields": {"__id": "$fullDocument._id", "cd": "$fullDocument.cd"}}
-            ],
-            fallback: {
-                pipeline: [{
-                    "$match": {"e": {"$in": ["[CLY]_star_rating"]}}
-                }]
-            },
-            "name": "star-rating",
-            "collection": "drill_events",
-            "onClose": async function(callback) {
-                if (callback) {
-                    callback();
+    //Single for all star-rating events
+    plugins.register("/aggregator", async function() {
+        const eventSource = new UnifiedEventSource('star-rating', {
+            mongo: {
+                db: common.drillDb,
+                pipeline: [
+                    {"$match": {"operationType": "insert", "fullDocument.e": {"$in": ["[CLY]_star_rating"]}}},
+                    {
+                        "$project": {
+                            "__id": "$fullDocument._id",
+                            "cd": "$fullDocument.cd",
+                            "sg": "$fullDocument.sg",
+                            "e": "$fullDocument.e",
+                            "ts": "$fullDocument.ts",
+                            "a": "$fullDocument.a",
+                            "uid": "$fullDocument.uid"
+                        }
+                    }
+                ],
+                fallback: {
+                    pipeline: [{"$match": {"e": {"$in": ["[CLY]_star_rating"]}}}]
                 }
             }
-        }, (token, next) => {
-        //Coming from stream is not a root document
-            if (next.fullDocument) {
-                next = next.fullDocument;
-            }
-            if (next && next.a) {
-                common.readBatcher.getOne("apps", common.db.ObjectID(next.a), function(err, app) {
-                    //record event totals in aggregated data
+        });
 
-                    if (err) {
-                        log.e("Error getting app data for session", err);
-                        return;
-                    }
-                    if (app && app._id) {
-                        //increases count
-                        localBatcher.add("feedback_widgets", common.db.ObjectID(next.sg.widget_id), {$inc: { ratingsSum: next.sg.rating, ratingsCount: 1 }}, "countly", {token: token, upsert: false});
-                        var allowed_segemnts = ["platform", "app_version", "rating", "widget_id", "platform_version_rate"];
-                        for (var seg in next.sg) {
-                            if (allowed_segemnts.indexOf(seg) === -1) {
-                                delete next.sg[seg];
+        try {
+            for await (const {token, events} of eventSource) {
+                if (events && Array.isArray(events)) {
+                    for (var z = 0; z < events.length; z++) {
+                        var next = events[z];
+                        if (next.e === "[CLY]_star_rating") {
+                            if (next && next.a) {
+                                try {
+                                    const app = await common.readBatcher.getOne("apps", common.db.ObjectID(next.a));
+                                    if (app && app._id) {
+                                        //increases count
+                                        common.manualWriteBatcher.add("feedback_widgets", common.db.ObjectID(next.sg.widget_id), {$inc: { ratingsSum: next.sg.rating, ratingsCount: 1 }}, "countly", {token: token, upsert: false});
+                                        var allowed_segemnts = ["platform", "app_version", "rating", "widget_id", "platform_version_rate"];
+                                        for (var seg in next.sg) {
+                                            if (allowed_segemnts.indexOf(seg) === -1) {
+                                                delete next.sg[seg];
+                                            }
+                                        }
+                                        usage.processEventFromStream(token, next, common.manualWriteBatcher);
+                                        //records aggregated data
+                                    }
+                                }
+                                catch (err) {
+                                    log.e("Error getting app data for session", err);
+                                }
                             }
                         }
-                        usage.processEventFromStream(token, next, localBatcher);
-                        //records aggregated data
                     }
-                });
+                    //Flush batchers
+                    await common.manualWriteBatcher.flush("countly", "feedback_widgets");
+                }
             }
-        });
-
-        localBatcher.addFlushCallback("*", function(token) {
-            changeStream.acknowledgeToken(token);
-        });
+        }
+        catch (err) {
+            log.e('Event processing error:', err);
+        }
     });
 }());
