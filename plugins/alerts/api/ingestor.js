3927c30f49766b4c07acb7a6d31ffaff4304b8a1
coskunaydinoglu <coskun.aydinoglu@gmail.com>
2025-10-22 09:56:25 +0300

    alerts ingestor

--- plugins/alerts/api/ingestor.js
+++ plugins/alerts/api/ingestor.js
@@ -44,8 +44,24 @@ const commonLib = require("./parts/common-lib.js");
         }
     });
 
-    plugins.register("/sdk/process_user", function() {
-        //var params = ob.params;
+    plugins.register("/sdk/process_request", async function(ob) {
+        const events = ob.params?.qstring?.events;
+        const app = ob.app;
+
+        if (!events || !app) {
+            return;
+        }
+
+        for (let { module, name } of TRIGGER_BY_EVENT) {
+            if (name !== "crashes") {
+                try {
+                    await module.triggerByEvent({ events, app });
+                }
+                catch (err) {
+                    log.e("Alert module '" + name + "' couldn't be triggered by event", err);
+                }
+            }
+        }
     });
 
 }(exported));
