9f51157e63f86fc24f069eef7a0040d15e3a465f
Hakan <hakanuludag27@gmail.com>
2025-10-30 14:58:16 +0300

    Modify script to install and use Python 3.12
    Updated the script to install Python 3.12 and its pip package manager. Changed the Python version used for installing requirements and running the model.

--- bin/docker/modify.sh
+++ bin/docker/modify.sh
@@ -18,11 +18,27 @@ if [ "${COUNTLY_CONTAINER}" != "frontend" ]; then
 	if [ -d /opt/countly/plugins/ab-testing ]; then
 		if [ "${ID}" == "debian" ] || [ "${ID}" == "ubuntu" ]; then
 			apt-get install -y python3-pip
+			add-apt-repository -y ppa:deadsnakes/ppa
+    		apt -y install python3.12 python3.12-dev
+    		curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    		python3.12 get-pip.py
 		else
-			yum install -y python36 python36-libs python36-devel python36-pip
+	    if ! command -v python3.12 &>/dev/null; then
+        # Install Python 3.12 if it's not installed
+          cd /tmp/ && wget https://www.python.org/ftp/python/3.12/Python-3.12.0.tgz
+          tar -xvf Python-3.12.0.tgz
+          cd Python-3.12.0/
+          ./configure --enable-optimizations
+          make altinstall
+          ln -sf /usr/local/bin/python3.12 /usr/bin/python3.12
+          else
+            echo "Python 3.12 is already installed."
+         fi
+
+    sudo curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    sudo python3.12 get-pip.py
 		fi
 		# shellcheck disable=SC1091
-		python3.8 -m pip install -r /opt/countly/plugins/ab-testing/api/bayesian/requirements.txt
-		cd /opt/countly/plugins/ab-testing/api/bayesian && python3.8 model.py
+		python3.12 -m pip install -r "/opt/countly/plugins/ab-testing/api/bayesian/requirements.txt" && python3.12 "/opt/countly/plugins/ab-testing/api/bayesian/models/cmdstanpy_model.py"
 	fi
 fi
