e0cd8637accf48aa189156b3b3f19c96de1bbd47
Arturs Sosins <as@count.ly>
2025-11-28 20:10:37 +0200

    One more fix

--- plugins/reports/api/api.js
+++ plugins/reports/api/api.js
@@ -258,23 +258,22 @@ const FEATURE_NAME = 'reports';
 
                 convertToTimezone(props);
 
-                // TODO: handle report type check
-
-                if (!props.apps || !Array.isArray(props.apps) || props.apps.length === 0) {
-                    common.returnMessage(params, 400, 'Invalid or missing apps');
-                    return;
-                }
-
-                let userApps = getUserApps(params.member);
-                let notPermitted = false;
-                for (var i = 0; i < props.apps.length; i++) {
-                    if (userApps.indexOf(props.apps[i]) === -1) {
-                        notPermitted = true;
+                if (props.report_type === "core") {
+                    if (!props.apps || !Array.isArray(props.apps) || props.apps.length === 0) {
+                        common.returnMessage(params, 400, 'Invalid or missing apps');
+                        return;
                     }
-                }
 
-                if (notPermitted && !params.member.global_admin) {
-                    return common.returnMessage(params, 401, 'User does not have right to access this information');
+                    let userApps = getUserApps(params.member);
+                    let notPermitted = false;
+                    for (var i = 0; i < props.apps.length; i++) {
+                        if (userApps.indexOf(props.apps[i]) === -1) {
+                            notPermitted = true;
+                        }
+                    }
+                    if (notPermitted && !params.member.global_admin) {
+                        return common.returnMessage(params, 401, 'User does not have right to access this information');
+                    }
                 }
 
                 common.db.collection('reports').insert(props, function(err0, result) {
