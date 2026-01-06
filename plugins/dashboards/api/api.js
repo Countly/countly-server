676d13713fcfe0f57f6fb6d2660af391871be020
Cookiezaurs <>
2026-01-06 16:06:57 +0200

    required functions

--- plugins/dashboards/api/api.js
+++ plugins/dashboards/api/api.js
@@ -12,7 +12,7 @@ var pluginOb = {},
     localize = require('../../../api/utils/localization.js'),
     async = require('async'),
     mail = require("../../../api/parts/mgmt/mail"),
-    { validateUser } = require('../../../api/utils/rights.js');
+    { validateUser, getAdminApps, getUserApps } = require('../../../api/utils/rights.js');
 
 var ejs = require("ejs");
 
