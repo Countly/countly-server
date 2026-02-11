ea3aba989d5bff712ba2217d0d2f9570640b8339
Danu Widatama <widatama@gmail.com>
2026-02-11 14:26:13 +0700

    [star-rating] Update rating color

--- plugins/star-rating/frontend/public/javascripts/countly.views.js
+++ plugins/star-rating/frontend/public/javascripts/countly.views.js
@@ -393,9 +393,10 @@
         computed: {
             preparedRows: function() {
                 return this.ratings.map(function(rating) {
-                    rating.rating += 1;
-                    rating.percentage = parseFloat(rating.percent) || 0;
-                    return rating;
+                    return Object.assign({}, rating, {
+                        rating: (Number(rating.rating) || 0) + 1,
+                        percentage: Number.parseFloat(rating.percent) || 0,
+                    });
                 });
             }
         },
