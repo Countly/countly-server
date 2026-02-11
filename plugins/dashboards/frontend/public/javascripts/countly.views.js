db1e2c0a2ee41a74c97a3a06540ed291346d6b81
Pavel <tryshankse@gmail.com>
2026-02-11 08:51:16 +0100

    fix: public dashboards disabled message

--- plugins/dashboards/frontend/public/javascripts/countly.views.js
+++ plugins/dashboards/frontend/public/javascripts/countly.views.js
@@ -512,12 +512,9 @@
                 ];
 
                 var allowPublicDashboards = countlyGlobal.allow_public_dashboards !== false;
-                var sharingOptions = allowPublicDashboards ? allSharingOptions : allSharingOptions.filter(function(option) {
-                    return option.value !== "all-users";
-                });
-
                 return {
-                    sharingOptions: sharingOptions
+                    sharingOptions: allSharingOptions,
+                    isPublicDisabled: !allowPublicDashboards
                 };
             },
             canShare: function() {
@@ -664,6 +661,14 @@
                     if (doc.__action === "create" ||
                         doc.__action === "duplicate") {
                         doc.share_with = "none";
+                    }
+                }
+                else {
+                    var allowPublicDashboards = countlyGlobal.allow_public_dashboards !== false;
+                    if (!allowPublicDashboards &&
+                        (doc.__action === "create" || doc.__action === "duplicate") &&
+                        doc.share_with === "all-users") {
+                        doc.share_with = "";
                     }
                 }
             },
