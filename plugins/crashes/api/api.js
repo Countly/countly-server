29e5a79a2ff5d7a650b11f34d3287f45f7c960f6
Arturs Sosins <as@count.ly>
2026-04-14 09:34:03 +0300

    Fix crash download field projections

--- plugins/crashes/api/api.js
+++ plugins/crashes/api/api.js
@@ -606,7 +606,7 @@ const FEATURE_NAME = 'crashes';
                 try {
                     [crash] = await getCrashesTable({
                         query: { _id: id },
-                        fields: { binary_crash_dump: 1 },
+                        fields: { error: 1 },
                         limit: 1,
                     });
                 }
@@ -647,7 +647,7 @@ const FEATURE_NAME = 'crashes';
                 try {
                     [crash] = await getCrashesTable({
                         query: { _id: id },
-                        fields: { error: 1 },
+                        fields: { binary_crash_dump: 1 },
                         limit: 1,
                     });
                 }
