c64bd210fe68b495024e5f984fbe8434296b9dae
Arturs Sosins <as@count.ly>
2026-04-17 20:16:06 +0300

    Split web SDK pre hook registration

--- plugins/web/api/api.js
+++ plugins/web/api/api.js
@@ -1,1 +1,7 @@
-module.exports = require('./ingestor.js');
+var pluginOb = {},
+    plugins = require('../../pluginManager.ts'),
+    registerWebSdkPre = require('./parts/sdk-pre.js');
+
+registerWebSdkPre(plugins);
+
+module.exports = pluginOb;
