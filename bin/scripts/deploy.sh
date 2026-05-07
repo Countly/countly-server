4e7cb4df001e54d5b61cf194f7fafca5e1b1392d
Arturs Sosins <ar2rsawseen@gmail.com>
2026-05-07 09:41:41 +0300

    Modify deploy script for branch-specific SSH commands

--- bin/scripts/deploy.sh
+++ bin/scripts/deploy.sh
@@ -3,14 +3,12 @@
 if [ -z "$GITHUB_HEAD_REF" ] && [ "$GITHUB_REPOSITORY" == "Countly/countly-server" ]; then
     GITHUB_BRANCH=${GITHUB_REF#refs/heads/}
     echo "$GITHUB_BRANCH"
-    if [ "$GITHUB_BRANCH" == "master" ] || [ "$GITHUB_BRANCH" == "next" ]; then
+    if [ "$GITHUB_BRANCH" == "master" ]; then
         echo "$SSH_PRIVATE_KEY" > deploy-key;
         mkdir -p ~/.ssh;
         mv deploy-key ~/.ssh/id_rsa;
         chmod 600 ~/.ssh/id_rsa;
-        ssh -oStrictHostKeyChecking=no "countly@$GITHUB_BRANCH.count.ly" "bash /home/countly/deploy.sh > /home/countly/logs/countly-deploy-github.log 2>&1 &"
-        if [ "$GITHUB_BRANCH" == "master" ]; then
-            ssh -oStrictHostKeyChecking=no "countly@ce.count.ly" "bash /home/countly/deploy.sh > /home/countly/logs/countly-deploy-github.log 2>&1 &"
-        fi
+        ssh -oStrictHostKeyChecking=no "countly@release-2503.count.ly" "bash /home/countly/deploy.sh > /home/countly/logs/countly-deploy-github.log 2>&1 &"
+        ssh -oStrictHostKeyChecking=no "countly@ce.count.ly" "bash /home/countly/deploy.sh > /home/countly/logs/countly-deploy-github.log 2>&1 &"
     fi
 fi
